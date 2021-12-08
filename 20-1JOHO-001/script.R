b911859813be62a3859f731f90409a911fd6858c
shayat <91986031+shhayat@users.noreply.github.com>
2021-12-08 15:41:39 -0600

    Update script.R

--- 20-1JOHO-001/script.R
+++ 20-1JOHO-001/script.R
@@ -1,4 +1,3 @@
-
 ################################################
 #merge htseq counts based on gene name
 ################################################
@@ -10,17 +9,19 @@ datalist <- lapply(filelist, FUN=read.table, header=FALSE)
 res <- Reduce(function(x,y) {merge(x,y, by="V1")}, datalist)
 res <- res[-c(1:5),]
 
-#names(res) <-c("gene_name","treated_female_low_R25","treated_female_high_R26","control_male_low_R27","treated_male_high_R28",
-#               "control_male_low_R29","control_female_high_R30","treated_female_high_R31","control_female_low_R32",
-#               "treated_female_high_R33","control_female_high_R34","treated_female_low_R35","control_male_low_R36",
-#               "control_male_low_R37","control_male_high_R38","control_male_low_R39","treated_male_high_R40",
-#               "control_female_low_R41","control_female_low_R42","treated_female_high_R43","control_female_high_R44",
-#               "treated_female_high_R45","control_female_high_R46","treated_male_low_R47","treated_male_high_R48",
-#               "treated_female_high_R49","treated_male_high_R50","treated_male_high_R51","control_female_low_R52",
-#               "treated_female_low_R53","treated_female_high_R54","treated_male_low_R55","control_male_low_R56",
-#               "treated_male_high_R57","control_female_high_R58","control_male_low_R59","treated_male_high_R60",
-#               "control_male_low_R61","treated_female_high_R62","treated_male_low_R63","treated_male_low_R64")
+#control male low / high , treated male low / high , control female low /hight , treated female low/high
+names(res) <-c("gene_name","treated_female_low_R25","treated_female_high_R26","control_male_low_R27","treated_male_high_R28",
+               "control_male_low_R29","control_female_high_R30","treated_female_high_R31","control_female_low_R32",
+               "treated_female_high_R33","control_female_high_R34","treated_female_low_R35","control_male_low_R36",
+               "control_male_low_R37","control_male_high_R38","control_male_low_R39","treated_male_high_R40",
+               "control_female_low_R41","control_female_low_R42","treated_female_high_R43","control_female_high_R44",
+               "treated_female_high_R45","control_female_high_R46","treated_male_low_R47","treated_male_high_R48",
+               "treated_female_high_R49","treated_male_high_R50","treated_male_high_R51","control_female_low_R52",
+               "treated_female_low_R53","treated_female_high_R54","treated_male_low_R55","control_male_low_R56",
+               "treated_male_high_R57","control_female_high_R58","control_male_low_R59","treated_male_high_R60",
+               "control_male_low_R61","treated_female_high_R62","treated_male_low_R63","treated_male_low_R64")
 
+# control female, control_male, treated_male, treated_female
 names(res) <-c("gene_name","treated_female_R25","treated_female_R26","control_male_R27","treated_male_R28",
                "control_male_R29","control_female_R30","treated_female_R31","control_female_R32",
                "treated_female_R33","control_female_R34","treated_female_R35","control_male_R36",
@@ -32,10 +33,34 @@ names(res) <-c("gene_name","treated_female_R25","treated_female_R26","control_ma
                "treated_male_R57","control_female_R58","control_male_R59","treated_male_R60",
                "control_male_R61","treated_female_R62","treated_male_R63","treated_male_R64")
 
-#write.table(res, "/globalhome/hxo752/HPC/projects/20-1JOHO-001/20-1JOHO-001_htseq_counts.txt", sep="\t", quote=FALSE, , row.names=FALSE)
-write.table(res, "/globalhome/hxo752/HPC/projects/20-1JOHO-001/20-1JOHO-001_htseq_counts_v1.txt", sep="\t", quote=FALSE, , row.names=FALSE)
 
-#library("biomaRt")
+#treated female low and high, treated male low and high
+res1 <- res[c(1:3, 8,10, 12, 20, 22, 26, 30, 31, 39)]
+names(res1) <-c("gene_name","treated_female_low_R25","treated_female_high_R26","treated_female_high_R31",
+               "treated_female_high_R33", "treated_female_low_R35", "treated_female_high_R43","treated_female_high_R45",
+               "treated_female_high_R49","treated_female_low_R53","treated_female_high_R54","treated_female_high_R62")
+               
+#treated male low and high, treated male low and high
+res1 <- res[c(1,5,17, 24,25,27,28,32,34,37,40,41)]
+names(res1) <-c("gene_name","treated_male_high_R28","treated_male_high_R40","treated_male_low_R47",
+                "treated_male_high_R48", "treated_male_high_R50", "treated_male_high_R51","treated_male_low_R55",
+                "treated_male_high_R57","treated_male_high_R60","treated_male_low_R63","treated_male_low_R64")
+
+
+#treated male low and high, treated male low and high
+res1 <- res[c(1,5,17, 24,25,27,28,32,34,37,40,41)]
+names(res1) <-c("gene_name","treated_male_high_R28","treated_male_high_R40","treated_male_low_R47",
+                "treated_male_high_R48", "treated_male_high_R50", "treated_male_high_R51","treated_male_low_R55",
+                "treated_male_high_R57","treated_male_high_R60","treated_male_low_R63","treated_male_low_R64")
+
+
+
+#write.table(res, "/globalhome/hxo752/HPC/projects/20-1JOHO-001_htseq_counts.txt", sep="\t", quote=FALSE, , row.names=FALSE)
+#write.table(res, "/globalhome/hxo752/HPC/projects/20-1JOHO-001_htseq_counts_v1.txt", sep="\t", quote=FALSE, , row.names=FALSE)
+#write.table(res1, "/globalhome/hxo752/HPC/projects/20-1JOHO-001_htseq_counts_v2.txt", sep="\t", quote=FALSE, , row.names=FALSE)
+write.table(res1, "/globalhome/hxo752/HPC/projects/20-1JOHO-001_htseq_counts_v3.txt", sep="\t", quote=FALSE, , row.names=FALSE)
+
+library("biomaRt")
 #ensembl <- useMart(host = "feb2021.archive.ensembl.org", biomart = "ENSEMBL_MART_ENSEMBL", dataset='rnorvegicus_gene_ensembl') 
 #attributes = biomaRt::listAttributes(ensembl)
 #ensembl <- useDataset(dataset = "rnorvegicus_gene_ensembl", mart = ensembl)
@@ -53,4 +78,7 @@ setwd("/Users/shahina/Projects/20-1JOHO-001")
 #write.table(res[1:21], "20-1JOHO-001_htseq_counts_v1.txt", sep="\t", quote=FALSE, , row.names=FALSE)
 
 #object <-  read_rnaseq_counts(file ='20-1JOHO-001_htseq_counts.txt', pca = TRUE, fit='limma', plot = TRUE)
-object <-  read_rnaseq_counts(file ='20-1JOHO-001_htseq_counts_v1.txt', pca = TRUE, fit='limma', plot = TRUE)
+#object <-  read_rnaseq_counts(file ='20-1JOHO-001_htseq_counts_v1.txt', pca = TRUE, fit='limma', plot = TRUE)
+#object <-  read_rnaseq_counts(file ='20-1JOHO-001_htseq_counts_v2.txt', pca = TRUE, fit='limma', plot = TRUE)
+object <-  read_rnaseq_counts(file ='20-1JOHO-001_htseq_counts_v3.txt', pca = TRUE, fit='limma', plot = TRUE)
+
