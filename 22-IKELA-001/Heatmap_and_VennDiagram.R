597f236d6c2b42f04efde2a87442d7aca8cb6fa7
shayat <91986031+shhayat@users.noreply.github.com>
2022-01-24 22:15:58 -0600

    Create Heatmap_and_VennDiagram.R

---
+++ 22-IKELA-001/Heatmap_and_VennDiagram.R
@@ -0,0 +1,93 @@
+library(dplyr)
+library(pheatmap)
+setwd("/Users/shahina/Projects/22-1KELA-001")
+
+
+diff_genes<- read.csv("diff_genes.csv")
+leukocyte_IFNa2 <- diff_genes[c(1,4,5,6)]
+leukocyte_IFNa14 <- diff_genes[c(1,9,10,11)]
+
+CD4_IFNa2 <- diff_genes[c(1,19,20,21)]
+CD4_IFNa14 <- diff_genes[c(1,24,25,26)]
+
+colnames(leukocyte_IFNa2) <- c("gene","log2FoldChange","pvalue","padj")
+colnames(leukocyte_IFNa14) <- c("gene","log2FoldChange","pvalue","padj")
+
+colnames(CD4_IFNa2) <- c("gene","log2FoldChange","pvalue","padj")
+colnames(CD4_IFNa14) <- c("gene","log2FoldChange","pvalue","padj")
+
+
+#filter on 0.05 fdr
+#filter on 0.01 fdr
+leukocyte_IFNa2_f <- filter(leukocyte_IFNa2, padj <= 0.01)
+leukocyte_IFNa14_f <- filter(leukocyte_IFNa14, padj <= 0.01)
+CD4_IFNa2_f <- filter(CD4_IFNa2, padj <= 0.01)
+CD4_IFNa14_f <- filter(CD4_IFNa14, padj <= 0.01)
+
+leukocyte_IFNa2_IFNa14 <- merge(leukocyte_IFNa2_f,leukocyte_IFNa14_f, by="gene") #all=TRUE
+CD4_IFNa2_IFNa14 <- merge(CD4_IFNa2_f,CD4_IFNa14_f, by="gene") #all=TRUE
+
+leukocyte_IFNa2_IFNa14 <- leukocyte_IFNa2_IFNa14[c(1,2,5)]
+CD4_IFNa2_IFNa14 <- CD4_IFNa2_IFNa14[c(1,2,5)]
+colnames(leukocyte_IFNa2_IFNa14) <- c("gene","leukocyte_IFNa2", "leukocyte_IFNa14")
+colnames(CD4_IFNa2_IFNa14) <- c("gene","CD4_IFNa2", "CD4_IFNa14")
+#rownames(leukocyte_IFNa2_IFNa14) <- leukocyte_IFNa2_IFNa14$gene
+#rownames(CD4_IFNa2_IFNa14) <- CD4_IFNa2_IFNa14$gene
+#leukocyte_IFNa2_IFNa14 <- leukocyte_IFNa2_IFNa14[-1]
+#CD4_IFNa2_IFNa14 <- CD4_IFNa2_IFNa14[-1]
+
+#leukocyte_IFNa2_IFNa14[is.na(leukocyte_IFNa2_IFNa14)] <- 0
+#CD4_IFNa2_IFNa14[is.na(CD4_IFNa2_IFNa14)] <- 0
+
+write.table(leukocyte_IFNa2_IFNa14, "/Users/shahina/Desktop/leukocyte_IFNa2_IFNa14.txt", sep="\t", quote=FALSE, row.names = FALSE)
+write.table(CD4_IFNa2_IFNa14, "/Users/shahina/Desktop/CD4_IFNa2_IFNa14.txt", sep="\t", quote=FALSE, row.names = FALSE)
+
+#MEV was used to create Heatmaps
+
+#################
+#VENN DIAGRAM
+#################
+library(BioVenn)
+
+setwd("/Users/shahina/Projects/22-1KELA-001")
+
+diff_genes<- read.csv("diff_genes.csv")
+leukocyte_IFNa2 <- diff_genes[c(1,4,5,6)]
+leukocyte_IFNa14 <- diff_genes[c(1,9,10,11)]
+
+CD4_IFNa2 <- diff_genes[c(1,19,20,21)]
+CD4_IFNa14 <- diff_genes[c(1,24,25,26)]
+
+colnames(leukocyte_IFNa2) <- c("gene","log2FoldChange","pvalue","padj")
+colnames(leukocyte_IFNa14) <- c("gene","log2FoldChange","pvalue","padj")
+
+colnames(CD4_IFNa2) <- c("gene","log2FoldChange","pvalue","padj")
+colnames(CD4_IFNa14) <- c("gene","log2FoldChange","pvalue","padj")
+
+leukocyte_IFNa2 <- leukocyte_IFNa2[-1,]
+leukocyte_IFNa14 <- leukocyte_IFNa14[-1,]
+CD4_IFNa2 <- CD4_IFNa2[-1,]
+CD4_IFNa14 <- CD4_IFNa14[-1,]
+#filter on 0.05 pvalue
+leukocyte_IFNa2_f <- filter(leukocyte_IFNa2, pvalue <= 0.05)
+leukocyte_IFNa14_f <- filter(leukocyte_IFNa14, pvalue <= 0.05)
+CD4_IFNa2_f <- filter(CD4_IFNa2, pvalue <= 0.05)
+CD4_IFNa14_f <- filter(CD4_IFNa14, pvalue <= 0.05)
+
+
+png("plots/VennDiagram_leukocyte.png")
+leukocyte_IFNa2_list <- leukocyte_IFNa2_f$gene
+leukocyte_IFNa14_list <- leukocyte_IFNa14_f$gene
+empty_list <- c()
+
+biovenn <- draw.venn(leukocyte_IFNa2_list, leukocyte_IFNa14_list, empty_list,title="Leukocyte",
+                     subtitle="", nrtype="abs", xtitle="IFNa2" , ytitle="IFNa14")
+dev.off()
+
+png("plots/VennDiagram_CD4.png")
+CD4_IFNa2_list <- CD4_IFNa2_f$gene
+CD4_IFNa14_list = CD4_IFNa14_f$gene
+
+biovenn <- draw.venn(CD4_IFNa2_list, CD4_IFNa14_list, empty_list,title="CD4",
+                     subtitle="", nrtype="abs", xtitle="IFNa2" , ytitle="IFNa14")
+dev.off()
