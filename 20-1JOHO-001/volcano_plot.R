4a8b6bbcedd3244094bb3eee05f44292f5ba7b84
shayat <91986031+shhayat@users.noreply.github.com>
2022-01-27 19:02:16 -0600

    Update volcano_plot.R

--- 20-1JOHO-001/volcano_plot.R
+++ 20-1JOHO-001/volcano_plot.R
@@ -1,18 +1,22 @@
+library(ggplot2)
 res <- read.csv("/Users/shahina/Projects/20-1JOHO-001/results/treated_and_control_male_after_5samples_removed.csv")
 res1 <- as.data.frame(res)
-#rm row with symbol = NA these are pseudogenes
-res1 <- subset(res1,!symbol == "NA")
-#assign up and down regulation and non signif based on log2fc
-res1$direction <- ifelse(res1$log2FoldChange < -0.5, "down_reg", 
-                               ifelse(res1$log2FoldChange > 0.5, "up_reg", "not_signf" ))
 
-pdf("Volcano_plot.pdf")
-                ggplot(res1, aes(log2FoldChange, -log10(pvalue))) +
-                geom_point(aes(col=direction),
-                               size=0.7,
-                               show.legend = FALSE) +
-                scale_color_manual(values=c("blue", "black", "green")) +
-                theme(axis.text.x = element_text(size=16),
-                      axis.text.y = element_text(size=16),
-                      text = element_text(size=16))
+#assign up and down regulation and non signif based on log2fc
+res1$direction <- ifelse(res1$effects < -1, "down_regulated", 
+                         ifelse(res1$effects > 1, "up_regulated", "signif" ))
+
+png("/Users/shahina/Projects/20-1JOHO-001/plots/Volcano_plot.png", width=1300, height=500, res=120)
+ggplot(res1, aes(effects, -log10(fdr))) +
+  geom_point(aes(col=direction),
+             size=0.5,
+             show.legend = FALSE) +
+  scale_color_manual(values=c("blue", "gray", "red")) +
+  theme(axis.text.x = element_text(size=11),
+        axis.text.y = element_text(size=11),
+        text = element_text(size=11)) +
+  annotate("text",x=2.2,y=1.7,hjust = 0,label=" Total (FDR <=0.05) = 6678 \n Total Upregulated = 3138 \n Total Down Regulated = 3540 \n Log2FC > 1 = 120 \n Log2FC < -1 = 90", size = 3)  +
+  xlab("log2(FC)") +
+  ylab("-log10(FDR)") 
+  
 dev.off()
