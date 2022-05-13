7c59cf77bd798c611758c8728898304138be3172
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-13 14:51:34 -0600

    Update index.sh

--- 21-1TOSH-001/index.sh
+++ 21-1TOSH-001/index.sh
@@ -4,7 +4,7 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=genome_index
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --time=1:00:00
 #SBATCH --mem=6G
 #SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
