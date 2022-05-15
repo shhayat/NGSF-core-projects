971cecd69487e5523317d2400a0b20294fd0863f
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-15 12:10:49 -0600

    Update 01_star_mapping.sh

--- 21-1TOSH-001/01_star_mapping.sh
+++ 21-1TOSH-001/01_star_mapping.sh
@@ -1,19 +1,32 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=star_mapping
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=1:00:00
+#SBATCH --mem=4G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+#loading required modules
+module load star/2.7.9a 
+
+
 DATA=/data/labs/bioinformatics_lab/raw_data/RNAseq/mazloum_nayef/UHRF1_knockout
 GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/indices
-GTF=/data/share/references/igenome/Mouse/GRCm38.p6/gencode.vM18.annotation.gtf
-OUTDATA=/data/labs/bioinformatics_lab/processed/RNASeq/mazloum_nayef/UHRF1_knockout/STAR_alignment
-DIR=/data/labs/bioinformatics_lab/shared/RNASeq/mazloum_nayef/UHRF1_knockout
-STAR=/data/home/shh2026/tools/STAR-2.6.0a/bin/Linux_x86_64_static/STAR
-NCPUS=2
+GFF=/datastore/NGSF001/analysis/references/bison/jhered/esab003/bison.liftoff.chromosomes.gff
+OUTDATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/STAR_alignment
 
 for i in $DATA/*R1_001.fastq.gz
 do
         R1="${i%_R1*}";
         R2=${R1##*/};
         fq1=${R1}_R1_001.fastq.gz
-	      fq2=${R1}_R2_001.fastq.gz
-	      mkdir -p $OUTDATA/${R2}_star
+	fq2=${R1}_R2_001.fastq.gz
+	mkdir -p $OUTDATA/${R2}_star
         mkdir -p $OUTDATA/${R2}_star/tmp 
 
-		echo "$STAR --genomeDir $GENOME --readFilesCommand zcat --readFilesIn $fq1 $fq2 --sjdbGTFfile $GTF --outSAMstrandField intronMotif --outFileNamePrefix $OUTDATA/${R2}_star/star_ --outSAMtype BAM SortedByCoordinate --outFilterIntronMotifs RemoveNoncanonical --runThreadN $NCPUS && samtools index $OUTDATA/${R2}_star/star_Aligned.sortedByCoord.out.bam" >> $DIR/run_star_mapping.sh 
+	$STAR --genomeDir $GENOME --readFilesCommand zcat --readFilesIn $fq1 $fq2 --sjdbGTFfile $GFF --sjdbGTFtagExonParentTranscript Parent --outSAMstrandField intronMotif --outFileNamePrefix $OUTDATA/${R2}_star/star_ --outSAMtype BAM SortedByCoordinate --outFilterIntronMotifs RemoveNoncanonical --runThreadN 2 && samtools index $OUTDATA/${R2}_star/star_Aligned.sortedByCoord.out.bam 
 done
