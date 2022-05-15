51cd2fa5ade59423caa12d5612785c81de864159
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-15 12:37:13 -0600

    Update 01_star_mapping.sh

--- 21-1TOSH-001/01_star_mapping.sh
+++ 21-1TOSH-001/01_star_mapping.sh
@@ -19,14 +19,18 @@ GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/ind
 GFF=/datastore/NGSF001/analysis/references/bison/jhered/esab003/bison.liftoff.chromosomes.gff
 OUTDATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/STAR_alignment
 
-for i in $DATA/*R1_001.fastq.gz
-do
-        R1="${i%_R1*}";
-        R2=${R1##*/};
-        fq1=${R1}_R1_001.fastq.gz
-	fq2=${R1}_R2_001.fastq.gz
-	mkdir -p $OUTDATA/${R2}_star
-        mkdir -p $OUTDATA/${R2}_star/tmp 
+#for i in $DATA/R2200001_S1_R1_001.fastq.gz
+#do
+        #R1="${i%_R1*}";
+        #R2=${R1##*/};
+        #fq1=${R1}_S1_R1_001.fastq.gz
+	#fq2=${R1}_S1_R2_001.fastq.gz
+	fq1=R2200001_S1_R1_001.fastq.gz
+	fq2=R2200001_S1_R2_001.fastq.gz
+	mkdir -p $OUTDATA/R2200001_star
+	mkdir -p $OUTDATA/R2200001_star/tmp 
+	#mkdir -p $OUTDATA/${R2}_star
+        #mkdir -p $OUTDATA/${R2}_star/tmp 
 
 	$STAR --genomeDir $GENOME --readFilesCommand zcat --readFilesIn $fq1 $fq2 --sjdbGTFfile $GFF --sjdbGTFtagExonParentTranscript Parent --outSAMstrandField intronMotif --outFileNamePrefix $OUTDATA/${R2}_star/star_ --outSAMtype BAM SortedByCoordinate --outFilterIntronMotifs RemoveNoncanonical --runThreadN 2 && samtools index $OUTDATA/${R2}_star/star_Aligned.sortedByCoord.out.bam 
-done
+#done
