f27912c2b237d71d8548524bd269d463bc2f87cc
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-16 16:18:12 -0600

    Update rnaseqc.sh

--- 21-1TOSH-001/rnaseqc.sh
+++ 21-1TOSH-001/rnaseqc.sh
@@ -14,16 +14,8 @@ set -eux
 cp /datastore/NGSF001/software/src/rnaseqc.v2.4.2.linux ${SLURM_TMPDIR}
 chmod u+x ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux
 
-GTF=/datastore/NGSF001/analysis/references/bison/jhered/esab003/bison.liftoff.chromosomes.gff
+GTF=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/bison.liftoff.chromosomes.gtf
 OUTDATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/STAR_alignment
-
-
-#sort the provided bam file
-# samtools sort -@4 -m4G -O BAM /datastore/NGSF001/projects/21-1MILE-005/alignment/minimap2/${name}.all-transcripts.mm10.sam > ${name}_mm10.pos-sorted.bam
-
-# run help because I can
-# ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux -h
-
 
 ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux ${GTF} \
                         ${OUTDATA}/R2200001_star/star_Aligned.sortedByCoord.out.bam \
