10310bf5b7001cc92476a4ce78094242f27edca8
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-16 16:10:02 -0600

    Update rnaseqc.sh

--- 21-1TOSH-001/rnaseqc.sh
+++ 21-1TOSH-001/rnaseqc.sh
@@ -14,9 +14,9 @@ set -eux
 cp /datastore/NGSF001/software/src/rnaseqc.v2.4.2.linux ${SLURM_TMPDIR}
 chmod u+x ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux
 
-name=$1; shift
-bam=$1
-gtf=/datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/GCF_000001895.5_Rnor_6.0/GCF_000001895.5_Rnor_6.0_genomic.gtf
+GTF=/datastore/NGSF001/analysis/references/bison/jhered/esab003/bison.liftoff.chromosomes.gff
+OUTDATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/STAR_alignment
+
 
 #sort the provided bam file
 # samtools sort -@4 -m4G -O BAM /datastore/NGSF001/projects/21-1MILE-005/alignment/minimap2/${name}.all-transcripts.mm10.sam > ${name}_mm10.pos-sorted.bam
@@ -25,7 +25,7 @@ gtf=/datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/
 # ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux -h
 
 
-${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux ${gtf} \
-                        ${bam} \
-                        ${SLURM_SUBMIT_DIR} \
-                        --sample=${name}
+${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux ${GTF} \
+                        ${OUTDATA}/R2200001_star/star_Aligned.sortedByCoord.out.bam \
+                        --sample="R2200001" \
+                        output ${OUTDATA}
