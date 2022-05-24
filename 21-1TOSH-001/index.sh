5cb92aae36d856e00bedd1da4cc1d50115356a88
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-24 14:43:26 -0600

    Update index.sh

--- 21-1TOSH-001/index.sh
+++ 21-1TOSH-001/index.sh
@@ -14,8 +14,8 @@ set -eux
 module load star/2.7.9a 
 
 rsync -avzP /datastore/NGSF001/analysis/references/bison/jhered/esab003/sequence.fasta ${SLURM_TMPDIR}/
-rsync -avzP /datastore/NGSF001/analysis/references/bison/jhered/esab003/bison.liftoff.chromosomes.gff ${SLURM_TMPDIR}/
-
+#rsync -avzP /datastore/NGSF001/analysis/references/bison/jhered/esab003/bison.liftoff.chromosomes.gff ${SLURM_TMPDIR}/
+rsync -avzP /globalhome/hxo752/HPC/bison.liftoff.gtf ${SLURM_TMPDIR}/
 mkdir -p ${SLURM_TMPDIR}/star-2.7.9a
 
 #GENOME=/datastore/NGSF001/analysis/references/bison/jhered/esab003/
@@ -29,6 +29,8 @@ STAR --runThreadN 8 \
     --runMode genomeGenerate \
     --genomeDir ${SLURM_TMPDIR}/star-index-2.7.9a \
     --genomeFastaFiles $REF \
-    --sjdbGTFfile ${SLURM_TMPDIR}/bison.liftoff.chromosomes.gff
+    --sjdbGTFfile ${SLURM_TMPDIR}/bison.liftoff.gtf
+    
+#--sjdbGTFfile ${SLURM_TMPDIR}/bison.liftoff.chromosomes.gff
 
 rsync -rvzP ${SLURM_TMPDIR}/star-index-2.7.9a /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/indices
