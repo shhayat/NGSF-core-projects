c902f45e825529646f3dce51cc5c7164d52e0365
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-26 15:34:46 -0600

    Update sbatch_run_fastqc.sh

--- new_proj/sbatch_run_fastqc.sh
+++ new_proj/sbatch_run_fastqc.sh
@@ -1,12 +1,12 @@
 DATA=
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/new_proj/fastqc
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/new_proj
 
 for i in $(seq -w 13 24);
 do
-  sbatch 01_FastQC.sh "${DATA}/E21000${i}.fq"
+  sbatch $OUTDIR/01_FastQC.sh "${DATA}/E21000${i}.fq"
  sleep 0.5
 done 
 
 wait
 
-~/.local/bin/multiqc $OUTDIR/*_fastqc.zip" -o  $OUTDIR
+~/.local/bin/multiqc $OUTDIR/*_fastqc.zip" -o  $OUTDIR/fastqc
