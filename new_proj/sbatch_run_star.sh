b62db368810b0bfbc51054311e669fa4a404549c
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-26 15:26:25 -0600

    Create sbatch_run_star.sh

---
+++ new_proj/sbatch_run_star.sh
@@ -0,0 +1,12 @@
+DATA=
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/new_proj/fastqc
+
+for i in $(seq -w 13 24);
+do
+  sbatch 01_FastQC.sh "${DATA}/E21000${i}.fq"
+ sleep 0.5
+done 
+
+wait
+
+~/.local/bin/multiqc $OUTDIR/*_fastqc.zip" -o  $OUTDIR
