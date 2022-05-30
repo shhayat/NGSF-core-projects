8dae3b8e08d39ad0ed56a4d0d357e97c3da1a42e
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-30 09:58:38 -0600

    Update sbatch_run_rnaseqc.sh

--- new_proj/sbatch_run_rnaseqc.sh
+++ new_proj/sbatch_run_rnaseqc.sh
@@ -1,15 +1,12 @@
-DATA=
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/new_proj
-
-for i in $(seq -w 13 24);
-do
+DATA=/datastore/NGSF001/experiments/depletion_tests/human/fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/depletion_tests
 
   for i in $DATA/*R1_001.fastq.gz
 do
         R1="${i%_R1*}";
-        R2=${R1##*/};
-        fq1=${R1}_R1_001.fastq.gz
-	    fq2=${R1}_R2_001.fastq.gz
-  sbatch $OUTDIR/03_RNASEQC.sh "sample_name" "${DATA}/${R1}.fq" "${DATA}/${R2}.fq"
+        R2=${R1###*/};
+        fq1=${R2}_R1_001.fastq.gz
+	fq2=${R2}_R2_001.fastq.gz
+  sbatch $OUTDIR/03_RNASEQC.sh "${R2}" "${fq1}" "${DATA}/${R2}.fq"
  sleep 0.5
 done 
