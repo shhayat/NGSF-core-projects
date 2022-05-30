f061c4ec366733c449e58f32267b4942d11c8e94
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-30 11:03:38 -0600

    Update sbatch_run_fastqc.sh

--- new_proj/sbatch_run_fastqc.sh
+++ new_proj/sbatch_run_fastqc.sh
@@ -1,4 +1,4 @@
-DATA=
+DATA=/datastore/NGSF001/experiments/depletion_tests/human/fastq
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/new_proj
 
 for i in $(seq -w 13 24);
@@ -12,4 +12,4 @@ wait
 cp ~/.local/bin/multiqc ${SLURM_TMPDIR}
 chmod u+x ${SLURM_TMPDIR}/multiqc
 
-${SLURM_TMPDIR}/multiqc "$OUTDIR/*_fastqc.zip" -o  $OUTDIR/fastqc
+${SLURM_TMPDIR}/multiqc "${OUTDIR}/*_fastqc.zip" -o  ${OUTDIR}/fastqc
