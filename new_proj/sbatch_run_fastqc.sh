40bcfbfe1b677934e7e43956912f598210eb96c6
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-30 11:18:57 -0600

    Update sbatch_run_fastqc.sh

--- new_proj/sbatch_run_fastqc.sh
+++ new_proj/sbatch_run_fastqc.sh
@@ -2,14 +2,9 @@ DATA=/datastore/NGSF001/experiments/depletion_tests/human/fastq
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/depletion_tests
 
 
-for i in $DATA/*R1_001.fastq.gz
+for fq in $DATA/*_001.fastq.gz
 do
-        path="${i%_R1*}";
-        basename=${path##*/};
-	      sample_name=${basename//_*};
-        fq1=${basename}_R1_001.fastq.gz;
-	      fq2=${basename}_R2_001.fastq.gz;
-  sbatch ${OUTDIR}/01_FastQC.sh "${sample_name}" "${fq1}" "${fq2}"
+  echo sbatch ${OUTDIR}/01_FastQC.sh "${fq}"
  sleep 0.5
 done 
 
