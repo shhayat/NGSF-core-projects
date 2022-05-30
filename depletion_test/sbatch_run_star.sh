7b705a1f58eff993952093454ce80fe24f64b9a7
Shahina Hayat (hxo752) <hxo752@plato-base-lgn-16002.cm.cluster>
2022-05-30 15:08:48 -0600

    `	modified:   sbatch_run_star.sh

--- depletion_test/sbatch_run_star.sh
+++ depletion_test/sbatch_run_star.sh
@@ -6,8 +6,8 @@ do
         path="${i%_R1*}";
         basename=${path##*/};
 	sample_name=${basename//_*};
-        fq1=${basename}_R1_001.fastq.gz;
-	fq2=${basename}_R2_001.fastq.gz;
+        fq1=${DATA}/${basename}_R1_001.fastq.gz;
+	fq2=${DATA}/${basename}_R2_001.fastq.gz;
   
       sbatch $OUTDIR/02_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}"
  sleep 0.5
