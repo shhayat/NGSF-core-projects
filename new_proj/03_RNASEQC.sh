08c8f5a00315ac8654aa6eb29796281b1a338384
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-30 10:46:34 -0600

    Update 03_RNASEQC.sh

--- new_proj/03_RNASEQC.sh
+++ new_proj/03_RNASEQC.sh
@@ -14,7 +14,7 @@ chmod u+x ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux
 sample_name=$1; shift
 bam_file=$2
 
-GTF/datastore/NGSF001/analysis/references/human/gencode-40/gencode.v40.annotation.gtf
+GTF=/datastore/NGSF001/analysis/references/human/gencode-40/gencode.v40.annotation.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/depletion_tests/human/rnaseqc
 
 mkdir -p ${OUTDIR}
