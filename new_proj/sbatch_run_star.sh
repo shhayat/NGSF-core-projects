9fe64fb48f6083861f9432b98a13c5bccd70cc1e
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-30 10:45:43 -0600

    Update sbatch_run_star.sh

--- new_proj/sbatch_run_star.sh
+++ new_proj/sbatch_run_star.sh
@@ -1,15 +1,13 @@
 DATA=/datastore/NGSF001/experiments/depletion_tests/human/fastq
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/depletion_tests/
-
-for i in $(seq -w 13 24);
-do
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/depletion_tests
 
   for i in $DATA/*R1_001.fastq.gz
 do
-        R1="${i%_R1*}";
-        R2=${R1##*/};
-        fq1=${R1}_R1_001.fastq.gz
-	    fq2=${R1}_R2_001.fastq.gz
-  sbatch $OUTDIR/02_star_mapping.sh "sample_name" "${DATA}/${R1}.fq" "${DATA}/${R2}.fq"
+        path="${i%_R1*}";
+        basename=${path##*/};
+	sample_name=${basename//_*};
+        fq1=${basename}_R1_001.fastq.gz;
+	fq2=${basename}_R2_001.fastq.gz;
+  sbatch $OUTDIR/03_RNASEQC.sh "${sample_name}" "${fq1}" "${fq2}"
  sleep 0.5
 done 
