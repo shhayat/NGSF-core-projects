2259bdd41d4b8d3bae2e12bc998be1303cc789c8
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-30 09:47:13 -0600

    Update sbatch_run_star.sh

--- new_proj/sbatch_run_star.sh
+++ new_proj/sbatch_run_star.sh
@@ -1,5 +1,5 @@
-DATA=
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/new_proj
+DATA=/datastore/NGSF001/experiments/depletion_tests/human/fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/depletion_tests/
 
 for i in $(seq -w 13 24);
 do
