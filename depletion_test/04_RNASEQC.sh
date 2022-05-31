adc3ead262e6d6d5643743abe2c56707d03671e3
Shahina Hayat (hxo752) <hxo752@platolgn01.cm.cluster>
2022-05-31 10:07:17 -0600

    new file:   03_collaspe_annotation.py 	renamed:    03_RNASEQC.sh -> 04_RNASEQC.sh 	new file:   readme_for_03_collaspe_annotation

--- depletion_test/04_RNASEQC.sh
+++ depletion_test/04_RNASEQC.sh
@@ -14,7 +14,8 @@ chmod u+x ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux
 sample_name=$1; shift
 bam_file=$1
 
-GTF=/datastore/NGSF001/analysis/references/human/gencode-40/gencode.v40.annotation.gtf
+#GTF=/datastore/NGSF001/analysis/references/human/gencode-40/gencode.v40.annotation.gtf
+GTF=/datastore/NGSF001/analysis/references/human/gencode-40/gencode.v40.annotation_mod.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/depletion_test/human/rnaseqc
 
 mkdir -p ${OUTDIR}
