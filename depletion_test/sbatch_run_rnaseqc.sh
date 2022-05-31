bced470dc3ca2dbd88ff602bb9e961f18fca92f0
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-30 19:49:34 -0600

    Update sbatch_run_rnaseqc.sh

--- depletion_test/sbatch_run_rnaseqc.sh
+++ depletion_test/sbatch_run_rnaseqc.sh
@@ -1,12 +1,12 @@
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/depletion_test/human/star_alignment
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/depletion_test
 
-for i in ${DATA}/*/Aligned.sortedByCoord.out.bam
+for i in ${DATA}/*/*.bam
 do
-        path="${i%Aligned*}";
-	sample_name=${path//_*};
+        path="${i%/Aligned*}";
+	sample_name="${path##*/}"
       
-  sbatch $OUTDIR/03_RNASEQC.sh "${sample_name}" "${basename}"
+  sbatch $OUTDIR/03_RNASEQC.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
  sleep 0.5
 done 
 
