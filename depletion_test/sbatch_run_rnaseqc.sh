7a63ba46fa7b821edab7de3506b447e5080a64a4
shayat <91986031+shhayat@users.noreply.github.com>
2022-05-30 19:38:24 -0600

    Update sbatch_run_rnaseqc.sh

--- depletion_test/sbatch_run_rnaseqc.sh
+++ depletion_test/sbatch_run_rnaseqc.sh
@@ -3,7 +3,7 @@ OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/depletion_test
 
 for i in ${DATA}/*/Aligned.sortedByCoord.out.bam
 do
-        path="${i%*}";
+        path="${i%Aligned*}";
         basename=${path##*/};
 	sample_name=${basename//_*};
       
