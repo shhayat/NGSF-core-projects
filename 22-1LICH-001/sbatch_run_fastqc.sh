45e122250372d7e009539313879b8175b304c335
shayat <91986031+shhayat@users.noreply.github.com>
2022-06-07 13:00:18 -0600

    Update sbatch_run_fastqc.sh

--- 22-1LICH-001/sbatch_run_fastqc.sh
+++ 22-1LICH-001/sbatch_run_fastqc.sh
@@ -2,7 +2,7 @@ DATA=/datastore/NGSF001/projects/22-1LICH-001/fastq
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1LICH-001
 
 
-for fq in $DATA/*_001.fastq.gz
+for fq in $DATA/*_R*.fastq.gz
 do
    sbatch ${OUTDIR}/01_FastQC.sh "${fq}"
    sleep 0.5
