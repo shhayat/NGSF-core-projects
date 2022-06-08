0f2904a8e17380b86ac7f9ce2296830dc5d34c6c
shayat <91986031+shhayat@users.noreply.github.com>
2022-06-08 10:10:55 -0600

    Create sbatch_run_rnaseqc.sh

---
+++ 22-1LICH-001/sbatch_run_rnaseqc.sh
@@ -0,0 +1,16 @@
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/depletion_test/human/star_alignment
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1LICH-001
+
+for i in ${DATA}/*/*.bam
+do
+        path="${i%/Aligned*}";
+	      sample_name="${path##*/}"
+      
+  sbatch $OUTDIR/04_RNASEQC.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
+ sleep 0.5
+done 
+
+
+#cd /globalhome/hxo752/HPC/tools/
+#./multiqc -d ${OUTDIR}/human/rnaseqc/*metrics.tsv -o ${OUTDIR}/human/rnaseqc
+#./multiqc -d ${OUTDIR}/human/rnaseqc/*exon_cv.tsv -o ${OUTDIR}/human/rnaseqc
