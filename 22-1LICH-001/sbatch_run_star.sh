4b1ec4a7db2f56e9fe7ca5b1846e2487c22ac009
shayat <91986031+shhayat@users.noreply.github.com>
2022-06-08 09:45:32 -0600

    Update sbatch_run_star.sh

--- 22-1LICH-001/sbatch_run_star.sh
+++ 22-1LICH-001/sbatch_run_star.sh
@@ -3,11 +3,10 @@ OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1LICH-001
 
 for i in $DATA/R2*.fastq.gz
 do
-        path="${i%_R1*}";
-        basename=${path##*/};
-	      sample_name=${basename//_*};
-        fq1=${DATA}/${basename}_R1.fastq.gz;
-	      fq2=${DATA}/${basename}_R2.fastq.gz;
+        path="${i%_R*}";
+        sample_name=${path##*/};
+        fq1=${DATA}/${sample_name}_R1.fastq.gz;
+	fq2=${DATA}/${sample_name}_R2.fastq.gz;
   
       sbatch $OUTDIR/02_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}"
  sleep 0.5
