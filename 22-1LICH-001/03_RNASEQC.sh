a28d419c6b47af98a8d5d60ce67c42060d5ba21f
Shahina Hayat (hxo752) <hxo752@plato-base-lgn-16002.cm.cluster>
2022-06-08 13:55:15 -0600

    new file:   03_RNASEQC.sh

---
+++ 22-1LICH-001/03_RNASEQC.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=00:20:00
+#SBATCH --mem=5G
+
+set -eux
+
+# copy tool to tempdir and give execute permission
+cp /datastore/NGSF001/software/src/rnaseqc.v2.4.2.linux ${SLURM_TMPDIR}
+chmod u+x ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux
+
+sample_name=$1; shift
+bam_file=$1
+
+GTF=/datastore/NGSF001/analysis/references/human/gencode-40/gencode.v40.annotation_mod.gtf
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1LICH-001/rnaseqc
+
+mkdir -p ${OUTDIR}
+
+${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux ${GTF} \
+                        ${bam_file} \
+                        ${OUTDIR} \
+                        --sample=${sample_name}
