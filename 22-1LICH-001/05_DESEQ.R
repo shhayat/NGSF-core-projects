a990b57af2cc66576a41ba0a5aaea82485b8a067
shayat <91986031+shhayat@users.noreply.github.com>
2022-06-09 10:39:21 -0600

    Update 05_DESEQ.R

--- 22-1LICH-001/05_DESEQ.R
+++ 22-1LICH-001/05_DESEQ.R
@@ -5,7 +5,7 @@ library("org.Hs.eg.db")
 library("ggplot2")
 library("biomaRt")
 
-dir.create("/Users/hxo752/Desktop/22-1LICH-001", recursive=TRUE, showWarnings = FALSE) 
+dir.create("/Users/hxo752/Desktop/22-1LICH-001/DESEQ2", recursive=TRUE, showWarnings = FALSE) 
 setwd("/Users/hxo752/Desktop/22-1LICH-001")
 
 load("feature_count.RData")
@@ -13,11 +13,13 @@ feature_count <- as.data.frame(feature_count)
 geneID <- gsub(".[0-9]*$", "", rownames(feature_count))
 rownames(feature_count) <- geneID
 
+feature_annotation <- feature_count[1:3]
+
 DEG_analysis <-  function(colnum,cond1, cond2, ref)
 {
-feature_count <- feature_count[colnum]
+#feature_count <- feature_count[colnum]
 
-feature_count <- feature_count[c(1,3,5,2,4,6)]
+feature_count <- feature_count[c(4,6,8,5,7,9)]
 #at least one column has number
 feature_count <- feature_count[apply(feature_count,1,function(z) any(z!=0)),]
 
@@ -42,50 +44,25 @@ dds_wald <- DESeq(dds, betaPrior=TRUE, minReplicatesForReplace=Inf)
 #results with independent filtering and outlier removal and padj is by default 0.1
 res <- results(dds_wald, independentFiltering=TRUE,cooksCutoff=TRUE)
 #summary(res)
-res$symbol <- mapIds(org.Hs.eg.db,
-                     keys=row.names(res),
-                     column="SYMBOL",
-                     keytype="ENSEMBL",
-                     multiVals="first")
-                     
-ensembl = biomaRt::useMart("ensembl", dataset='hsapiens_gene_ensembl')
-attributes = biomaRt::listAttributes(ensembl)
-select_attributes = biomaRt::getBM(attributes = c('ensembl_gene_id','gene_biotype'), mart = ensembl)
-names(select_attributes) <- c("gene_id","gene_biotype")  
-
-
-res_pval <- subset(res, pvalue <= 0.05)
+resDF <- data.frame(GeneID=rownames(res),res)
+resDF1 <- merge(feature_annotation,resDF, by="GeneID")
+res_pval <- subset(resDF1, pvalue <= 0.05)
 res_pval_ordered <- res_pval[order(res_pval$pvalue),]
-resDF <- data.frame(gene_id=rownames(res_pval_ordered),res_pval_ordered)
-resDF1 <- merge(resDF, select_attributes, by="gene_id")
 
 #All significant
-write.csv(resDF1,file=sprintf("DESEQ2_DEG_%s_vs_%s_filter_on_pval.csv",cond2,cond1),quote=FALSE, row.names = FALSE)
-
+write.csv(resDF1,file=sprintf("DESEQ2/DESEQ2_DEG_%s_vs_%s_filter_on_pval.csv",cond2,cond1),quote=FALSE, row.names = FALSE)
 
 ##########
 #PCA PLOT
 ##########
-#gernate rlog for PCA
+  #gernate rlog for PCA
   rld <-rlog(dds,blind=FALSE)
-  pdf(sprintf("PCA_%s_%s.pdf",cond1,cond2), width=5, height=30)
-    nudge <- position_nudge(y = 1)
-    p <- plotPCA(rld,intgroup=c("sample_group"))  
-    p <- p + geom_text(aes_string(label = "name"), color="black", position = nudge)
-    print(p)
-dev.off()
-
-
-#########
-#MA PLOT
-#########
-pdf(sprintf("MA_plot_%s_vs_%s.pdf",cond2,cond1))
-   ylim <- c(-3,3)
-   resGA <- results(dds_wald, lfcThreshold=.5, altHypothesis="greaterAbs")
-   drawLines <- function() abline(h=c(-.5,.5),col="dodgerblue",lwd=2)
-   plotMA(resGA, ylim=ylim); drawLines()
-dev.off()
-
+  pdf(sprintf("DESEQ2/PCA_%s_%s.pdf",cond2,cond1), width=15,height=15)
+  nudge <- position_nudge(y = 0.5)
+  p <- plotPCA(rld,intgroup=c("sample_group"))  
+  p <- p + geom_text(aes_string(label = "name"), color="black", position = nudge, size=2.8)
+  print(p)
+  dev.off()
 
 }
 Untreated_vs_Treated   <- DEG_analysis(c(1:6),"Untreated","Treated","Untreated")
