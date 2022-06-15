a2e5ca5c216bac3e6877bf2dc7d714465265e2ca
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-15 13:51:09 -0600

    Update bams_to_juncs.sh

--- leafcutter/bams_to_juncs.sh
+++ leafcutter/bams_to_juncs.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=00:20:00
+#SBATCH --mem=5G
+
+set -eux
+
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects//analysis/bam_to_junct
+
+module load samtools
+cp /globalhome/hxo752/HPC/tools/regtools/build ${SLURM_TMPDIR}
+chmod u+x ${SLURM_TMPDIR}/regtools/build
+
+sample_name=$1; shift
+bam_file=$1
+
+echo Converting $bamfile to $bamfile.junc
+regtools junctions extract -a 8 -m 50 -M 500000 /$bam_file -o $bamfile.junc
