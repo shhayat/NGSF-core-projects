6a9a469e313ed9b3850452ba92aacb5957908829
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-15 14:17:26 -0600

    Create sbatch_bam_to_junc.sh

---
+++ leafcutter/sbatch_bam_to_junc.sh
@@ -0,0 +1,11 @@
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects//star_alignment
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects//
+
+for i in ${DATA}/*/*.bam
+do
+        path="${i%/Aligned*}";
+	      sample_name="${path##*/}"
+      
+  sbatch ${OUTDIR}/bam_to_junc.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
+ sleep 0.5
+done 
