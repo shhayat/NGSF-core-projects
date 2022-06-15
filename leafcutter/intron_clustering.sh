353fc034b5f20b439637c85054449a0017bef2eb
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-15 14:29:52 -0600

    Update intron_clustering.sh

--- leafcutter/intron_clustering.sh
+++ leafcutter/intron_clustering.sh
@@ -1,6 +1,16 @@
+#SBATCH --job-name=intron_clustering
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+
+
+DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects//analysis/bam_to_junct
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects//analysis/intron_clustering
+
+mkdir -p ${OUTDIR}
 
 
 
-
-
-python ../clustering/leafcutter_cluster_regtools.py -j test_juncfiles.txt -m 50 -o testYRIvsEU -l 500000
+python ../clustering/leafcutter_cluster_regtools.py -j ${OUTDIR}/juncfiles.txt -m 50 -o testYRIvsEU -l 500000
