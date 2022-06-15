6b5f19247e9d3e7ef6a7ea68ce94ada85b3e2feb
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-15 14:24:22 -0600

    Update sbatch_bam_to_junc.sh

--- leafcutter/sbatch_bam_to_junc.sh
+++ leafcutter/sbatch_bam_to_junc.sh
@@ -4,7 +4,7 @@ OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects//
 for i in ${DATA}/*/*.bam
 do
         path="${i%/Aligned*}";
-	      sample_name="${path##*/}"
+	sample_name="${path##*/}"
       
   sbatch ${OUTDIR}/bam_to_junc.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
  sleep 0.5
