2016d2b3525fe1cda33eef83b6665f7b42d335c9
Shahina Hayat (hxo752) <hxo752@plato-base-lgn-16002.cm.cluster>
2022-06-20 11:54:35 -0600

    renamed:    bams_to_juncs.sh -> 03_bams_to_juncs.sh 	renamed:    intron_clustering.sh -> 04_intron_clustering.sh 	modified:   sbatch_bam_to_junc.sh

--- 22-1MILE-001/sbatch_bam_to_junc.sh
+++ 22-1MILE-001/sbatch_bam_to_junc.sh
@@ -1,13 +1,13 @@
 DATA=/datastore/NGSF001/projects/22-1MILE-001/deduplication/umi-tools
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001
 
-for i in ${DATA}/*/*.bam
+for i in ${DATA}/*.bam
 do
 
 	path="${i%*}";     
 	sample_path="${path%.no-rRNA.primary-aln.dedup.bam}"; 
 	sample_name="${sample_path##*/}";
       
-  sbatch ${OUTDIR}/bam_to_junc.sh "${sample_name}" "${DATA}/${sample_name}.no-rRNA.primary-aln.dedup.bam"
+  sbatch ${OUTDIR}/03_bam_to_junc.sh "${sample_name}" "${DATA}/${sample_name}.no-rRNA.primary-aln.dedup.bam"
  sleep 0.5
 done 
