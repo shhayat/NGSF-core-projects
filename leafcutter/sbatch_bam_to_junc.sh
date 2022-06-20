f0300de869b1fe7e13aa170584c267cfde2b482a
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-20 11:32:09 -0600

    Update sbatch_bam_to_junc.sh

--- leafcutter/sbatch_bam_to_junc.sh
+++ leafcutter/sbatch_bam_to_junc.sh
@@ -1,11 +1,13 @@
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects//star_alignment
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects//
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/
 
 for i in ${DATA}/*/*.bam
 do
-        path="${i%/Aligned*}";
-	sample_name="${path##*/}"
+
+	path="${i%*}";     
+	sample_path="${path%.no-rRNA.primary-aln.dedup.bam}"; 
+	sample_name="${sample_path##*/}";
       
-  sbatch ${OUTDIR}/bam_to_junc.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
+  sbatch ${OUTDIR}/bam_to_junc.sh "${sample_name}" "${DATA}/${sample_name}.no-rRNA.primary-aln.dedup.bam"
  sleep 0.5
 done 
