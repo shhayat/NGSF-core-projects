1de2739331f0df20cbdc68183cf1a8c91384bd4d
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-20 16:32:32 -0600

    Update 05_differential_splicing.R

--- 22-1MILE-001/05_differential_splicing.R
+++ 22-1MILE-001/05_differential_splicing.R
@@ -9,8 +9,9 @@
 
 module load r/4.1.2
 
-OUTDIR=""
-mkdir ${OUTDIR} 
+OUTDIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/analysis/differntial_splicing"
+mkdir -p ${OUTDIR} 
+
 cp /mnt/NGSF001/analysis/references/mouse/mm10/leafcutter/m10.exon.txt.gz ${SLURM_TMPDIR}
 
-Rscript ~/envs/leafcutter/scripts/leafcutter_ds.R -o  --num_threads 4 -g 3 -i 3 -e ${SLURM_TMPDIR}/m10.exon.txt.gz ../junction/CTRLvsA1_perind_numers.counts.gz ../junction/groups_file.txt
+Rscript /globalhome/hxo752/HPC/tools/leafcutter/leafcutter_ds.R -o ${OUTDIR} --num_threads 4 -g 3 -i 3 -e ${SLURM_TMPDIR}/m10.exon.txt.gz ../junction/CTRLvsA1_perind_numers.counts.gz ../junction/groups_file.txt
