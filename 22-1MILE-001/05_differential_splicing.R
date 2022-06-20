f8fa1a461a2ef44d0284ad5dacd3d4b857516fd2
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-20 17:07:50 -0600

    Update 05_differential_splicing.R

--- 22-1MILE-001/05_differential_splicing.R
+++ 22-1MILE-001/05_differential_splicing.R
@@ -16,5 +16,5 @@ cp /datastore/NGSF001/analysis/references/mouse/mm10/leafcutter/m10.exon.txt.gz 
 
 #Rscript /globalhome/hxo752/HPC/tools/leafcutter/scripts/leafcutter_ds.R -o ${OUTDIR} --num_threads 4 -g 3 -i 3 -e ${SLURM_TMPDIR}/m10.exon.txt.gz /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/analysis/intron_clustering/leafcutter_perind_numers.counts.gz /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/groups_file.txt
 
-Rscript /Users/shahina/Tools/leafcutter/scripts/leafcutter_ds.R -o /Users/shahina/Projects/22-1MILE-001 --num_threads 4 -g 3 -i 3 -e /Users/shahina/Projects/22-1MILE-001/m10.exon.txt.gz /Users/shahina/Projects/22-1MILE-001/leafcutter_perind_numers.counts.gz /Users/shahina/Projects/22-1MILE-001/groups_file.txt
+Rscript /Users/shahina/Tools/leafcutter/scripts/leafcutter_ds.R -o /Users/shahina/Projects/22-1MILE-001 --num_threads 4 -g 3 -i 3 -e /Users/shahina/Projects/22-1MILE-001/m10.exon.txt.gz /Users/shahina/Projects/22-1MILE-001/leafcutter_perind_numers.counts.gz /Users/shahina/Projects/22-1MILE-001/group_file.txt
 
