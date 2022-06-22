665a7285763672d61cbc3e3b98a30b1b0b399d5a
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-22 09:59:37 -0600

    Update sbatch_run_star.sh

--- 22-1LICH-001/sbatch_run_star.sh
+++ 22-1LICH-001/sbatch_run_star.sh
@@ -1,5 +1,5 @@
 DATA=/datastore/NGSF001/projects/22-1LICH-001/fastq
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1LICH-001
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1LICH-001
 
 for i in $DATA/R2*.fastq.gz
 do
@@ -8,6 +8,6 @@ do
         fq1=${DATA}/${sample_name}_R1.fastq.gz;
 	fq2=${DATA}/${sample_name}_R2.fastq.gz;
   
-      sbatch $OUTDIR/02_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}"
+      sbatch ${SCRIPT_DIR}/02_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}"
  sleep 0.5
 done
