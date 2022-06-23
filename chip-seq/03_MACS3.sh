81d43556e20ecff5a2bbed30ba67ef56d9abc287
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-23 16:01:30 -0600

    Update 03_MACS3.sh

--- chip-seq/03_MACS3.sh
+++ chip-seq/03_MACS3.sh
@@ -3,6 +3,7 @@
 
 cd /globalhome/hxo752/HPC/.local/bin/
 
+OUTDIR=
 sample_name=
 
-macs3 callpeak -t ChIP.bam -c Control.bam -f BAM -g hs -n ${sample_name} -B -q 0.01
+macs3 callpeak -t ChIP.bam -c Control.bam -f BAM -g hs -n ${sample_name} -B -q 0.01 --outdir OUTDIR
