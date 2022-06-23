739fa78188107b7a0545b55add36fec8344701cd
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-23 10:34:27 -0600

    Update 02_bowtie2.sh

--- chip-seq/02_bowtie2.sh
+++ chip-seq/02_bowtie2.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=bams_to_juncs
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+
+set -eux
+
+cd /globalhome/hxo752/HPC/tools/bowtie2-2.4.5-linux-x86_64
+
+./bowtie2
+
 #https://www.hdsu.org/chipatac2020/
-/globalhome/hxo752/HPC/tools/bowtie2-2.4.5-linux-x86_64
+
