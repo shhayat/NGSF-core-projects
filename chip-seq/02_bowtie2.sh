a42d00fe57b4d39e46296fcd14dd67a2f809f367
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-23 11:03:07 -0600

    Update 02_bowtie2.sh

--- chip-seq/02_bowtie2.sh
+++ chip-seq/02_bowtie2.sh
@@ -9,9 +9,28 @@
 
 set -eux
 
+module load samtools
 cd /globalhome/hxo752/HPC/tools/bowtie2-2.4.5-linux-x86_64
 
-./bowtie2
+RAW_DATA=
+GENOME=
+NCPU=1
 
+mkdir -p ${OUTDIR}
+sample_name=$1; shift
+fq1=$1; shift
+fq2=$1
+
+
+./bowtie2\
+--phred33 \
+--mm \
+--very-sensitive \
+--threads  ${NCPU} \
+-x ${GENOME}/index \
+-q <input fastq> \
+2> \
+<bowtie2.log> \
+| samtools view -h -b - > <aligned.bam>
 #https://www.hdsu.org/chipatac2020/
 
