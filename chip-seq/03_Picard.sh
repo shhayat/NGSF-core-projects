6401f026ba7deeae57e10b129380d28a1843f70c
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-27 10:20:46 -0600

    Update 03_Picard.sh

--- chip-seq/03_Picard.sh
+++ chip-seq/03_Picard.sh
@@ -12,13 +12,18 @@
 set -eux
 
 module load picard/2.23.3 
+module load samtools
+
 BAMDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/alignment
 NCPU=2
 
 sample_name=$1
 
 java -Xmx64G -jar $PICARD_HOME/picard.jar MarkDuplicates \
-             I=${BAMDIR}/${sample_name}/${sample_name}.aligned.bam O= ${BAMDIR}/${sample_name}/${sample_name}.aligned_dedup.bam \
+             I=${BAMDIR}/${sample_name}/${sample_name}.aligned.bam \
+             O= ${BAMDIR}/${sample_name}/${sample_name}.aligned_dedup.bam \
              M=${BAMDIR}/${sample_name}/dedup_metrics.txt VALIDATION_STRINGENCY=LENIENT \
              REMOVE_DUPLICATES=true \
-             ASSUME_SORTED=true
+             ASSUME_SORTED=true && \
+             samtools sort -T sort_tempdir -o ${BAMDIR}/${sample_name}.aligned_dedup.sort.bam ${BAMDIR}/${sample_name}/${sample_name}.aligned_dedup.bam && \
+             samtools index ${BAMDIR}/${sample_name}.aligned_dedup.sort.bam
