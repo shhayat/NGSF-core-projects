23c6913ccf75fe627d691fd51c2140f07468dab6
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-27 10:39:53 -0600

    Update 04_MACS3.sh

--- chip-seq/04_MACS3.sh
+++ chip-seq/04_MACS3.sh
@@ -3,14 +3,13 @@
 
 cd /globalhome/hxo752/HPC/.local/bin/
 
-BAMDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/analysis/alignment
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/analysis/peakcall
 sample_name=$1; shift
 treatBAM=$1; shift
 controlBAM=$1
 
-macs3 callpeak  --treatment ${BAMDIR}/${sample_name}/${treatBAM} \
-                --control ${BAMDIR}/${sample_name}/${controlBAM} \
+macs3 callpeak  --treatment ${sample_name}/${treatBAM} \
+                --control ${sample_name}/${controlBAM} \
                 --format BAM \
                 --gsize hs \
                 --name ${sample_name} -B \
