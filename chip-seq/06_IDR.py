2a28391059faab8fd3f2cc674b08ad2b5efa9602
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-27 15:48:43 -0600

    Update 06_IDR.py

--- chip-seq/06_IDR.py
+++ chip-seq/06_IDR.py
@@ -1,2 +1,6 @@
 #https://github.com/hbctraining/Intro-to-ChIPseq/blob/master/lessons/07_handling-replicates-idr.md
 #Combining replicates to only get the highly reproducible peaks using the IDR method
+
+OUTDIR=
+cd /globalhome/hxo752/HPC/tools/idr-2.0.3
+
