b13e0a27700f30adef306ef01838439507820a59
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-28 12:59:47 -0600

    Update 06_IDR.py

--- chip-seq/06_IDR.py
+++ chip-seq/06_IDR.py
@@ -1,12 +1,15 @@
 #https://github.com/hbctraining/Intro-to-ChIPseq/blob/master/lessons/07_handling-replicates-idr.md
 #Combining replicates to only get the highly reproducible peaks using the IDR method
 
+peaks=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/analysis/peakcall
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/analysis/IDR
+
+sample_name=1;
 
 mkdir -p $OUTDIR
 cd /globalhome/hxo752/HPC/tools/idr-2.0.3
 
-idr --samples Nanog_Rep1_sorted_peaks.narrowPeak Nanog_Rep2_sorted_peaks.narrowPeak \
+idr --samples  ${peaks}/${sample_name}/${sample_name}.peaks.sorted.narrowPeak  ${peaks}/${sample_name}/${sample_name}.peaks.sorted.narrowPeak \
     --input-file-type narrowPeak \
     --rank p.value \
     --output-file idr \
