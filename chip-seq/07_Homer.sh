9468d969978b31b0b570851c94d9e734405949ae
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-29 16:38:57 -0600

    Update 07_Homer.sh

--- chip-seq/07_Homer.sh
+++ chip-seq/07_Homer.sh
@@ -12,8 +12,9 @@
 cd /globalhome/hxo752/HPC/anaconda3/share/homer/bin
 
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/analysis/homer
+GTF=/datastore/NGSF001/analysis/references/human/iGenomes/Homo_sapiens/NCBI/GRCh38/Annotation/Genes/genes.gtf
 
 mkdir -p $OUTDIR
 sample_name=$1;
 
-./annotatePeaks.pl ERpeaks.txt hg18 -gtf gencode.gtf  > ${OUTDIR}/{sample_name}_annotated_peaks.txt
+./annotatePeaks.pl ERpeaks.txt hg18 -gtf ${GTF} > ${OUTDIR}/{sample_name}_annotated_peaks.txt
