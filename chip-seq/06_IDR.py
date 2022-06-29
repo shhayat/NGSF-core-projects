13da68abdd6b735afd877cd21352b1a49f937e15
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-29 09:57:34 -0600

    Update 06_IDR.py

--- chip-seq/06_IDR.py
+++ chip-seq/06_IDR.py
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=idr
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=2:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+
+
 #https://github.com/hbctraining/Intro-to-ChIPseq/blob/master/lessons/07_handling-replicates-idr.md
 #Combining replicates to only get the highly reproducible peaks using the IDR method
 
