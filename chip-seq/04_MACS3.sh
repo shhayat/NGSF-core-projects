d08e8f03e8e28d108512e660de07bd098dbf3bcd
Shahina <91986031+shhayat@users.noreply.github.com>
2022-06-29 09:56:59 -0600

    Update 04_MACS3.sh

--- chip-seq/04_MACS3.sh
+++ chip-seq/04_MACS3.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=macs3
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=2:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
 
 cd /globalhome/hxo752/HPC/.local/bin/
