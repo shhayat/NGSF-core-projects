75e0897d8bbe277937fe9e18b1a5d09c1a957ca1
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-11 13:57:41 -0600

    Update 02_bowtie2.sh

--- chip-seq/02_bowtie2.sh
+++ chip-seq/02_bowtie2.sh
@@ -14,7 +14,7 @@ set -eux
 module load samtools
 cd /globalhome/hxo752/HPC/tools/bowtie2-2.4.5-linux-x86_64
 
-RAW_DATA=
+RAW_DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/raw_data
 GENOME=/datastore/NGSF001/analysis/references/human/iGenomes/Homo_sapiens/NCBI/GRCh38/Sequence/Bowtie2Index
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis
 NCPU=2
