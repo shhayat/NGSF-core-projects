2a38f56074ca17b5d0378e02d482e113c4bcf188
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-11 14:54:30 -0600

    Update sbatch_bowtie2.sh

--- chip-seq/sbatch_bowtie2.sh
+++ chip-seq/sbatch_bowtie2.sh
@@ -1,5 +1,5 @@
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/raw_data
-for i in $(seq -w 86 89);
+for i in $(seq -w 86);
 do
   sbatch 02_bowtie2.sh ${OUTDIR}/SRR197542${i} ${OUTDIR}/SRR197542${i}_pass.fastq.gz
   sleep 0.2
