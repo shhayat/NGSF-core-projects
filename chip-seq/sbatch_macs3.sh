8988838a72ed1907daf6ee613a9de41ccf30a95c
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-13 12:29:47 -0600

    Create sbatch_macs3.sh

---
+++ chip-seq/sbatch_macs3.sh
@@ -0,0 +1,7 @@
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/analysis/peakcall
+
+
+treatBAM=${SRR19754288, SRR19754289}
+controlBAM=${SRR19754286, SRR19754287}
+
+for i in ${treatBAM}
