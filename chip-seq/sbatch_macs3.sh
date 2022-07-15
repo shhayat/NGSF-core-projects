6266ddd02810c6dbba9f6fb10bd641964b3aa3e2
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-15 12:39:15 -0600

    Update sbatch_macs3.sh

--- chip-seq/sbatch_macs3.sh
+++ chip-seq/sbatch_macs3.sh
@@ -4,4 +4,12 @@ OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/ana
 treatBAM=${SRR19754288, SRR19754289}
 controlBAM=${SRR19754286, SRR19754287}
 
+filename='peptides.txt'
+echo Start
+while read p; do 
+    echo "$p[1]"
+done < "$filename"
+
+
+
 for i in ${treatBAM}
