e1105eb84109677c476b4b39db3644186558e9ed
Shahina Hayat (hxo752) <hxo752@platolgn01.cm.cluster>
2022-07-15 15:55:47 -0600

    modified:   00_genome_index.sh

--- chip-seq/00_genome_index.sh
+++ chip-seq/00_genome_index.sh
@@ -4,13 +4,13 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=genome_index
 #SBATCH --ntasks=1
-#BATCH --cpus-per-task=8
+#BATCH --cpus-per-task=2
 #SBATCH --time=06:00:00
 #SBATCH --mem=60G
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 set -eux
 
-NCPU=8
+NCPU=2
 cd /globalhome/hxo752/HPC/tools/bowtie2-2.4.5-linux-x86_64
 #OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/indices
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/indices_mouse
