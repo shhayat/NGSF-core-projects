0f9b164ae40861ff96489306bbf6f8a61a130c2b
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-15 14:37:18 -0600

    Update 00_genome_index.sh

--- chip-seq/00_genome_index.sh
+++ chip-seq/00_genome_index.sh
@@ -12,10 +12,11 @@ set -eux
 
 NCPU=4
 cd /globalhome/hxo752/HPC/tools/bowtie2-2.4.5-linux-x86_64
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/indices
+#OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/indices
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/indices_mouse
 mkdir -p ${OUTDIR}
 
 
 #rsync -avzP /datastore/NGSF001/analysis/references/human/iGenomes/Homo_sapiens/NCBI/GRCh38/Sequence/WholeGenomeFasta ${SLURM_TMPDIR}/
 
-./bowtie2-build ${OUTDIR}/genome.fa ${OUTDIR}/HS_GRCh38 --threads ${NCPU}
+./bowtie2-build ${OUTDIR}/genome.fa ${OUTDIR}/MM_GRCm38 --threads ${NCPU}
