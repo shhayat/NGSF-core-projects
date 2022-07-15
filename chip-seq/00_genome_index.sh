97bc5b3fc3dbbc46fbe75b30b2564b96d216f501
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-15 14:37:45 -0600

    Update 00_genome_index.sh

--- chip-seq/00_genome_index.sh
+++ chip-seq/00_genome_index.sh
@@ -4,13 +4,13 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=genome_index
 #SBATCH --ntasks=1
-#BATCH --cpus-per-task=4
+#BATCH --cpus-per-task=8
 #SBATCH --time=06:00:00
-#SBATCH --mem=40G
+#SBATCH --mem=60G
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 set -eux
 
-NCPU=4
+NCPU=8
 cd /globalhome/hxo752/HPC/tools/bowtie2-2.4.5-linux-x86_64
 #OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/indices
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/indices_mouse
