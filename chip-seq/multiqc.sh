88b2b072d8b10305cfd3075606f3c9ea31758e69
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-18 11:40:14 -0600

    Update multiqc.sh

--- chip-seq/multiqc.sh
+++ chip-seq/multiqc.sh
@@ -7,7 +7,7 @@ cd /globalhome/hxo752/HPC/tools/
 #./multiqc ${OUTDIR}/fastqc/*_fastqc.zip -o ${OUTDIR}/fastqc
 
 #BOWTIE ALIGNMENT
-./multiqc -d /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/alignment/*/*bowtie2.log  -o ${OUTDIR}/bowtie2
+./multiqc -d /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/alignment/*/*bowtie2.log  -o ${OUTDIR}/bowtie2 -n bowtie2
 
 #PICARD
-./multiqc -d /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/alignment/*/*picard.log  -o ${OUTDIR}/picard
+./multiqc -d /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/alignment/*/dedup_metrics.txt  -o ${OUTDIR}/picard -n picard
