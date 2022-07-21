a363147865475a71266ea0f9c2ac0c2986acae51
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-20 18:17:38 -0600

    Update 05_QC.sh

--- chip-seq/05_QC.sh
+++ chip-seq/05_QC.sh
@@ -1,6 +1,7 @@
 #https://github.com/hbctraining/Intro-to-ChIPseq/blob/master/lessons/06_combine_chipQC_and_metrics.md
 library(ChIPQC)
-
+setwd("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis")
+dir.create("QC")
 #create sample infor object
 sampl_info <- read.table(sampleSheet_for_chiqQC.txt)
 
@@ -10,8 +11,4 @@ View(sampl_info)
 chipObj <- ChIPQC(sampl_info, annotation="mm10") 
 
 # Create ChIPQC report
-ChIPQCreport(chipObj, reportName="ChIP QC Report", reportFolder="ChIPQCreport")
-
-
-#SampleID,Factor,Replicate,bamReads,ControlID,bamControl,Peaks,PeakCaller,Tissue,Condition
-#Nanog.Rep1,Nanog,1,data/bams/H1hesc_Nanog_Rep1_aln.bam,Nanog-Input1,data/bams/H1hesc_Input_Rep1_aln.bam,data/peakcalls/Nanog-rep1_peaks.narrowPeak,narrow,NA,NA
+ChIPQCreport(chipObj, reportName="ChIPQC Report", reportFolder="QC")
