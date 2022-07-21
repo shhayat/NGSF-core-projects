4286efb775702a437e7169c2670a85ec8555dc32
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-21 15:33:07 -0600

    Update 04_QC.sh

--- chip-seq/04_QC.sh
+++ chip-seq/04_QC.sh
@@ -13,8 +13,8 @@ module load r/4.1.2
 OUTDIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis"
 
 mkdir -p ${OUTDIR}/QC/phantompeakqualtools
-sample=$1;
+sample_name=$1;
 
 cd /globalhome/hxo752/HPC/tools/phantompeakqualtools
 
-Rscript run_spp.R -c=${OUTDIR}/alignment/${sample_name}/${sample_name}.aligned_dedup.bam -savp=xcor_${sample}.pdf -odir=${OUTDIR}/QC/phantompeakqualtools -out=xcor_metrics_${sample}.txt
+Rscript run_spp.R -c=${OUTDIR}/alignment/${sample_name}/${sample_name}.aligned_dedup.bam -savp=xcor_${sample_name}.pdf -odir=${OUTDIR}/QC/phantompeakqualtools -out=xcor_metrics_${sample_name}.txt
