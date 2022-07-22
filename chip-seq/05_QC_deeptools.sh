907db8596e90c365718f8ca5ff48ab2a1fbc3630
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-22 13:53:47 -0600

    Update 05_QC_deeptools.sh

--- chip-seq/05_QC_deeptools.sh
+++ chip-seq/05_QC_deeptools.sh
@@ -1,7 +1,14 @@
 
-
+module load python/3.7.7
 #deeptools
 cd /globalhome/hxo752/HPC/.local/lib/python3.7/site-packages/deeptools/
 
-
-python plotFingerprint.py --bamfiles 
+python plotFingerprint.py \
+            --bamfiles ENCFF000PED.chr12.rmdup.filt.sort.bam  \
+            ../../data/bam/hela/ENCFF000PEE.chr12.rmdup.sort.bam \
+            ../../data/bam/hela/ENCFF000PET.chr12.rmdup.sort.bam \
+             --extendReads 110  \
+             --binSize=1000 \
+             --plotFile HeLa.fingerprint.pdf \
+             --labels HeLa_rep1 HeLa_rep2 HeLa_input -p 5 &> fingerprint.log
+  
