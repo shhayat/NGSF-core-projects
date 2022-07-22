48c260db8b7247b483fc57f615c39941641b52a7
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-22 14:02:34 -0600

    Update 05_QC_deeptools.sh

--- chip-seq/05_QC_deeptools.sh
+++ chip-seq/05_QC_deeptools.sh
@@ -1,14 +1,29 @@
 
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=QC-deeptools
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=2:00:00
+#SBATCH --mem=20G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+
 module load python/3.7.7
 #deeptools
 cd /globalhome/hxo752/HPC/.local/lib/python3.7/site-packages/deeptools/
 
+NCPUS=2
+OUTDIR=
+bam_files=1;
+
+mkdir ${OUTDIR}/deeptools
 python plotFingerprint.py \
-            --bamfiles ENCFF000PED.chr12.rmdup.filt.sort.bam  \
-            ../../data/bam/hela/ENCFF000PEE.chr12.rmdup.sort.bam \
-            ../../data/bam/hela/ENCFF000PET.chr12.rmdup.sort.bam \
-             --extendReads 110  \
-             --binSize=1000 \
-             --plotFile HeLa.fingerprint.pdf \
-             --labels HeLa_rep1 HeLa_rep2 HeLa_input -p 5 &> fingerprint.log
+            --bamfiles ${bam_files} \
+            --extendReads 110  \
+            --binSize=1000 \
+            --plotFile fingerprint.pdf \
+            --labels HeLa_rep1 HeLa_rep1 input_rep1 input_rep2 \
+            -p ${NCPUS} &> fingerprint.log
   
