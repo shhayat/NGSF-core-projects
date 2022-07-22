dd730d3aa55dde0b7b36d55c91a98417ff20d373
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-22 12:13:28 -0600

    Update multiqc.sh

--- chip-seq/multiqc.sh
+++ chip-seq/multiqc.sh
@@ -16,9 +16,11 @@ cd /globalhome/hxo752/HPC/tools/
 #phantompeakqualtools
 mkdir ${OUTDIR}/phantompeakqual_after_alignment
 
-./multiqc -d ${DIR}/QC/phantompeakqualtools/*.pdf  -o ${OUTDIR}/phantompeakqual_after_alignment -n phantompeakqual_xcor
-./multiqc -d ${DIR}/QC/phantompeakqualtools/xcor_metrics_*  -o ${OUTDIR}/phantompeakqual_after_alignment -n phantompeakqual_metrics
+#./multiqc -d ${DIR}/QC/phantompeakqualtools/xcor_*.pdf  -o ${OUTDIR}/phantompeakqual_after_alignment -n phantompeakqual_xcor
+#./multiqc -d ${DIR}/QC/phantompeakqualtools/xcor_*.txt -o ${OUTDIR}/phantompeakqual_after_alignment -n phantompeakqual_metrics
 
 
 $MACS2
-./multiqc -d ${DIR}/peakcall/*/*_peaks.xls -o ${OUTDIR}/MACS3 -n MACS3
+./multiqc -d ${DIR}/peakcall/*/*_peaks.xls -o ${OUTDIR}/MACS2 -n MACS3
+#./multiqc -d ${DIR}/peakcall/*/*peaks.narrowPeak -o ${OUTDIR}/MACS3 -n MACS3_narrowPeak
+#./multiqc -d ${DIR}/peakcall/*/*summits.bed -o ${OUTDIR}/MACS3 -n MACS3_bed
