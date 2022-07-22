cd6389f999c87327441177fd33c9588ba7f42ce6
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-22 15:57:11 -0600

    Update sbatch_deeptools.sh

--- chip-seq/sbatch_deeptools.sh
+++ chip-seq/sbatch_deeptools.sh
@@ -3,11 +3,10 @@ DIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/
 
 samples_id="492444 492445 507859 507860"
 
-sample=""
+bam_files=""
 for samples_id in ${samples_id[*]}
 do
     bam_files+="${DIR}/SRR${samples_id}/SRR${samples_id}.aligned_dedup.bam " 
 done
 
 sbatch 05_QC_deeptools.sh ${bam_files}
-
