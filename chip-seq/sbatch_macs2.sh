c06ad2e3c6b4a58df22da0e1125be9305b07f069
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-22 14:08:29 -0600

    Update and rename sbatch_macs3.sh to sbatch_macs2.sh

--- chip-seq/sbatch_macs2.sh
+++ chip-seq/sbatch_macs2.sh
@@ -2,6 +2,6 @@
 
 while read control treat sample_name; 
 do
-    sbatch 04_MACS3.sh "$control" "$treat" "$sample_name";
+    sbatch 06_MACS2.sh "$control" "$treat" "$sample_name";
     sleep 0.3
 done < 'samples.txt'
