00ac39eb3856b4193f368a0116764d2c189e465f
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-25 13:24:23 -0600

    Update 05_QC_deeptools.sh

--- chip-seq/05_QC_deeptools.sh
+++ chip-seq/05_QC_deeptools.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=2:00:00
-#SBATCH --mem=60G
+#SBATCH --mem=40G
 #SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
 module load python/3.7.7
