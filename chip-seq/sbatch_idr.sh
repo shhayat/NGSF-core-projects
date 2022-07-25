c6f72e0baa43d7d8bb1864773d59c7ace5a45260
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-25 16:18:04 -0600

    Update sbatch_idr.sh

--- chip-seq/sbatch_idr.sh
+++ chip-seq/sbatch_idr.sh
@@ -5,8 +5,8 @@ peak_files=""
 for peak_file in $DIR/SRR*/*_peaks.sorted.narrowPeak_selected_columns.bed
 do  
     peak_files+="${peak_file}"
-    peak_files=$(echo "$peak_files" | sed 's/.narrowPeak\//.narrowPeak \//g')
-
+    #peak_files=$(echo "$peak_files" | sed 's/.narrowPeak\//.narrowPeak \//g')
+    peak_files=$(echo "$peak_files" | sed 's/.bed\//.bed \//g')
  done
  
 sbatch 07_IDR.sh "${peak_files}";
