02c4bd8cba5d13eeef85fc5a5ab26ce60262fe82
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-25 15:03:24 -0600

    Update sbatch_idr.sh

--- chip-seq/sbatch_idr.sh
+++ chip-seq/sbatch_idr.sh
@@ -1,9 +1,9 @@
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/peakcall
 
 narrowPeak_files=""
-for narrowPeaks in $DIR/*/*.peaks.sorted.narrowPeak
+for narrowPeak_files in $DIR/*/*.peaks.sorted.narrowPeak
 do  
-    narrowPeak_files+="${narrowPeaks} "
+    narrowPeak_files+="${narrowPeak_files} "
  done
  
 sbatch 07_IDR.sh "${narrowPeak_files}";
