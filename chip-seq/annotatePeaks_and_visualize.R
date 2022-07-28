86876aa995fc1b22179d9ee1072b740c45289570
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-27 22:09:03 -0600

    Update annotatePeaks_and_visualize.R

--- chip-seq/annotatePeaks_and_visualize.R
+++ chip-seq/annotatePeaks_and_visualize.R
@@ -9,12 +9,17 @@ txdb <- TxDb.Mmusculus.UCSC.mm10.knownGene
 
 
 samplefiles <- list.files("/globalhome/hxo752/HPC/chipseq/analysis/IDR/", pattern= ".bed", full.names=T)
+peak <- readPeakFile(samplefiles[[0]])
+
 samplefiles <- as.list(samplefiles)
-names(samplefiles) <- c("TAL")
+names(samplefiles) <- c("G1E")
+
 
 #ChIP peaks coverage plot
-
-peaks.consensus <- dba.peakset(res.cnt3, bRetrieve = T)
+#4000bp = 2Kbp
+peakAnnoList <- lapply(samplefiles, annotatePeak, TxDb=txdb, 
+                       tssRegion=c(-2000, 2000), verbose=TRUE)
+                       
 
 
 #Profile of ChIP peaks binding to TSS regions
