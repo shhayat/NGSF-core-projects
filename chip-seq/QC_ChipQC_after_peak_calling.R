beb2a56c437834d3b4ad0eda0848f84b0797be20
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-28 13:59:24 -0600

    Update QC_ChipQC_after_peak_calling.R

--- chip-seq/QC_ChipQC_after_peak_calling.R
+++ chip-seq/QC_ChipQC_after_peak_calling.R
@@ -1,10 +1,11 @@
 #https://github.com/hbctraining/Intro-to-ChIPseq/blob/master/lessons/06_combine_chipQC_and_metrics.md
 library(ChIPQC)
-dir.create("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/QC/peak_calling")
-setwd("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/QC/peak_calling")
+dir.create("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/QC/ChIPQC")
+setwd("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/QC/ChIPQC")
 
 #create sample infor object
-sampl_info <- read.table("./sampleSheet_for_chiqQC.txt", header=TRUE, sep="\t")
+sampl_info <- read.table("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/sampleSheet_for_chiqQC.txt", 
+                         header=TRUE, sep="\t")
 
 #Create ChIPQC object
 chipObj <- ChIPQC(sampl_info, annotation="mm10") 
