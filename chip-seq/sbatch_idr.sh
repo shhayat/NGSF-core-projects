073b3ef6f4b847471eaf45857cf2fd514f196fbb
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-27 21:42:14 -0600

    Update sbatch_idr.sh

--- chip-seq/sbatch_idr.sh
+++ chip-seq/sbatch_idr.sh
@@ -2,10 +2,10 @@ DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/p
 
 peak_files=""
 #for peak_file in $DIR/SRR*/*_peaks.sorted.narrowPeak
-for peak_file in $DIR/SRR*/*_summits.bed
+for peak_file in $DIR/SRR*/*.narrowPeak
 do  
     peak_files+="${peak_file}"
-    peak_files=$(echo "$peak_files" | sed 's/_summits.bed\//_summits.bed \//g')
+    peak_files=$(echo "$peak_files" | sed 's/.narrowPeak\//.narrowPeak \//g')
  done
  
 sbatch 07_IDR_handling_replicates.sh "${peak_files}";
