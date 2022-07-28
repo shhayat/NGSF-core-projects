60e32c9738674d38dfecd03e75859ccb524e6fe8
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-27 22:43:53 -0600

    Update annotatePeaks_and_visualize.R

--- chip-seq/annotatePeaks_and_visualize.R
+++ chip-seq/annotatePeaks_and_visualize.R
@@ -1,17 +1,22 @@
 library(ChIPseeker)
 library(clusterProfiler)
 library(biomaRt)
+library(diffloop)
 
 library(org.Mm.eg.db)
 library(TxDb.Mmusculus.UCSC.mm10.knownGene)
 txdb <- TxDb.Mmusculus.UCSC.mm10.knownGene
 
-
+#list bed files
 samplefiles <- list.files("/globalhome/hxo752/HPC/chipseq/analysis/IDR/", pattern= "filtered.bed", full.names=T)
-peaks <- readPeakFile(samplefiles[[1]])
-names(peaks) <- c("G1E")
+samplefiles <- as.list(samplefiles)
+names(samplefiles) <- c("GE1")
 
-GenomeInfoDb::seqlevels(peak)
+#read peaks
+ReadPeakList <- lapply(samplefiles, readPeakFile)
+
+#checking seqlevels for ReadPeakList
+lapply(ReadPeakList, GenomeInfoDb::seqlevels)
 #[1] "11" "7"  "8"  "19" "16" "4"  "10" "X"  "6"  "5"  "15" "2"  "1"  "9"  "12"
 
 GenomeInfoDb::seqlevels(txdb)
@@ -19,17 +24,10 @@ GenomeInfoDb::seqlevels(txdb)
 
 
 #since seqlevels for txdb has chr string before chromosome number we have to add chr string to our peaks column 1
+PeakList_with_added_chr_str <- lapply(ReadPeakList, diffloop::addchr)
 
-
-
+#annotate peaks
+#2000bp = 2Kbp
+peakAnnoList <- lapply(PeakList_with_added_chr_str, annotatePeak, TxDb=txdb,tssRegion=c(-2000, 2000), verbose=TRUE)
 
 #ChIP peaks coverage plot
-#4000bp = 2Kbp
-peakAnnoList <- lapply(peaks, annotatePeak, TxDb=txdb, 
-                       tssRegion=c(-2000, 2000), verbose=TRUE)
-                       
-
-
-#Profile of ChIP peaks binding to TSS regions
-# extracting peaks present across replicates
-peaks <- peaks.consensus[called.peaks[,1]==1 & called.peaks[,2]==1]
