c176888a38ab394ef5c0c6724ddd1d11498863bb
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-27 19:10:52 -0600

    Update sbatch_bowtie2.sh

--- chip-seq/sbatch_bowtie2.sh
+++ chip-seq/sbatch_bowtie2.sh
@@ -1,6 +1,6 @@
 #SRR492444  SRR492445  SRR507859  SRR507860
 for i in 492444 492445 507859 507860
 do
-  sbatch 02_bowtie2.sh SRR${i} SRR${i}.fastq
+  sbatch 02_bowtie2_alignment.sh SRR${i} SRR${i}.fastq
   sleep 0.2
 done
