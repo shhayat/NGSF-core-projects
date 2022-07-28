ed06b686ab00d72a3043ec6688512aa4bfa330df
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-27 21:21:39 -0600

    Update 07_IDR_handling_replicates.sh

--- chip-seq/07_IDR_handling_replicates.sh
+++ chip-seq/07_IDR_handling_replicates.sh
@@ -24,15 +24,7 @@ cd /globalhome/hxo752/HPC/anaconda3/bin
 ./idr --samples ${files} \
     --rank p.value \
     --output-file ${OUTDIR}/idr \
-    --plot \
-    --log-output-file ${OUTDIR}/idr.log \
-    --verbose
-
-
-./idr --samples ${files} \
-    --rank p.value \
-    --output-file ${OUTDIR}/idr.bed \
     --output-file-type bed \
     --plot \
-    --log-output-file ${OUTDIR}/idr.b.log \
+    --log-output-file ${OUTDIR}/idr.log \
     --verbose
