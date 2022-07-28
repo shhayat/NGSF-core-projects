6f5e5e2603c30c4c11905e99af617bb8aab17674
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-27 20:05:35 -0600

    Create annotatePeaks_and_visualize.R

---
+++ chip-seq/annotatePeaks_and_visualize.R
@@ -0,0 +1,17 @@
+#library(DiffBind)
+library(ChIPseeker)
+library(clusterProfiler)
+library(biomaRt)
+
+library(org.Mm.eg.db)
+library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+txdb <- TxDb.Mmusculus.UCSC.mm10.knownGene
+
+#ChIP peaks coverage plot
+
+peaks.consensus <- dba.peakset(res.cnt3, bRetrieve = T)
+
+
+#Profile of ChIP peaks binding to TSS regions
+# extracting peaks present across replicates
+peaks <- peaks.consensus[called.peaks[,1]==1 & called.peaks[,2]==1]
