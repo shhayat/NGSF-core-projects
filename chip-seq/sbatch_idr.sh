cd79095966bb110ee1037c3bf19fa242d772e657
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-27 21:31:08 -0600

    Update sbatch_idr.sh

--- chip-seq/sbatch_idr.sh
+++ chip-seq/sbatch_idr.sh
@@ -5,7 +5,7 @@ peak_files=""
 for peak_file in $DIR/SRR*/*_summits.bed
 do  
     peak_files+="${peak_file}"
-    peak_files=$(echo "$peak_files" | sed 's/_summits\//_summits \//g')
+    peak_files=$(echo "$peak_files" | sed 's/_summits.bed\//_summits.bed \//g')
  done
  
 sbatch 07_IDR_handling_replicates.sh "${peak_files}";
