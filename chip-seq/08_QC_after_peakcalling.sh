b138ef5dc2277339e8f763246e286376aa0ff3cd
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-27 18:46:44 -0600

    Update 08_QC_after_peakcalling.sh

--- chip-seq/08_QC_after_peakcalling.sh
+++ chip-seq/08_QC_after_peakcalling.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=QC-deeptools
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=2:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
+module load python/3.7.7
+#deeptools
+cd /globalhome/hxo752/HPC/.local/lib/python3.7/site-packages/deeptools/
 
 idr_bed_file=$1; shift
 bam_files=$1;
