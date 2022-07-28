ec9ba2b7982f3eb98f02e99ebcc6b33590379875
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-28 14:10:18 -0600

    Update QC_ChipQC_after_peak_calling.R

--- chip-seq/QC_ChipQC_after_peak_calling.R
+++ chip-seq/QC_ChipQC_after_peak_calling.R
@@ -1,7 +1,7 @@
 #https://github.com/hbctraining/Intro-to-ChIPseq/blob/master/lessons/06_combine_chipQC_and_metrics.md
 library(ChIPQC)
-dir.create("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/QC/ChIPQC")
-setwd("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/QC/ChIPQC")
+dir.create("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/QC/ChIPQC")
+setwd("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/QC/ChIPQC")
 
 #create sample infor object
 sampl_info <- read.table("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/sampleSheet_for_chiqQC.txt", 
