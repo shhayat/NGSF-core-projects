95faf02ab2564b157a54513ecf221434461d5d1d
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-29 13:09:13 -0600

    Update QC_ChipQC_after_peak_calling.R

--- chip-seq/QC_ChipQC_after_peak_calling.R
+++ chip-seq/QC_ChipQC_after_peak_calling.R
@@ -6,8 +6,10 @@ setwd("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysi
 # reading in the sample information (metadata)
 sampl_info <- read.csv("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/sampleSheet_for_chiqQC.csv")
 
+res=dba(sampleSheet=sampl_info, config=data.frame(RunParallel=FALSE))
+
 #Create ChIPQC object
-chipObj <- ChIPQC(sampl_info, annotation="mm10") 
+resQC = ChIPQC(res,annotation="mm10", config=data.frame(RunParallel=FALSE))
 
 # Create ChIPQC report
-ChIPQCreport(chipObj, reportName="ChIPQC Report", reportFolder="ChIPQC")
+ChIPQCreport(resQC, reportName="ChIPQC Report", reportFolder="ChIPQC")
