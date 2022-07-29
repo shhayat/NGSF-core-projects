a66a144f81422af5210c082dfe00fbe48f68d873
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-29 12:43:42 -0600

    Update QC_ChipQC_after_peak_calling.R

--- chip-seq/QC_ChipQC_after_peak_calling.R
+++ chip-seq/QC_ChipQC_after_peak_calling.R
@@ -1,7 +1,7 @@
 #https://github.com/hbctraining/Intro-to-ChIPseq/blob/master/lessons/06_combine_chipQC_and_metrics.md
 library(ChIPQC)
 dir.create("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/QC/ChIPQC")
-setwd("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/QC/ChIPQC")
+setwd("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/QC")
 
 #create sample infor object
 sampl_info <- read.csv("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/sampleSheet_for_chiqQC.csv")
@@ -10,4 +10,4 @@ sampl_info <- read.csv("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects
 chipObj <- ChIPQC(sampl_info, annotation="mm10") 
 
 # Create ChIPQC report
-ChIPQCreport(chipObj, reportName="ChIPQC Report", reportFolder="QC")
+ChIPQCreport(chipObj, reportName="ChIPQC Report", reportFolder="ChIPQC")
