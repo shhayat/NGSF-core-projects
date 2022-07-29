c96f9b8ef0d8ca44635aec34c0aefb37f59a2e6c
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-29 14:42:14 -0600

    Update 10_motif_discovery.sh

--- chip-seq/10_motif_discovery.sh
+++ chip-seq/10_motif_discovery.sh
@@ -2,10 +2,12 @@
 
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/
 
-mkdir -p ${OUTDIR}/motif_discovery
+mkdir -p ${DIR}/motif_discovery
 
 bed_peak=$1; shift
 sample_name=1;
 
+bedtools getfasta -fo CTCF_top500_peak_seq.fa -fi hg38.masked.fa -bed ENCFF693MY_top500.bed
+
 #select peaks with the strongest signal for motif finding
-sort -k 7,7nr  ${DIR}/${bed_peak}| head -n 200 > ${OUTDIR}/motif_discovery/ENCFF693MY_top.bed
+#sort -k 7,7nr  ${DIR}/${bed_peak} | head -n 200 > ${DIR}/motif_discovery/${sample_name}_top.bed
