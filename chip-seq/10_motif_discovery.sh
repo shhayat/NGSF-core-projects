629317e247c2267d73f62f3d0ee29d019e34ac45
Shahina <91986031+shhayat@users.noreply.github.com>
2022-07-30 18:31:09 -0600

    Update 10_motif_discovery.sh

--- chip-seq/10_motif_discovery.sh
+++ chip-seq/10_motif_discovery.sh
@@ -1,11 +1,21 @@
 
 
+
+
+#there was environment problem while installing meme with conda. 
+#For fixing this issue conda env "meme" was created 
+#./globalhome/hxo752/HPC/anaconda3/condabin/conda create -n meme -c bioconda meme 
+#For activating "meme" env first source .bashrc then activate meme env then use meme commands
+source $HOME/.bashrc
+conda activate meme
+
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/
 
-mkdir -p ${DIR}/motif_discovery
+meme -h
+#mkdir -p ${DIR}/motif_discovery
 
-bed_peak=$1; shift
-sample_name=1;
+#bed_peak=$1; shift
+#sample_name=1;
 
 #bedtools getfasta -fo CTCF_top500_peak_seq.fa -fi hg38.masked.fa -bed ENCFF693MY_top500.bed
 
