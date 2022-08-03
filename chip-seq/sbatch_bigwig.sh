8bf853f764ae4607153054e5a89e118fe4908ab9
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-03 16:49:30 -0600

    Update sbatch_bigwig.sh

--- chip-seq/sbatch_bigwig.sh
+++ chip-seq/sbatch_bigwig.sh
@@ -1,5 +1,6 @@
+DIR=/globalhome/hxo752/HPC/chipseq/analysis/alignment
 for i in 492444 492445 507859 507860
 do
-  sbatch bigwig.sh SRR${i}.aligned_dedup.bam SRR${i}
+  sbatch bigwig.sh ${DIR}/SRR${i}.aligned_dedup.bam SRR${i}
   sleep 0.2
 done
