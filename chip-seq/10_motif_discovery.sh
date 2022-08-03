427feb7537067dc60a600d9748c9e476ae8d6e09
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-02 21:24:20 -0600

    Update 10_motif_discovery.sh

--- chip-seq/10_motif_discovery.sh
+++ chip-seq/10_motif_discovery.sh
@@ -18,10 +18,9 @@ conda activate meme
 
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/
 
-meme -h
 #mkdir -p ${DIR}/motif_discovery
 
-#bed_peak=$1; shift
+bed_peak=$1; 
 #sample_name=1;
 
 #bedtools getfasta -fo CTCF_top500_peak_seq.fa -fi hg38.masked.fa -bed ENCFF693MY_top500.bed
@@ -31,3 +30,5 @@ meme -h
 
 #select peaks with the strongest signal for motif finding
 #sort -k 7,7nr  ${DIR}/${bed_peak} | head -n 200 > ${DIR}/motif_discovery/${sample_name}_top.bed
+
+meme-chip ${sample_name}_top.bed -oc ${DIR}/motif_discovery
