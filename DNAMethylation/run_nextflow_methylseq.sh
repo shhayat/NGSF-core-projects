29284e063b2fd5fe9ba4f803780b2db8865c48a7
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-08 15:45:29 -0600

    Update run_nextflow_methylseq.sh

--- DNAMethylation/run_nextflow_methylseq.sh
+++ DNAMethylation/run_nextflow_methylseq.sh
@@ -13,7 +13,7 @@ module purge
 module load nextflow/22.04.3
 module load singularity/3.9.2
 
-CONFIG=$1;
-nextflow run nf-core/methylseq -profile data,singularity
+config_file=$1;
+nextflow run nf-core/methylseq -profile data,singularity -c ${config_file}
 
 #sbatch run_nextflow_methylseq.sh data.config
