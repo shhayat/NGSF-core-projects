bcfb2d3d1247976c76fc74068339328c0777d50c
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-12 14:42:50 -0600

    Update run_nextflow_methylseq.sh

--- DNAMethylation/run_nextflow_methylseq.sh
+++ DNAMethylation/run_nextflow_methylseq.sh
@@ -9,7 +9,7 @@
 #SBATCH --mem=40G
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
-module --force purge
+module load purge
 module spider nextflow/21.10.3
 module spider singularity/3.9.2
 
