bc31bd91009054ff175e3337a736c53c248a5c65
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-12 13:37:30 -0600

    Update run_nextflow_methylseq.sh

--- DNAMethylation/run_nextflow_methylseq.sh
+++ DNAMethylation/run_nextflow_methylseq.sh
@@ -15,7 +15,7 @@ module load singularity/3.9.2
 
 config_file=$1;
 nextflow run nf-core/methylseq -profile singularity -c ${config_file}
-nextflow run nf-core/methylseq -profile singularity --input '*_R{1,2}.fastq.gz' --genome GRCh37
+#nextflow run nf-core/methylseq -profile singularity --input '*_R{1,2}.fastq.gz' --genome GRCh37
 
 #sbatch run_nextflow_methylseq.sh data.config
 #nextflow run $NF_CORE_PIPELINES/methylseq/1.6.1/workflow -profile uppmax --input 
