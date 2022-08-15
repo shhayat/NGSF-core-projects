f3fcb01bfe90dd25cbf0081cc0f9770e877e7308
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-15 14:14:16 -0600

    Update run_nextflow_methylseq.sh

--- DNAMethylation/run_nextflow_methylseq.sh
+++ DNAMethylation/run_nextflow_methylseq.sh
@@ -20,7 +20,8 @@ condition="Colon_Normal_Primary"
 #sample_name="SRR949214"
 mkdir -p  $DIR/test
 #nextflow run nf-core/methylseq -profile singularity -c testdata.config
-nextflow run nf-core/methylseq -profile singularity --input ${DATA}/${condition}/'*_{1,2}.fastq.gz' -w $DIR/test --genome GRCh38 --single_end false -resume
+nextflow run nf-core/methylseq -profile singularity --input ${DATA}/${condition}/'*_{1,2}.fastq.gz' -w $DIR/test --genome GRCh38 --single_end false
+#-resume
 
 #sbatch run_nextflow_methylseq.sh data.config
 #nextflow run $NF_CORE_PIPELINES/methylseq/1.6.1/workflow -profile uppmax --input 
