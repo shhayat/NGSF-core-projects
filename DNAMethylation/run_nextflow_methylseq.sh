892f367a7974b87313eb36c3ed2d1babf23c36a0
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-15 16:27:28 -0600

    Update run_nextflow_methylseq.sh

--- DNAMethylation/run_nextflow_methylseq.sh
+++ DNAMethylation/run_nextflow_methylseq.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 #SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
 #SBATCH --job-name=nf
-#SBATCH --cpus-per-task=40
 #SBATCH --time=10:00:00
-#SBATCH --mem=185G
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
 #module --force purge
@@ -24,4 +23,5 @@ nextflow run nf-core/methylseq -profile singularity --input ${DATA}/${condition}
 #nextflow run $NF_CORE_PIPELINES/methylseq/1.6.1/workflow -profile uppmax --input 
 #'/sw/courses/epigenomics/DNAmethylation/pipeline_bsseq_data/Sample1_PE_R{1,2}.fastq.gz' 
 #--aligner bismark --project g2021025 --genome mm10 --clusterOptions '--reservation g2021025_28'
-
+##SBATCH --cpus-per-task=40
+##SBATCH --mem=185G
