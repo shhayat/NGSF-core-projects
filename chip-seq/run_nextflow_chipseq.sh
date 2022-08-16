9106410565957f479d94e47cf5dee7390ed87207
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-16 10:38:05 -0600

    Update run_nextflow_chipseq.sh

--- chip-seq/run_nextflow_chipseq.sh
+++ chip-seq/run_nextflow_chipseq.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --account=hpc_p_anderson
 #SBATCH --constraint=skylake
-#SBATCH --job-name=nf
+#SBATCH --job-name=nf_chipseq
 #SBATCH --cpus-per-task=40
 #SBATCH --mem=185G
 #SBATCH --time=10:00:00
@@ -12,13 +12,5 @@
 module spider nextflow/22.04.3
 module spider singularity/3.4.1
 
-DIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/DNAMethylation"
-DATA="/datastore/NGSF001/datasets/bisulfite_seq/"
-#config_file=$1;
-condition="Colon_Normal_Primary"
 
-mkdir -p  $DIR/test
-#nextflow run nf-core/methylseq -profile singularity -c testdata.config
-#nextflow run nf-core/methylseq -profile singularity --input ${DATA}/${condition}/'*_{1,2}.fastq.gz' -w $DIR/test --genome GRCh38 --single_end false
-
-nextflow run nf-core/chipseq -profile singularity --input chip_design.csv --genome GRCh38
+nextflow run nf-core/chipseq -profile singularity --input chip_design.csv --genome GRCm38 --single_end true --gtf 
