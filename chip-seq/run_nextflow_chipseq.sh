2ae6b69865d2ae3909e77424afabd6164a9d0a75
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-16 11:28:56 -0600

    Update run_nextflow_chipseq.sh

--- chip-seq/run_nextflow_chipseq.sh
+++ chip-seq/run_nextflow_chipseq.sh
@@ -15,10 +15,14 @@ module spider singularity/3.4.1
 DIR=/globalhome/hxo752/HPC/chipseq/analysis
 GTF=/datastore/NGSF001/analysis/references/mouse/gencode-m30/gencode.vM30.annotation.gtf
 mkdir -p ${DIR}/chipseq-nf
+chmod a+x ${DIR}/chipseq-nf
 
 #nextflow run nf-core/chipseq -profile singularity --input chip_design.csv --genome GRCm38 --single_end true
 nextflow run nf-core/chipseq -profile singularity \
                              --input /globalhome/hxo752/HPC/chipseq/chip_design.csv \
                              --bwa_index ${DIR}/indices_mouse/genome.fa \
                              --blacklist ${DIR}/blacklist_file/mm10-blacklist.v2.bed.gz \
-                             --gtf ${GTF}
+                             --gtf ${GTF} \
+                             -w ${DIR}/chipseq-nf
+                            
+                            
