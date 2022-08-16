e83af31c10e5f821124d65dd0b71e89d71720f8f
Shahina Hayat (hxo752) <hxo752@plato-base-lgn-16002.cm.cluster>
2022-08-15 21:11:48 -0600

    modified:   run_nextflow_methylseq.sh

--- DNAMethylation/run_nextflow_methylseq.sh
+++ DNAMethylation/run_nextflow_methylseq.sh
@@ -3,6 +3,8 @@
 #SBATCH --account=hpc_p_anderson
 #SBATCH --constraint=skylake
 #SBATCH --job-name=nf
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=185G
 #SBATCH --time=10:00:00
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
