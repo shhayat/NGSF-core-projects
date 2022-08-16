979f330c1a0cbd3728ff0091b7fde9668473bc9b
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-16 13:24:39 -0600

    Update run_nextflow_chipseq.sh

--- chip-seq/run_nextflow_chipseq.sh
+++ chip-seq/run_nextflow_chipseq.sh
@@ -20,6 +20,7 @@ chmod a+x ${DIR}/.nextflow/cache
 #nextflow run nf-core/chipseq -profile singularity --input chip_design.csv --genome GRCm38 --single_end true
 nextflow run nf-core/chipseq -profile singularity \
                              --input ${DIR}/chip_design.csv \
+                             --single_end true \
                              --fasta ${DIR}/analysis/indices_mouse/genome.fa \
                              --bwa_index ${DIR}/analysis/indices_mouse/genome.fa \
                              --blacklist ${DIR}/analysis/blacklist_file/mm10-blacklist.v2.bed.gz \
