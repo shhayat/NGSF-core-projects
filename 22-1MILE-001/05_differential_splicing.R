82522bb0b40054d43f96be3a29d45b644d1c65e6
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-16 13:06:09 -0600

    Update 05_differential_splicing.R

--- 22-1MILE-001/05_differential_splicing.R
+++ 22-1MILE-001/05_differential_splicing.R
@@ -1,3 +1,3 @@
-#RAN FOLLOWING COMMAND UNDER LOACL MACHINE
+#RAN FOLLOWING COMMAND UNDER LOCAL MACHINE
 Rscript /Users/shahina/Tools/leafcutter/scripts/leafcutter_ds.R -o /Users/shahina/Projects/22-1MILE-001 --num_threads 4 -g 3 -i 3 -e /Users/shahina/Projects/22-1MILE-001/m10.exon.txt.gz /Users/shahina/Projects/22-1MILE-001/leafcutter_perind_numers.counts.gz /Users/shahina/Projects/22-1MILE-001/group_file.txt
 
