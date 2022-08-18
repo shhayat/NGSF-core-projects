878bca3ee0b633701a1900bde5113292419c2b3f
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-18 16:25:18 -0600

    Update rMATS.sh

--- 22-1MILE-002/rMATS.sh
+++ 22-1MILE-002/rMATS.sh
@@ -1,1 +1,7 @@
 
+#there was environment problem while installing rmats with conda. 
+#For fixing this issue conda env "rMATS" was created 
+#./globalhome/hxo752/HPC/anaconda3/condabin/conda create -n rMATS -c bioconda rMATS 
+#For activating "rMATS" env first source .bashrc then activate rMATS env then use rMATS commands
+source $HOME/.bashrc
+conda activate rMATS
