43611036f051141cf4a0b64fad4883af7ec09d87
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-18 10:01:16 -0600

    Update run_nextflow_chipseq.sh

--- chip-seq/run_nextflow_chipseq.sh
+++ chip-seq/run_nextflow_chipseq.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=nf_chipseq
 #SBATCH --cpus-per-task=40
 #SBATCH --mem=185G
-#SBATCH --time=10:00:00
+#SBATCH --time=40:00:00
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
 module --force purge
