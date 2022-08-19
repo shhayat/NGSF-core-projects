ed194a9f94d633b9537f277c8ba3cc42ed2b3d47
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-19 12:07:10 -0600

    Update rMATS.sh

--- 22-1MILE-002/rMATS.sh
+++ 22-1MILE-002/rMATS.sh
@@ -4,5 +4,17 @@
 #./globalhome/hxo752/HPC/anaconda3/condabin/conda create -n rMATS -c bioconda rMATS then go to path /globalhome/hxo752/HPC/anaconda3/envs/rMATS/bin and run commands
 
 rmat=/globalhome/hxo752/HPC/anaconda3/envs/rMATS/bin
+NCPU=4
 
-$rmat/
+python rmats.py --s1 /path/to/s1.txt \
+                --s2 /path/to/s2.txt \
+                --gtf /path/to/the.gtf \
+                --bi /path/to/STAR_binary_index \
+                -t paired \
+                --readLength 50 \
+                --nthread $NCPU \
+                --od /path/to/output \
+                --tmp /path/to/tmp_output
+
+
+python $rmat/rmats.py --s1
