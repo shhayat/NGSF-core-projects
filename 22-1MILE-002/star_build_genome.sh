3a06ec8f41a7f8806e13d3bba9d3c3f694a126f1
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-23 12:14:09 -0600

    Update star_build_genome.sh

--- 22-1MILE-002/star_build_genome.sh
+++ 22-1MILE-002/star_build_genome.sh
@@ -9,7 +9,6 @@
 #SBATCH --mem=80G
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
-NCPU=10
 
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002/indices
 star=/datastore/NGSF001/software/tools/STAR-2.7.4a/bin/Linux_x86_64
@@ -17,7 +16,7 @@ mkdir -p ${SLURM_TMPDIR}/Rnor_6.0
 mkdir -p ${OUTDIR}
 
 #STAR-2.7.4a
-${star}/STAR --runThreadN ${NCPU} \
+${star}/STAR --runThreadN 16 \
      --runMode genomeGenerate \
      --genomeDir star-index \
      --genomeFastaFiles /datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/GCF_000001895.5_Rnor_6.0/GCF_000001895.5_Rnor_6.0_genomic.fa \
