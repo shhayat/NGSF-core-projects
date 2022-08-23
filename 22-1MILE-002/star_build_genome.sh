e1afa5cca10fee5237faf6ebcda41535b333c82b
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-23 11:24:40 -0600

    Update star_build_genome.sh

--- 22-1MILE-002/star_build_genome.sh
+++ 22-1MILE-002/star_build_genome.sh
@@ -1,4 +1,11 @@
 
-/datastore/NGSF001/software/tools/STAR-2.7.4a/bin/Linux_x86_64
+star=/datastore/NGSF001/software/tools/STAR-2.7.4a/bin/Linux_x86_64
 
-./STAR 
+
+
+${star}/STAR --runMode genomeGenerate 
+     --runThreadN 8 
+     --genomeDir star-index 
+     --genomeFastaFiles /datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/GCF_000001895.5_Rnor_6.0/GCF_000001895.5_Rnor_6.0_genomic.fa --sjdbGTFfile /datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/GCF_000001895.5_Rnor_6.0/GCF_000001895.5_Rnor_6.0_genomic.gtf 
+     --sjdbOverhang 99
+
