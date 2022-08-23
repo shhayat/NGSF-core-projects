50066549ccd137d07b2f1a265f78e22d6af959c3
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-23 15:09:52 -0600

    Update rMATS.sh

--- 22-1MILE-002/rMATS.sh
+++ 22-1MILE-002/rMATS.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
 #SBATCH --account=hpc_p_anderson
-#SBATCH --constraint=skylake
+#SBATCH --constraint=ivybridge
 #SBATCH --job-name=rMATS
-#SBATCH --cpus-per-task=20
-#SBATCH --mem=80G
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=30G
 #SBATCH --time=04:00:00
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
 #there was environment problem while installing rmats with conda. 
 #For fixing this issue conda env "rMATS" was created 
 #./globalhome/hxo752/HPC/anaconda3/condabin/conda create -n rMATS -c bioconda rMATS then go to path /globalhome/hxo752/HPC/anaconda3/envs/rMATS/bin and run commands
-module load cellranger
-PATH=/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cellranger/2.1.0/STAR/5dda596/:$PATH
 
 rmat=/globalhome/hxo752/HPC/anaconda3/envs/rMATS/bin
-python=/globalhome/hxo752/HPC/anaconda3/bin/
-index=/datastore/NGSF001/analysis/indices/rat/Rnor_6.0/star/2.5.1b/
+index=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002/star-index
 GTF=/datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/GCF_000001895.5_Rnor_6.0/GCF_000001895.5_Rnor_6.0_genomic.gtf
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002
 mkdir -p ${DIR}/rmat_analysis
-NCPU=20
+NCPU=16
 
-source $HOME/.bashrc
-conda activate rMATS
+#module load cellranger
+#PATH=/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cellranger/2.1.0/STAR/5dda596/:$PATH
+#source $HOME/.bashrc
+#conda activate rMATS
 
 mkdir -p ${DIR}/tmp
-${python}/python ${rmat}/rmats.py --s1 group1_bam_files.txt \
-                        --s2 group2_bam_files.txt \
+${rmat}/python ${rmat}/rmats.py --s1 $DIR/group1_bam_files.txt \
+                        --s2 $DIR/group2_bam_files.txt \
                         --gtf ${GTF} \
                         --bi ${index} \
                         -t paired \
