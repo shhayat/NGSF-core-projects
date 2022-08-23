3f6f53dac7588d3f8b8757a496f3a617cef6b353
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-23 14:46:53 -0600

    Update star_build_genome.sh

--- 22-1MILE-002/star_build_genome.sh
+++ 22-1MILE-002/star_build_genome.sh
@@ -12,8 +12,6 @@
 
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002/indices
 star=/datastore/NGSF001/software/tools/STAR-2.7.4a/bin/Linux_x86_64
-mkdir -p ${SLURM_TMPDIR}/Rnor_6.0
-mkdir -p ${OUTDIR}
 
 #STAR-2.7.4a
 ${star}/STAR --runThreadN 16 \
@@ -22,5 +20,3 @@ ${star}/STAR --runThreadN 16 \
      --genomeFastaFiles /datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/GCF_000001895.5_Rnor_6.0/GCF_000001895.5_Rnor_6.0_genomic.fa \
      --sjdbGTFfile /datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/GCF_000001895.5_Rnor_6.0/GCF_000001895.5_Rnor_6.0_genomic.gtf \
      --sjdbOverhang 99
-
-rsync -rvzP ${SLURM_TMPDIR}/Rnor_6.0 ${OUTDIR}
