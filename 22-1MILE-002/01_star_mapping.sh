dc4a767f2ce1598b88875f7f4026644e3bf2bb0b
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-24 12:02:04 -0600

    Update 01_star_mapping.sh

--- 22-1MILE-002/01_star_mapping.sh
+++ 22-1MILE-002/01_star_mapping.sh
@@ -1,5 +1,22 @@
+#!/bin/sh
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=mapping
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=8
+#SBATCH --time=03:00:00
+#SBATCH --mem=20G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
-
+NCPUS=8
 #work adapted from https://github.com/ngsf-usask/scripts/tree/main/RNAseq/22-1MILE-002
-/datastore/NGSF001/projects/22-1MILE-002/processed_umi
+INDEX=
+#star-twopass
+STAR --runMode alignReads \
+    --runThreadN $NCPUS \
+    --genomeDir $INDEX \
+    --readFilesIn $(echo "${SLURM_TMPDIR}/${R1%.*}") $(echo "${SLURM_TMPDIR}/${R2%.*}") \
+    --twopassMode Basic \
+    --outSAMstrandField intronMotif \
+    --outSAMtype BAM Unsorted
