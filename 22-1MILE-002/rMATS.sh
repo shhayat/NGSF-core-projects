01f3ffbe83cb012cbf619daaee95f147ab3608c7
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-23 20:02:16 -0600

    Update rMATS.sh

--- 22-1MILE-002/rMATS.sh
+++ 22-1MILE-002/rMATS.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --account=hpc_p_anderson
-#SBATCH --constraint=skylake
+#SBATCH --constraint=ivybridge
 #SBATCH --job-name=rMATS
-#SBATCH --cpus-per-task=30
-#SBATCH --mem=100G
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=30G
 #SBATCH --time=08:00:00
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
@@ -17,7 +17,7 @@ index=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002/star
 GTF=/datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/GCF_000001895.5_Rnor_6.0/GCF_000001895.5_Rnor_6.0_genomic.gtf
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002
 mkdir -p ${DIR}/rmat_analysis
-NCPU=16
+NCPU=10
 
 #module load cellranger
 #PATH=/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cellranger/2.1.0/STAR/5dda596/:$PATH
