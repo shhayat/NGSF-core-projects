ac4c30570973de635f55a2038b27960d407840f4
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-25 12:27:41 -0600

    Update 01_star_mapping.sh

--- 22-1MILE-002/01_star_mapping.sh
+++ 22-1MILE-002/01_star_mapping.sh
@@ -4,7 +4,7 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=mapping
 #SBATCH --ntasks=1
-#BATCH --cpus-per-task=2
+#BATCH --cpus-per-task=4
 #SBATCH --time=03:00:00
 #SBATCH --mem=80G
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
