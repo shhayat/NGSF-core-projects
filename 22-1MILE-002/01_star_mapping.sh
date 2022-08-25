7c2c6019db9f20bde12dca80d66aa1a5e6f015c6
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-25 12:31:45 -0600

    Update 01_star_mapping.sh

--- 22-1MILE-002/01_star_mapping.sh
+++ 22-1MILE-002/01_star_mapping.sh
@@ -13,7 +13,7 @@ set -eux
 
 module load samtools
 
-NCPUS=${SLURM_CPUS_PER_TASK}
+NCPUS=4
 #work adapted from https://github.com/ngsf-usask/scripts/tree/main/RNAseq/22-1MILE-002
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002/
 star=/datastore/NGSF001/software/tools/STAR-2.7.4a/bin/Linux_x86_64
