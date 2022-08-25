0289534a85c7c21dda389c9dcd87b12598138d0e
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-25 14:56:17 -0600

    Create sbatch_umitools.sh

---
+++ 22-1MILE-002/sbatch_umitools.sh
@@ -0,0 +1,7 @@
+DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002/alignemnt
+
+for i in 098 099 100 101 102 103
+do
+   bam=${DIR}/R2200${i}/star_Aligned.sortedByCoord.out.bam
+   sbatch 01_star_mapping.sh R2200${i} ${bam}
+done
