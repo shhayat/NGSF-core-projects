0eb86da372ae189e2e88892e8b35cfbfa4f6ceb7
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-25 14:53:33 -0600

    Update 02_umitools_UMIdedup.sh

--- 22-1MILE-002/02_umitools_UMIdedup.sh
+++ 22-1MILE-002/02_umitools_UMIdedup.sh
@@ -1,18 +1,27 @@
+#!/bin/sh
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=mapping
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=4
+#SBATCH --time=03:00:00
+#SBATCH --mem=80G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
+set -eux
 
-
-
-#work adapted from 
+#work adapted from https://github.com/ngsf-usask/scripts/tree/main/RNAseq/22-1MILE-002
 module load samtools
+
 umi_tools=/globalhome/hxo752/HPC/anaconda3/bin
+DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002/
 RRNA=/datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/GCF_000001895.5_Rnor_6.0/rRNA_intervals.bed
 
-NCPU=$SLURM_CPUS_PER_TASK
+NCPU=4
 
-DIR=
-sample_name=
-bam=
+sample_name=$1; shift
+BAM=$1;
 
 mkdir -p ${DIR}/deduplication/{sample_name} && cd ${DIR}/deduplication/{sample_name}
 
@@ -21,7 +30,7 @@ samtools view -@ ${NCPU} \
               -U {sample_name}.no-rRNA.bam \
               -O BAM \
               -L ${RRNA} \
-              ${bam}
+              $DIR/alignment/${sample_name}/${BAM}
 
 
 # keep only primary alignments
@@ -33,7 +42,7 @@ samtools view -@ ${NCPU} \
 
 
 
-umi_tools dedup -I ${sample_name}.no-rRNA.primary-aln.bam 
+umi_tools dedup -I ${sample_name}.no-rRNA.primary-aln.bam \
                 --log="${sample_name}.umi.log" \
                 --umi-separator=":" \
                 --unpaired-reads="discard" \
