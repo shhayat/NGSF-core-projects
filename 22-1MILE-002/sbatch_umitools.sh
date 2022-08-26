81f54d1d93933452c6996ff12aab9129f9347759
Shahina <91986031+shhayat@users.noreply.github.com>
2022-08-25 19:39:42 -0600

    Update sbatch_umitools.sh

--- 22-1MILE-002/sbatch_umitools.sh
+++ 22-1MILE-002/sbatch_umitools.sh
@@ -2,6 +2,6 @@ DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002/aligne
 
 for i in 098 099 100 101 102 103
 do
-   bam=${DIR}/R2200${i}/star_Aligned.sortedByCoord.out.bam
+   bam=star_Aligned.sortedByCoord.out.bam
    sbatch 02_umitools_UMIdedup.sh "R2200${i}" ${bam}
 done
