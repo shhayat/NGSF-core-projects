8d21a0856e1263acf1266ed075cec37f8f3efe23
Shahina <91986031+shhayat@users.noreply.github.com>
2022-09-02 15:56:56 -0600

    Update 02_run_feature_count.sh

--- 22-1ELSI-001/02_run_feature_count.sh
+++ 22-1ELSI-001/02_run_feature_count.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=star-align
+#SBATCH --constraint=skylake
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=4:00:00
+#SBATCH --mem=80G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 set -eux
 
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1ELSI-001
