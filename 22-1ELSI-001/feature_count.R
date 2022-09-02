323033bd6d15ac9498085f42f9d60cd3b5b4e533
Shahina <91986031+shhayat@users.noreply.github.com>
2022-09-02 16:30:38 -0600

    Update feature_count.R

--- 22-1ELSI-001/feature_count.R
+++ 22-1ELSI-001/feature_count.R
@@ -7,7 +7,7 @@ sample_names <- list.files(result_dir, "R2")
 
 #count features using featureCounts function
 feature_count <- sapply(sample_names, function(x)
-			   featureCounts(files = sprintf('Alignment/%s/Aligned.sortedByCoord.out.bam', x),
+			   featureCounts(files = sprintf('/datastore/NGSF001/projects/22-1ELSI-001/analysis/Alignment/%s/Aligned.sortedByCoord.out.bam', x),
 			   annot.ext="/datastore/NGSF001/analysis/references/horse/EquCab3.0/ensembl/release-103/Equus_caballus.EquCab3.0.103.gtf",
 			   isGTFAnnotationFile = TRUE,
 			   GTF.attrType.extra  = c('gene_name'),
