5171729f1d4b715d177a20e8c9f6f783c9eaed07
Shahina <91986031+shhayat@users.noreply.github.com>
2022-10-20 16:44:48 -0600

    Update 03_DESEQ2.R

--- 22-1ELSI-001/03_DESEQ2.R
+++ 22-1ELSI-001/03_DESEQ2.R
@@ -21,7 +21,7 @@ feature_annotation <- feature_count1[1:2]
 #feature_count <- feature_count[colnum]
 #
 #this will only store your samples with their expression values AND samples are arranged according to groups
-feature_count <- feature_count1[c(6,10,8,7,3,13,12,11,5,17,14,9,15,16,4),]
+feature_count <- feature_count1[c(6,10,8,7,3,13,12,11,5,17,14,9,15,16,4)]
 #at least one column has number
 #feature_count3 <- feature_count2[apply(feature_count2,1,function(z) any(z!=0)),]
 #feature_count3 <- feature_count2[apply(feature_count2, 1,function(x) all(x[1:5] >=1) && all(x[6:10]==0) && all(x[11:15]==0)),]
@@ -46,15 +46,15 @@ f6 <- feature_count %>%
 
 feature_count3 <- rbind(f1,f2,f3,f4,f5,f6)         
 #creating SAMPLE INFORMATION VARIABLE with group definition
-sampleInfo=data.frame(sample_name=dput(as.character(names(feature_count3)))[3:17],
-                      sample_type=dput(as.character(names(feature_count3)))[3:17],
+sampleInfo=data.frame(sample_name=dput(as.character(names(feature_count3))),
+                      sample_type=dput(as.character(names(feature_count3))),
                       sample_group=dput(as.character(c(rep("D1",5),rep("D4",5), rep("LPS",5)))))
 
 
 
 group <- data.frame(sample_group=sampleInfo$sample_group)
 
-dds <- DESeqDataSetFromMatrix(countData=feature_count3[3:!7],
+dds <- DESeqDataSetFromMatrix(countData=feature_count3,
                               colData=group,
                               design=~sample_group)
 
@@ -130,14 +130,14 @@ resDF <- merge(feature_annotation,resDF, by="GeneID")
 
 resDF1 <- resDF[resDF$pvalue <= 0.05,]
 res_pval_ordered <- resDF1[order(resDF1$pvalue),]
-res_pval_ordered <- res_pval_ordered[rowSums(is.na(res_pval_ordered)) == 0, ] 
 
 write.csv(res_pval_ordered,file="DESEQ2_res_D1_D4_at_pvalue_0.05.csv",quote=FALSE, row.names = FALSE)
 
 #All significant at FDR 0.01
 resDF2 <- resDF[resDF$padj <= 0.01,]
-res_padj_ordered <- resDF2[order(resDF2$padj),]
-res_padj_ordered1 <- res_padj_ordered[rowSums(is.na(res_padj_ordered)) == 0, ] 
+res_padj_ordered1 <- resDF2[order(resDF2$padj),]
+#remove rows with all NAs
+res_padj_ordered1 <- res_padj_ordered1[rowSums(is.na(res_padj_ordered1)) != ncol(res_padj_ordered1), ]
 
 write.csv(res_padj_ordered1,file="DESEQ2_res_D1_D4_at_fdr_0.01.csv",quote=FALSE, row.names = FALSE)
 
@@ -177,14 +177,13 @@ resDF <- merge(feature_annotation,resDF, by="GeneID")
 
 resDF1 <- resDF[resDF$pvalue <= 0.05,]
 res_pval_ordered <- resDF1[order(resDF1$pvalue),]
-res_pval_ordered <- res_pval_ordered[rowSums(is.na(res_pval_ordered)) == 0, ] 
-
-write.csv(res_pval_ordered,file="DESEQ2/DESEQ2_res_D1_LPS_at_pvalue_0.05.csv",quote=FALSE, row.names = FALSE)
+write.csv(res_pval_ordered,file="DESEQ2_res_D1_LPS_at_pvalue_0.05.csv",quote=FALSE, row.names = FALSE)
 
 #All significant at FDR 0.01
 resDF2 <- resDF[resDF$padj <= 0.01,]
-res_padj_ordered <- resDF2[order(resDF2$padj),]
-res_padj_ordered2 <- res_padj_ordered[rowSums(is.na(res_padj_ordered)) == 0, ] 
+res_padj_ordered2 <- resDF2[order(resDF2$padj),]
+#remove rows with all NAs
+res_padj_ordered2 <- res_padj_ordered2[rowSums(is.na(res_padj_ordered2)) != ncol(res_padj_ordered2), ]
 
 write.csv(res_padj_ordered2,file="DESEQ2_res_D1_LPS_at_fdr_0.01.csv",quote=FALSE, row.names = FALSE)
 
@@ -225,26 +224,30 @@ resDF <- merge(feature_annotation,resDF, by="GeneID")
 
 resDF1 <- resDF[resDF$pvalue <= 0.05,]
 res_pval_ordered <- resDF1[order(resDF1$pvalue),]
-res_pval_ordered <- res_pval_ordered[rowSums(is.na(res_pval_ordered)) == 0, ] 
-
-write.csv(res_pval_ordered,file="DESEQ2/DESEQ2_res_D1_LPS_at_pvalue_0.05.csv",quote=FALSE, row.names = FALSE)
+write.csv(res_pval_ordered,file="DESEQ2_res_D4_LPS_at_pvalue_0.05.csv",quote=FALSE, row.names = FALSE)
 
 #All significant at FDR 0.01
 resDF2 <- resDF[resDF$padj <= 0.01,]
-res_padj_ordered <- resDF2[order(resDF2$padj),]
-res_padj_ordered2 <- res_padj_ordered[rowSums(is.na(res_padj_ordered)) == 0, ] 
+res_padj_ordered3 <- resDF2[order(resDF2$padj),]
+#remove rows with all NAs
+res_padj_ordered3 <- res_padj_ordered3[rowSums(is.na(res_padj_ordered3)) != ncol(res_padj_ordered3), ]
 
-write.csv(res_padj_ordered2,file="DESEQ2/DESEQ2_res_D1_LPS_at_fdr_0.01.csv",quote=FALSE, row.names = FALSE)
+write.csv(res_padj_ordered3,file="DESEQ2_res_D4_LPS_at_fdr_0.01.csv",quote=FALSE, row.names = FALSE)
 
 #comman genes between three contrasts
-comman_genes_at_fdr0.01 <- merge(res_padj_ordered1,res_padj_ordered2, by="GeneID", suffix=c(".D4_vs_D1",".LPS_vs_D1",".LPS_vs_D4"))
+df_list <- list(res_padj_ordered1, res_padj_ordered2, res_padj_ordered3)
+
+#merge all data frames in list
+comman_genes_at_fdr0.01 <- Reduce(function(x, y) merge(x, y, by="GeneID", suffix=c(".D4_vs_D1",".LPS_vs_D1")), df_list)
+
 colnames(comman_genes_at_fdr0.01)[2] <- "gene_name"
-comman_genes_at_fdr0.01 <- comman_genes_at_fdr0.01[-8]
+comman_genes_at_fdr0.01 <- comman_genes_at_fdr0.01[c(-9,-16)]
+names(comman_genes_at_fdr0.01)[15:20] <- c("baseMean.LPS_vs_D4","log2FoldChange.LPS_vs_D4","lfcSE.LPS_vs_D4","stat.LPS_vs_D4","pvalue.LPS_vs_D4","padj.LPS_vs_D4")                                  
 write.csv(comman_genes_at_fdr0.01,file="comman_genes_at_fdr_0.01_three_contrasts.csv",quote=FALSE, row.names = FALSE)
 
 #volcano plot
 #remove rows if padj is NA
-d4lps_df <- res_padj_ordered[!is.na(res_padj_ordered$padj), ]
+d4lps_df <- res_padj_ordered3[!is.na(res_padj_ordered3$padj), ]
 #assign up and down regulation and non signif based on log2fc
 d4lps_df$direction <- ifelse(as.numeric(d4lps_df$log2FoldChange) < -4, "down_regulated", 
                                  ifelse(as.numeric(d4lps_df$log2FoldChange) > 4, "up_regulated", "signif" ))
@@ -260,17 +263,15 @@ p <- ggplot(d4lps_df, aes(as.numeric(log2FoldChange), -log10(as.numeric(padj))))
   ylab("-log10(FDR)") 
 
 #order up and down on lowest adj pvalue
-up <- d1lps_df[d1lps_df$direction == "up_regulated",]
+up <- d4lps_df[d4lps_df$direction == "up_regulated",]
 up_ordered <- up[order(up$padj),]
-down <- d1lps_df[d1lps_df$direction == "down_regulated",]
+down <- d4lps_df[d4lps_df$direction == "down_regulated",]
 down_ordered <- down[order(down$padj),]
 
 p1 <- p + geom_text_repel(data=head(up_ordered,10),aes(label=gene_name),size=2, box.padding = unit(0.7, "lines"), max.overlaps = Inf)
 p2 <- p1 + geom_text_repel(data=head(down_ordered,10),aes(label=gene_name),size=2, box.padding = unit(0.7, "lines"), max.overlaps = Inf)
 print(p2)
 dev.off()
-
-
 
 
 
@@ -313,16 +314,41 @@ plot(euler(s3, shape = "circle"), quantities = TRUE, fill=yarrr::transparent(c('
 dev.off()
 
 
+                                  
+                                  
+                                  
+                                  
 library(ggtree)
 #plot count top 10 differntially expressed genes for D4 vs D1
-pdf("count_plot_D1_D4.pdf", height=20, width=15)
+pdf("count_plot_D1_D4_without_normalization.pdf", height=20, width=15)
 #select top 10 genes on lowest fdr
 gene_ids = head(res_padj_ordered1$GeneID, 10)
 myplots <- list()  # new empty list
 for (gene in gene_ids)
 {
   d <- plotCounts(dds_wald, gene= gene, 
-                  intgroup="sample_group", returnData=TRUE)
+                  intgroup="sample_group", returnData=TRUE,returnData=TRUE,normalized=FALSE,transform=FALSE)
+
+  p <- ggplot(d, aes(x=sample_group, y=count)) + 
+    geom_point(position=position_jitter(w=0.1,h=0)) +
+    ylab("Count") +
+    ggtitle(paste(gene,":",res_padj_ordered1[res_padj_ordered1$GeneID==gene,]$gene_name,": FDR ",res_padj_ordered1[res_padj_ordered1$GeneID==gene,]$padj))
+  
+  myplots[[gene]] <- p 
+  
+}
+multiplot(plotlist = myplots, ncol = 2)
+
+dev.off()
+
+pdf("count_plot_D1_D4_with_normalization.pdf", height=20, width=15)
+#select top 10 genes on lowest fdr
+gene_ids = head(res_padj_ordered1$GeneID, 10)
+myplots <- list()  # new empty list
+for (gene in gene_ids)
+{
+  d <- plotCounts(dds_wald, gene= gene, 
+                  intgroup="sample_group", returnData=TRUE,returnData=TRUE)
 
   p <- ggplot(d, aes(x=sample_group, y=count)) + 
     geom_point(position=position_jitter(w=0.1,h=0)) +
@@ -335,10 +361,32 @@ for (gene in gene_ids)
 multiplot(plotlist = myplots, ncol = 2)
 
 dev.off()
+                                  
 
+                                  
 
+pdf("count_plot_D1_LPS_without_normalization.pdf", height=20, width=15)
+gene_ids = head(res_padj_ordered2$GeneID, 10)
+myplots <- list()  # new empty list
+for (gene in gene_ids)
+{
+  d <- plotCounts(dds_wald, gene= gene, 
+                  intgroup="sample_group", returnData=TRUE,normalized=FALSE,transform=FALSE)
+  
+  p <- ggplot(d, aes(x=sample_group, y=count)) + 
+    geom_point(position=position_jitter(w=0.1,h=0)) + 
+    ylab("Count") +
+    #scale_y_log10(breaks=c(100,1000,3000,6000)) +
+    ggtitle(paste(gene,":",res_padj_ordered2[res_padj_ordered2$GeneID==gene,]$gene_name,": FDR ",res_padj_ordered2[res_padj_ordered2$GeneID==gene,]$padj))
+  
+  myplots[[gene]] <- p 
+  
+}
+multiplot(plotlist = myplots, ncol = 2)
 
-pdf("count_plot_D1_LPS.pdf", height=20, width=15)
+dev.off()
+
+pdf("count_plot_D1_LPS_with_normalization.pdf", height=20, width=15)
 gene_ids = head(res_padj_ordered2$GeneID, 10)
 myplots <- list()  # new empty list
 for (gene in gene_ids)
@@ -359,13 +407,56 @@ multiplot(plotlist = myplots, ncol = 2)
 
 dev.off()
 
-
 #plotCounts(dds_wald, gene="KBTBD7", intgroup="sample_group")
 
+pdf("count_plot_D4_LPS_without_normalization.pdf", height=20, width=15)
+gene_ids = head(res_padj_ordered3$GeneID, 10)
+myplots <- list()  # new empty list
+for (gene in gene_ids)
+{
+  d <- plotCounts(dds_wald, gene= gene, 
+                  intgroup="sample_group", returnData=TRUE,normalized=FALSE,transform=FALSE)
+  
+  p <- ggplot(d, aes(x=sample_group, y=count)) + 
+    geom_point(position=position_jitter(w=0.1,h=0)) + 
+    ylab("Count") +
+    #scale_y_log10(breaks=c(100,1000,3000,6000)) +
+    ggtitle(paste(gene,":",res_padj_ordered3[res_padj_ordered3$GeneID==gene,]$gene_name,": FDR ",res_padj_ordered3[res_padj_ordered3$GeneID==gene,]$padj))
+  
+  myplots[[gene]] <- p 
+  
+}
+multiplot(plotlist = myplots, ncol = 2)
+
+dev.off()
+                                  
+pdf("count_plot_D4_LPS_with_normalization.pdf", height=20, width=15)
+gene_ids = head(res_padj_ordered3$GeneID, 10)
+myplots <- list()  # new empty list
+for (gene in gene_ids)
+{
+  d <- plotCounts(dds_wald, gene= gene, 
+                  intgroup="sample_group", returnData=TRUE)
+  
+  p <- ggplot(d, aes(x=sample_group, y=count)) + 
+    geom_point(position=position_jitter(w=0.1,h=0)) + 
+    ylab("Normalized Count") +
+    #scale_y_log10(breaks=c(100,1000,3000,6000)) +
+    ggtitle(paste(gene,":",res_padj_ordered3[res_padj_ordered3$GeneID==gene,]$gene_name,": FDR ",res_padj_ordered3[res_padj_ordered3$GeneID==gene,]$padj))
+  
+  myplots[[gene]] <- p 
+  
+}
+multiplot(plotlist = myplots, ncol = 2)
+
+dev.off()
+                                  
+                                  
+                                  
 
 #Get normalized count for differentially expressed genes at fdr 0.01
 select <- order(rowMeans(counts(dds_wald,normalized=TRUE)),decreasing=FALSE)[1:nrow(counts(dds_wald))]
-nt <- normTransform(dds_wald)
+nt <- normTransform(dds_wald,pc=0)
 log2.norm.counts <- assay(nt)[select,]
 log2.norm.counts<- as.data.frame(log2.norm.counts)
 log2.norm.counts <- cbind(GeneID=rownames(log2.norm.counts), log2.norm.counts)
@@ -379,4 +470,10 @@ write.csv(D1_D4_with_norm_counts,file="D1_D4_with_norm_counts_at_fdr0.01.csv",qu
 D1_LPS_with_norm_counts <- merge(D1_LPS,log2.norm.counts, by="GeneID")
 colnames(D1_LPS_with_norm_counts)[8:22] <- c("E1L1","E2L1","E3L1","E4L1","E5L1","E1L4","E2L4","E3L4","E4L4","E5L4","L1L1","L3L1","L4L1","L5L1","L6L1")
 write.csv(D1_LPS_with_norm_counts,file="D1_LPS_with_norm_counts_at_fdr0.01.csv",quote=FALSE, row.names = FALSE)
+
+#D4 vs LPS
+D4_LPS_with_norm_counts <- merge(D4_LPS,log2.norm.counts, by="GeneID")
+colnames(D4_LPS_with_norm_counts)[8:22] <- c("E1L1","E2L1","E3L1","E4L1","E5L1","E1L4","E2L4","E3L4","E4L4","E5L4","L1L1","L3L1","L4L1","L5L1","L6L1")
+write.csv(D4_LPS_with_norm_counts,file="D4_LPS_with_norm_counts_at_fdr0.01.csv",quote=FALSE, row.names = FALSE)
+                                  
 
