805a8a6762e135b7f0b314ff72513bb0395bd08d
Shahina <91986031+shhayat@users.noreply.github.com>
2022-10-20 13:31:13 -0600

    Update autonomics_latest_run.R

--- 20-1JOHO-001/autonomics_latest_run.R
+++ 20-1JOHO-001/autonomics_latest_run.R
@@ -1,5 +1,9 @@
 library("autonomics")
 library("magrittr")
+library("biomaRt")
+library("org.Rn.eg.db")
+
+
 setwd("/Users/shahina/Projects/20-1JOHO-001")
 res <- read.table("20-1JOHO-001_htseq_counts.txt", head=TRUE)
 #Contrasts
@@ -41,8 +45,6 @@ names(fdata1_pval) <- c("gene_name","effects","pvalue", "fdr")
 write.csv(fdata1_pval, "/Users/shahina/Projects/20-1JOHO-001/latest_analysis/DEG_20male_samples_treated_vs_control.csv")
 
 
-
-
 #After excluding 5 samples from 20 samples
 #Excluded samples
 #control_male_R56
@@ -64,7 +66,6 @@ dev.off()
 
 object2 <- fit_limma(object, formula = ~ 0 + subgroup, contrastdefs = c('subgrouptreated - subgroupcontrol'), plot = TRUE)
 
-
 fdata1 <- data.frame(fdt(object2))
 fdata1_select <- fdata1[c(2,5,6,7)]
 fdata1_pval=fdata1_select[fdata1_select[3] <=0.05,]
@@ -76,6 +77,10 @@ names(fdata1_fdr) <- c("gene_name","effects","pvalue", "fdr")
 fdata1_fdr_order <- fdata1_fdr[order(fdata1_fdr$fdr),]
 #NROW(fdata1_fdr)
 #[1] 6660
+
+fdata1_fdr_order$GeneID <- mapIds(org.Rn.eg.db, keys=fdata1_fdr_order$gene_name, column="ENSEMBL", keytype="SYMBOL", multiVals="first")
+
+fdata1_fdr_order <- fdata1_fdr_order[c(5,1:4)]
 write.csv(fdata1_fdr_order, "/Users/shahina/Projects/20-1JOHO-001/latest_analysis/DEG_15male_samples_treated_vs_control_at_fdr0.05.csv")
 
 low <- fdata1_fdr_order[fdata1_fdr_order$effects <= -0.6,]
