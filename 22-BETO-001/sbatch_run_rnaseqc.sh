8c34a28bf99b10b76f6dfbd9bec900d49446ee12
Shahina <91986031+shhayat@users.noreply.github.com>
2022-10-27 15:30:10 -0600

    Update sbatch_run_rnaseqc.sh

--- 22-BETO-001/sbatch_run_rnaseqc.sh
+++ 22-BETO-001/sbatch_run_rnaseqc.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-BETO-001
 for i in ${DATA}/*/*.bam
 do
         path="${i%/Aligned*}";
-	      sample_name="${path##*/}"
+	sample_name="${path##*/}"
       
-  sbatch ${SCRIPT_DIR}/04_RNASEQC.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
+  sbatch ${SCRIPT_DIR}/04_RNASeQC.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
  sleep 0.5
 done 
