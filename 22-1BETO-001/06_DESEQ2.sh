42580acafc39856f5590c418493d43239172eafc
Shahina <91986031+shhayat@users.noreply.github.com>
2022-10-31 11:35:19 -0600

    Update 06_DESEQ2.sh

--- 22-1BETO-001/06_DESEQ2.sh
+++ 22-1BETO-001/06_DESEQ2.sh
@@ -41,10 +41,9 @@ DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2)
   ##########
   #PCA PLOT
   ##########
-  pdf(sprintf("PCA_%s_%s.csv",cond2,cond1))
   #gernate rlog for PCA
   rld <-rlog(dds,blind=FALSE)
-  pdf(sprintf("PCA_%s_%s.pdf",cond2,cond1), width=15,height=15)
+  pdf(sprintf("PCA_%s_%s.pdf",cond2,cond1), width=8,height=8)
   nudge <- position_nudge(y = 0.5)
   p <- plotPCA(rld,intgroup=c("sample_group"))  
   p <- p + geom_text(aes_string(label = "name"), color="black", position = nudge, size=2.8)
