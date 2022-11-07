7864ad46ceabe9ab6157415a39cb98630f2f5551
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-07 09:41:38 -0600

    Update 02_RNASeQC.sh

--- 22-1ELSI-001/02_RNASeQC.sh
+++ 22-1ELSI-001/02_RNASeQC.sh
@@ -14,8 +14,8 @@ chmod u+x ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux
 sample_name=$1; shift
 bam_file=$1
 
-GTF=/datastore/NGSF001/analysis/references/human/gencode-40/gencode.v40.annotation_mod.gtf
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1LICH-001/rnaseqc/analysis
+GTF=/datastore/NGSF001/analysis/references/horse/EquCab3.0/ensembl/release-103/Equus_caballus.EquCab3.0.103.gtf
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1ELSI-001/rnaseqc
 
 mkdir -p ${OUTDIR}
 
