a809b248ff2e28af5ed240f9ddafae180a69ca3d
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-07 10:51:56 -0600

    Update 04_RNASeQC.sh

--- 22-1BETO-001/04_RNASeQC.sh
+++ 22-1BETO-001/04_RNASeQC.sh
@@ -19,7 +19,7 @@ bam_file=$1
 #run following python code on GTF file
 #python collaspe_annotation.py /datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf genes_mod.gtf
 
-GTF=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
+GTF=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes_mod.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001/analysis/rnaseqc
 
 mkdir -p ${OUTDIR}
