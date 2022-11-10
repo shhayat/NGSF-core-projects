dbf941bf9ca7dba96d6e9b52c7c7318dc6b30af3
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-10 11:07:14 -0600

    Update 01_generateEvents.sh

--- 22-1MILE-001/suppa2/01_generateEvents.sh
+++ 22-1MILE-001/suppa2/01_generateEvents.sh
@@ -1,13 +1,19 @@
+#!/bin/bash
 
-
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=ivybridge
+#SBATCH --job-name=rMATS
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30G
+#SBATCH --time=24:00:00
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
 SUPPA=/globalhome/hxo752/HPC/anaconda3/envs/suppa/bin
-GTF=
+GTF=/datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/GCF_000001895.5_Rnor_6.0/GCF_000001895.5_Rnor_6.0_genomic.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2_analysis
 
 ${SUPPA}/python ${SUPPA}/suppa.py generateEvents \
                               -i $GTF \ 
                               -o $OUTDIR/events_from_gtf \
                               -f ioe \
-                              -e SE SS MX RI FL
-                
+                              -e SE SS MX RI FL  
