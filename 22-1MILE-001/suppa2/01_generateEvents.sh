28bdc8e5794332ed9736834a3b951774f60729f1
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-10 11:02:54 -0600

    Update 01_generateEvents.sh

--- 22-1MILE-001/suppa2/01_generateEvents.sh
+++ 22-1MILE-001/suppa2/01_generateEvents.sh
@@ -2,6 +2,12 @@
 
 
 
+GTF=
+OUTDIR=
 
-
-python3.4 suppa.py generateEvents
+python3.4 suppa.py generateEvents \
+                          -i $GTF \ 
+                          -o $OUTDIR/events_from_gtf \
+                          -f ioe \
+                          -e SE SS MX RI FL
+                
