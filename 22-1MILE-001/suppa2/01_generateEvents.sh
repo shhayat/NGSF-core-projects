0c43d8b39e3a558175da51e972246611a124df6c
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-10 11:05:22 -0600

    Update 01_generateEvents.sh

--- 22-1MILE-001/suppa2/01_generateEvents.sh
+++ 22-1MILE-001/suppa2/01_generateEvents.sh
@@ -1,13 +1,13 @@
 
 
 
-
+SUPPA=/globalhome/hxo752/HPC/anaconda3/envs/suppa/bin
 GTF=
-OUTDIR=
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2_analysis
 
-python3.4 suppa.py generateEvents \
-                          -i $GTF \ 
-                          -o $OUTDIR/events_from_gtf \
-                          -f ioe \
-                          -e SE SS MX RI FL
+${SUPPA}/python ${SUPPA}/suppa.py generateEvents \
+                              -i $GTF \ 
+                              -o $OUTDIR/events_from_gtf \
+                              -f ioe \
+                              -e SE SS MX RI FL
                 
