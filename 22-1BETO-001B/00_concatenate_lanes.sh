c0f7bf8907d88d19c5eee9b3d07a12eeb3384a48
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-15 10:23:56 -0600

    Create 00_concatenate_lanes.sh

---
+++ 22-1BETO-001B/00_concatenate_lanes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+	
+#SBATCH --job-name=combine_fastq
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2:00:00
+#SBATCH --mem=8G
+#SBATCH --account=hpc_p_anderson
+	
+# goal is to pull data from datastore, and compile into a file on the compute node
+# then send that file back to the working directory
+	
+set -eux
+	
+mkdir -p /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001B/analysis/Fastq 
+# Move all fastq files from the run onto the node
+FASTQ_FOLDER=/datastore/NGSF001/NB551711/221108_NB551711_0060_AHK53KBGXL/Alignment_1/20221108_225459/Fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-BETO-001B/analysis/Fastq
+	
+
+for i in $(seq -w 166 177)
+do
+	echo Combining library R22000${i}
+        # Cat all the original seq (each lane) files into a new file on the node
+	# This will also remove that stupid sample number
+	cat ${FASTQ_FOLDER}/R22000${i}_*_R1_* > ${OUTDIR}/R22000${i}_R1.fastq.gz
+	cat ${FASTQ_FOLDER}/R22000${i}_*_R2_* > ${OUTDIR}/R22000${i}_R2.fastq.gz
+done
