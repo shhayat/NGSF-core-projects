db743bdc52ea8bc5b6366f4af61c390eeecda678
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-16 10:22:39 -0600

    Update sbatch_run_rnaseqc.sh

--- 22-1BETO-001/sbatch_run_rnaseqc.sh
+++ 22-1BETO-001/sbatch_run_rnaseqc.sh
@@ -6,6 +6,6 @@ do
         path="${i%/Aligned*}";
 	sample_name="${path##*/}"
       
-  sbatch ${SCRIPT_DIR}/03_RNASeQC.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
+  sbatch ${SCRIPT_DIR}/04_RNASeQC.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
  sleep 0.5
 done 
