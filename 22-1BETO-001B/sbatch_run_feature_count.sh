f76bfb671c2d9dfd555c276954e00224d091d33b
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-16 11:24:33 -0600

    Update sbatch_run_feature_count.sh

--- 22-1BETO-001B/sbatch_run_feature_count.sh
+++ 22-1BETO-001B/sbatch_run_feature_count.sh
@@ -11,4 +11,4 @@ set -eux
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001B
 module load r/4.1.2
 
-R CMD BATCH ${SCRIPT_DIR}/05_feature_count.R
+R CMD BATCH ${SCRIPT_DIR}/04_feature_count.R
