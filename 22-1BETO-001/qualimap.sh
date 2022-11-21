b998daf38ae3bc3e59d8fc43e5b8b5f25d176a3d
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-21 16:31:11 -0600

    Update qualimap.sh

--- 22-1BETO-001/qualimap.sh
+++ 22-1BETO-001/qualimap.sh
@@ -2,7 +2,7 @@ qualimap=/datastore/NGSF001/software/tools/
 gtf=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001/analysis/qualimap
 
-.{qualimap}/qualimap rnaseq \
+.${qualimap}/qualimap rnaseq \
     -outdir $OUTDIR \
     -a proportional \
     -bam /datastore/NGSF001/projects/22-1BETO-001/analysis/star_alignment/R22000154/Aligned.sortedByCoord.out.bam \
