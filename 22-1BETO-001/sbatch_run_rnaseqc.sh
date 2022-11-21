4dd8eb1fc960574cd6fa31f502e436aa106adcdd
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-21 09:30:35 -0600

    Update sbatch_run_rnaseqc.sh

--- 22-1BETO-001/sbatch_run_rnaseqc.sh
+++ 22-1BETO-001/sbatch_run_rnaseqc.sh
@@ -1,4 +1,5 @@
-DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-BETO-001/analysis/star_alignment
+#DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-BETO-001/analysis/star_alignment
+DATA=/datastore/NGSF001/projects/22-1BETO-001/analysis/star_alignment
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001
 
 for i in ${DATA}/*/*.bam
