e1bf43358b2b0b857561f493e4d15a5b15d0f721
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-22 10:46:01 -0600

    Update 04_qualimap.sh

--- 22-1BETO-001/04_qualimap.sh
+++ 22-1BETO-001/04_qualimap.sh
@@ -7,12 +7,7 @@
 
 set -eux
 
-# copy tool to tempdir and give execute permission
-cp /datastore/NGSF001/software/src/rnaseqc.v2.4.2.linux ${SLURM_TMPDIR}
-chmod u+x ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux
-
-sample_name=$1; shift
-bam_file=$1
+bam_file=$1;
 
 qualimap=/datastore/NGSF001/software/tools/qualimap_v2.2.1
 gtf=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
@@ -22,6 +17,6 @@ ${qualimap}/qualimap rnaseq \
     -outdir ${OUTDIR} \
     -a proportional \
     -bam ${bam_file} \
-    -p strand-specific-reverse \
+    -p strand-specific-forward \
     -gtf ${gtf} \
     --java-mem-size=8G
