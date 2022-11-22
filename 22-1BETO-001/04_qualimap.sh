f5d9da8e7b1e2edd10ffd69beee6613f2a424ccd
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-22 13:44:05 -0600

    Update 04_qualimap.sh

--- 22-1BETO-001/04_qualimap.sh
+++ 22-1BETO-001/04_qualimap.sh
@@ -9,20 +9,19 @@ set -eux
 sample_name=1; shift
 bam_file=$1;
 
-qualimap=/datastore/NGSF001/software/tools/qualimap_v2.2.1
-gtf=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001/analysis/qualimap
+#qualimap=/datastore/NGSF001/software/tools/qualimap_v2.2.1
+#gtf=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
+#OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001/analysis/qualimap
 
-${qualimap}/qualimap rnaseq \
-    -outdir ${OUTDIR} \
-    -oc ${sample_name} \
-    -a proportional \
-    -bam ${bam_file} \
-    -p strand-specific-forward \
-    -gtf ${gtf} \
-    -pe \
-    --java-mem-size=375G
-
+#${qualimap}/qualimap rnaseq \
+#    -outdir ${OUTDIR} \
+#    -oc ${sample_name} \
+#    -a proportional \
+#    -bam ${bam_file} \
+#    -p strand-specific-forward \
+#    -gtf ${gtf} \
+#    -pe \
+#    --java-mem-size=375G
 
 
 qualimap=/datastore/NGSF001/software/tools/qualimap_v2.2.1
@@ -43,5 +42,5 @@ do
     -p strand-specific-forward \
     -gtf ${gtf} \
     -pe \
-    --java-mem-size=80G
+    --java-mem-size=100G
 done   
