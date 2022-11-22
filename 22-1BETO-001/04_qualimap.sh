480e6a96d4d035dffdb49778cc06b1bba21bc825
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-22 12:03:54 -0600

    Update 04_qualimap.sh

--- 22-1BETO-001/04_qualimap.sh
+++ 22-1BETO-001/04_qualimap.sh
@@ -7,14 +7,15 @@
 
 set -eux
 
-bam_file=$1;
-
+bam_file=$1;shift
+sample_name=1;
 qualimap=/datastore/NGSF001/software/tools/qualimap_v2.2.1
 gtf=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001/analysis/qualimap
 
 ${qualimap}/qualimap rnaseq \
     -outdir ${OUTDIR} \
+    -oc \
     -a proportional \
     -bam ${bam_file} \
     -p strand-specific-forward \
