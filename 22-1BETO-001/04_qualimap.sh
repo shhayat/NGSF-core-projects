5eadff800043b9ce8b2ec4a966bf7bd94064eea6
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-22 12:57:18 -0600

    Update 04_qualimap.sh

--- 22-1BETO-001/04_qualimap.sh
+++ 22-1BETO-001/04_qualimap.sh
@@ -22,3 +22,26 @@ ${qualimap}/qualimap rnaseq \
     -gtf ${gtf} \
     -pe \
     --java-mem-size=375G
+
+
+
+qualimap=/datastore/NGSF001/software/tools/qualimap_v2.2.1
+gtf=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001/analysis/qualimap
+DATA=/datastore/NGSF001/projects/22-1BETO-001/analysis/star_alignment
+
+for i in ${DATA}/*/*.bam
+do
+      path="${i%/Aligned*}";
+	  sample_name="${path##*/}"
+      
+ ${qualimap}/qualimap rnaseq \
+    -outdir ${OUTDIR} \
+    -oc ${sample_name} \
+    -a proportional \
+    -bam ${bam_file} \
+    -p strand-specific-forward \
+    -gtf ${gtf} \
+    -pe \
+    --java-mem-size=80G
+done   
