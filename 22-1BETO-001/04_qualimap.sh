2cca3581549e639172dc1cf6acf2363332db2d3b
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-22 14:33:50 -0600

    Update 04_qualimap.sh

--- 22-1BETO-001/04_qualimap.sh
+++ 22-1BETO-001/04_qualimap.sh
@@ -9,38 +9,15 @@ set -eux
 sample_name=1; shift
 bam_file=$1;
 
-#qualimap=/datastore/NGSF001/software/tools/qualimap_v2.2.1
-#gtf=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
-#OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001/analysis/qualimap
-
-#${qualimap}/qualimap rnaseq \
-#    -outdir ${OUTDIR} \
-#    -oc ${sample_name} \
-#    -a proportional \
-#    -bam ${bam_file} \
-#    -p strand-specific-forward \
-#    -gtf ${gtf} \
-#    -pe \
-#    --java-mem-size=375G
-
-
 qualimap=/datastore/NGSF001/software/tools/qualimap_v2.2.1
 gtf=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001/analysis/qualimap
-DATA=/datastore/NGSF001/projects/22-1BETO-001/analysis/star_alignment
 
-for i in ${DATA}/*/*.bam
-do
-      path="${i%/Aligned*}";
-	  sample_name="${path##*/}"
-      
- ${qualimap}/qualimap rnaseq \
+${qualimap}/qualimap rnaseq \
     -outdir ${OUTDIR} \
-    -oc ${sample_name} \
     -a proportional \
-    -bam ${i} \
+    -bam ${bam_file} \
     -p strand-specific-forward \
     -gtf ${gtf} \
     -pe \
-    --java-mem-size=100G
-done   
+    --java-mem-size=375G
