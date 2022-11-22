ccebb158240f98905675821ab2a07990f90b3d40
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-22 10:38:35 -0600

    Update and rename qualimap.sh to 04_qualimap.sh

--- 22-1BETO-001/04_qualimap.sh
+++ 22-1BETO-001/04_qualimap.sh
@@ -1,11 +1,27 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=00:20:00
+#SBATCH --mem=5G
+
+set -eux
+
+# copy tool to tempdir and give execute permission
+cp /datastore/NGSF001/software/src/rnaseqc.v2.4.2.linux ${SLURM_TMPDIR}
+chmod u+x ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux
+
+sample_name=$1; shift
+bam_file=$1
+
 qualimap=/datastore/NGSF001/software/tools/qualimap_v2.2.1
 gtf=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001/analysis/qualimap
 
 ${qualimap}/qualimap rnaseq \
-    -outdir $OUTDIR \
+    -outdir ${OUTDIR} \
     -a proportional \
-    -bam /datastore/NGSF001/projects/22-1BETO-001/analysis/star_alignment/R22000154/Aligned.sortedByCoord.out.bam \
+    -bam ${bam_file} \
     -p strand-specific-reverse \
-    -gtf $gtf \
+    -gtf ${gtf} \
     --java-mem-size=8G
