6a47d83e3d653eafb31df7004722b7e804b9f0d4
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-23 09:22:40 -0600

    Update 02_calculate_TPM.sh

--- 22-1MILE-001/suppa2/02_calculate_TPM.sh
+++ 22-1MILE-001/suppa2/02_calculate_TPM.sh
@@ -1,7 +1,9 @@
 stringtie=/datastore/NGSF001/software/tools/stringtie
-OUTDIR=
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/tpm
 
+sample_name=1; shift
+bam_file=1;
 
-${stringtie}/stringtie ${bam_file}
+${stringtie}/stringtie ${bam_file} \
 -G ${GTF} \
--o $OUTDIR/${sample}.gtf
+-o $OUTDIR/${sample_name}.gtf
