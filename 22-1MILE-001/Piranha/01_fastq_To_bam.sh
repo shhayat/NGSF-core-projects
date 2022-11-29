22eb0d00465bf18b1f4c96d6e3ad14e170a60817
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-29 14:02:58 -0600

    Update 01_fastq_To_bam.sh

--- 22-1MILE-001/Piranha/01_fastq_To_bam.sh
+++ 22-1MILE-001/Piranha/01_fastq_To_bam.sh
@@ -4,7 +4,7 @@
 #SBATCH --constraint=ivybridge
 #SBATCH --job-name=FastqToSam
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=40G
+#SBATCH --mem=30G
 #SBATCH --time=24:00:00
 #SBATCH --output=%j.out
 
@@ -14,7 +14,7 @@ module load picard/2.23.3
 fastq_path=/datastore/NGSF001/projects/2021/21-1MILE-001/fastq
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/Piranha
 
-java -Xmx40G -XX:ParallelGCThreads=$NCPU -jar $EBROOTPICARD/picard.jar FastqToSam \
+java -Xmx30G -XX:ParallelGCThreads=$NCPU -jar $EBROOTPICARD/picard.jar FastqToSam \
     FASTQ=$fastq_path/R2100080_S1_R1_001.fastq.gz  \ #first read file of pair
     FASTQ2=$fastq_path/R2100080_S1_R2_001.fastq.gz \ #second read file of pair
     OUTPUT=$OUTDIR/R2100080_fastqtosam.bam
