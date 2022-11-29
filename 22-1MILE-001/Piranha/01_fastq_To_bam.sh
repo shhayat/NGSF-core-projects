108c0cb5d8714f4b45f3d1b0a4b63e238efbfa9d
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-29 14:04:14 -0600

    Update 01_fastq_To_bam.sh

--- 22-1MILE-001/Piranha/01_fastq_To_bam.sh
+++ 22-1MILE-001/Piranha/01_fastq_To_bam.sh
@@ -15,8 +15,8 @@ fastq_path=/datastore/NGSF001/projects/2021/21-1MILE-001/fastq
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/Piranha
 
 java -Xmx30G -XX:ParallelGCThreads=$NCPU -jar $EBROOTPICARD/picard.jar FastqToSam \
-    FASTQ=$fastq_path/R2100080_S1_R1_001.fastq.gz  \ #first read file of pair
-    FASTQ2=$fastq_path/R2100080_S1_R2_001.fastq.gz \ #second read file of pair
+    $fastq_path/R2100080_S1_R1_001.fastq.gz \
+    $fastq_path/R2100080_S1_R2_001.fastq.gz \
     OUTPUT=$OUTDIR/R2100080_fastqtosam.bam
 
     
