2eb0a33e99ea170f763bfa96462771c2eade0425
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-29 13:49:07 -0600

    Update Piranha.sh

--- 22-1MILE-001/Piranha/Piranha.sh
+++ 22-1MILE-001/Piranha/Piranha.sh
@@ -1,1 +1,8 @@
-/datastore/NGSF001/projects/2021/21-1MILE-001/alignment/star
+#/datastore/NGSF001/projects/2021/21-1MILE-001/alignment/star
+fastq_path=/datastore/NGSF001/projects/2021/21-1MILE-001/fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/Piranha
+java -Xmx8G -jar picard.jar FastqToSam \
+    FASTQ=$fastq_path/R2100080_S1_R1_001.fastq.gz  \ #first read file of pair
+    FASTQ2=$fastq_path/R2100080_S1_R2_001.fastq.gz \ #second read file of pair
+    OUTPUT=$OUTDIR/R2100080_fastqtosam.bam
+    
