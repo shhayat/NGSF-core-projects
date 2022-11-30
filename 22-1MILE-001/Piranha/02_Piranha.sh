15aa3d0885ea4a038ef53eed0ffbe20f277fc078
Shahina <91986031+shhayat@users.noreply.github.com>
2022-11-30 10:02:25 -0600

    Create 02_Piranha.sh

---
+++ 22-1MILE-001/Piranha/02_Piranha.sh
@@ -0,0 +1,5 @@
+Data=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/Piranha
+piranha=/globalhome/hxo752/HPC/anaconda3/envs/piranha/bin
+OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/Piranha
+
+./piranha/Piranha -b $Data/R2100080_fastqtosam.bam -s -o $OUTPUT/R2100080_peakcalling
