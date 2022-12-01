f39dc61d5ed89a30652efd10a505299dfa6d9247
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-01 10:38:27 -0600

    Update 01_generateEvents.sh

--- 22-1MILE-001/suppa2/01_generateEvents.sh
+++ 22-1MILE-001/suppa2/01_generateEvents.sh
@@ -9,8 +9,8 @@
 #SBATCH --output=%j.out
 
 SUPPA=/globalhome/hxo752/HPC/anaconda3/envs/suppa/bin
-#GTF=/datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/GCF_000001895.5_Rnor_6.0/GCF_000001895.5_Rnor_6.0_genomic.gtf
-GTF=/datastore/NGSF001/analysis/references/iGenomes/Rat/Rattus_norvegicus/Ensembl/Rnor_6.0/Annotation/Genes/genes.gtf
+GTF=/datastore/NGSF001/analysis/references/rat/Rnor_6.0/ncbi-genomes-2020-10-30/GCF_000001895.5_Rnor_6.0/GCF_000001895.5_Rnor_6.0_genomic.gtf
+#GTF=/datastore/NGSF001/analysis/references/iGenomes/Rat/Rattus_norvegicus/Ensembl/Rnor_6.0/Annotation/Genes/genes.gtf
 
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/events
 
