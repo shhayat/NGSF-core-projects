0d5c8f4fd91cfcf8b5c0aa22b0e242f64fc4f631
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-07 15:56:18 -0600

    Update sbatch_run_featureCount.sh

--- 21-1JOHO-001/sbatch_run_featureCount.sh
+++ 21-1JOHO-001/sbatch_run_featureCount.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --time=3:00:00
 #SBATCH --mem=40G
-#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+#SBATCH  --output=%j.out
 set -eux
 
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1JOHO-001
