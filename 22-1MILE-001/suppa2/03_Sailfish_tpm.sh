76996761eef7174adb7872a19ee4fb841ffb5f61
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-07 10:12:00 -0600

    Update 03_Sailfish_tpm.sh

--- 22-1MILE-001/suppa2/03_Sailfish_tpm.sh
+++ 22-1MILE-001/suppa2/03_Sailfish_tpm.sh
@@ -1,1 +1,20 @@
-cc
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=sailfish_index
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=1
+#SBATCH --time=02:00:00
+#SBATCH --mem=80G
+#SBATCH --output=%j.out
+
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/tpm/tpm
+index=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/tpm
+sailfish=/datastore/NGSF001/software/tools/SailfishBeta-0.10.0_CentOS5/bin/
+
+NCPU=1
+fq1=
+fq2=
+
+${sailfish}/sailfish quant -i ${index} -l "<libtype>" -1 ${fq1} -2 ${fq2} -o ${OUTDIR}
