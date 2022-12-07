dd77e6466f90c8554481c00d41d4cdf528121eb0
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-07 14:46:28 -0600

    Update 01_featurcount.R

--- 21-1JOHO-001/01_featurcount.R
+++ 21-1JOHO-001/01_featurcount.R
@@ -1,2 +1,34 @@
+library(Rsubread)
+library(magrittr)
+setwd("/datastore/NGSF001/projects/2021/21-1JOHO-001/")
+result_dir="alignment"
+sample_names <- list.files(result_dir, "R2")
 
-/datastore/NGSF001/projects/2021/21-1JOHO-001/alignment
+
+#count features using featureCounts function
+feature_count <- sapply(sample_names, function(x)
+			   featureCounts(files = sprintf('%s/%s/Aligned.sortedByCoord.out.bam',result_dir, x),
+			   annot.ext="/datastore/NGSF001/analysis/references/human/gencode-40/gencode.v40.annotation_mod.gtf",
+			   isGTFAnnotationFile = TRUE,
+			   GTF.attrType.extra  = c('gene_name'),
+			   nthreads = 8, 
+			   isPairedEnd = TRUE), 
+			   simplify = FALSE, 
+			   USE.NAMES = TRUE)
+
+
+#convet list to a dataframe
+#COUNTS
+feature_count1 <- feature_count %>% lapply(function(x) x$counts) %>% 
+	do.call(cbind, .) %>% 
+	magrittr::set_colnames(names(feature_count))
+
+#ANNOTATIONS					  
+feature_annotation <- feature_count %>% lapply(function(x) x$annotation) %>% 
+	do.call(cbind, .) %>% 
+	magrittr::extract(,c(1,7)) %>%
+	magrittr::set_colnames(c('GeneID', 'gene_name'))
+
+feature_count <- cbind(feature_annotation,feature_count1)
+
+save(feature_count, file = 'feature_count.RData', compress = 'xz')
