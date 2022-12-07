8e54428b5505887292a345e05138758e170aa114
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-07 09:35:40 -0600

    Update 02_Sailfish_index.sh

--- 22-1MILE-001/suppa2/02_Sailfish_index.sh
+++ 22-1MILE-001/suppa2/02_Sailfish_index.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-#SBATCH --constraint=ivybridge
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
 #SBATCH --job-name=sailfish_index
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=30G
-#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=4
+#SBATCH --time=02:00:00
+#SBATCH --mem=80G
 #SBATCH --output=%j.out
 
-NCPU=1
+NCPU=4
 ref=/datastore/NGSF001/analysis/references/mouse/mm10/mm10.fa
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/tpm
 sailfish=/datastore/NGSF001/software/tools/SailfishBeta-0.10.0_CentOS5/bin/
