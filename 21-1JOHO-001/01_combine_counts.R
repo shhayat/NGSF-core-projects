373b8a7769f309da9e43402ff0e9dfbfebf24310
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-08 12:09:18 -0600

    Update 01_combine_counts.R

--- 21-1JOHO-001/01_combine_counts.R
+++ 21-1JOHO-001/01_combine_counts.R
@@ -1,3 +1,4 @@
-setwd("/datastore/NGSF001/projects/2021/21-1JOHO-001/expression")
-
+setwd("/datastore/NGSF001/projects/2021/21-1JOHO-001/")
+result_dir="expression"
+sample_names <- list.files(result_dir, "R2")
 
