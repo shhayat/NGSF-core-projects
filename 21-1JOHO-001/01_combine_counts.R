d9ecf42286cc476388d693765fb08def4180545a
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-08 12:50:43 -0600

    Update 01_combine_counts.R

--- 21-1JOHO-001/01_combine_counts.R
+++ 21-1JOHO-001/01_combine_counts.R
@@ -1,4 +1,9 @@
-setwd("/datastore/NGSF001/projects/2021/21-1JOHO-001/")
-result_dir="expression"
-sample_names <- list.files(result_dir, "R2")
+library(magrittr)
 
+setwd("/datastore/NGSF001/projects/2021/21-1JOHO-001/expression")
+
+Files <- lapply(Sys.glob("*.counts.htseq.txt"), read.table)
+
+ df <- list.files(pattern="*.counts.htseq.txt") %>% 
+                  lapply(read.table) %>% 
+                  bind_cols 
