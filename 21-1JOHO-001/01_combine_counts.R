5c49a5d6d7c61a2c01505edabf8a837a4ca5fe34
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-09 13:17:07 -0600

    Update 01_combine_counts.R

--- 21-1JOHO-001/01_combine_counts.R
+++ 21-1JOHO-001/01_combine_counts.R
@@ -9,5 +9,7 @@ setwd("/Users/shahina/Projects/21-1JOHO-001/expression/")
                   select(c(1,2,4,6,8,10,12)) %>%
                   setNames(c("gene_name","R2100175","R2100176","R2100177","R2100178","R2100179","R2100180"))
 
+rownames(df) <- df$gene_name
+df <- df[2:7]
 
-write.table(df, file="/Users/shahina/Projects/21-1JOHO-001/expression/feature_count.txt", quote=FALSE, sep="\t")
+write.table(df, file="/Users/shahina/Projects/21-1JOHO-001/expression/feature_counts.txt", quote=FALSE, sep="\t")
