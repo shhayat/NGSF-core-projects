c2e2e923441fcdc24e7c167d941cc3e11f591031
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-09 13:25:33 -0600

    Update 01_combine_counts.R

--- 21-1JOHO-001/01_combine_counts.R
+++ 21-1JOHO-001/01_combine_counts.R
@@ -7,9 +7,10 @@ setwd("/Users/shahina/Projects/21-1JOHO-001/expression/")
                   lapply(read.table) %>% 
                   bind_cols %>%
                   select(c(1,2,4,6,8,10,12))
+
+colnames(df) <- c("gene_name","R2100175","R2100176","R2100177","R2100178","R2100179","R2100180")
+
 rownames(df) <- df$gene_name
 df <- df[2:7]
-colnames(df) <- c("R2100175","R2100176","R2100177","R2100178","R2100179","R2100180")
 
-
-write.table(df, file="/Users/shahina/Projects/21-1JOHO-001/expression/feature_counts.txt", quote=FALSE, sep="\t")
+write.table(df, file="/Users/shahina/Projects/21-1JOHO-001/expression/feature_counts.txt", quote=FALSE, sep="\t",row.names=TRUE)
