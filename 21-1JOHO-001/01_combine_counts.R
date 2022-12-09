6268532a0da3d61f6363ed2a1a57882f1ccba2b7
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-09 13:20:09 -0600

    Update 01_combine_counts.R

--- 21-1JOHO-001/01_combine_counts.R
+++ 21-1JOHO-001/01_combine_counts.R
@@ -6,10 +6,10 @@ setwd("/Users/shahina/Projects/21-1JOHO-001/expression/")
  df <- list.files(pattern="*.counts.htseq.txt") %>% 
                   lapply(read.table) %>% 
                   bind_cols %>%
-                  select(c(1,2,4,6,8,10,12)) %>%
-                  setNames(c("gene_name","R2100175","R2100176","R2100177","R2100178","R2100179","R2100180"))
-
+                  select(c(1,2,4,6,8,10,12))
 rownames(df) <- df$gene_name
 df <- df[2:7]
+colnames(df) <- c("R2100175","R2100176","R2100177","R2100178","R2100179","R2100180")
+
 
 write.table(df, file="/Users/shahina/Projects/21-1JOHO-001/expression/feature_counts.txt", quote=FALSE, sep="\t")
