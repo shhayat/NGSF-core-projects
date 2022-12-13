e982fd2a33663189c3f6979693907fdc36377e44
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-13 13:02:00 -0600

    Update sbatch_salmon_quantification.sh

--- 22-1MILE-001/suppa2/sbatch_salmon_quantification.sh
+++ 22-1MILE-001/suppa2/sbatch_salmon_quantification.sh
@@ -1,1 +1,13 @@
+DATA=
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1LICH-001
 
+for i in $DATA/R2*.fastq.gz
+do
+        path="${i%_R*}";
+        sample_name=${path##*/};
+        fq1=${DATA}/${sample_name}_R1.fastq.gz;
+	fq2=${DATA}/${sample_name}_R2.fastq.gz;
+  
+      sbatch ${SCRIPT_DIR}/02_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}"
+ sleep 0.5
+done
