e566939a020cafd1489fee0d917bae5f69ecd912
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-13 10:47:47 -0600

    Update 02_salmon.sh

--- 22-1MILE-001/suppa2/02_salmon.sh
+++ 22-1MILE-001/suppa2/02_salmon.sh
@@ -1,1 +1,16 @@
-/datastore/NGSF001/software/tools/salmon-1.9.0_linux_x86_64/bin
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=salmon_index
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=1
+#SBATCH --time=08:00:00
+#SBATCH --mem=185G
+#SBATCH --output=%j.out
+
+
+salmon=/datastore/NGSF001/software/tools/salmon-1.9.0_linux_x86_64/bin
+GENOME=
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/tpm/index
+$salmon/salmon index -t ${GENOME} -i ${OUTDIR}/athal_index
