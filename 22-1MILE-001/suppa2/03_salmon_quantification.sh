6926f06dc383a9b2549cc45cd173c6ca3e37c362
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-13 13:19:59 -0600

    Update 03_salmon_quantification.sh

--- 22-1MILE-001/suppa2/03_salmon_quantification.sh
+++ 22-1MILE-001/suppa2/03_salmon_quantification.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=salmon_tpm
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=8
+#SBATCH --time=08:00:00
+#SBATCH --mem=185G
+#SBATCH --output=%j.out
+
 index=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/tpm/index/mm10_index
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/tpm/tpm
 sample_name=$1; shift
@@ -9,6 +20,6 @@ $salmon/salmon quant -i mm10_index \
              -l A \
              -1 ${fq1} \
              -2 ${fq2} \
-             -p 8 \
+             -p $NCPU \
              --validateMappings \
              -o $OUTDIR/${sample_name}_quant
