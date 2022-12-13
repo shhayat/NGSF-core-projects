85968f2e02c49a93f507de95825e6df6475332bc
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-13 12:37:29 -0600

    Update 03_salmon_quantification.sh

--- 22-1MILE-001/suppa2/03_salmon_quantification.sh
+++ 22-1MILE-001/suppa2/03_salmon_quantification.sh
@@ -1,14 +1,14 @@
 index=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/tpm/index/mm10_index
-fq1=1;
-fq2=2;
-sample_name=
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/tpm/tpm
-NCPU=8
+sample_name=$1; shift
+fq1=$2;
+fq2=$3;
 
+NCPU=8
 $salmon/salmon quant -i mm10_index \
              -l A \
              -1 ${fq1} \
              -2 ${fq2} \
              -p 8 \
              --validateMappings \
-             -o $OUTDIR/${samp}_quant
+             -o $OUTDIR/${sample_name}_quant
