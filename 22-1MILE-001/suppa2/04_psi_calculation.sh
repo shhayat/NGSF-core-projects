b10dbee9ea767a686dc0a32c36e422365c800595
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-16 15:29:23 -0600

    Update 04_psi_calculation.sh

--- 22-1MILE-001/suppa2/04_psi_calculation.sh
+++ 22-1MILE-001/suppa2/04_psi_calculation.sh
@@ -1,3 +1,14 @@
-#psi calculation for events
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=ivybridge
+#SBATCH --job-name=suppa2_psi_calculation
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30G
+#SBATCH --time=24:00:00
+#SBATCH --output=%j.out
+
+SUPPA=/globalhome/hxo752/HPC/anaconda3/envs/suppa/bin
+
+#psi calculation for events
 python3.4 suppa.py psiPerEvent --ioe-file <ioe-file> --expression-file <expression-file> -o <output-file> 
