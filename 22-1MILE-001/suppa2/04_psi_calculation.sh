73671d94f9c7901263d8f912af685dd4de3112ab
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-16 15:32:37 -0600

    Update 04_psi_calculation.sh

--- 22-1MILE-001/suppa2/04_psi_calculation.sh
+++ 22-1MILE-001/suppa2/04_psi_calculation.sh
@@ -9,6 +9,8 @@
 #SBATCH --output=%j.out
 
 SUPPA=/globalhome/hxo752/HPC/anaconda3/envs/suppa/bin
+events=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/events
+
 
 #psi calculation for events
 python3.4 suppa.py psiPerEvent --ioe-file <ioe-file> --expression-file <expression-file> -o <output-file> 
