64e8203950a29c13e00fda0a703d8098dfb12862
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-16 12:20:31 -0600

    Update sbatch_salmon_quantification.sh

--- 22-1MILE-001/suppa2/sbatch_salmon_quantification.sh
+++ 22-1MILE-001/suppa2/sbatch_salmon_quantification.sh
@@ -4,11 +4,12 @@ SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001
 
 for i in $DATA/D2*_R1*.fastq.gz
 do
-        path="${i%_D2*_R1*}";
+	path="${i%_D*}";
         sample_name=${path##*/};
+	sample_name1=${sample_name%%_*};
         fq1=${DATA}/${sample_name1}_R1_umi.fastq.gz;
 	fq2=${DATA}/${sample_name1}_R3_umi.fastq.gz;
   
-      sbatch ${SCRIPT_DIR}/03_salmon_quantification.sh "${sample_name}" "${fq1}" "${fq2}"
+      sbatch ${SCRIPT_DIR}/03_salmon_quantification.sh "${sample_name1}" "${fq1}" "${fq2}"
  sleep 0.5
 done
