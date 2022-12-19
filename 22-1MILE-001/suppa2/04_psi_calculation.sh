57af6dc92831d923fcb8893b3fcd4a079b452588
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-19 14:46:57 -0600

    Update 04_psi_calculation.sh

--- 22-1MILE-001/suppa2/04_psi_calculation.sh
+++ 22-1MILE-001/suppa2/04_psi_calculation.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --output=%j.out
 
-module load python
+module load python/3.7
 
 SUPPA=/globalhome/hxo752/HPC/anaconda3/envs/suppa/bin
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/tpm/tpm/
