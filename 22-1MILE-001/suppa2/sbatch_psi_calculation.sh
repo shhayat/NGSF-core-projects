a17f80684329b025585c9cc9cb49f47a5d13657e
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-19 13:56:53 -0600

    Update sbatch_psi_calculation.sh

--- 22-1MILE-001/suppa2/sbatch_psi_calculation.sh
+++ 22-1MILE-001/suppa2/sbatch_psi_calculation.sh
@@ -2,14 +2,11 @@ DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2
 
 
-for i in $DATA/*_quant/quant.sf
+for i in $DATA/*_quant/*.sf
 do
-	      path="${i%_D*}";
-        sample_name=${path##*/};
-	      sample_name1=${sample_name%%_*};
-        fq1=${DATA}/${sample_name1}_R1_umi.fastq.gz;
-	      fq2=${DATA}/${sample_name1}_R3_umi.fastq.gz;
+	path="${i%/quant*}";
+	sample_name="${path##*/}"
   
-      sbatch ${SCRIPT_DIR}/04_salmon_quantification.sh "${sample_name1}" "${fq1}" "${fq2}"
+        sbatch ${SCRIPT_DIR}/04_salmon_quantification.sh "${sample_name}"
  sleep 0.5
 done
