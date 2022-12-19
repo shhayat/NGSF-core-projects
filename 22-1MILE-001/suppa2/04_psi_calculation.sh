3c120d2842492c58c2841f5ff693043a4b82b676
Shahina <91986031+shhayat@users.noreply.github.com>
2022-12-19 14:08:56 -0600

    Update 04_psi_calculation.sh

--- 22-1MILE-001/suppa2/04_psi_calculation.sh
+++ 22-1MILE-001/suppa2/04_psi_calculation.sh
@@ -8,6 +8,8 @@
 #SBATCH --time=24:00:00
 #SBATCH --output=%j.out
 
+module load python
+
 SUPPA=/globalhome/hxo752/HPC/anaconda3/envs/suppa/bin
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/tpm/tpm/
 events=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-001/suppa2/suppa2_analysis/events
@@ -18,7 +20,7 @@ mkdir -p ${OUTPUT}
 sample_name=$1;
 
 #psi calculation for events
-python3.4 suppa.py psiPerEvent \
+python suppa.py psiPerEvent \
                 --ioe-file ${events}/events_from_gtf_AL_strict.ioe \
                 --expression-file ${DATA}/${sample_name}/qunat.sf \
                 -o ${OUTPUT}
