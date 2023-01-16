f139a01f582bf44c490ee03ccc35aaecc3be242f
Shahina <91986031+shhayat@users.noreply.github.com>
2023-01-16 14:06:30 -0600

    Update 02_genome_index.sh

--- SingleCellSequencing/CellRanger/02_genome_index.sh
+++ SingleCellSequencing/CellRanger/02_genome_index.sh
@@ -1,10 +1,26 @@
+#!/bin/sh
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --job-name=genome_index
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=10
+#SBATCH --time=03:00:00
+#SBATCH --mem=80G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
 
-NCPUS=10
 cellranger=/datastore/NGSF001/software/tools/cellranger-7.1.0/bin
+GENOME=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta
+GTF=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/analysis/indices
+
+NCPU=10
+
+mkdir -p $OUTDIR
+cd ${OUTDIR}
 
 ${cellranger}/cellranger mkref \
-  --nthreads={NCPUS} \
-  --genome={OUTPUT FOLDER FOR INDEX} \
-  --fasta={GENOME FASTA} \
-  --genes={ANNOTATION GTF}
+         --nthreads=${NCPUS} \
+         --genome=${OUTDIR} \
+         --fasta=${GENOME} \
+         --genes=${GTF}
