da5e8a24580a90e2464eed57778192d87f727053
Shahina <91986031+shhayat@users.noreply.github.com>
2023-01-16 13:50:35 -0600

    Update 02_genome_index.sh

--- SingleCellSequencing/CellRanger/02_genome_index.sh
+++ SingleCellSequencing/CellRanger/02_genome_index.sh
@@ -1,8 +1,10 @@
 
 
+NCPUS=10
+cellranger=/datastore/NGSF001/software/tools/cellranger-7.1.0/bin
 
-cellranger mkref \
-  --nthreads={CPUS} \
+${cellranger}/cellranger mkref \
+  --nthreads={NCPUS} \
   --genome={OUTPUT FOLDER FOR INDEX} \
   --fasta={GENOME FASTA} \
   --genes={ANNOTATION GTF}
