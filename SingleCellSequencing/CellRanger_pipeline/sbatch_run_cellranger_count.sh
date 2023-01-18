4c7cfa4638f0cca1c536de99f571675d37e63f72
Shahina <91986031+shhayat@users.noreply.github.com>
2023-01-18 14:18:32 -0600

    Update sbatch_run_cellranger_count.sh

--- SingleCellSequencing/CellRanger_pipeline/sbatch_run_cellranger_count.sh
+++ SingleCellSequencing/CellRanger_pipeline/sbatch_run_cellranger_count.sh
@@ -1,11 +1,11 @@
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/analysis/Fastq
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/
 
-for i in $DATA/Brain_Tumor_3p_S2_L00*.fastq.gz
-do
-        path="${i%_L*}";
-        sample_name=${path##*/};
+#for i in $DATA/Brain_Tumor_3p_S2_L00*.fastq.gz
+#do
+#        path="${i%_L*}";
+#        sample_name=${path##*/};
   
-      sbatch ${SCRIPT_DIR}/02_cellranger_count.sh "${sample_name}"
- sleep 0.5
-done
+#      sbatch ${SCRIPT_DIR}/02_cellranger_count.sh "${sample_name}"
+# sleep 0.5
+#done
