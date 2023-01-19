cc9e201bc37ce136b7e2f076cf9200df330979ef
Shahina <91986031+shhayat@users.noreply.github.com>
2023-01-19 09:24:43 -0600

    Update 02_cellranger_count.sh

--- SingleCellSequencing/CellRanger_pipeline/02_cellranger_count.sh
+++ SingleCellSequencing/CellRanger_pipeline/02_cellranger_count.sh
@@ -4,7 +4,7 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=cellranger-count
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=16
 #SBATCH --time=24:00:00
 #SBATCH --mem=250G
 #SBATCH  --output=%j.out
@@ -18,8 +18,8 @@ REF=/datastore/NGSF001/analysis/references/cell_ranger_genome_builds/refdata-gex
 OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/analysis/
 
 mkdir -p ${OUTPUT}
-NCPUS=20
-RAM_MEMORY=250
+NCPUS=16
+RAM_MEMORY=128
 #SAMPLE_NAME=1;
 SAMPLE_NAME="Brain_Tumor_3p"
 cellranger count --id=${OUTPUT}/${SAMPLE_NAME} \
