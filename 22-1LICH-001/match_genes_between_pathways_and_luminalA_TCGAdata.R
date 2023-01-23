193254940396d1f77f47c6f2d6d7afffc8503a25
Shahina <91986031+shhayat@users.noreply.github.com>
2023-01-23 14:56:57 -0600

    Update match_genes_between_pathways_and_luminalA_TCGAdata.R

--- 22-1LICH-001/match_genes_between_pathways_and_luminalA_TCGAdata.R
+++ 22-1LICH-001/match_genes_between_pathways_and_luminalA_TCGAdata.R
@@ -9,7 +9,7 @@
 #4. Extracted original significant gene list sent by Linda (Folder: significant_pairwise_gene_list)
 #5. Checked the directionality of genes from step3 to significant genes from step4  (Folder: results)
 
-setwd("~/Desktop/TCGA_analysis_for_pathways/")
+setwd("~/Desktop/core-projects/TCGA_analysis_for_pathways/")
 library("readxl")
 
 #LuminalA_vs_Normal extracted from cbioPortal
@@ -73,5 +73,36 @@ pairwise_A3H_p2 <- merge(A3H_p22,pairwise_A3H, by="Gene")
 write.csv(pairwise_A3A_p2,"results/pairwise_A3A_pulmonary_healing_signaling.csv", row.names = FALSE)
 write.csv(pairwise_A3B_p2,"results/pairwise_A3B_pulmonary_healing_signaling.csv", row.names = FALSE)
 write.csv(pairwise_A3H_p2,"results/pairwise_A3H_pulmonary_healing_signaling.csv", row.names = FALSE)
+
+
+
+
+
+#Wound Healing Signaling Pathway
+A3A_p3 <- read_excel("pathways_used_from_IPA/A3A_wound_healing_signaling_pathway.xls")[1]
+colnames(A3A_p3) <- c("Gene")
+A3B_p3 <- read_excel("pathways_used_from_IPA/A3B_wound_healing_signaling_pathway.xls")[1]
+colnames(A3B_p3) <- c("Gene")
+A3H_p3 <- read_excel("pathways_used_from_IPA/A3H_wound_healing_signaling_pathway.xls")[1]
+colnames(A3H_p3) <- c("Gene")
+#4
+A3A_p33 <-  merge(luminalA, A3A_p3, by = "Gene")
+#5
+A3B_p33 <-  merge(luminalA, A3B_p3, by = "Gene")
+#4
+A3H_p33 <-  merge(luminalA, A3H_p3, by = "Gene")
+
+pairwise_A3A <- read.csv("significant_pairwise_gene_list/A3A_Pairwise Analysis  genes_fc2_pv05.csv")[2:3]
+pairwise_A3A_p3 <- merge(A3A_p33,pairwise_A3A, by="Gene")
+
+pairwise_A3B <- read.csv("significant_pairwise_gene_list/A3B_Pairwise Analysis  genes_fc2_pv05.csv")[2:3]
+pairwise_A3B_p3 <- merge(A3B_p33,pairwise_A3B, by="Gene")
+
+pairwise_A3H <- read.csv("significant_pairwise_gene_list/A3H_Pairwise Analysis  genes_fc2_pv05.csv")[2:3]
+pairwise_A3H_p3 <- merge(A3H_p33,pairwise_A3H, by="Gene")
+
+write.csv(pairwise_A3A_p3,"results/pairwise_A3A_wound_healing_signaling.csv", row.names = FALSE)
+write.csv(pairwise_A3B_p3,"results/pairwise_A3B_wound_healing_signaling.csv", row.names = FALSE)
+write.csv(pairwise_A3H_p3,"results/pairwise_A3H_wound_healing_signaling.csv", row.names = FALSE)
 
 
