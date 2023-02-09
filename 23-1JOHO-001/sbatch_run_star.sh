b52deb0d2b2b0b8fdc1db165dce129e311507a14
Shahina <91986031+shhayat@users.noreply.github.com>
2023-02-09 14:56:50 -0600

    Update sbatch_run_star.sh

--- 23-1JOHO-001/sbatch_run_star.sh
+++ 23-1JOHO-001/sbatch_run_star.sh
@@ -3,12 +3,12 @@ SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1JOHO-001
 
 
 #for i in $DATA/R*.fastq.gz
-for i in $DATA/R*48*.fastq.gz $DATA/R*50*.fastq.gz $DATA/R*51*.fastq.gz $DATA/R*61*.fastq.gz
+for i in $DATA/R*14_*.fastq.gz $DATA/R*07_*.fastq.gz $DATA/R*08_*.fastq.gz $DATA/R*09_*.fastq.gz
 do
         path="${i%_R*}";
         sample_name=${path##*/};
         fq1=${DATA}/${sample_name}_R1.fastq.gz;
-	      fq2=${DATA}/${sample_name}_R2.fastq.gz;
+	fq2=${DATA}/${sample_name}_R2.fastq.gz;
   
       sbatch ${SCRIPT_DIR}/03_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}"
  sleep 0.5
