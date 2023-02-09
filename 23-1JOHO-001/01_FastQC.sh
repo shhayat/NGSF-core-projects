359714877390c24a6a143cefc2853804cda48819
Shahina <91986031+shhayat@users.noreply.github.com>
2023-02-09 11:10:20 -0600

    Update 01_FastQC.sh

--- 23-1JOHO-001/01_FastQC.sh
+++ 23-1JOHO-001/01_FastQC.sh
@@ -4,20 +4,19 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=fastqc
 #SBATCH --ntasks=6
-#BATCH --cpus-per-task=4
+#BATCH --cpus-per-task=10
 #SBATCH --time=02:00:00
-#SBATCH --mem=6G
+#SBATCH --mem=20G
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 set -eux
 
-#/datastore/NGSF001/NB551711/230208_NB551711_0063_AHT2WGBGXN/Alignment_1/20230208_231511/Fastq
 module load fastqc
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1JOHO-001/analysis/Fastq
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1JOHO-001/analysis
 
 mkdir -p ${OUTDIR}/fastqc
 
-for fq in $DATA/R22*.fastq.gz
+for fq in $DATA/R23*.fastq.gz
 do
    fastqc -o ${OUTDIR}/fastqc --extract ${fq}
    
