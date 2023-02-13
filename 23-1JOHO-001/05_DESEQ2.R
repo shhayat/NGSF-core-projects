2680c2a9702e82c9ad2b60226f6b5278d657c0cb
Shahina <91986031+shhayat@users.noreply.github.com>
2023-02-13 15:54:16 -0600

    Update 05_DESEQ2.R

--- 23-1JOHO-001/05_DESEQ2.R
+++ 23-1JOHO-001/05_DESEQ2.R
@@ -8,11 +8,11 @@ dir.create("DESEQ2", recursive=TRUE, showWarnings = FALSE)
 
 load("feature_count.RData")
 feature_count1 <- as.data.frame(feature_count)
-#geneID <- gsub(".[0-9]*$", "", rownames(feature_count))
-#rownames(feature_count) <- geneID
+geneID <- gsub(".[0-9]*$", "", rownames(feature_count))
+rownames(feature_count) <- geneID
 
 #your first columns which are gene id and gene name
-feature_annotation <- feature_count1[1:2]
+#feature_annotation <- feature_count1[1:2]
 
 setwd("/Users/shahina/Projects/22-1BETO-001B/DESEQ2")
 
