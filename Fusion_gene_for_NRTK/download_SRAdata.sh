317bfbf0a6256e9016391c6ad057859509e756b9
Shahina <91986031+shhayat@users.noreply.github.com>
2023-02-15 16:01:28 -0600

    Update and rename download_SRAdata to download_SRAdata.sh

--- Fusion_gene_for_NRTK/download_SRAdata.sh
+++ Fusion_gene_for_NRTK/download_SRAdata.sh
@@ -1,14 +1,17 @@
 #Normal Skin Tissue
 
 module load sra-toolkit/2.10.8
-
+OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/Fusion_gene_for_NRTK/Normal_Skin
+mkdir $OUTPUT
 for i in SRR8474298 SRR8474287 SRR8474236 SRR8474246 SRR8474266
 do
-  prefetch $i;
+  prefetch $i -O $;
   fasterq-dump $i;
 done
 
 #Fibrosarcoma 
+OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/Fusion_gene_for_NRTK/Fibrosarcoma
+mkdir $OUTPUT
 for i in SRR20327014 SRR20327015 SRR20327016 SRR20327011 SRR20327012 SRR20327013 SRR20327035 SRR20327036 SRR20327037 SRR20327006	
 do
   prefetch $i;
