bdd90d9aaa328255278c52843ac2ff79f5152522
Shahina <91986031+shhayat@users.noreply.github.com>
2023-02-16 12:05:05 -0600

    Update download_SRAdata.sh

--- Fusion_gene_for_NRTK/download_SRAdata.sh
+++ Fusion_gene_for_NRTK/download_SRAdata.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=sra_download
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=185G
+#SBATCH --time=20:00:00
+#SBATCH --output=%j.out
+
 sratoolkit=/globalhome/hxo752/HPC/tools/sratoolkit.3.0.1-ubuntu64/bin
 NCPU=10
 #Normal Skin Tissue
