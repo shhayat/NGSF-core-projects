50c84f4cdf1a7bcd69c366fb007a68fd6f046b37
Shahina <91986031+shhayat@users.noreply.github.com>
2023-02-16 15:04:56 -0600

    Update download_SRAdata.sh

--- Fusion_gene_for_NRTK/download_SRAdata.sh
+++ Fusion_gene_for_NRTK/download_SRAdata.sh
@@ -13,19 +13,17 @@ sratoolkit=/globalhome/hxo752/HPC/tools/sratoolkit.3.0.1-ubuntu64/bin
 OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/Fusion_gene_for_NRTK/Normal_Skin
 mkdir -p $OUTPUT
 mkdir -p $OUTPUT/fastq
-for i in SRR8474298 SRR8474287 SRR8474236 SRR8474246 SRR8474266
-do
-  echo "Generating sra file for: ${i}";
-  ${sratoolkit}/prefetch $i -O $OUTPUT --progress;
+#for i in SRR8474298 SRR8474287 SRR8474236 SRR8474246 SRR8474266
+#do
+#  echo "Generating sra file for: ${i}";
+#  ${sratoolkit}/prefetch $i -O $OUTPUT --progress;
   
-  echo "Generating fastq for: ${i}";
-  ${sratoolkit}/fastq-dump --outdir $OUTPUT/fastq --gzip --skip-technical --readids --read-filter pass --split-3 --clip ${OUTPUT}/${i}/${i}.sra;
-  #${sratoolkit}/fasterq-dump --split-files $i -O $OUTPUT --threads $NCPU --progress;
-  #${sratoolkit}/fasterq-dump --split-files $i -O $OUTPUT --threads $NCPU;
-done
+#  echo "Generating fastq for: ${i}";
+#  ${sratoolkit}/fastq-dump --outdir $OUTPUT/fastq --gzip --skip-technical --readids --read-filter pass --split-3 --clip ${OUTPUT}/${i}/${i}.sra; 
+#done
 
 #Fibrosarcoma (Single ended reads)
-OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/Fusion_gene_for_NRTK/Fibrosarcoma
+OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/Fusion_gene_for_NRTK/Fibrosarcoma_v1
 mkdir $OUTPUT
 for i in SRR20327014 SRR20327015 SRR20327016 SRR20327011 SRR20327012 SRR20327013 SRR20327035 SRR20327036 SRR20327037 SRR20327006	
 do
