e33c2e8b826ceb7aba41dd178d76f4c3ab61c837
Shahina <91986031+shhayat@users.noreply.github.com>
2023-02-16 12:02:01 -0600

    Update download_SRAdata.sh

--- Fusion_gene_for_NRTK/download_SRAdata.sh
+++ Fusion_gene_for_NRTK/download_SRAdata.sh
@@ -1,3 +1,5 @@
+sratoolkit=/globalhome/hxo752/HPC/tools/sratoolkit.3.0.1-ubuntu64/bin
+NCPU=10
 #Normal Skin Tissue
 OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/Fusion_gene_for_NRTK/Normal_Skin
 mkdir $OUTPUT
@@ -5,7 +7,7 @@ for i in SRR8474298 SRR8474287 SRR8474236 SRR8474246 SRR8474266
 do
   #prefetch $i -O $OUTPUT;
   #fasterq-dump $i -O $OUTPUT;
-  fasterq-dump --split-files $i
+  ${sratoolkit}/fasterq-dump --split-files $i -O $OUTPUT --threads $NCPU;
 done
 
 #Fibrosarcoma 
@@ -15,5 +17,5 @@ for i in SRR20327014 SRR20327015 SRR20327016 SRR20327011 SRR20327012 SRR20327013
 do
   #prefetch $i -O $OUTPUT;
   #fasterq-dump $i -O $OUTPUT;
-  fasterq-dump --split-files $i
+  fasterq-dump --split-files $i -O $OUTPUT --threads $NCPU;
 done
