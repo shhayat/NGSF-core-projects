57c151b90e0919320eece85ddb6133995c1bccce
Shahina <91986031+shhayat@users.noreply.github.com>
2023-02-16 15:07:04 -0600

    Update download_SRAdata.sh

--- Fusion_gene_for_NRTK/download_SRAdata.sh
+++ Fusion_gene_for_NRTK/download_SRAdata.sh
@@ -24,7 +24,8 @@ mkdir -p $OUTPUT/fastq
 
 #Fibrosarcoma (Single ended reads)
 OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/Fusion_gene_for_NRTK/Fibrosarcoma_v1
-mkdir $OUTPUT
+mkdir -p $OUTPUT
+mkdir -p $OUTPUT/fastq
 for i in SRR20327014 SRR20327015 SRR20327016 SRR20327011 SRR20327012 SRR20327013 SRR20327035 SRR20327036 SRR20327037 SRR20327006	
 do
   echo "Generating sra file for: ${i}";
