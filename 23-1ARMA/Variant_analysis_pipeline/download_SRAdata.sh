3f6a1695529918f54ff9c85102dce4882241783c
Shahina Hayat (hxo752) <hxo752@platolgn01.cm.cluster>
2023-02-17 15:35:59 -0600

    new file:   Variant_analysis_pipeline/download_SRAdata.sh

---
+++ 23-1ARMA/Variant_analysis_pipeline/download_SRAdata.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=sra_download
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=185G
+#SBATCH --time=20:00:00
+#SBATCH --output=%j.out
+
+sratoolkit=/globalhome/hxo752/HPC/tools/sratoolkit.3.0.1-ubuntu64/bin
+#Normal Skin Tissue (Paired reads)
+OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/Fusion_gene_for_NRTK/Normal_Skin
+mkdir -p $OUTPUT
+mkdir -p $OUTPUT/fastq
+#for i in SRR8474298 SRR8474287 SRR8474236 SRR8474246 SRR8474266
+#do
+#  echo "Generating sra file for: ${i}";
+#  ${sratoolkit}/prefetch $i -O $OUTPUT --progress;
+  
+#  echo "Generating fastq for: ${i}";
+#  ${sratoolkit}/fastq-dump --outdir $OUTPUT/fastq --gzip --skip-technical --readids --read-filter pass --split-3 --clip ${OUTPUT}/${i}/${i}.sra; 
+#done
+
+#Fibrosarcoma (Single ended reads)
+OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/Fusion_gene_for_NRTK/Fibrosarcoma_v1
+mkdir -p $OUTPUT
+mkdir -p $OUTPUT/fastq
+for i in SRR20327014 SRR20327015 SRR20327016 SRR20327011 SRR20327012 SRR20327013 SRR20327035 SRR20327036 SRR20327037 SRR20327006	
+do
+  echo "Generating sra file for: ${i}";
+  ${sratoolkit}/prefetch $i -O $OUTPUT --progress;
+  
+  echo "Generating fastq for: ${i}";
+  ${sratoolkit}/fastq-dump --outdir $OUTPUT/fastq --gzip --clip ${OUTPUT}/${i}/${i}.sra;
+done
