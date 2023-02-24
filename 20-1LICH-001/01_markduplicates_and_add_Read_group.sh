e245f5f168daafd775836b44ed965b17764976eb
Shahina <91986031+shhayat@users.noreply.github.com>
2023-02-24 15:25:39 -0600

    Update 01_markduplicates_and_add_Read_group.sh

--- 20-1LICH-001/01_markduplicates_and_add_Read_group.sh
+++ 20-1LICH-001/01_markduplicates_and_add_Read_group.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --time=2:30:00
-#SBATCH --mem=20G
+#SBATCH --mem=64G
 #SBATCH  --output=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/markdup_add_RG.out
 set -eux
 
@@ -19,7 +19,7 @@ REF='/datastore/NGSF001/analysis/references/human/gencode-30/GRCh38.primary_asse
 OUTDIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis'
 OUTDIR_NAME=$1
 BAM_FILE=$2
-
+$NCPU=10
 mkdir -p ${OUTDIR}/${OUTDIR_NAME}
 
 
