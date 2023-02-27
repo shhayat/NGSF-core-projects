05d238f63a374b42f706ff2e066efaa0d312c978
Shahina <91986031+shhayat@users.noreply.github.com>
2023-02-27 13:59:16 -0600

    Update 04_remove_shared_variants.sh

--- 20-1LICH-001/04_remove_shared_variants.sh
+++ 20-1LICH-001/04_remove_shared_variants.sh
@@ -16,9 +16,10 @@ module load StdEnv/2020
 module load gcc/9.3.0
 module load bcftools/1.13
 
-INPUT_DIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/concatenated_uninduced_samples"
+INPUT_DIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis"
+CONCATE_UNINDUCED="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/concatenated_uninduced_samples"
 mkdir -p ${INPUT_DIR}/filtered_vcfs
-OUTPUT_DIR='${INPUT_DIR}/filtered_vcfs/'
+OUTPUT_DIR='${INPUT_DIR}/filtered_vcfs'
 CLONE_ID=$1
 INDUCED_SAMPLE=$2
 
@@ -29,6 +30,6 @@ bcftools isec -C \
               -w 1 \
               -o ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}.vcf.gz \
               ${INPUT_DIR}/${INDUCED_SAMPLE}/${INDUCED_SAMPLE}.vcf.gz \
-              ${INPUT_DIR}/${CLONE_ID}_uninduced_concat.vcf.gz
+              ${CONCATE_UNINDUCED}/${CLONE_ID}_uninduced_concat.vcf.gz
 
 bcftools index -t ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}.vcf.gz
