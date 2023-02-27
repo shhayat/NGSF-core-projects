e1f2f13b6d6a9e88dd532a9ecba4388d61ff17d0
Shahina <91986031+shhayat@users.noreply.github.com>
2023-02-27 12:28:59 -0600

    Update 03_concat_uninduced.sh

--- 20-1LICH-001/03_concat_uninduced.sh
+++ 20-1LICH-001/03_concat_uninduced.sh
@@ -18,8 +18,8 @@ module load bcftools/1.13
 INPUT_DIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis'
 OUTDIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis'
 
-CLONE_ID=$1
-UNINDUCED_1=$2
+CLONE_ID=$1;
+UNINDUCED_1=$2;
 UNINDUCED_2=$3
 
 echo "${INPUT_DIR}/${UNINDUCED_1}/${UNINDUCED_1}.vcf.gz" >> ${INPUT_DIR}/${CLONE_ID}_uniduced_vcf.txt
