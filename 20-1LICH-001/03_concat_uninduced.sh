abf25c1622721985b07d98edd4e82e242bcc7013
Shahina <91986031+shhayat@users.noreply.github.com>
2023-02-27 13:52:21 -0600

    Update 03_concat_uninduced.sh

--- 20-1LICH-001/03_concat_uninduced.sh
+++ 20-1LICH-001/03_concat_uninduced.sh
@@ -16,19 +16,21 @@ module load gcc/9.3.0
 module load bcftools/1.13
 
 INPUT_DIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis'
-OUTDIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis'
+OUTDIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/concatenated_uninduced_samples'
+
+mkdir -p ${OUTDIR}
 
 CLONE_ID=$1;
 UNINDUCED_1=$2;
 UNINDUCED_2=$3
 
-echo "${INPUT_DIR}/${UNINDUCED_1}/${UNINDUCED_1}.vcf.gz" >> ${INPUT_DIR}/${CLONE_ID}_uniduced_vcf.txt
-echo "${INPUT_DIR}/${UNINDUCED_2}/${UNINDUCED_2}.vcf.gz" >> ${INPUT_DIR}/${CLONE_ID}_uniduced_vcf.txt
+echo "${INPUT_DIR}/${UNINDUCED_1}/${UNINDUCED_1}.vcf.gz" >> ${OUTDIR}/${CLONE_ID}_uniduced_vcf.txt
+echo "${INPUT_DIR}/${UNINDUCED_2}/${UNINDUCED_2}.vcf.gz" >> ${OUTDIR}/${CLONE_ID}_uniduced_vcf.txt
 
 bcftools concat -a \
                 -d all \
                 -O z \
-                -f ${INPUT_DIR}/${CLONE_ID}_uniduced_vcf.txt \
-                -o ${INPUT_DIR}/${CLONE_ID}_uninduced_concat.vcf.gz 
+                -f ${OUTDIR}/${CLONE_ID}_uniduced_vcf.txt \
+                -o ${OUTDIR}/${CLONE_ID}_uninduced_concat.vcf.gz 
                 
-bcftools index -t ${INPUT_DIR}/${CLONE_ID}_uninduced_concat.vcf.gz 
+bcftools index -t ${OUTDIR}/${CLONE_ID}_uninduced_concat.vcf.gz 
