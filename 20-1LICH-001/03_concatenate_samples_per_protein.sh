c31665c077a0a72ea2645c5a2acff9154c6bfb16
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-02 09:12:25 -0600

    Update 03_concatenate_samples_per_protein.sh

--- 20-1LICH-001/03_concatenate_samples_per_protein.sh
+++ 20-1LICH-001/03_concatenate_samples_per_protein.sh
@@ -1,18 +1,36 @@
-OUTDIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/concatenated_uninduced_samples'
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=bcftools
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:30:00
+#SBATCH --mem=2G
+#SBATCH  --output=%j.out
+#set -eux
+
+#loading required modules
+module load StdEnv/2020
+module load gcc/9.3.0
+module load bcftools/1.13
+
+INPUT_DIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis'
+OUTDIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/concatenated_samples_per_protein'
 
 mkdir -p ${OUTDIR}
 
-CLONE_ID=$1;
-UNINDUCED_1=$2;
-UNINDUCED_2=$3
+CLONE_ID=$1
+SAMPLE1=$2
+SAMPLE2=$3
 
-echo "${INPUT_DIR}/${UNINDUCED_1}/${UNINDUCED_1}.vcf.gz" >> ${OUTDIR}/${CLONE_ID}_uniduced_vcf.txt
-echo "${INPUT_DIR}/${UNINDUCED_2}/${UNINDUCED_2}.vcf.gz" >> ${OUTDIR}/${CLONE_ID}_uniduced_vcf.txt
+echo "${INPUT_DIR}/${SAMPLE1}/${SAMPLE1}.vcf.gz" >> ${OUTDIR}/${CLONE_ID}_${SAMPLE1}_vcf.txt
+echo "${INPUT_DIR}/${SAMPLE2}/${SAMPLE2}.vcf.gz" >> ${OUTDIR}/${CLONE_ID}_${SAMPLE2}_vcf.txt
 
 bcftools concat -a \
                 -d all \
                 -O z \
                 -f ${OUTDIR}/${CLONE_ID}_uniduced_vcf.txt \
-                -o ${OUTDIR}/${CLONE_ID}_uninduced_concat.vcf.gz 
+                -o ${OUTDIR}/${CLONE_ID}_concat_${SAMPLE1}_${SAMPLE2}.vcf.gz 
                 
-bcftools index -t ${OUTDIR}/${CLONE_ID}_uninduced_concat.vcf.gz 
+bcftools index -t ${OUTDIR}/${CLONE_ID}_concat_${SAMPLE1}_${SAMPLE2}.vcf.gz 
