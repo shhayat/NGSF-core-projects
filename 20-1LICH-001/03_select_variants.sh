bc40fda9aa7adaf15e679aa7bf4ffdcc4fbf1aff
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-02 09:03:55 -0600

    Update 03_select_variants.sh

--- 20-1LICH-001/03_select_variants.sh
+++ 20-1LICH-001/03_select_variants.sh
@@ -17,11 +17,15 @@ INPUT_DIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001
 OUTPUT_DIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/filtered_vcfs'
 
 mkdir -p ${OUTPUT_DIR}
-SAMPLE=$1
+CLONE_ID=$1
+OUTDIR_NAME=$2
+
+#first concatenate all induced and uninduced samples per protein
+${INPUT_DIR}/${OUTDIR_NAME}/${OUTDIR_NAME}.vcf.gz
 
 gunzip ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}.vcf.gz
 #Extract bases C converting. to T or G base
-grep -P 'C\tG' your_vcf_file.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}_filtered.vcf
+grep -P 'C\tG' ${INPUT_DIR}/${OUTDIR_NAME}/${OUTDIR_NAME}.vcf.gz >> ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}_filtered.vcf
 grep -P 'C\tT' your_vcf_file.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}_filtered.vcf
 grep -P 'C\tT/G' your_vcf_file.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}_filtered.vcf
 grep -P 'C\tG/T' your_vcf_file.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}_filtered.vcf
