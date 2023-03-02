16ac93734b936dba239b8ae8889e0023b263485c
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-02 09:55:12 -0600

    Update 03_select_variants.sh

--- 20-1LICH-001/03_select_variants.sh
+++ 20-1LICH-001/03_select_variants.sh
@@ -15,21 +15,20 @@ module load bedtools
 
 INPUT_DIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis"
 OUTPUT_DIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/filtered_vcfs'
-
+GENOME=
 mkdir -p ${OUTPUT_DIR}
 CLONE_ID=$1
-OUTDIR_NAME=$2
+COND=$2
+ 
 
-#first concatenate all induced and uninduced samples per protein
-${INPUT_DIR}/${OUTDIR_NAME}/${OUTDIR_NAME}.vcf.gz
+gunzip ${INPUT_DIR}/${OUTDIR_NAME}/${CLONE_ID}_${COND}_concat.vcf.gz
 
-gunzip ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}.vcf.gz
 #Extract bases C converting. to T or G base
-grep -P 'C\tG' ${INPUT_DIR}/${OUTDIR_NAME}/${OUTDIR_NAME}.vcf.gz >> ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}_filtered.vcf
-grep -P 'C\tT' your_vcf_file.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}_filtered.vcf
-grep -P 'C\tT/G' your_vcf_file.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}_filtered.vcf
-grep -P 'C\tG/T' your_vcf_file.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}_filtered.vcf
+grep -P 'C\tG' $${INPUT_DIR}/${OUTDIR_NAME}/${CLONE_ID}_${COND}_concat.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.vcf
+grep -P 'C\tT' ${INPUT_DIR}/${OUTDIR_NAME}/${CLONE_ID}_${COND}_concat.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.vcf
+grep -P 'C\tT/G' ${INPUT_DIR}/${OUTDIR_NAME}/${CLONE_ID}_${COND}_concat.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.vcf
+grep -P 'C\tG/T' ${INPUT_DIR}/${OUTDIR_NAME}/${CLONE_ID}_${COND}_concat.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.vcf
 
 
-bedtools flank -i ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}_filtered.vcf -g ${GENOME} -b 2
+bedtools flank -i ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.vcf -g ${GENOME} -b 2
 
