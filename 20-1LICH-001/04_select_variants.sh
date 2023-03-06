2e408c27ea196c3ef6efc5493473a098fd667f9e
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-06 15:14:04 -0600

    Update 04_select_variants.sh

--- 20-1LICH-001/04_select_variants.sh
+++ 20-1LICH-001/04_select_variants.sh
@@ -21,84 +21,19 @@ mkdir -p ${OUTPUT_DIR}
 CLONE_ID=$1
 COND=$2
  
-
 gunzip -k ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.vcf.gz
-
-#Extract bases C converting. to T or G base
-grep -P 'C\tG' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.vcf
-grep -P 'C\tT' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.vcf
-grep -P 'C\tTG' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.vcf
-grep -P 'C\tGT' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.vcf >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.vcf
-
-awk -v OFS='\t' '{print $5, $1}'
 
 #step1: Convert vcf to bed file
-/globalhome/hxo752/HPC/tools/bedops/convert2bed -i vcf < MCF7_A3H_I_concat.vcf -d >  MCF7_A3H_I_concat.bed
+/globalhome/hxo752/HPC/tools/bedops/convert2bed -i vcf < ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.vcf -d >  ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed
 
-#step2: Extract C to T/G conversions and select first 3 columns (chrom, start, end position)
-grep -P '\tC\tT' MCF7_A3H_I_concat.bed | awk -v OFS='\t' '{print $1,$2,$3}' > c_to_t.bed
+##step2: Extract C to T or G conversions and select first 3 columns (chrom, start, end position)
+grep -P '\tC\tG' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | awk -v OFS='\t' '{print $1,$2,$3}' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.bed
+grep -P '\tC\tT' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | awk -v OFS='\t' '{print $1,$2,$3}' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.bed
+grep -P '\tC\tTG' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | awk -v OFS='\t' '{print $1,$2,$3}' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.bed
+grep -P '\tC\tGT' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | awk -v OFS='\t' '{print $1,$2,$3}' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.bed
 
-#step3:
-bedtools flank -i c_to_t.bed -g /datastore/NGSF001/analysis/references/human/gencode-30/chrom.sizes -b 2 > c_to_t_flanked_up_down_2bp.bed
+#step3: select 2 bases upstream and downstream of bases in step2
+bedtools flank -i ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered.bed -g /datastore/NGSF001/analysis/references/human/gencode-30/chrom.sizes -b 2 > ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered_2bpflanked.bed
 
-#step4
-bedtools getfasta -fi /datastore/NGSF001/analysis/references/human/gencode-30/GRCh38.primary_assembly.genome.fa -bed c_to_t_flanked_up_down_2bp.bed -fo c_to_t_flanked_up_down_2bp.fasta
-
-
-
-#bedtools flank -i c_to_t_flanked.vcf -g /datastore/NGSF001/analysis/references/human/gencode-30/chrom.sizes -b 2 > c_to_t_flanked_2bp.vcf
-
-
-
-
-
-
-
-
-
-
-gunzip -k ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.vcf.gz
-#bedtools flank -i MCF7_A3B_U_concat.vcf -g chrom.sizes -b 2 > test.vcf
-
-bedtools intersect -a MCF7_A3H_I_concat.vcf -b MCF7_A3H_I_concat.vcf -wa -wb | awk '$4=="C" && $5=="T"' > c_to_t_flanked.vcf
-
-#bedtools flank -i c_to_t_flanked.vcf -g /datastore/NGSF001/analysis/references/human/gencode-30/chrom.sizes -b 2 > c_to_t_flanked_2bp.vcf
-
-
-
-First, you'll need to convert your VCF file to a BED file using the vcf2bed utility from bedtools:
-lua
-Copy code
-vcf2bed < input.vcf > output.bed
-This will create a BED file containing the genomic coordinates of each variant in the VCF file.
-
-Next, you can use the bedtools flank command to add flanking regions to each variant in the BED file:
-css
-Copy code
-bedtools flank -i output.bed -g genome_file.txt -l 1 -r 1 > output_flanked.bed
-Here's a breakdown of the options used:
-
--i output.bed specifies the input BED file
--g genome_file.txt specifies a file containing the sizes of each chromosome in the genome
--l 1 -r 1 specifies that we want to add 1 base of flanking sequence to each side of the variant.
-This will create a new BED file called output_flanked.bed, which contains the original genomic coordinates of each variant, as well as one base of flanking sequence on either side.
-
-Finally, you can use the bedtools intersect command to find all C to T conversions and their flanking bases. This command will intersect the flanked BED file with itself, keeping only variants where the reference allele is 'C' and the alternate allele is 'T':
-swift
-Copy code
-bedtools intersect -a output_flanked.bed -b output_flanked.bed -wa -wb | awk '$5=="C" && $10=="T"' > c_to_t_flanked.bed
-Here's a breakdown of the options used:
-
--a output_flanked.bed -b output_flanked.bed specifies that we want to intersect the flanked BED file with itself
--wa -wb specifies that we want to output both the A and B entries (i.e. the two intersecting intervals) for each overlap
--f 1 -r specifies that we want to require a complete overlap between the two intervals (i.e. the entire variant) and that the overlap must be on the same strand
--v -e -i specifies that we want to exclude variants that don't overlap with themselves (i.e. no single-base variants) and that we want to ignore any overlaps within the same interval (i.e. ignore self-overlaps)
-awk '$5=="C" && $10=="T"' filters the output to only include variants where the reference allele is 'C' and the alternate allele is 'T', and saves the output to a new BED file called c_to_t_flanked.bed.
-The resulting c_to_t_flanked.bed file will contain the genomic coordinates of all C to T conversions in the original VCF file, as well as one base of flanking sequence on either side.
-
-
-
-
-
-
-
+#step4: extract flanked bases for file in step3
+bedtools getfasta -fi ${GENOME} -bed ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered_2bpflanked.bed -fo ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_filtered_2bpflanked.fasta
