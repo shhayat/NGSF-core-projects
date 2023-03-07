a5f636eab8ff73c20c4f58aba2ed0f355d81866b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-07 11:21:38 -0600

    Update trim_adaptors.sh

--- 23-1MILE-001/trim_adaptors.sh
+++ 23-1MILE-001/trim_adaptors.sh
@@ -3,18 +3,19 @@
 #SBATCH --account=hpc_p_anderson
 #SBATCH --constraint=skylake
 #SBATCH --job-name=fastp
-#SBATCH --ntasks=6
+#SBATCH --ntasks=1
 #BATCH --cpus-per-task=4
 #SBATCH --time=02:00:00
 #SBATCH --mem=6G
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 set -eux
 
-module load fastqc
+module load fastp
+
 DATA=/datastore/NGSF001/projects/23-1MILE-001/Analysis/Fastq
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-001/analysis
 
-mkdir -p ${OUTDIR}/fastqc
+mkdir -p ${OUTDIR}/fastq_trimmed
 
 for fq in $DATA/R23*.fastq.gz
 do
