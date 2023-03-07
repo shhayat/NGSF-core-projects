29cabc9a49d9e8e08e3f33284128dcbeae76c521
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-07 12:31:17 -0600

    Update sbatch_run_star.sh

--- 23-1MILE-001/sbatch_run_star.sh
+++ 23-1MILE-001/sbatch_run_star.sh
@@ -1,14 +1,15 @@
-DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-001/analysis/Fastq
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-001/analysis/fastq_trimmed
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-001
 
 
-for i in $DATA/R*_R1_*.fastq.gz
+for i in $DATA/R23*_L001_R1_001.fastq.gz
 do
         path="${i%_R1*}";
         sample_name=${path##*/};
-        fq1=${DATA}/${sample_name}_R1.fastq.gz;
-	      fq2=${DATA}/${sample_name}_R2.fastq.gz;
-  
-      sbatch ${SCRIPT_DIR}/04_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}"
+   
+        fq1=${DATA}/${sample_name}_R1_001.fastq.gz
+        fq2=${DATA}/${sample_name}_R2_001.fastq.gz
+
+sbatch ${SCRIPT_DIR}/04_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}"
  sleep 0.5
-done
+done	      
