b22236eb560ba1a659d18ffa548bf93d3c7925ad
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-08 11:37:50 -0600

    Update sbatch_remove_rnrna_and_dedupUMI.sh

--- 23-1MILE-001/sbatch_remove_rnrna_and_dedupUMI.sh
+++ 23-1MILE-001/sbatch_remove_rnrna_and_dedupUMI.sh
@@ -1,8 +1,8 @@
 BAMDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-001/star_alignment
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-001
 
-for i in 098 099 100 101 102 103
+for i in ${BAMDIR}/
 do
    bam=star_Aligned.sortedByCoord.out.bam
-   sbatch 02_umitools_UMIdedup.sh "R2200${i}" ${bam}
+   sbatch 06_remove_rnrna_and_dedupUMI.sh "R2200${i}" ${bam}
 done
