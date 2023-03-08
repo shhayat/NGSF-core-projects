96277c089ce2018da248e919fd88ab5b598a2ea4
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-08 14:39:03 -0600

    Update sbatch_run_rnaseqc.sh

--- 23-1MILE-001/sbatch_run_rnaseqc.sh
+++ 23-1MILE-001/sbatch_run_rnaseqc.sh
@@ -1,12 +1,14 @@
 
-DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1JOHO-001/analysis/umi_deduplication
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-001/analysis/fastq_trimmed
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-001
 
 
-for i in ${DATA}//*.bam
+
+for i in ${DATA}/R23*_L001_R1_001.fastq.gz
 do
-        path="${i%/Aligned*}";
-	      sample_name="${path##*/}"
+        path="${i%_L001*}";
+        sample_name=${path##*/};
+	bam=
       
   sbatch ${SCRIPT_DIR}/07_RNASeQC.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
  sleep 0.5
