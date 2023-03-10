9489d7b36fee472ac596f56cd8f604e860b1d2f8
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-10 12:20:07 -0600

    Update PON.sh

--- 20-1LICH-001/PON.sh
+++ 20-1LICH-001/PON.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --account=hpc_p_anderson
 #SBATCH --constraint=skylake
-#SBATCH --job-name=gatk-mutect2
+#SBATCH --job-name=gatk-pon
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=2:00:00
@@ -13,9 +13,18 @@ set -eux
 #loading required modules
 module load gatk/4.2.2.0
 
+INPUT_DIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis'
+OUTDIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/panel_of_normal'
+
+mkdir -p ${OUTDIR}
+
+CLONE_ID=$1
+SAMPLE1=$2
+SAMPLE2=$3
+COND=$4 #INDUCED OR UNINDUCED
+
 
 gatk CreateSomaticPanelOfNormals \
-   -vcfs normal1_for_pon_vcf.gz \
-   -vcfs normal2_for_pon_vcf.gz \
-   -vcfs normal3_for_pon_vcf.gz \
-   -O pon.vcf.gz
+   -vcfs ${OUTDIR}/${CLONE_ID}_${COND}_concat.vcf.gz \
+   -vcfs ${OUTDIR}/${CLONE_ID}_${COND}_concat.vcf.gz
+   -O ${CLONE_ID}_${COND}_pon.vcf.gz
