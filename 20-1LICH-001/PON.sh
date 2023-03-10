48f13a9ae0ca12448a4f65b7560f31a52f1c5205
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-10 11:59:38 -0600

    Update PON.sh

--- 20-1LICH-001/PON.sh
+++ 20-1LICH-001/PON.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
 
- gatk CreateSomaticPanelOfNormals \
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=gatk-mutect2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=2:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+#loading required modules
+module load gatk/4.2.2.0
+
+
+gatk CreateSomaticPanelOfNormals \
    -vcfs normal1_for_pon_vcf.gz \
    -vcfs normal2_for_pon_vcf.gz \
    -vcfs normal3_for_pon_vcf.gz \
