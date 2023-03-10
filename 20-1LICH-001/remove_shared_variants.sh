b973fc872ac3d89f4558ed51e7e8f3e9c18dc8b5
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-10 14:52:46 -0600

    Update remove_shared_variants.sh

--- 20-1LICH-001/remove_shared_variants.sh
+++ 20-1LICH-001/remove_shared_variants.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=00:30:00
 #SBATCH --mem=2G
-#SBATCH  --output=%j.out
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
 set -eux
 
@@ -16,20 +16,20 @@ module load StdEnv/2020
 module load gcc/9.3.0
 module load bcftools/1.13
 
-INPUT_DIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis"
-CONCATE_UNINDUCED="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/concatenated_uninduced_samples"
-mkdir -p ${INPUT_DIR}/filtered_vcfs
-OUTPUT_DIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/filtered_vcfs'
-CLONE_ID=$1
-INDUCED_SAMPLE=$2
+INPUT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/removed_shared_varaints_between_uninduced_induced
 
+CLONE_ID=$1
+COND=$2
+
+mkdir -p ${OUTDIR}
 
 bcftools isec -C \
               -c none \
               -O z \
               -w 1 \
-              -o ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}.vcf.gz \
-              ${INPUT_DIR}/${INDUCED_SAMPLE}/${INDUCED_SAMPLE}.vcf.gz \
-              ${CONCATE_UNINDUCED}/${CLONE_ID}_uninduced_concat.vcf.gz
+              -o ${OUTDIR}${CLONE_ID}.vcf.gz \
+              ${INPUT_DIR}${PREP1}_${PREP2}.vcf.gz \
+              ${INPUT_DIR}${CLONE_ID}_uninduced_concat.vcf.gz
 
-bcftools index -t ${OUTPUT_DIR}/${CLONE_ID}_${INDUCED_SAMPLE}.vcf.gz
+bcftools index -t ${OUTPUT_DIR}${CLONE_ID}_${PREP1}_${PREP2}.vcf.gz
