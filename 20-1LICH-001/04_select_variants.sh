a62f47353a467e3c289af19e448a52372ed63ba1
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-12 16:58:44 -0600

    Update 04_select_variants.sh

--- 20-1LICH-001/04_select_variants.sh
+++ 20-1LICH-001/04_select_variants.sh
@@ -20,8 +20,9 @@ module load intel/2017.1
 module load bedtools
 
 INPUT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/concatenated_samples_per_protein/removed_shared_varaints_between_uninduced_induced
+OUTPUT_DIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/filtered_v1'
 #INPUT_DIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/concatenated_samples_per_protein"
-OUTPUT_DIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/filtered'
+#OUTPUT_DIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/filtered'
 GENOME='/datastore/NGSF001/analysis/references/human/gencode-30/GRCh38.primary_assembly.genome.fa'
 mkdir -p ${OUTPUT_DIR}
 
@@ -32,21 +33,34 @@ COND=$2
 echo "convert vcf to bed file"
 #step1: Convert vcf to bed file
 #/globalhome/hxo752/HPC/tools/bedops/convert2bed -i vcf < ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.vcf -d >  ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed
-
+/globalhome/hxo752/HPC/tools/bedops/convert2bed -i vcf < ${INPUT_DIR}/${CLONE_ID}_${COND}.vcf.gz -d >  ${INPUT_DIR}/${CLONE_ID}_${COND}.bed
 echo "Base Conversions"
 ##step2: Extract C to T or G conversions and select first 3 columns (chrom, start, end position, ref allele and alternate allele)
-awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | grep -P '\tC\tG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed
-awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | grep -P '\tC\tT$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed
+#awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | grep -P '\tC\tG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed
+#awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | grep -P '\tC\tT$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed
 #only select CC as REF
-awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | grep -P '\tCC\tTG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed
-awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | grep -P '\tCC\tGT$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed
+#awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | grep -P '\tCC\tTG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed
+#awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | grep -P '\tCC\tGT$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed
 #only select CCC as REF
-awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | grep -P '\tCCC\tGTG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed
-awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | grep -P '\tCCC\tGGG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed
+#awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | grep -P '\tCCC\tGTG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed
+#awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}_concat.bed | grep -P '\tCCC\tGGG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed
+
+
+##step2: Extract C to T or G conversions and select first 3 columns (chrom, start, end position, ref allele and alternate allele)
+awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}.bed | grep -P '\tC\tG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_base_conversion.bed
+awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}.bed | grep -P '\tC\tT$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_base_conversion.bed
+#only select CC as REF
+awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}.bed | grep -P '\tCC\tTG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_base_conversion.bed
+awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}.bed | grep -P '\tCC\tGT$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_base_conversion.bed
+#only select CCC as REF
+awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}.bed | grep -P '\tCCC\tGTG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_base_conversion.bed
+awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}.bed | grep -P '\tCCC\tGGG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_base_conversion.bed
+
 
 echo "select 2 bases upstream and downstream of bases"
 #step3: select 2 bases upstream and downstream of bases in step2
-bedtools flank -i ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed -g /datastore/NGSF001/analysis/references/human/gencode-30/chrom.sizes -b 2 > ${OUTPUT_DIR}/${CLONE_ID}_${COND}_flanked_2bp_upstream_downstream.bed
+#bedtools flank -i ${OUTPUT_DIR}/${CLONE_ID}_${COND}_concat_base_conversion.bed -g /datastore/NGSF001/analysis/references/human/gencode-30/chrom.sizes -b 2 > ${OUTPUT_DIR}/${CLONE_ID}_${COND}_flanked_2bp_upstream_downstream.bed
+bedtools flank -i ${OUTPUT_DIR}/${CLONE_ID}_${COND}_base_conversion.bed -g /datastore/NGSF001/analysis/references/human/gencode-30/chrom.sizes -b 2 > ${OUTPUT_DIR}/${CLONE_ID}_${COND}_flanked_2bp_upstream_downstream.bed
 
 echo "extract flanked bases from fasta file"
 #step4: extract flanked bases for file in step3. tab delimited bed file is produced which is written as text file
