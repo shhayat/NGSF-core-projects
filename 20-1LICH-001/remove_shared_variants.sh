2bc976ea48cbef1a6b73669779c8081f7419a520
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-13 08:05:19 -0600

    Update remove_shared_variants.sh

--- 20-1LICH-001/remove_shared_variants.sh
+++ 20-1LICH-001/remove_shared_variants.sh
@@ -16,7 +16,7 @@ module load StdEnv/2020
 module load gcc/9.3.0
 module load bcftools/1.13
 
-INPUT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis
+INPUT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/concatenated_samples_per_protein
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/removed_shared_varaints_between_uninduced_induced
 mkdir -p ${OUTDIR}
 
