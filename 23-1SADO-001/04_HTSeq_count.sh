6d33ad7fca980c1e2f4bd40d0671367c16fd7cbc
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-15 12:58:37 -0600

    Update 04_HTSeq_count.sh

--- 23-1SADO-001/04_HTSeq_count.sh
+++ 23-1SADO-001/04_HTSeq_count.sh
@@ -1,1 +1,6 @@
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SADO-001/analysis/star_alignment
+
+
+
+for bam in $DATA/*/
+/globalhome/hxo752/HPC/anaconda3/envs/htseq/bin/htseq-count -f ${BAM}
