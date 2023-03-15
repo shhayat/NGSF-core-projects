c04bcd85b268a36489b00106ef1ff9d50cfabd42
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-15 12:14:43 -0600

    Update sbatch_run_star.sh

--- 22-1BETO-001B/sbatch_run_star.sh
+++ 22-1BETO-001B/sbatch_run_star.sh
@@ -1,9 +1,9 @@
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001B/analysis/Fastq
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001B
 
-for i in $DATA/R2*.fastq.gz
+for i in $DATA/R2*_R1*.fastq.gz
 do
-        path="${i%_R*}";
+        path="${i%_R1*}";
         sample_name=${path##*/};
         fq1=${DATA}/${sample_name}_R1.fastq.gz;
 	      fq2=${DATA}/${sample_name}_R2.fastq.gz;
