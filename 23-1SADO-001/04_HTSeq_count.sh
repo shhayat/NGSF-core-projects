66cb552cd760f9a4ffe87da7303fc17fbfe3a011
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-15 13:19:08 -0600

    Update 04_HTSeq_count.sh

--- 23-1SADO-001/04_HTSeq_count.sh
+++ 23-1SADO-001/04_HTSeq_count.sh
@@ -2,5 +2,8 @@ DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SADO-001/analy
 
 
 
-for bam in $DATA/*/
-/globalhome/hxo752/HPC/anaconda3/envs/htseq/bin/htseq-count -f ${BAM}
+for BAM in $DATA/*/Aligned.sortedByCoord.out.bam
+/globalhome/hxo752/HPC/anaconda3/envs/htseq/bin/htseq-count -f ${BAM} \
+                                                            -s yes \
+                                                            -t exon \
+                                                            -
