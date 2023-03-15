62d81f35bf0f2d71f98ef9d836fdeb791ffa1390
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-15 11:57:03 -0600

    Update 01_FastQC.sh

--- 23-1SADO-001/01_FastQC.sh
+++ 23-1SADO-001/01_FastQC.sh
@@ -1,1 +1,28 @@
-/datastore/NGSF001/NB551711/230314_NB551711_0067_AH2VMJBGXM/Alignment_1/20230315_045627/Fastq
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=fastqc
+#SBATCH --ntasks=6
+#BATCH --cpus-per-task=4
+#SBATCH --time=02:00:00
+#SBATCH --mem=6G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+module load fastqc
+DATA=/datastore/NGSF001/NB551711/230314_NB551711_0067_AH2VMJBGXM/Alignment_1/20230315_045627/Fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SADO-001/analysis
+
+mkdir -p ${OUTDIR}/fastqc
+
+for fq in $DATA/R23*.fastq.gz
+do
+   fastqc -o ${OUTDIR}/fastqc --extract ${fq}
+   
+done 
+
+wait 
+
+cd /datastore/NGSF001/software/tools/
+./multiqc ${OUTDIR}/fastqc/*_fastqc.zip -o ${OUTDIR}/fastqc
