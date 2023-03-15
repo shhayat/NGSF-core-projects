ae0107b78b7cf6c2c0ff5fecb7173a6a29944795
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-15 12:19:16 -0600

    Update sbatch_run_star.sh

--- 23-1SADO-001/sbatch_run_star.sh
+++ 23-1SADO-001/sbatch_run_star.sh
@@ -1,9 +1,10 @@
 DATA=/datastore/NGSF001/NB551711/230314_NB551711_0067_AH2VMJBGXM/Alignment_1/20230315_045627/Fastq
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SADO-001
 
-for i in $DATA/R2*_R1*.fastq.gz
+
+for i in $DATA/R*_R1*.fastq.gz
 do
-        path="${i%_R*}";
+         path="${i%_R*}";
         sample_name=${path##*/};
         fq1=${DATA}/${sample_name}_R1.fastq.gz;
 	      fq2=${DATA}/${sample_name}_R2.fastq.gz;
