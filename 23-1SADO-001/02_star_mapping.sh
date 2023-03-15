7285f577b33c49309180667f5d0f0bf263190738
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-15 12:06:05 -0600

    Create 02_star_mapping.sh

---
+++ 23-1SADO-001/02_star_mapping.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#SBATCH --job-name=star-align
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=1:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+#loading required modules
+module load star/2.7.9a 
+module load samtools
+
+
+DATA=/datastore/NGSF001/NB551711/230314_NB551711_0067_AH2VMJBGXM/Alignment_1/20230315_045627/Fastq
+GENOME=/datastore/NGSF001/analysis/indices/human/gencode-40
+GTF=/datastore/NGSF001/analysis/references/human/gencode-40/gencode.v40.annotation.gtf
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001B/analysis/star_alignment
+NCPU=4
+
+mkdir -p ${OUTDIR}
+sample_name=$1; shift
+fq1=$1; shift
+fq2=$1
+
+rsync -v $fq1 ${SLURM_TMPDIR}
+rsync -v $fq2 ${SLURM_TMPDIR}
+
+mkdir -p ${SLURM_TMPDIR}/${sample_name} && cd ${SLURM_TMPDIR}/${sample_name}
+
+STAR --genomeDir $GENOME \
+	--readFilesCommand zcat \
+	--readFilesIn ${fq1} ${fq2} \
+	--outSAMtype BAM SortedByCoordinate \
+	--sjdbGTFfile ${GTF} \
+	--runThreadN ${NCPU} \
+	&& samtools index Aligned.sortedByCoord.out.bam 
+	
+	
+rsync -rvzP ${SLURM_TMPDIR}/${sample_name} ${OUTDIR}
