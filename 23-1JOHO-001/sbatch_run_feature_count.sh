8cc70b2952969d6047dbfd60834d6e01afa9c069
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-15 11:59:07 -0600

    Update sbatch_run_feature_count.sh

--- 23-1JOHO-001/sbatch_run_feature_count.sh
+++ 23-1JOHO-001/sbatch_run_feature_count.sh
@@ -12,4 +12,4 @@ SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1JOHO-001
 
 module load r/4.1.2
 
-R CMD BATCH ${SCRIPT_DIR}/06_feature_count.R
+R CMD BATCH ${SCRIPT_DIR}/05_feature_count.R
