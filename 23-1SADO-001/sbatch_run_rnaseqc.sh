d81040a492881f6fe555f5fac34323f9e7367db5
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-15 12:08:56 -0600

    Create sbatch_run_rnaseqc.sh

---
+++ 23-1SADO-001/sbatch_run_rnaseqc.sh
@@ -0,0 +1,12 @@
+
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SADO-001/analysis/star_alignment
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001B
+
+for i in ${DATA}/*/*.bam
+do
+        path="${i%/Aligned*}";
+	sample_name="${path##*/}"
+      
+  sbatch ${SCRIPT_DIR}/03_RNASeQC.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
+ sleep 0.5
+done 
