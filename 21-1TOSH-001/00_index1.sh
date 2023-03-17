ed6c210c27fc1994ac73ccdd3b404c5e39bac6f9
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-17 11:26:42 -0600

    Create 00_index1.sh

---
+++ 21-1TOSH-001/00_index1.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --job-name=genome_index
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=03:00:00
+#SBATCH --mem=80G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+
+module load star/2.7.9a 
+
+
+GENOME=/datastore/NGSF001/analysis/references/bison/ftp.ensembl.org/pub/release-109/fasta
+GTF=/datastore/NGSF001/analysis/references/bison/ftp.ensembl.org/pub/release-109/gtf/Bison_bison_bison.Bison_UMD1.0.109.gtf
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/analysis/indices
+
+NCPU=8
+
+mkdir -p $OUTDIR
+cd ${OUTDIR}
+
+STAR --runThreadN ${NCPU} \
+     --runMode genomeGenerate \
+     --genomeDir star-index \
+     --genomeFastaFiles ${GENOME}/Bison_bison_bison.Bison_UMD1.0.dna.toplevel.fa \
+     --sjdbGTFfile ${GTF} \
+     --sjdbOverhang 99
