f0a7efc1127f6c9c19b0921645b50843944a9555
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-17 20:28:18 -0600

    Update 00_index1.sh

--- 21-1TOSH-001/00_index1.sh
+++ 21-1TOSH-001/00_index1.sh
@@ -3,8 +3,8 @@
 #SBATCH --account=hpc_p_anderson
 #SBATCH --job-name=genome_index
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --time=03:00:00
+#SBATCH --cpus-per-task=10
+#SBATCH --time=10:00:00
 #SBATCH --mem=80G
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
@@ -15,7 +15,7 @@ GENOME=/datastore/NGSF001/analysis/references/bison/ftp.ensembl.org/pub/release-
 GTF=/datastore/NGSF001/analysis/references/bison/ftp.ensembl.org/pub/release-109/gtf/Bison_bison_bison.Bison_UMD1.0.109.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/analysis/indices
 
-NCPU=8
+NCPU=10
 
 mkdir -p $OUTDIR
 cd ${OUTDIR}
@@ -23,6 +23,7 @@ cd ${OUTDIR}
 STAR --runThreadN ${NCPU} \
      --runMode genomeGenerate \
      --genomeDir star-index \
+     --limitGenomeGenerateRAM 791032727477 \
      --genomeFastaFiles ${GENOME}/Bison_bison_bison.Bison_UMD1.0.dna.toplevel.fa \
      --sjdbGTFfile ${GTF} \
      --sjdbOverhang 99
