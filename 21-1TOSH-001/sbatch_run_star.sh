82da186877d7cbdf68454744d2001eaef8bc717b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-20 13:49:51 -0600

    Update sbatch_run_star.sh

--- 21-1TOSH-001/sbatch_run_star.sh
+++ 21-1TOSH-001/sbatch_run_star.sh
@@ -1,14 +1,12 @@
 DATA=/datastore/NGSF001/projects/21-1TOSH-001/new_analysis/Trimming
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001
 
-
+for i in $(seq -w 42 52);
 #for i in $DATA/*/R20*R1*.fastq.gz
 do
-        path="${i%_R*}";
-        sample_name=${path##*/};
-        fq1=${DATA}/${sample_name}_R1_trimmed.fastq.gz;  
-	      fq2=${DATA}/${sample_name}_R2_trimmed.fastq.gz;  
+        fq1=${DATA}/R23000{i}_R1_trimmed.fastq.gz;  
+	fq2=${DATA}/R23000{i}_R2_trimmed.fastq.gz;  
   
-      sbatch ${SCRIPT_DIR}/01_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}"
+      sbatch ${SCRIPT_DIR}/01_star_mapping.sh R23000{i} "${fq1}" "${fq2}"
  sleep 0.5
 done
