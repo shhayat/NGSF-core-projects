e3edeffa1c0d6bd740d35ca3223a5b90a23c9984
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-20 16:10:45 -0600

    Update 01_star_mapping.sh

--- 21-1TOSH-001/01_star_mapping.sh
+++ 21-1TOSH-001/01_star_mapping.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=star-align
 #SBATCH --constraint=skylake
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=4
 #SBATCH --time=1:25:00
 #SBATCH --mem=80G
 #SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
