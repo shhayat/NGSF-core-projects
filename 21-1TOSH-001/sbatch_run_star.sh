2be7538b6c3dc7490468661fae7c3109d4950408
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-20 14:42:05 -0600

    Update sbatch_run_star.sh

--- 21-1TOSH-001/sbatch_run_star.sh
+++ 21-1TOSH-001/sbatch_run_star.sh
@@ -3,8 +3,8 @@ SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001
 
 for i in $(seq -w 42 52);
 do
-        fq1="${DATA}/R23000${i}_R1_trimmed.fastq.gz";  
-	fq2="${DATA}/R23000${i}_R2_trimmed.fastq.gz";  
+        fq1="${DATA}/R23000${i}/R23000${i}_R1_trimmed.fastq.gz";  
+	fq2="${DATA}/R23000${i}/R23000${i}_R2_trimmed.fastq.gz";  
   
       sbatch ${SCRIPT_DIR}/01_star_mapping.sh R23000${i} ${fq1} ${fq2}
  sleep 0.5
