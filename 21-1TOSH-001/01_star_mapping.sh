cc59686768174ad0c23c497012e03670b161f984
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-20 12:48:29 -0600

    Update 01_star_mapping.sh

--- 21-1TOSH-001/01_star_mapping.sh
+++ 21-1TOSH-001/01_star_mapping.sh
@@ -16,31 +16,19 @@ DATA=/datastore/NGSF001/projects/21-1TOSH-001/new_analysis/Trimming
 GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/indices/star-index
 OUTDATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/Alignment_v109
 
-sample_name=$1;
-fq1=$1;
+sample_name=$1; shift
+fq1=$1; shift
 fq2=$1;
+
+cd ${OUTDATA}/${sample_name}
 
 STAR --genomeDir $GENOME \
 	--readFilesCommand zcat \
-	--readFilesIn $fq1 $fq2 \
+	--readFilesIn ${fq1} ${fq2} \
 	--outSAMstrandField intronMotif \
-	--outFileNamePrefix ${OUTDATA}/${sample_name}/star_ \
 	--outSAMtype BAM SortedByCoordinate \
 	--outFilterIntronMotifs RemoveNoncanonical \
 	--sjdbGTFfeatureExon exon \
 	--runThreadN 4 \
-	&& samtools index $OUTDATA/R2200001_star/star_Aligned.sortedByCoord.out.bam 
+	&& samtools index Aligned.sortedByCoord.out.bam
 	
-	#STAR --genomeDir $GENOME \
-	#--readFilesCommand zcat \
-	#--readFilesIn $fq1 $fq2 \
-	#--sjdbGTFfile $GFF \
-	#--sjdbGTFtagExonParentTranscript Parent \
-	#--sjdbGTFtagExonParentGene ID \
-	#--outSAMstrandField intronMotif \
-	#--outFileNamePrefix $OUTDATA/R2200001_star/star_ \
-	#--outSAMtype BAM SortedByCoordinate \
-	#--outFilterIntronMotifs RemoveNoncanonical \
-	#--runThreadN 4 \
-	#&& samtools index $OUTDATA/R2200001_star/star_Aligned.sortedByCoord.out.bam 
-#done
