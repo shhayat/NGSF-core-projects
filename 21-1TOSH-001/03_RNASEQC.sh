ed40d371d822524dc8e9e32fa67658bc4ee69c92
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-20 13:30:09 -0600

    Update 03_RNASEQC.sh

--- 21-1TOSH-001/03_RNASEQC.sh
+++ 21-1TOSH-001/03_RNASEQC.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH --time=00:20:00
-#SBATCH --mem=10G
+#SBATCH --mem=5G
 
 set -eux
 
-# module load gcc/9.3.0
-# module load samtools/1.13
-
-# move binary to tempdir and give execute permission
+# copy tool to tempdir and give execute permission
 cp /datastore/NGSF001/software/src/rnaseqc.v2.4.2.linux ${SLURM_TMPDIR}
 chmod u+x ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux
 
-GTF=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/bison.liftoff.chromosomes.gtf
-OUTDATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/STAR_alignment
+sample_name=$1; shift
+bam_file=$1
+
+
+#GTF file needs to be modified for running RNASeQC
+#run following python code on GTF file
+#python collaspe_annotation.py /datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf genes_mod.gtf
+
+GTF=/datastore/NGSF001/analysis/references/human/gencode-40/gencode.v40.annotation_mod.gtf
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SADO-001/analysis/rnaseqc
+
+mkdir -p ${OUTDIR}
 
 ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux ${GTF} \
-                        ${OUTDATA}/R2200001_star/star_Aligned.sortedByCoord.out.bam \
-                        --sample="R2200001" \
-                        ${OUTDATA}
+                        ${bam_file} \
+                        ${OUTDIR} \
+                        --sample=${sample_name}
