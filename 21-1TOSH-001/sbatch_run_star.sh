44e31350d9eee068162f0670f355b0b1f071a582
Shahina Hayat (hxo752) <hxo752@usask.ca>
2023-03-21 09:28:27 -0600

    modified:   sbatch_run_star.sh
    	modified:   sbatch_run_star.sh

--- 21-1TOSH-001/sbatch_run_star.sh
+++ 21-1TOSH-001/sbatch_run_star.sh
@@ -1,7 +1,8 @@
 DATA=/datastore/NGSF001/projects/21-1TOSH-001/new_analysis/Trimming
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001
 
-for i in $(seq -w 42 52);
+#for i in $(seq -w 42 52);
+for i in 42
 do
         fq1="${DATA}/R23000${i}/R23000${i}_R1_trimmed.fastq.gz";  
 	fq2="${DATA}/R23000${i}/R23000${i}_R2_trimmed.fastq.gz";  
