e0d5c6497cc42eb5b2a4c6a5ee5d7a57fa61b14a
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-22 14:38:20 -0600

    Update sbatch_run_aggregate.sh

--- SingleCellSequencing/CellRanger_pipeline/sbatch_run_aggregate.sh
+++ SingleCellSequencing/CellRanger_pipeline/sbatch_run_aggregate.sh
@@ -1,6 +1,12 @@
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/analysis/count_files/
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/CellRanger_pipeline
 
 #generate one csv file for all samples 
-for i in ${DIR}/*/out/molecule_info.h5
+for i in $(seq -w 1 2);
 do
-  
+      echo "$R230000${i},${DIR}/out/molecule_info.h5" >> ${DIR}/sample_info.csv
+done
+printf '%b' 'sample_id,molecule_h5g\n' | cat - ${DIR}/sample_info.csv 
+
+
+sbatch ${SCRIPT_DIR}/02_cellranger_count.sh "${SAMPLE_NAME}"
