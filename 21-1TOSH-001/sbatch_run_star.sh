5c550923570f1ba996393da3f2d03cc953d96801
Shahina Hayat (hxo752) <hxo752@usask.ca>
2023-03-22 15:44:04 -0600

    pick 95ec857 deleted:    bison.liftoff.chromosomes.gff 	deleted:    gff_to_gtf_conversion.txt 	modified:   21-1TOSH-001/03_RNASEQC.sh 	modified:   21-1TOSH-001/sbatch_run_star.sh
    	modified:   ../../21-1TOSH-001/sbatch_run_star.sh

--- 21-1TOSH-001/sbatch_run_star.sh
+++ 21-1TOSH-001/sbatch_run_star.sh
@@ -2,6 +2,10 @@ DATA=/datastore/NGSF001/projects/21-1TOSH-001/new_analysis/Trimming
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001
 
 for i in $(seq -w 42 52);
+<<<<<<< HEAD
+=======
+#for i in 42
+>>>>>>> 0a058dc (    pick 95ec857 deleted:    bison.liftoff.chromosomes.gff 	deleted:    gff_to_gtf_conversion.txt)
 do
         fq1="${DATA}/R23000${i}/R23000${i}_R1_trimmed.fastq.gz";  
 	fq2="${DATA}/R23000${i}/R23000${i}_R2_trimmed.fastq.gz";  
