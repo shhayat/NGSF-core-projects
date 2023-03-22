458fb4b350b32019737f141b7372f5bb009bb19f
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-22 13:09:59 -0600

    Update 03_aggregate.sh

--- SingleCellSequencing/CellRanger_pipeline/03_aggregate.sh
+++ SingleCellSequencing/CellRanger_pipeline/03_aggregate.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=cellranger-count
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=24:00:00
+#SBATCH --mem=128G
+#SBATCH  --output=%j.out
+
+#source /globalhome/hxo752/HPC/cell_ranger_env/bin/activate
+
+export PATH=/globalhome/hxo752/HPC/tools/cellranger-7.1.0/bin:$PATH
+
 cellranger aggr --id=AGG123 \
                 --csv=AGG123_libraries.csv
