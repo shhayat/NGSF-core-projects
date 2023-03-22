5cda9cc682dc34907edbbe861324cdc9264d4bc3
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-22 11:58:22 -0600

    Update sbatch_run_cellranger_count.sh

--- SingleCellSequencing/CellRanger_pipeline/sbatch_run_cellranger_count.sh
+++ SingleCellSequencing/CellRanger_pipeline/sbatch_run_cellranger_count.sh
@@ -2,7 +2,7 @@
 DATA=/datastore/NGSF001/projects/23-1ANLE-001/Analysis/fastq
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/CellRanger_pipeline
 
-for i in $DATA/R2300001_S*_L001_R1*
+for i in $DATA/R23*_R1*
 do
         path="${i%_S*}";
         sample_name=${path##*/};
