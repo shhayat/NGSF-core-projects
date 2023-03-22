dd781f2dd4238264b1ea84d29e689af8e2270637
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-22 11:34:30 -0600

    Update sbatch_run_cellranger_count.sh

--- SingleCellSequencing/CellRanger_pipeline/sbatch_run_cellranger_count.sh
+++ SingleCellSequencing/CellRanger_pipeline/sbatch_run_cellranger_count.sh
@@ -1,6 +1,6 @@
 #DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/analysis/Fastq
 DATA=/datastore/NGSF001/projects/23-1ANLE-001/Analysis/fastq
-SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/CellRanger_pipeline
 
 for i in $DATA/R2300001_S*_L001_R1*
 do
