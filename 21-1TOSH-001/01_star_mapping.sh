6ce4c8670a8ad01173764abe036cb371930bf1bd
Shahina Hayat (hxo752) <hxo752@usask.ca>
2023-03-22 10:44:05 -0600

    modified:   01_star_mapping.sh 	modified:   03_RNASEQC.sh 	modified:   sbatch_run_rnaseqc.sh

--- 21-1TOSH-001/01_star_mapping.sh
+++ 21-1TOSH-001/01_star_mapping.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=star-align
 #SBATCH --constraint=skylake
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=2:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --time=3:00:00
 #SBATCH --mem=375G
 #SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 set -eux
@@ -13,7 +13,6 @@ set -eux
 module load star/2.7.9a 
 module load samtools
 
-DATA=/datastore/NGSF001/projects/21-1TOSH-001/new_analysis/Trimming
 GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/analysis/indices/star-index
 OUTDATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/analysis/Alignment_v109
 
@@ -24,13 +23,28 @@ fq2=$1;
 mkdir -p ${OUTDATA}/${sample_name}
 cd ${OUTDATA}/${sample_name}
 
-STAR --genomeDir $GENOME \
-	--readFilesCommand zcat \
+#STAR --genomeDir $GENOME \
+#	--readFilesCommand zcat \
+#	--readFilesIn ${fq1} ${fq2} \
+#	--outSAMstrandField intronMotif \
+#	--outSAMtype BAM SortedByCoordinate \
+#	--outFilterIntronMotifs RemoveNoncanonical \
+#	--sjdbGTFfeatureExon exon \
+#	--runThreadN 4 \
+#	&& samtools index Aligned.sortedByCoord.out.bam
+
+STAR --runMode alignReads \
+	--runThreadN 8 \
+	--genomeDir $GENOME \
 	--readFilesIn ${fq1} ${fq2} \
-	--outSAMstrandField intronMotif \
+	--outFilterType BySJout \
+	--outFilterMultimapNmax 20 \
+	--alignSJoverhangMin 8 \
+	--alignSJDBoverhangMin 1 \
+	--outFilterMismatchNmax 999 \
+	--outFilterMismatchNoverReadLmax 0.04 \
+	--alignIntronMin 20 \
+	--alignIntronMax 1000000 \
+	--alignMatesGapMax 1000000 \
 	--outSAMtype BAM SortedByCoordinate \
-	--outFilterIntronMotifs RemoveNoncanonical \
-	--sjdbGTFfeatureExon exon \
-	--runThreadN 4 \
 	&& samtools index Aligned.sortedByCoord.out.bam
-	
--- 21-1TOSH-001/sbatch_run_rnaseqc.sh
+++ 21-1TOSH-001/sbatch_run_rnaseqc.sh
@@ -1,11 +1,11 @@
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001/analysis/Alignment_v109
-SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SADO-001
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001
 
 for i in ${DATA}/*/*.bam
 do
         path="${i%/Aligned*}";
 	sample_name="${path##*/}"
       
-  sbatch ${SCRIPT_DIR}/03_RNASeQC.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
+  sbatch ${SCRIPT_DIR}/03_RNASEQC.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
  sleep 0.5
 done 
