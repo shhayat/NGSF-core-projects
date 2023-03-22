2a512d287a33c0259f881503750202449e72272b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-22 15:43:50 -0600

    Update sbatch_run_aggregate.sh

--- SingleCellSequencing/CellRanger_pipeline/sbatch_run_aggregate.sh
+++ SingleCellSequencing/CellRanger_pipeline/sbatch_run_aggregate.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSe
 #generate one csv file for all samples 
 for i in $(seq -w 1 2);
 do
-      echo "R230000${i},${DIR}/out/molecule_info.h5" >> ${DIR}/sample_info.csv
+      echo "R230000${i},${DIR}/R230000${i}/out/molecule_info.h5" >> ${DIR}/sample_info.csv
 done
 #add header to csv file
 sed -i 1i'sample_id,molecule_h5' ${DIR}/sample_info.csv
