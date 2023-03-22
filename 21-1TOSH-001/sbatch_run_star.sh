0cb12fad14a831325a216acbabad2a3b76f34e37
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-22 12:33:38 -0600

    Update sbatch_run_star.sh

--- 21-1TOSH-001/sbatch_run_star.sh
+++ 21-1TOSH-001/sbatch_run_star.sh
@@ -1,8 +1,7 @@
 DATA=/datastore/NGSF001/projects/21-1TOSH-001/new_analysis/Trimming
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001
 
-#for i in $(seq -w 42 52);
-for i in 42
+for i in $(seq -w 42 52);
 do
         fq1="${DATA}/R23000${i}/R23000${i}_R1_trimmed.fastq.gz";  
 	fq2="${DATA}/R23000${i}/R23000${i}_R2_trimmed.fastq.gz";  
