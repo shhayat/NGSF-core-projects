489b7a6bb91a88b992cad5b959f619e42ed90f72
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-22 14:16:59 -0600

    Create sbatch_run_aggregate.sh

---
+++ SingleCellSequencing/CellRanger_pipeline/sbatch_run_aggregate.sh
@@ -0,0 +1,6 @@
+DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/analysis/count_files/
+
+#generate one csv file for all samples 
+for i in ${DIR}/*/out/molecule_info.h5
+do
+  
