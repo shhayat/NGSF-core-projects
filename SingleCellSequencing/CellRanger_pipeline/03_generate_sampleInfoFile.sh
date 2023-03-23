7785ef4d14ffb3eb82969cdd4db503529caee3ba
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-23 13:03:52 -0600

    Update and rename sbatch_run_aggregate.sh to 03_generate_sampleInfoFile.sh

--- SingleCellSequencing/CellRanger_pipeline/03_generate_sampleInfoFile.sh
+++ SingleCellSequencing/CellRanger_pipeline/03_generate_sampleInfoFile.sh
@@ -1,5 +1,4 @@
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/analysis/count_files
-SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/CellRanger_pipeline
 
 #generate one csv file for all samples 
 for i in $(seq -w 1 2);
@@ -8,5 +7,3 @@ do
 done
 #add header to csv file
 sed -i 1i'sample_id,molecule_h5' ${DIR}/sample_info.csv
-
-sbatch ${SCRIPT_DIR}/02_cellranger_count.sh ${DIR}/sample_info.csv
