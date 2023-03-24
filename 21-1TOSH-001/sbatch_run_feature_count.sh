ebabae839507789c3262915622e006a130b78fc9
Shahina <91986031+shhayat@users.noreply.github.com>
2023-03-24 12:32:53 -0600

    Create sbatch_run_feature_count.sh

---
+++ 21-1TOSH-001/sbatch_run_feature_count.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=feature-count
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=3:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/21-1TOSH-001
+module load r/4.1.2
+
+R CMD BATCH ${SCRIPT_DIR}/04_feature_count.R
