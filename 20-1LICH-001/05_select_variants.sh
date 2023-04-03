7abcae373860afbcdc8c1eb24098abbfff1f4009
Shahina <91986031+shhayat@users.noreply.github.com>
2023-04-03 08:45:59 -0600

    Update 05_select_variants.sh

--- 20-1LICH-001/05_select_variants.sh
+++ 20-1LICH-001/05_select_variants.sh
@@ -84,3 +84,4 @@ awk '{gsub("T","A",$2); gsub("A","T",$2); print $1,$2}' ${OUTPUT_DIR}/${CLONE_ID
 echo "join G to A/C conversions with their 2upstream and downtream base pairs and reverse compliments of flanking bases"
 paste --delimiters='\t' ${OUTPUT_DIR}/${CLONE_ID}_${COND}_base_conversion.bed ${OUTPUT_DIR}/${CLONE_ID}_${COND}_flanked_bases_v1.txt ${OUTPUT_DIR}/${CLONE_ID}_${COND}_flanked_bases_reverse_compliments_v1.txt > ${OUTPUT_DIR}/${CLONE_ID}_${COND}.txt
 
+rm ${OUTPUT_DIR}/${CLONE_ID}_${COND}.vcf
