02bc42b588a59b81b8a02a3f4a478150634f7972
Shahina <91986031+shhayat@users.noreply.github.com>
2023-04-04 12:13:49 -0600

    Update 00_generated_sampleInfo.sh

--- SingleCellSequencing/CellRanger_pipeline/nextflow/00_generated_sampleInfo.sh
+++ SingleCellSequencing/CellRanger_pipeline/nextflow/00_generated_sampleInfo.sh
@@ -3,10 +3,10 @@ OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequen
 #generate one csv file for all samples 
 for i in $(seq -w 1 2);
 do
-      echo "R230000${i},${DIR}/R230000${i}_S${i}_L001_R1_001.fastq.gz,${DIR}/R230000${i}_S${i}_L001_R2_001.fastq.gz" >> ${DIR}/sample_info.csv
-      echo "R230000${i},${DIR}/R230000${i}_S${i}_L002_R1_001.fastq.gz,${DIR}/R230000${i}_S${i}_L002_R2_001.fastq.gz" >> ${DIR}/sample_info.csv
-      echo "R230000${i},${DIR}/R230000${i}_S${i}_L003_R1_001.fastq.gz,${DIR}/R230000${i}_S${i}_L003_R2_001.fastq.gz" >> ${DIR}/sample_info.csv
-      echo "R230000${i},${DIR}/R230000${i}_S${i}_L004_R1_001.fastq.gz,${DIR}/R230000${i}_S${i}_L004_R2_001.fastq.gz" >> ${DIR}/sample_info.csv
+      echo "R230000${i},${DIR}/R230000${i}_S${i}_L001_R1_001.fastq.gz,${DIR}/R230000${i}_S${i}_L001_R2_001.fastq.gz" >> ${OUTDIR}/sample_info.csv
+      echo "R230000${i},${DIR}/R230000${i}_S${i}_L002_R1_001.fastq.gz,${DIR}/R230000${i}_S${i}_L002_R2_001.fastq.gz" >> ${OUTDIR}/sample_info.csv
+      echo "R230000${i},${DIR}/R230000${i}_S${i}_L003_R1_001.fastq.gz,${DIR}/R230000${i}_S${i}_L003_R2_001.fastq.gz" >> ${OUTDIR}/sample_info.csv
+      echo "R230000${i},${DIR}/R230000${i}_S${i}_L004_R1_001.fastq.gz,${DIR}/R230000${i}_S${i}_L004_R2_001.fastq.gz" >> ${OUTDIR}/sample_info.csv
 done
 #add header to csv file
 sed -i 1i'sample,fastq_1,fastq_2' ${OUTDIR}/sample_info.csv
