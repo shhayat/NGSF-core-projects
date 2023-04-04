c2195d2dfd56cd525e49c133dd60adfd01ccddbe
Shahina <91986031+shhayat@users.noreply.github.com>
2023-04-04 12:16:09 -0600

    Update 01_run_cell_ranger.sh

--- SingleCellSequencing/CellRanger_pipeline/nextflow/01_run_cell_ranger.sh
+++ SingleCellSequencing/CellRanger_pipeline/nextflow/01_run_cell_ranger.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --account=hpc_p_anderson
 #SBATCH --constraint=skylake
-#SBATCH --job-name=nf_chipseq
+#SBATCH --job-name=nf_singleCell
 #SBATCH --cpus-per-task=40
 #SBATCH --mem=185G
 #SBATCH --time=40:00:00
