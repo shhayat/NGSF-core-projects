e03dd51131e0e79dfe907253227541fe8ea69e94
Shahina <91986031+shhayat@users.noreply.github.com>
2023-04-05 14:04:44 -0600

    Update 01_run_cell_ranger.sh

--- SingleCellSequencing/CellRanger_pipeline/nextflow/01_run_cell_ranger.sh
+++ SingleCellSequencing/CellRanger_pipeline/nextflow/01_run_cell_ranger.sh
@@ -19,13 +19,14 @@ DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencin
 REF=/datastore/NGSF001/analysis/references/cell_ranger_genome_builds/refdata-gex-mm10-2020-A/fasta/genome.fa
 gtf=/datastore/NGSF001/analysis/references/cell_ranger_genome_builds/refdata-gex-mm10-2020-A/genes/genes.gtf
 
-mkdir -p ${DIR}/analysis/results
+#mkdir -p ${DIR}/analysis/results
+mkdir -p ${DIR}/analysis/results_stars
 
 nextflow run nf-core/scrnaseq -profile singularity \
                               --input ${DIR}/sample_info.csv \
                               --skip_fastqc 'TRUE' \
                               #--aligner cellranger \
                               --aligner star \
-                              --outdir ${DIR}/analysis/results \
+                              --outdir ${DIR}/analysis/results_stars \
                               --fasta ${REF} \
                               --gtf ${gtf}
