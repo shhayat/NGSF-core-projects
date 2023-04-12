84018d06a22974c3e86daad11b659eb2a47f863f
Shahina <91986031+shhayat@users.noreply.github.com>
2023-04-12 15:52:10 -0600

    Update 06_convert_bam_to_bedfiles.sh

--- 23-1SADO-001/06_convert_bam_to_bedfiles.sh
+++ 23-1SADO-001/06_convert_bam_to_bedfiles.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=select_variants
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:30:00
+#SBATCH --mem=2G
+#SBATCH  --output=%j.out
+
+##loading required modules for bedtools
+
+#module load bedtools
+module load nixpkgs/16.09 
+module load gcc/5.4.0
+module load intel/2016.4
+module load intel/2017.1
+module load bedtools
+
 
