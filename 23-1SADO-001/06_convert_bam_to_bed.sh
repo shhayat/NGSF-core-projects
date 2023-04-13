f6576a79ebb81c23f1d105b70a2937d8c171f159
Shahina <91986031+shhayat@users.noreply.github.com>
2023-04-13 15:49:13 -0600

    Update 06_convert_bam_to_bed.sh

--- 23-1SADO-001/06_convert_bam_to_bed.sh
+++ 23-1SADO-001/06_convert_bam_to_bed.sh
@@ -31,7 +31,7 @@ BAM=$2;
 #bedtools sort ${DIR}/${SAMPLE_NAME}/${SAMPLE_NAME}_cov.bed > ${DIR}/${SAMPLE_NAME}/${SAMPLE_NAME}_cov_sorted.bed
 #tabix -pbed ${DIR}/${SAMPLE_NAME}/${SAMPLE_NAME}_cov_sorted.bed
 
-/globalhome/hxo752/HPC/anaconda3/envs/deeptools/bin bamCoverage --bam ${DIR}/${SAMPLE_NAME}/${BAM} \
+/globalhome/hxo752/HPC/anaconda3/envs/deeptools/bin/bamCoverage --bam ${DIR}/${SAMPLE_NAME}/${BAM} \
                                                                 --outFileName ${SAMPLE_NAME} \
                                                                  --outFileFormat bedgraph
           
