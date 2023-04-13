8cacfde059d512dc5145e726088057f134a9c840
Shahina <91986031+shhayat@users.noreply.github.com>
2023-04-13 12:31:36 -0600

    Update 06_convert_bam_to_bed.sh

--- 23-1SADO-001/06_convert_bam_to_bed.sh
+++ 23-1SADO-001/06_convert_bam_to_bed.sh
@@ -16,10 +16,14 @@ module load gcc/5.4.0
 module load intel/2016.4
 module load intel/2017.1
 module load bedtools
+module load tabix
 
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SADO-001/analysis/star_alignment
 
 SAMPLE_NAME=$1;
 BAM=$2;
 
-bedtools bamtobed -i ${DIR}/${SAMPLE_NAME}/${BAM} > ${DIR}/${SAMPLE_NAME}/${SAMPLE_NAME}.bed
+#bedtools bamtobed -i ${DIR}/${SAMPLE_NAME}/${BAM} > ${DIR}/${SAMPLE_NAME}/${SAMPLE_NAME}.bed
+bedtools genomecov -ibam ${DIR}/${SAMPLE_NAME}/${BAM} -bg ${DIR}/${SAMPLE_NAME}/${SAMPLE_NAME}_cov.bed
+bedtools sort -k 1,1 -k 2,2n -k 3,3n ${DIR}/${SAMPLE_NAME}/${SAMPLE_NAME}_cov.bed > ${DIR}/${SAMPLE_NAME}/${SAMPLE_NAME}_cov_sorted.bed
+tabix -pbed ${DIR}/${SAMPLE_NAME}/${SAMPLE_NAME}_cov_sorted.bed
