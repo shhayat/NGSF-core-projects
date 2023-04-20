830345b206d17ddbad439de737cc3741e291461b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-04-20 10:51:56 -0600

    Update 02_Bases_Quality_recalibration.sh

--- 20-1LICH-001/02_Bases_Quality_recalibration.sh
+++ 20-1LICH-001/02_Bases_Quality_recalibration.sh
@@ -1,1 +1,21 @@
 Recalibration Base quality score step should be run before calling mutect2. This was part of pre processing step which was missed in this project
+
+
+java -jar GenomeAnalysisTK.jar \ 
+    -T BaseRecalibrator \ 
+    -R reference.fa \ 
+    -I input_reads.bam \ 
+    -L 20 \ 
+    -knownSites dbsnp.vcf \ 
+    -knownSites gold_indels.vcf \ 
+    -o recal_data.table 
+    
+    java -jar GenomeAnalysisTK.jar \ 
+    -T BaseRecalibrator \ 
+    -R reference.fa \ 
+    -I realigned_reads.bam \ 
+    -L 20 \ 
+    -knownSites dbsnp.vcf \ 
+    -knownSites gold_indels.vcf \ 
+    -BQSR recal_data.table \ 
+    -o post_recal_data.table 
