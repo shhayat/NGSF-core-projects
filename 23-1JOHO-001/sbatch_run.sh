4ec4788f23fbedc7c15acfbca4789b3db86a7a09
Shahina <91986031+shhayat@users.noreply.github.com>
2023-04-27 10:45:49 -0600

    Update sbatch_run.sh

--- 23-1JOHO-001/sbatch_run.sh
+++ 23-1JOHO-001/sbatch_run.sh
@@ -1,1 +1,21 @@
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1JOHO-001
 
+#run fastqc
+jid1=$(sbatch ${SCRIPT_DIR}/01_FastQC.sh)
+
+#run genome build
+jid2=$(sbatch ${SCRIPT_DIR}/02_genome_build.sh)
+#run star alignment
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1JOHO-001/analysis/Fastq
+for i in $DATA/R*_R1.fastq.gz
+do
+      path="${i%_R*}";
+      sample_name=${path##*/};
+      fq1=${DATA}/${sample_name}_R1.fastq.gz;
+	    fq2=${DATA}/${sample_name}_R2.fastq.gz;
+      
+      jid3=$(sbatch  --dependency=afterok:$jid2 ${SCRIPT_DIR}/03_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}")
+ sleep 0.5
+done
+
+#run rnaseqc
