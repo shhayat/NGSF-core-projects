1eff681342ca0932a17dd51fcaca432eea141676
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-04 08:39:13 -0600

    Update and rename 01 to download_data.sh

---
+++ Metagenomics/download_data.sh
@@ -0,0 +1,38 @@
+
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=sra_download
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=185G
+#SBATCH --time=20:00:00
+#SBATCH --output=%j.out
+
+sratoolkit=/globalhome/hxo752/HPC/tools/sratoolkit.3.0.1-ubuntu64/bin
+OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/Metagenomics
+
+mkdir -p $OUTPUT
+mkdir -p $OUTPUT/fastq/control_fl
+
+#control FL 
+for i in SRR17156294 SRR17156293
+do
+  echo "Generating sra file for: ${i}";
+  ${sratoolkit}/prefetch $i -O $OUTPUT --progress;
+  
+  echo "Generating fastq for: ${i}";
+  ${sratoolkit}/fastq-dump --outdir $OUTPUT/fastq/control_fl --gzip --clip $OUTPUT/fastq/control_fl/${i}/${i}.sra;
+done
+
+
+mkdir -p $OUTPUT/fastq/inulin_fl
+
+for i in SRR17156289 SRR17156290
+do
+  echo "Generating sra file for: ${i}";
+  ${sratoolkit}/prefetch $i -O $OUTPUT --progress;
+  
+  echo "Generating fastq for: ${i}";
+  ${sratoolkit}/fastq-dump --outdir $OUTPUT/fastq/inulin_fl --gzip --clip $OUTPUT/fastq/inulin_fl/${i}/${i}.sra;
+done
