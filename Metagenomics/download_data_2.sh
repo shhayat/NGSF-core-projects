7d352eed8c309bbfebac43a338bf78697c6af46b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-04 09:04:20 -0600

    Update download_data_2.sh

--- Metagenomics/download_data_2.sh
+++ Metagenomics/download_data_2.sh
@@ -10,3 +10,15 @@
 
 sratoolkit=/globalhome/hxo752/HPC/tools/sratoolkit.3.0.1-ubuntu64/bin
 OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/Metagenomics
+
+mkdir -p $OUTPUT
+mkdir -p $OUTPUT/fastq/inulin_fl
+
+for i in SRR17156289 SRR17156290
+do
+  echo "Generating sra file for: ${i}";
+  ${sratoolkit}/prefetch $i -O $OUTPUT --progress;
+  
+  echo "Generating fastq for: ${i}";
+  ${sratoolkit}/fastq-dump --outdir $OUTPUT/fastq/inulin_fl --gzip --clip $OUTPUT/fastq/inulin_fl/${i}/${i}.sra;
+done
