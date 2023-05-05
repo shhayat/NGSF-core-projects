2086ee40e43d95fd26ab50c21ffc9056653e1291
Shahina Hayat (hxo752) <hxo752@usask.ca>
2023-05-05 13:13:12 -0600

    new file:   nextflow/00_generated_sampleInfo.sh 	new file:   nextflow/01_run_cell_ranger.sh 	new file:   nextflow/01_run_star.sh

---
+++ SingleCellSequencing/CellRanger_pipeline/nextflow/01_run_star.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=nf_singleCell
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=185G
+#SBATCH --time=40:00:00
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+
+module --force purge
+module load StdEnv/2020
+module load nextflow/22.10.6
+module load gentoo/2020
+module load singularity/3.9.2
+
+                              
+DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/CellRanger_pipeline/nextflow
+REF=/datastore/NGSF001/analysis/references/cell_ranger_genome_builds/refdata-gex-mm10-2020-A/fasta/genome.fa
+gtf=/datastore/NGSF001/analysis/references/cell_ranger_genome_builds/refdata-gex-mm10-2020-A/genes/genes.gtf
+
+mkdir -p ${DIR}/analysis/results_star
+
+nextflow run nf-core/scrnaseq -profile singularity \
+                              --input ${DIR}/sample_info.csv \
+                              --skip_fastqc 'TRUE' \
+                              --aligner star \
+                              --outdir ${DIR}/analysis/results_star \
+                              --fasta ${REF} \
+                              --gtf ${gtf}
