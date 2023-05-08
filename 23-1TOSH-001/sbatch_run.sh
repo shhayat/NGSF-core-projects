f687e514512d24459f08130760ef8e055d2a2bcf
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-08 11:53:33 -0600

    Update sbatch_run.sh

--- 23-1TOSH-001/sbatch_run.sh
+++ 23-1TOSH-001/sbatch_run.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1TOSH-001
 #submit fastqc job
 #jid2=$(sbatch --dependency=afterok:$jid1 ${SCRIPT_DIR}/02_FastQC.sh)
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1TOSH-001/fastq
-for i in $DATA/R23*_001.fastq.gz
+for i in $DATA/R23*_R1_001.fastq.gz;
 do
       path="${i%_R1*}";
       sample_name=${path##*/};
@@ -17,14 +17,14 @@ done
 
 #submit star alignment job
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1TOSH-001/fastq
-for i in $DATA/R23*_001.fastq.gz
+for i in $DATA/R23*_R1_001.fastq.gz;
 do
-      path="${i%_R1*}";
+      path="${i%_S*}";
       sample_name=${path##*/};
       path1="${i%_R*}";
       sample_name1=${path1##*/};
-      fq1=${DATA}/${sample_name}_R1_001.fastq.gz;
-      fq2=${DATA}/${sample_name}_R2_001.fastq.gz;
+      fq1=${DATA}/${sample_name1}_R1_001.fastq.gz;
+      fq2=${DATA}/${sample_name1}_R2_001.fastq.gz;
       jid3=$(sbatch ${SCRIPT_DIR}/03_star_mapping.sh "${sample_name1}" "${fq1}" "${fq2}")
  sleep 0.5
 done
