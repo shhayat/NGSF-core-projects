c259ea731865617694430d2fcdfaad5419421125
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-09 12:52:03 -0600

    Update 04_RNASeQC.sh

--- 23-1TOSH-001/04_RNASeQC.sh
+++ 23-1TOSH-001/04_RNASeQC.sh
@@ -13,7 +13,7 @@ bam_file=$1
 
 
 #GTF file needs to be modified for running RNASeQC
-GTF=/datastore/NGSF001/analysis/references/bison/ftp.ensembl.org/pub/release-109/gtf/Bison_bison_bison.Bison_UMD1.0.109_mod.gtf
+GTF=/datastore/NGSF001/analysis/references/bison/ftp.ensembl.org/pub/release-105/gtf/bison_bison_bison/Bison_bison_bison.Bison_UMD1.0.105.mod.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1TOSH-001/analysis/rnaseqc
 
 mkdir -p ${OUTDIR}
