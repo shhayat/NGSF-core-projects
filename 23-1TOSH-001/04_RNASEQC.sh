df2227ba9d5983628417ecf046c897ff32ddd0b4
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-09 07:37:25 -0600

    Update 04_RNASEQC.sh

--- 23-1TOSH-001/04_RNASEQC.sh
+++ 23-1TOSH-001/04_RNASEQC.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-ivybridge
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=ivybridge
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --time=00:20:00
+#SBATCH --time=00:30:00
 #SBATCH --mem=5G
 
 set -eux
 
-# copy tool to tempdir and give execute permission
-cp /datastore/NGSF001/software/src/rnaseqc.v2.4.2.linux ${SLURM_TMPDIR}
-chmod u+x ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux
+rnaseqc=/datastore/NGSF001/software/src/rnaseqc.v2.4.2.linux
 
 sample_name=$1; shift
 bam_file=$1
@@ -21,7 +20,7 @@ OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1TOSH-001/ana
 
 mkdir -p ${OUTDIR}
 
-${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux ${GTF} \
+${rnaseqc}/rnaseqc.v2.4.2.linux ${GTF} \
                         ${bam_file} \
                         ${OUTDIR} \
                         --sample=${sample_name}
