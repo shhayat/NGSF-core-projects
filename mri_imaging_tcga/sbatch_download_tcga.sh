8062f2ef5dee8a49b8a64e01409048460f00fd92
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-15 13:25:11 -0600

    Create sbatch_download_tcga.sh

---
+++ mri_imaging_tcga/sbatch_download_tcga.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=download_tcga
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=10:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/mri_imaging_tcga
+
+module load r/4.1.2
+
+R CMD BATCH ${SCRIPT_DIR}/05_download_tcga.R
