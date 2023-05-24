b90d835aba620265f09b03645f651a4e1145e5f6
Shahina Hayat (hxo752) <hxo752@usask.ca>
2023-05-24 09:11:56 -0600

    deleted:    00_star_build_genome.sh 	deleted:    01_star_mapping.sh 	deleted:    02_umitools_UMIdedup.sh 	deleted:    group11_fastq_files.txt 	deleted:    group1_bam_files.txt 	deleted:    group22_fastq_files.txt 	deleted:    group2_bam_files.txt 	deleted:    sbatch_run_star.sh 	deleted:    sbatch_umitools.sh

--- 22-1MILE-002/sbatch_umitools.sh
+++
@@ -1,7 +0,0 @@
-DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002/alignemnt
-
-for i in 098 099 100 101 102 103
-do
-   bam=star_Aligned.sortedByCoord.out.bam
-   sbatch 02_umitools_UMIdedup.sh "R2200${i}" ${bam}
-done
