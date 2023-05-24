30ce9f52a0cce6be645b48cdb63e005edb111d20
Shahina Hayat (hxo752) <hxo752@usask.ca>
2023-05-24 09:12:20 -0600

    new file:   pilot_study/00_star_build_genome.sh 	new file:   pilot_study/01_star_mapping.sh 	new file:   pilot_study/02_umitools_UMIdedup.sh 	new file:   pilot_study/group11_fastq_files.txt 	new file:   pilot_study/group1_bam_files.txt 	new file:   pilot_study/group22_fastq_files.txt 	new file:   pilot_study/group22_fastq_files_drop_R102.txt 	new file:   pilot_study/group2_bam_files.txt 	new file:   pilot_study/sbatch_run_star.sh 	new file:   pilot_study/sbatch_umitools.sh

---
+++ 22-1MILE-002/pilot_study/sbatch_run_star.sh
@@ -0,0 +1,11 @@
+DATA=/datastore/NGSF001/projects/22-1MILE-002/fastq_umi
+#098 099 100 
+
+for i in 101 102 103
+do
+  fq1=${DATA}/R2200${i}_R1_umi.fastq.gz
+  fq2=${DATA}/R2200${i}_R3_umi.fastq.gz
+  
+  sbatch 01_star_mapping.sh R2200${i} ${fq1} ${fq2}
+  
+done
