545aaf29bf3c7ff1a40c93f28fa81c6ce7e6a3f2
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-25 14:38:55 -0600

    Update SRA_data_download.sh

--- 23-1ARMA/fusion_genes/SRA_data_download.sh
+++ 23-1ARMA/fusion_genes/SRA_data_download.sh
@@ -1,1 +1,25 @@
 
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=sra_download
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=185G
+#SBATCH --time=20:00:00
+#SBATCH --output=%j.out
+
+sratoolkit=/globalhome/hxo752/HPC/tools/sratoolkit.3.0.1-ubuntu64/bin
+#Normal Skin Tissue (Paired reads)
+OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes
+
+mkdir -p $OUTPUT/fastq
+
+for i in SRR17624369
+do
+  echo "Generating sra file for:  SRR176243${i}";
+  ${sratoolkit}/prefetch SRR176243&{i} -O $OUTPUT --progress;
+  
+  echo "Generating fastq for: SRR${i}";
+  ${sratoolkit}/fastq-dump --outdir $OUTPUT/fastq --gzip --clip ${OUTPUT}/SRR176243${i}/SRR176243${i}.sra;
+done
