e1727b4db9e8d262610590edcfb1b2871da6b52b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-25 15:39:24 -0600

    Update 01_FastQC.sh

--- 22-1MILE-002/01_FastQC.sh
+++ 22-1MILE-002/01_FastQC.sh
@@ -11,7 +11,7 @@
 set -eux
 
 module load fastqc
-DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002/analysis/Fastq
+DATA=/datastore/NGSF001/projects/23-1MILE-002/fastq
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002/analysis
 mkdir -p ${OUTDIR}/fastqc
 
