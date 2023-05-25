bef7680a11317b9ce5b4b4fbaf5e70b9e59b92ec
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-25 15:36:46 -0600

    Update 01_FastQC.sh

--- 22-1MILE-002/01_FastQC.sh
+++ 22-1MILE-002/01_FastQC.sh
@@ -4,20 +4,18 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=fastqc
 #SBATCH --ntasks=6
-#BATCH --cpus-per-task=4
-#SBATCH --time=02:00:00
-#SBATCH --mem=6G
+#BATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=5G
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 set -eux
 
 module load fastqc
-DATA=/datastore/NGSF001/projects/23-1MILE-002/fastq/
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002/analysis/Fastq
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002/analysis
-
 mkdir -p ${OUTDIR}/fastqc
 
-for fq in $DATA/R23*.fastq.gz
-do
-   fastqc -o ${OUTDIR}/fastqc --extract ${fq}
-   
-done
+fq1=$1; shift
+fq2=$1;
+fastqc -o ${OUTDIR}/fastqc --extract ${fq1}
+fastqc -o ${OUTDIR}/fastqc --extract ${fq2}
