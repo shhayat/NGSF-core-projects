c9084d35843b4740f5449f6524f86543ba48d97b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-26 14:24:41 -0600

    Update 03_RNASeQC.sh

--- 23-1MILE-002/03_RNASeQC.sh
+++ 23-1MILE-002/03_RNASeQC.sh
@@ -19,8 +19,8 @@ bam_file=$1
 #run following python code on GTF file
 #python collaspe_annotation.py /datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf genes_mod.gtf
 
-GTF=/datastore/NGSF001/analysis/references/human/gencode-40/gencode.v40.annotation_mod.gtf
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SADO-001/analysis/rnaseqc
+GTF=/datastore/NGSF001/analysis/references/iGenomes/Mouse/Mus_musculus/Ensembl/GRCm38/Annotation/Genes/genes.gtf
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002/analysis/rnaseqc
 
 mkdir -p ${OUTDIR}
 
