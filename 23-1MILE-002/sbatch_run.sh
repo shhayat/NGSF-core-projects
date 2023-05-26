9f5e8d075c214bbd3e5363aaee723aa37d2a99f8
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-26 09:35:07 -0600

    Update sbatch_run.sh

--- 23-1MILE-002/sbatch_run.sh
+++ 23-1MILE-002/sbatch_run.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002
 
 for i in ${DATA}/R23*_R1_001.fastq.gz 
 do
-      path="${i%_S*_R1*}";
+      path="${i%_R1*}";
       sample_name=${path##*/};
       fq1=${DATA}/${sample_name}_R1_001.fastq.gz;
       fq2=${DATA}/${sample_name}_R2_001.fastq.gz;
