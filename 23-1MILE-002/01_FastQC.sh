09b8c0f3f9a73fde08ff9f05ba15997c036549d0
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-26 11:22:52 -0600

    Update 01_FastQC.sh

--- 23-1MILE-002/01_FastQC.sh
+++ 23-1MILE-002/01_FastQC.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=hpc_p_anderson
 #SBATCH --constraint=skylake
 #SBATCH --job-name=fastqc
-#SBATCH --ntasks=6
+#SBATCH --ntasks=1
 #BATCH --cpus-per-task=1
 #SBATCH --time=1:00:00
 #SBATCH --mem=5G
