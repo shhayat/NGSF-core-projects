0703c1c43f95655710554aaace34912cd0321918
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-26 11:42:16 -0600

    Create 02_genome_index.sh

---
+++ 23-1MILE-002/02_genome_index.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --job-name=genome_index
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=03:00:00
+#SBATCH --mem=80G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+
+module load star/2.7.9a 
+
+
+GENOME=/datastore/NGSF001/analysis/references/iGenomes/Mouse/Mus_musculus/Ensembl/GRCm38/Sequence/WholeGenomeFasta
+GTF=/datastore/NGSF001/analysis/references/iGenomes/Mouse/Mus_musculus/Ensembl/GRCm38/Annotation/Genes/genes.gtf
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-001/analysis/indices
+
+NCPU=8
+
+mkdir -p $OUTDIR
+cd ${OUTDIR}
+
+STAR --runThreadN ${NCPU} \
+     --runMode genomeGenerate \
+     --genomeDir star-index \
+     --genomeFastaFiles ${GENOME}/genome.fa \
+     --sjdbGTFfile ${GTF} \
+     --sjdbOverhang 99
