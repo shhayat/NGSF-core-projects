da6db6b5fdcd5807832d1a467919a1123f759d5f
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-26 09:14:59 -0600

    Update sbatch_run_star_mapping.sh

--- 23-1MILE-002/sbatch_run_star_mapping.sh
+++ 23-1MILE-002/sbatch_run_star_mapping.sh
@@ -1,4 +1,3 @@
-DATA=
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002
 for i in $DATA/R23*_R1_001.fastq.gz 
 do
@@ -7,8 +6,8 @@ do
       
       path1="${i%_S*_R1*}";
       sample_name1=${path1##*/};
-      fq1=${DATA}/${sample_name}_R1_001.fastq.gz;
-      fq2=${DATA}/${sample_name}_R2_001.fastq.gz;
+      fq1=${sample_name}_R1_001.fastq.gz;
+      fq2=${sample_name}_R2_001.fastq.gz;
       
-      sbatch ${DATA}/02_star_mapping.sh "${sample_name1}" "${fq1}" "${fq2}"
+      sbatch ${SCRIPT_DIR}/02_star_mapping.sh "${sample_name1}" "${fq1}" "${fq2}"
 done
