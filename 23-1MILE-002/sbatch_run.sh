ba8dab8858d1c29d88b6f64e4e7b69d687d3e3eb
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-26 09:29:16 -0600

    Update and rename sbatch_run_star_mapping.sh to sbatch_run.sh

---
+++ 23-1MILE-002/sbatch_run.sh
@@ -0,0 +1,29 @@
+DATA=/datastore/NGSF001/projects/23-1MILE-002/fastq
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002
+
+for i in $DATA/R23*_R1.fastq.gz 
+do
+      path1="${i%_S*_R1*}";
+      sample_name1=${path1##*/};
+      fq1=${DATA}/${sample_name}_R1_001.fastq.gz;
+      fq2=${DATA}/${sample_name}_R2_001.fastq.gz;
+      
+      sbatch ${SCRIPT_DIR}/01_FastQC.sh "${fq1}" "${fq2}"
+ done
+ 
+
+
+DATA=/datastore/NGSF001/projects/23-1MILE-002/fastq
+
+for i in ${DATA}/R23*_R1_001.fastq.gz 
+do
+      path="${i%_R1*}";
+      sample_name=${path##*/};
+      
+      path1="${i%_S*_R1*}";
+      sample_name1=${path1##*/};
+      fq1=${DATA}/${sample_name}_R1_001.fastq.gz;
+      fq2=${DATA}/${sample_name}_R2_001.fastq.gz;
+      
+      sbatch ${SCRIPT_DIR}/02_star_mapping.sh "${sample_name1}" "${fq1}" "${fq2}"
+done
