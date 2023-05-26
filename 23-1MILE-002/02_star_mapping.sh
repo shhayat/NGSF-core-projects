0b2d1449e047ec31e368111d0b34ab20918c00e7
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-26 11:32:38 -0600

    Update 02_star_mapping.sh

--- 23-1MILE-002/02_star_mapping.sh
+++ 23-1MILE-002/02_star_mapping.sh
@@ -17,7 +17,7 @@ NCPUS=4
 #work adapted from https://github.com/ngsf-usask/scripts/tree/main/RNAseq/22-1MILE-002
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002/analysis
 star=/datastore/NGSF001/software/tools/STAR-2.7.4a/bin/Linux_x86_64
-INDEX=/datastore/NGSF001/projects/2022/22-1MILE-002/analysis_by_shahina/star-index
+INDEX=/datastore/NGSF001/projects/23-1MILE-001/Analysis/indices/star-index
 
 sample_name=$1; shift
 fq1=$1; shift
