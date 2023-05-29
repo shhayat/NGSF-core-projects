0293d863a4cdeb4b6391f3706dce45649d44b8e2
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-29 13:54:42 -0600

    Update sbatch_run.sh

--- 23-1MILE-002/sbatch_run.sh
+++ 23-1MILE-002/sbatch_run.sh
@@ -22,7 +22,7 @@ do
       path="${i%_R1*}";
       sample_name=${path##*/};
       fq1=${DATA}/${sample_name}_R1.fastq.gz;
-      fq2=${DATA}/${sample_name}_R1.fastq.gz;
+      fq2=${DATA}/${sample_name}_R2.fastq.gz;
       
       sbatch ${SCRIPT_DIR}/02_FastQC.sh "${fq1}" "${fq2}"
  done
@@ -35,13 +35,11 @@ for i in ${DATA}/R23*_R1_001.fastq.gz
 do
       path="${i%_R1*}";
       sample_name=${path##*/};
-      
-      path1="${i%_S*_R1*}";
-      sample_name1=${path1##*/};
+
       fq1=${DATA}/${sample_name}_R1_001.fastq.gz;
       fq2=${DATA}/${sample_name}_R2_001.fastq.gz;
       
-      sbatch ${SCRIPT_DIR}/02_star_mapping.sh "${sample_name1}" "${fq1}" "${fq2}"
+      sbatch ${SCRIPT_DIR}/03_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}"
 done
 
 
@@ -50,7 +48,7 @@ for i in ${DATA}/*/*.bam
 do
         path="${i%/Aligned*}";
 	     sample_name="${path##*/}"
-        sbatch ${SCRIPT_DIR}/03_RNASeQC.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
+        sbatch ${SCRIPT_DIR}/04_RNASeQC.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
 	sleep 0.5
 done 
 
@@ -58,7 +56,7 @@ done
 for i in {122..130}
 do
    bam=Aligned.sortedByCoord.out.bam
-   sbatch ${SCRIPT_DIR}/04_remove_rnrna_and_dedupUMI.sh "R2300${i}" ${bam}
+   sbatch ${SCRIPT_DIR}/05_remove_rnrna_and_dedupUMI.sh "R2300${i}" ${bam}
 done
 
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002/analysis/deduplication
@@ -68,7 +66,7 @@ for i in ${DATA}/*/*.bam
 do
      path="${i%/Aligned*}";
      sample_name="${path##*/}"
-     sbatch ${SCRIPT_DIR}/04_HTSeq_count.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
+     sbatch ${SCRIPT_DIR}/06_HTSeq_count.sh "${sample_name}" "${DATA}/${sample_name}/Aligned.sortedByCoord.out.bam"
      sleep 0.5
 done 
 
