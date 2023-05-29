0c639c7b7509612af206e00a2cd3a5fa07a514df
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-29 11:39:49 -0600

    Update 01_Add_UMIs.sh

--- 23-1MILE-002/01_Add_UMIs.sh
+++ 23-1MILE-002/01_Add_UMIs.sh
@@ -8,7 +8,6 @@
 
 fastq=/datastore/NGSF001/projects/23-1MILE-002/fastq
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002/analysis/fq_with_umi_header
-umi_tools=/globalhome/hxo752/HPC/anaconda3/bin
 mkdir -p ${OUTDIR}
 
 sample_name=1;
@@ -16,7 +15,7 @@ fq1=1;
 fq2=1;
 
 #Add UMIs to header of Fastq R1 and R2 file
-${umi_tools}/umi_tools extract -p NNNNNNNNNNN \ 
+umi_tools extract -p NNNNNNNNNNN \ 
                                -I ${fq1} \
                                -S ${OUTDIR}/${sample_name}_R1.fastq.gz  \
                                --read2-in=${fq2} \
