3f19412e9228037716f0af36c1101367f5f1f7e1
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-29 11:22:18 -0600

    Update 01_Add_UMIs.sh

--- 23-1MILE-002/01_Add_UMIs.sh
+++ 23-1MILE-002/01_Add_UMIs.sh
@@ -1,2 +1,14 @@
-#Add UMIs to header of Fastq file
+#!/bin/bash
 
+#SBATCH --job-name=umi_tools
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2:00:00
+#SBATCH --mem=4G
+
+#Add UMIs to header of Fastq file
+umi_tools extract -p NNNNNNNNNNN \ 
+                  -I IN_FASTQ.gz \
+                  -S OUT_FASTQ.gz \
+                  --read2-in=IN2_FASTQ.gz \
+                  --read2-out=OUT2_FASTQ.gz
