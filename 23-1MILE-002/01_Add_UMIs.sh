7f2b804adbc9f548435757f65c1db27e9dfc02ef
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-29 11:24:19 -0600

    Update 01_Add_UMIs.sh

--- 23-1MILE-002/01_Add_UMIs.sh
+++ 23-1MILE-002/01_Add_UMIs.sh
@@ -6,9 +6,11 @@
 #SBATCH --time=2:00:00
 #SBATCH --mem=4G
 
+fastq=/datastore/NGSF001/projects/23-1MILE-002/fastq
+fq_with_umi_header=
 #Add UMIs to header of Fastq R1 and R2 file
 umi_tools extract -p NNNNNNNNNNN \ 
-                  -I IN_FASTQ.gz \
-                  -S OUT_FASTQ.gz \
+                  -I R2300125_S4_R1_001.fastq.gz \
+                  -S R2300125_S4_R1_001.fastq.gz \
                   --read2-in=IN2_FASTQ.gz \
                   --read2-out=OUT2_FASTQ.gz
