b5c84871b9fc0ab6359d92b83ff2e3ecb0cbc1ac
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-30 09:46:38 -0600

    Update 06_HTSeq_count.sh

--- 23-1MILE-002/06_HTSeq_count.sh
+++ 23-1MILE-002/06_HTSeq_count.sh
@@ -25,7 +25,7 @@ BAM=$1
                                                             ${GTF} > ${OUTDIR}/${sample_name}_htseq_counts.txt
 
 #remove .[0-9] from each line from ffrist columm
-awk '{ gsub(".[0-9]*$", "", $1); print }' ${OUTDIR}/${sample_name}_htseq_counts.txt > ${OUTDIR}/${sample_name}_htseq_counts.tmp && mv ${OUTDIR}/${sample_name}_htseq_counts.tmp ${OUTDIR}/${sample_name}_htseq_counts.txt
+#awk '{ gsub(".[0-9]*$", "", $1); print }' ${OUTDIR}/${sample_name}_htseq_counts.txt > ${OUTDIR}/${sample_name}_htseq_counts.tmp && mv ${OUTDIR}/${sample_name}_htseq_counts.tmp ${OUTDIR}/${sample_name}_htseq_counts.txt
 #Add tabs
 awk -v OFS="\t" '$1=$1' ${OUTDIR}/${sample_name}_htseq_counts.txt > ${OUTDIR}/${sample_name}_htseq_counts.tmp && mv ${OUTDIR}/${sample_name}_htseq_counts.tmp ${OUTDIR}/${sample_name}_htseq_counts.txt
 
