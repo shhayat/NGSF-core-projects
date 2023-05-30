81bd59994a103dc39c916ca01d18ad27ba7fdeb8
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-30 08:21:38 -0600

    Update 01_Add_UMIs.sh

--- 23-1MILE-002/01_Add_UMIs.sh
+++ 23-1MILE-002/01_Add_UMIs.sh
@@ -8,7 +8,7 @@
 
 #umitool_folder=/globalhome/hxo752/HPC/anaconda3/bin
 umitools=/globalhome/hxo752/HPC/.local/bin
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002/analysis/fq_with_umi_header
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002/analysis/fq_with_umi_header_v1
 mkdir -p ${OUTDIR}
 
 sample_name=$1; shift
@@ -16,10 +16,20 @@ fq1=$1; shift
 fq2=$1;
 
 #Add UMIs to header of Fastq R1 and R2 file
+#${umitools}/umi_tools extract -I ${fq1} \
+#                  -S ${OUTDIR}/${sample_name}_R1.fastq.gz \
+#                  --read2-in=${fq2} \
+#                  --read2-out=${OUTDIR}/${sample_name}_R2.fastq.gz \
+#                  --bc-pattern=NNNNNNNNNNN \
+#                  --bc-pattern2=NNNNNNNNNNN \
+#                  --log=${OUTDIR}/${sample_name}.log
+
 ${umitools}/umi_tools extract -I ${fq1} \
                   -S ${OUTDIR}/${sample_name}_R1.fastq.gz \
-                  --read2-in=${fq2} \
-                  --read2-out=${OUTDIR}/${sample_name}_R2.fastq.gz \
                   --bc-pattern=NNNNNNNNNNN \
-                  --bc-pattern2=NNNNNNNNNNN \
-                  --log=${OUTDIR}/${sample_name}.log
+                  -log=${OUTDIR}/${sample_name}_R1.log
+      
+${umitools}/umi_tools extract -I ${fq1} \
+                  -S ${OUTDIR}/${sample_name}_R2.fastq.gz \
+                  --bc-pattern=NNNNNNNNNNN \
+                  -log=${OUTDIR}/${sample_name}_R2.log
