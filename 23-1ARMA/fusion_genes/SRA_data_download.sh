ff835cf696901571ede94cb4ba21605f39147ee3
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-30 23:09:08 -0600

    Update SRA_data_download.sh

--- 23-1ARMA/fusion_genes/SRA_data_download.sh
+++ 23-1ARMA/fusion_genes/SRA_data_download.sh
@@ -3,9 +3,9 @@
 #SBATCH --account=hpc_p_anderson
 #SBATCH --constraint=skylake
 #SBATCH --job-name=sra_download
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=185G
-#SBATCH --time=20:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=%j.out
 
 sratoolkit=/globalhome/hxo752/HPC/tools/sratoolkit.3.0.1-ubuntu64/bin
