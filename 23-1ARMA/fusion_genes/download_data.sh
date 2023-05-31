e69e8e7459243cbc50ba0996aa5bb79bdd9f943d
Shahina <91986031+shhayat@users.noreply.github.com>
2023-05-31 12:22:11 -0600

    Update download_data.sh

--- 23-1ARMA/fusion_genes/download_data.sh
+++ 23-1ARMA/fusion_genes/download_data.sh
@@ -2,15 +2,15 @@
 
 #SBATCH --account=hpc_p_anderson
 #SBATCH --constraint=skylake
-#SBATCH --job-name=sra_download
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=100G
-#SBATCH --time=24:00:00
+#SBATCH --job-name=download_data
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=40G
+#SBATCH --time=2:00:00
 #SBATCH --output=%j.out
 
+NUM=$1; shift
+SRR=$1;
 
-for i in {4369..4389}
-do
-  wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR176/069/SRR17624369/SRR17624369_1.fastq.gz
-  wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR176/069/SRR17624369/SRR17624369_2.fastq.gz
+wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR176/${NUM}/${SRR}/${SRR}_1.fastq.gz
+wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR176/${NUM}/${SRR}/${SRR}_2.fastq.gz
 
