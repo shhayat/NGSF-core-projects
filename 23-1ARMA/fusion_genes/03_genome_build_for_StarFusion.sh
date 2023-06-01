c941cdb913adfe4cfe63fd5ecc0c956dba7c22ae
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-01 15:34:48 -0600

    Update 03_genome_build_for_StarFusion.sh

--- 23-1ARMA/fusion_genes/03_genome_build_for_StarFusion.sh
+++ 23-1ARMA/fusion_genes/03_genome_build_for_StarFusion.sh
@@ -9,11 +9,14 @@
 #SBATCH --mem=80G
 #SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
+module load perl/5.30.2 
 conda ~anaconda3/envs/star-fusion/star-fusion
 
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/indices-star-fusion
+GTF=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
+
 perl prep_genome_lib.pl --genome_fa /path/to/reference_genome.fasta \
---gtf /path/to/gene_annotation.gtf \
+--gtf ${GTF} \
 --fusion_annot_lib /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/indices \
 --CPU 8 \
 --out_dir ${OUTDIR}
