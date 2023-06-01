79aaa3530679bcf3825ae8cb355054df54085d7e
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-01 11:24:07 -0600

    Update sbatch_run.sh

--- 23-1ARMA/fusion_genes/sbatch_run.sh
+++ 23-1ARMA/fusion_genes/sbatch_run.sh
@@ -1,1 +1,22 @@
+#submit fastqc job
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/fastq
+for i in $DATA/*_1.fastq.gz
+do
+      path="${i%_1*}";
+      sample_name=${path##*/};
+      fq1=${DATA}/${sample_name}_1.fastq.gz;
+      fq2=${DATA}/${sample_name}_2.fastq.gz;
+      jid2=$(sbatch ${SCRIPT_DIR}/01_FastQC.sh "${fq1}" "${fq2}")
+ done
 
+#submit star alignment job
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/fastq
+
+for i in $DATA/*_1.fastq.gz
+do
+      path="${i%_1*}";
+      sample_name=${path##*/};
+      fq1=${DATA}/${sample_name}_1.fastq.gz;
+      fq2=${DATA}/${sample_name}_2.fastq.gz;
+      sbatch ${SCRIPT_DIR}/02_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}"
+done
