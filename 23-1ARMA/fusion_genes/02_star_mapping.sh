a78fea09f2ce66eb763071aa58d38199cdfbb3b1
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-01 12:13:38 -0600

    Update 02_star_mapping.sh

--- 23-1ARMA/fusion_genes/02_star_mapping.sh
+++ 23-1ARMA/fusion_genes/02_star_mapping.sh
@@ -16,7 +16,7 @@ module load samtools
 
 
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/fastq
-GENOME=
+GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/indices
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/star_alignment
 NCPU=4
 
@@ -29,19 +29,27 @@ mkdir -p ${OUTDIR}/${sample_name} && cd ${OUTDIR}/${sample_name}
 STAR --genomeDir $GENOME \
 	--readFilesCommand zcat \
 	--readFilesIn ${fq1} ${fq2} \
+	--outReadsUnmapped None \
+        --twopassMode Basic \
 	--outSAMtype BAM SortedByCoordinate \
 	--runThreadN ${NCPU} \
-        --chimSegmentMin 12 \
-        --chimJunctionOverhangMin 12 \
-        --chimOutJunctionFormat 1 \
+	--outSAMstrandField intronMotif \
+        --outSAMunmapped Within \
+	--chimSegmentMin 12 \  # ** essential to invoke chimeric read detection & reporting **
+        --chimJunctionOverhangMin 8 \
+        --chimOutJunctionFormat 1 \   # **essential** includes required metadata in Chimeric.junction.out file.
         --alignSJDBoverhangMin 10 \
-        --alignMatesGapMax 1000000 \
-        --alignIntronMax 1000000 \
-        --alignSJstitchMismatchNmax 5 -1 5 5 \
-        --outSAMstrandField intronMotif \
-        --chimMultimapScoreRange 10 \
-        --chimMultimapNmax 10 \
+        --alignMatesGapMax 100000 \   # avoid readthru fusions within 100k
+        --alignIntronMax 100000 \
+        --alignSJstitchMismatchNmax 5 -1 5 5 \   # settings improved certain chimera detections
+        --outSAMattrRGline ID:GRPundef \
+        --chimMultimapScoreRange 3 \
+        --chimScoreJunctionNonGTAG -4 \
+        --chimMultimapNmax 20 \
         --chimNonchimScoreDropMin 10 \
         --peOverlapNbasesMin 12 \
         --peOverlapMMp 0.1 \
+        --alignInsertionFlush Right \
+        --alignSplicedMateMapLminOverLmate 0 \
+        --alignSplicedMateMapLmin 30
 	&& samtools index Aligned.sortedByCoord.out.bam 
