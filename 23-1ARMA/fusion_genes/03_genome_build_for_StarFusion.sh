f666470c65d416fd3520297eec48643f698ef326
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-01 15:26:06 -0600

    Update 03_genome_build_for_StarFusion.sh

--- 23-1ARMA/fusion_genes/03_genome_build_for_StarFusion.sh
+++ 23-1ARMA/fusion_genes/03_genome_build_for_StarFusion.sh
@@ -1,6 +1,21 @@
+#!/bin/sh
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=genome_index
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=8
+#SBATCH --time=03:00:00
+#SBATCH --mem=80G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
 conda ~anaconda3/envs/star-fusion/star-fusion
 
-prep_genome_lib.pl 
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/indices-star-fusion
+perl prep_genome_lib.pl --genome_fa /path/to/reference_genome.fasta \
+--gtf /path/to/gene_annotation.gtf \
+--fusion_annot_lib /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/indices \
+--CPU 8 \
+--out_dir ${OUTDIR}
+
 conda deactivate
