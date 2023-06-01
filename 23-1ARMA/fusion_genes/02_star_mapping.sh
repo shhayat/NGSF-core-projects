b032a9225c57cfb3c086302445a0edc52a00eeed
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-01 11:04:46 -0600

    Update and rename 02_Star_alignment.sh to 02_star_mapping.sh

---
+++ 23-1ARMA/fusion_genes/02_star_mapping.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=star-align
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=3:00:00
+#SBATCH --mem=60G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+#loading required modules
+module load star/2.7.9a 
+module load samtools
+
+
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/fq_hystiocystic_sarcoma
+#GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1JOHO-001/analysis/indices/star-index-m32gencode
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1JOHO-001/analysis/star_alignment
+NCPU=4
+
+sample_name=$1; shift
+fq1=$1; shift
+fq2=$1
+
+mkdir -p ${OUTDIR}/${sample_name} && cd ${OUTDIR}/${sample_name}
+
+STAR --genomeDir $GENOME \
+	--readFilesCommand zcat \
+	--readFilesIn ${fq1} ${fq2} \
+	--outSAMtype BAM SortedByCoordinate \
+	--runThreadN ${NCPU} \
+	&& samtools index Aligned.sortedByCoord.out.bam 
