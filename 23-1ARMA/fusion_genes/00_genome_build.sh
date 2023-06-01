5cd929ce18574a20343f4d7dafd13468e0d24158
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-01 11:46:21 -0600

    Update 00_genome_build.sh

--- 23-1ARMA/fusion_genes/00_genome_build.sh
+++ 23-1ARMA/fusion_genes/00_genome_build.sh
@@ -1,1 +1,28 @@
+#!/bin/sh
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=genome_index
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=8
+#SBATCH --time=03:00:00
+#SBATCH --mem=80G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+
+module load star/2.7.9a 
+
+GENOME=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta
+GTF=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/analysis/indices
+
+NCPU=8
+
+mkdir -p $OUTDIR
+cd ${OUTDIR}
+
+STAR --runThreadN ${NCPU} \
+     --runMode genomeGenerate \
+     --genomeDir star-indice \
+     --genomeFastaFiles ${GENOME} \
+     --sjdbGTFfile ${GTF} \
+     --sjdbOverhang 99
