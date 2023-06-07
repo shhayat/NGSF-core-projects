54d867f2a8161ad383fb49cf36d39055312ba0a6
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-07 11:51:58 -0600

    Update 03_genome_build_for_StarFusion.sh

--- 23-1ARMA/fusion_genes/03_genome_build_for_StarFusion.sh
+++ 23-1ARMA/fusion_genes/03_genome_build_for_StarFusion.sh
@@ -11,19 +11,15 @@
 
 module load perl/5.30.2 
 module load star/2.7.9a 
-module load blast/2.2.26 
+/globalhome/hxo752/HPC/anaconda3/condabin/conda activate fusion-gene-env
 
-/globalhome/hxo752/HPC/anaconda3/condabin/conda activate hmmer
-/globalhome/hxo752/HPC/anaconda3/condabin/conda activate dfam
-
-lib_builder=/globalhome/hxo752/HPC/tools/ctat-genome-lib-builder
 
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/indices_for_star_fusion
 GTF=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
 GENOME=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta/genome.fa
 
 #https://github.com/STAR-Fusion/STAR-Fusion/blob/master/Docker/Dockerfile
-perl ${lib_builder}/prep_genome_lib.pl --genome_fa ${GENOME} \
+perl prep_genome_lib.pl --genome_fa ${GENOME} \
                                        --gtf ${GTF} \
                                        --dfam_db 'dog' \
                                        --CPU 8 \
