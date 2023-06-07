092da095749d3901ffc837d800944f636e8e4330
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-07 13:30:57 -0600

    Update 03_genome_build_for_StarFusion.sh

--- 23-1ARMA/fusion_genes/03_genome_build_for_StarFusion.sh
+++ 23-1ARMA/fusion_genes/03_genome_build_for_StarFusion.sh
@@ -11,18 +11,16 @@
 
 module load perl/5.30.2 
 module load star/2.7.9a 
-/globalhome/hxo752/HPC/anaconda3/condabin/conda activate fusion-gene-env
 
+export STAR_FUSION_HOME="/globalhome/hxo752/HPC/tools/STAR-Fusion"
 
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/indices_for_star_fusion
 GTF=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
 GENOME=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta/genome.fa
 
 #https://github.com/STAR-Fusion/STAR-Fusion/blob/master/Docker/Dockerfile
-perl prep_genome_lib.pl --genome_fa ${GENOME} \
-                                       --gtf ${GTF} \
-                                       --dfam_db 'dog' \
-                                       --CPU 8 \
-                                      --output_dir ${OUTDIR}
-
-/globalhome/hxo752/HPC/anaconda3/bin/conda deactivate
+${STAR_FUSION_HOME}/ctat-genome-lib-builder/prep_genome_lib.pl --genome_fa ${GENOME} \
+                                                               --gtf ${GTF} \
+                                                               --dfam_db 'dog' \
+                                                               --CPU 8 \
+                                                               --output_dir ${OUTDIR}
