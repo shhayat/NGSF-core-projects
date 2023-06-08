f902eca66f6fe89f2012c5f4f443a7542645b2ba
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-08 10:58:52 -0600

    Update download_data.sh

--- 23-1ARMA/fusion_genes/download_data.sh
+++ 23-1ARMA/fusion_genes/download_data.sh
@@ -1,10 +1,11 @@
 #fastq files for canine hystiocystic sarcoma
 #study: Whole exome and transcriptomeanalysis revealed the activation of ERK andAkt signaling pathway in canine histiocytic sarcoma
-for i in {6..9}
-do
-  #NUM="${i:2}"
-  OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/fq_hystiocystic_sarcoma
-  mkdir -p $OUTDIR
-  echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/DRR345/DRR34598${i}/DRR34598${i}_1.fastq.gz" >> $OUTDIR/path_to_fastq_files_R1.txt
-  echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/DRR345/DRR34598${i}/DRR34598${i}_2.fastq.gz" >> $OUTDIR/path_to_fastq_files_R2.txt
-done
+#for i in {6..9}
+#do
+  ##NUM="${i:2}"
+ # OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/fq_hystiocystic_sarcoma
+ # mkdir -p $OUTDIR
+ # echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/DRR345/DRR34598${i}/DRR34598${i}_1.fastq.gz" >> $OUTDIR/path_to_fastq_files_R1.txt
+ # echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/DRR345/DRR34598${i}/DRR34598${i}_2.fastq.gz" >> $OUTDIR/path_to_fastq_files_R2.txt
+#done
+
