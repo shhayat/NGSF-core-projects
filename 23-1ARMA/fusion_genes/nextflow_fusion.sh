6115604934e442de5fc53026fd6892f6ff2728d6
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-09 23:18:58 -0600

    Update nextflow_fusion.sh

--- 23-1ARMA/fusion_genes/nextflow_fusion.sh
+++ 23-1ARMA/fusion_genes/nextflow_fusion.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=nf_singleCell
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=185G
+#SBATCH --time=40:00:00
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+
+module --force purge
+module load StdEnv/2020
+module load nextflow/22.10.6
+module load gentoo/2020
+module load singularity/3.9.2
+
+                              
+DIR=/globalhome/hxo752/HPC/tools/
+
+
+mkdir -p ${DIR}/analysis/results_star
+
+nextflow run nf-core/scrnaseq -profile singularity \
+                              --input ${DIR}/sample_info.csv \
+                              --skip_fastqc 'TRUE' \
+                              --aligner star \
+                              --outdir ${DIR}/analysis/results_star \
+                              --fasta ${REF} \
+                              --gtf ${gtf}
