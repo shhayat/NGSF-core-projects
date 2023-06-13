391f2692405c19627c2723ec35462088d20666be
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-13 14:10:07 -0600

    Update nextflow_fusion.sh

--- 23-1ARMA/fusion_genes/nextflow_fusion.sh
+++ 23-1ARMA/fusion_genes/nextflow_fusion.sh
@@ -18,7 +18,7 @@ DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_gen
 star_indice=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/indices                              
 REF=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta
 FASTA=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta/genome.fa
-FASTA_FAI=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta/genome.fa.fai
+#FASTA_FAI=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta/genome.fa.fai
 GTF=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
 mkdir -p ${DIR}/analysis/nextflow
 
@@ -28,11 +28,10 @@ nextflow run nf-core/rnafusion -profile singularity \
                               --fusion_inspector \
                               --genomes_base ${REF} \
                               --fasta ${FASTA} \ 
-                              --fai ${FASTA_FAI} \
                               --gtf ${GTF} \
                               --star_index ${star_indice} \
                               --starfusion_build 'TRUE' \
-                              --outdir ${DIR}/analysis/nextflow \
                               --max_memory '80.GB' \
                               --max_cpus 16 \
+                              --outdir ${DIR}/analysis/nextflow \
                               --email 'shahina.hayat@usask.ca'
