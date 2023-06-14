29ae33c2f1a6dde54e5e12e8026c75190654f9e7
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-14 10:39:52 -0600

    Update nextflow_fusion.sh

--- 23-1ARMA/fusion_genes/nextflow_fusion.sh
+++ 23-1ARMA/fusion_genes/nextflow_fusion.sh
@@ -15,9 +15,9 @@ module load gentoo/2020
 module load singularity/3.9.2
 
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma
-star_indice=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/indices                              
+#star_indice=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/indices                              
 REF=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta
-#FASTA=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta/genome.fa
+FASTA=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta/genome.fa
 #FASTA_FAI=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta/genome.fa.fai
 #GTF=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
 mkdir -p ${DIR}/analysis/nextflow
@@ -25,11 +25,11 @@ mkdir -p ${DIR}/analysis/nextflow
 nextflow run nf-core/rnafusion -profile singularity \
                               --input ${DIR}/samplesheet.csv \
                               --outdir ${DIR}/analysis/nextflow \
-                              --genomes_base ${REF} \
-                              --star_index ${star_indice} \
                               --star_fusion \
                               --fusion_inspector \
                               --starfusion_build 'TRUE' \
                               --max_memory '80.GB' \
                               --max_cpus 16
 # #  --fasta ${FASTA} \
+#  --genomes_base ${REF} \
+#--star_index ${star_indice} \
