d8bd17c3aed73a4c533d755dbc2abf30f3d49ea4
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-14 22:28:19 -0600

    Update 01_fusioncatcherBuild.sh

--- 23-1ARMA/fusion_genes/01_fusioncatcherBuild.sh
+++ 23-1ARMA/fusion_genes/01_fusioncatcherBuild.sh
@@ -4,13 +4,13 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=genome_build
 #SBATCH --ntasks=1
-#BATCH --cpus-per-task=16
+#BATCH --cpus-per-task=8
 #SBATCH --time=10:00:00
-#SBATCH --mem=80G
+#SBATCH --mem=60G
 #SBATCH --output=%j.out
 
 module load python/2.7
-/globalhome/hxo752/HPC/anaconda3/condabin/conda activate 
+/globalhome/hxo752/HPC/anaconda3/condabin/conda activate biopython
 
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis
 fusioncatcher=/globalhome/hxo752/HPC/tools/fusioncatcher/bin
@@ -21,4 +21,4 @@ ${fusioncatcher}/fusioncatcher-build \
                               -o ${DIR}/fusioncatcher/build \
                               --organism="canis_familiaris" \
                               -w WEB_ENSEMBL="www.ensembl.org" \
-                              --threads=16
+                              --threads=8
