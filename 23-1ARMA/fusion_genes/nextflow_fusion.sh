ebaf390e4cd858d20e0a828be685a16a48f83f31
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-15 09:27:59 -0600

    Update nextflow_fusion.sh

--- 23-1ARMA/fusion_genes/nextflow_fusion.sh
+++ 23-1ARMA/fusion_genes/nextflow_fusion.sh
@@ -13,7 +13,6 @@ module load StdEnv/2020
 module load nextflow/22.10.6
 module load gentoo/2020
 module load singularity/3.9.2
-module load python/3.9
 
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma
 #star_indice=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/indices                              
