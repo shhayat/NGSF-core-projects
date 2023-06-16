fa7ae346966b279ff9abca23163c8486a9b45b06
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-16 14:49:50 -0600

    Update 03_genome_build_for_StarFusion.sh

--- 23-1ARMA/fusion_genes/03_genome_build_for_StarFusion.sh
+++ 23-1ARMA/fusion_genes/03_genome_build_for_StarFusion.sh
@@ -12,17 +12,14 @@
 source /globalhome/hxo752/HPC/.bashrc
 #conda activate star-fusion
 conda activate ame
-#OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/CanFamDB
-OUTDIR=/globalhome/hxo752/HPC/CanFamDB
-GTF=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Annotation/Genes/genes.gtf
-GENOME=/datastore/NGSF001/analysis/references/iGenomes/Dog/Canis_familiaris/Ensembl/CanFam3.1/Sequence/WholeGenomeFasta/genome.fa
+
+GTF=/datastore/NGSF001/analysis/references/dog/CanFam3.1/Canis_lupus_familiaris.ROS_Cfam_1.0.109.gtf
+GENOME=/datastore/NGSF001/analysis/references/dog/CanFam3.1/Canis_lupus_familiaris.ROS_Cfam_1.0.dna.toplevel.fa
 DFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/Pfam-A.hmm
 
-mkdir -p ${OUTDIR}
 #https://github-wiki-see.page/m/NCIP/ctat-genome-lib-builder/wiki/Building-a-Custom-CTAT-Genome-Lib
 #https://github.com/STAR-Fusion/STAR-Fusion/blob/master/Docker/Dockerfile
 prep_genome_lib.pl --genome_fa ${GENOME} \
                        --gtf ${GTF} \
                        --dfam_db ${DFAM_DATABASE} \
-                       --CPU 4 \
-                       --output_dir ${OUTDIR}
+                       --CPU 8
