0ee3198be116622787dc547d33d957dd3a338869
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-16 16:28:50 -0600

    Update 04_StarFusion.sh

--- 23-1ARMA/fusion_genes/04_StarFusion.sh
+++ 23-1ARMA/fusion_genes/04_StarFusion.sh
@@ -1,1 +1,11 @@
-/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/ctat_genome_lib_build_dir
+
+
+output_dir=
+CanineStarFusionBuild=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/ctat_genome_lib_build_dir
+
+mkdir -p ${output_dir}
+STAR-Fusion --left_fq ../Rawfastq/fname.R1 \
+            --right_fq ../Rawfastq/fname.R2 \
+            --genome_lib_dir CanineStarFusionBuild \
+            --output_dir ${output_dir}/fname.code.fusion
+
