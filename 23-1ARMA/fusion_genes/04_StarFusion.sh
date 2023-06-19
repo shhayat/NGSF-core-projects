55ca7de7314bec7717e62e1ea2e5b068a7119fcd
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-18 23:11:39 -0600

    Update 04_StarFusion.sh

--- 23-1ARMA/fusion_genes/04_StarFusion.sh
+++ 23-1ARMA/fusion_genes/04_StarFusion.sh
@@ -16,9 +16,11 @@ conda activate star-fusion
 output_dir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/starFusion
 CanineStarFusionBuild=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/ctat_genome_lib_build_dir
 
+fq1=$1;
+fq2=$1
 mkdir -p ${output_dir}
-STAR-Fusion --left_fq ../Rawfastq/fname.R1 \
-            --right_fq ../Rawfastq/fname.R2 \
+STAR-Fusion --left_fq $fq1 \
+            --right_fq $fq2 \
             --chimeric_junction \
             --genome_lib_dir CanineStarFusionBuild \
             --output_dir ${output_dir}/fname.code.fusion \
