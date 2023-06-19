2a80f19316cce4da2bebf80192bb4586402b4ed0
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-19 16:26:07 -0600

    Update 03_genome_build_for_StarFusion.sh

--- 23-1ARMA/fusion_genes/03_genome_build_for_StarFusion.sh
+++ 23-1ARMA/fusion_genes/03_genome_build_for_StarFusion.sh
@@ -9,18 +9,18 @@
 #SBATCH --time=40:00:00
 #SBATCH --output=%j.out
 
-source /globalhome/hxo752/HPC/.bashrc
-#conda activate star-fusion
-conda activate ame
+#source /globalhome/hxo752/HPC/.bashrc
+#conda activate ame
 
+ctat_genome=/globalhome/hxo752/HPC/tools/STAR-Fusion/ctat-genome-lib-builder
 GTF=/datastore/NGSF001/analysis/references/dog/CanFam3.1/Canis_lupus_familiaris.ROS_Cfam_1.0.109.gtf
 GENOME=/datastore/NGSF001/analysis/references/dog/CanFam3.1/Canis_lupus_familiaris.ROS_Cfam_1.0.dna.toplevel.fa
 DFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/Pfam/Pfam-A.hmm
 
 #https://github-wiki-see.page/m/NCIP/ctat-genome-lib-builder/wiki/Building-a-Custom-CTAT-Genome-Lib
 #https://github.com/STAR-Fusion/STAR-Fusion/blob/master/Docker/Dockerfile
-prep_genome_lib.pl --genome_fa ${GENOME} \
-                       --gtf ${GTF} \
-                       --dfam_db ${DFAM_DATABASE} \
-                       --CPU 4
+${ctat_genome}/prep_genome_lib.pl --genome_fa ${GENOME} \
+                                  --gtf ${GTF} \
+                                  --dfam_db ${DFAM_DATABASE} \
+                                  --CPU 4
 conda deactivate
