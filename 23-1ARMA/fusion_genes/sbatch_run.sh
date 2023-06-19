5e52db95e15c0795cbbfe0a9bb42fff6c84d5c67
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-19 13:23:05 -0600

    Update sbatch_run.sh

--- 23-1ARMA/fusion_genes/sbatch_run.sh
+++ 23-1ARMA/fusion_genes/sbatch_run.sh
@@ -23,10 +23,20 @@ do
 done
 
 
-star_dir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/star_alignment
-for i in ${star_dir}/D*/Chimeric.out.junction
+#star_dir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/star_alignment
+#for i in ${star_dir}/D*/Chimeric.out.junction
+#do
+#      sbatch ${SCRIPT_DIR}/04_StarFusion.sh "${i}" 
+#done
+
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/fastq
+
+for i in $DATA/*_1.fastq.gz
 do
-      sbatch ${SCRIPT_DIR}/04_StarFusion.sh "${i}" 
+      path="${i%_1*}";
+      sample_name=${path##*/};
+      fq1=${DATA}/${sample_name}_1.fastq.gz;
+      fq2=${DATA}/${sample_name}_2.fastq.gz;
+      sbatch ${SCRIPT_DIR}/02_star_mapping.sh "${fq1}" "${fq2}"
 done
-      
 
