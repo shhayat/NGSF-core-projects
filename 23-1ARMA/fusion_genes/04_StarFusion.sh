f4d105318aad573c59e94238415d55c23ecefa7f
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-19 13:22:25 -0600

    Update 04_StarFusion.sh

--- 23-1ARMA/fusion_genes/04_StarFusion.sh
+++ 23-1ARMA/fusion_genes/04_StarFusion.sh
@@ -4,24 +4,24 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=star-fusion
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH --mem=40G
 #SBATCH --time=06:00:00
 #SBATCH --output=%j.out
 
-
 source /globalhome/hxo752/HPC/.bashrc
 conda activate star-fusion
 
-star_dir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/star_alignment
 output_dir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/starFusion
 CanineStarFusionBuild=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/ctat_genome_lib_build_dir
 
-chimeric_junction=$1
+fq1=$1; shift
+fq2=$1;
 
 mkdir -p ${output_dir}
  STAR-Fusion --genome_lib_dir ${CanineStarFusionBuild} \
-             -J ${chimeric_junction} \
+             --left_fq ${fq1} \
+             --right_fq ${fq2} \
              --output_dir ${output_dir} \
              --CPU 4 
 conda deactivate
