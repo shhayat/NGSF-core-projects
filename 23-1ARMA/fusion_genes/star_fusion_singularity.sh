82d89034ea995ac765c7c7ff67395a7777cf7b52
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-20 12:20:43 -0600

    Update star_fusion_singularity.sh

--- 23-1ARMA/fusion_genes/star_fusion_singularity.sh
+++ 23-1ARMA/fusion_genes/star_fusion_singularity.sh
@@ -9,15 +9,17 @@
 #SBATCH --time=06:00:00
 #SBATCH --output=%j.out
 
-module load apptainer/1.1
+module load apptainer/1.1.5
 GTF=/datastore/NGSF001/analysis/references/dog/CanFam3.1/Canis_lupus_familiaris.ROS_Cfam_1.0.109.gtf
 GENOME=/datastore/NGSF001/analysis/references/dog/CanFam3.1/Canis_lupus_familiaris.ROS_Cfam_1.0.dna.toplevel.fa
-DFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/Pfam/Pfam-A.hmm
+#DFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/Pfam/Pfam-A.hmm
+DFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/Dfam/canis_lupus_familiaris_dfam.hmm
 
 apptainer exec -e /globalhome/hxo752/HPC/tools/star-fusion.v1.11.0.simg \
                     /globalhome/hxo752/HPC/tools/STAR-Fusion-v1.11.0/ctat-genome-lib-builder/prep_genome_lib.pl \
-                 --genome_fa ${GENOME} \
-                 --gtf ${GTF} \
-                 --dfam_db ${DFAM_DATABASE} \
-                 --CPU 4
+                --genome_fa ${GENOME} \
+                --gtf ${GTF} \
+                --dfam_db ${DFAM_DATABASE} \
+                --pfam_db current \
+                --CPU 4
                  
