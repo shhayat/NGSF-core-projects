9f3ab03f75da3ff9765813408495747f2d488447
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-20 10:24:47 -0600

    Update star_fusion_singularity.sh

--- 23-1ARMA/fusion_genes/star_fusion_singularity.sh
+++ 23-1ARMA/fusion_genes/star_fusion_singularity.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=genome_index
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=80G
+#SBATCH --time=40:00:00
+#SBATCH --output=%j.out
+
+/globalhome/hxo752/HPC/tools/star-fusion.v1.11.0.simg
