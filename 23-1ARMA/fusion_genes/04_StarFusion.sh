8fecc7ed7e9d7168e683a781df99d547bf9be323
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-20 14:53:59 -0600

    Update 04_StarFusion.sh

--- 23-1ARMA/fusion_genes/04_StarFusion.sh
+++ 23-1ARMA/fusion_genes/04_StarFusion.sh
@@ -4,7 +4,7 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=star-fusion
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=80G
 #SBATCH --time=10:00:00
 #SBATCH --output=%j.out
@@ -15,6 +15,7 @@ conda activate star-fusion
 #starfusion=/globalhome/hxo752/HPC/tools/STAR-Fusion
 output_dir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/starFusion
 #CanineStarFusionBuild=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/ctat_genome_lib_build_dir
+#CanineStarFusionBuild=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/ctat_genome_lib_build_dir
 CanineStarFusionBuild=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/ctat_genome_lib_build_dir
 sample_name=$1; shift
 fq1=$1; shift
@@ -26,7 +27,7 @@ STAR-Fusion --genome_lib_dir ${CanineStarFusionBuild} \
              --left_fq ${fq1} \
              --right_fq ${fq2} \
              --output_dir ${output_dir}/${sample_name} \
-             --CPU 1
+             --CPU 4
 
 conda deactivate
 
