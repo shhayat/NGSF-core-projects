20bc228ac2808a18c377385f061aadb6187de61a
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-20 15:51:50 -0600

    Update star_fusion_singularity.sh

--- 23-1ARMA/fusion_genes/star_fusion_singularity.sh
+++ 23-1ARMA/fusion_genes/star_fusion_singularity.sh
@@ -1,3 +1,16 @@
+#!/bin/sh
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=genome_index
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=80G
+#SBATCH --time=06:00:00
+#SBATCH --output=%j.out
+
+module load singularity/3.9.2
+
 singularity exec -e -B `pwd` -B /path/to/ctat_genome_lib_build_dir \
         star-fusion-v$version.simg \
         STAR-Fusion \
