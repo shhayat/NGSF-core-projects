7213d03365d5663d1e78050163840daf298d2cba
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-20 10:36:00 -0600

    Update star_fusion_singularity.sh

--- 23-1ARMA/fusion_genes/star_fusion_singularity.sh
+++ 23-1ARMA/fusion_genes/star_fusion_singularity.sh
@@ -9,14 +9,10 @@
 #SBATCH --time=40:00:00
 #SBATCH --output=%j.out
 
-/globalhome/hxo752/HPC/tools/star-fusion.v1.11.0.simg
-singularity exec -e \
-/globalhome/hxo752/HPC/tools/star-fusion.v1.11.0.simg \
-/usr/local/src/STAR-Fusion/ctat-genome-lib-builder/prep_genome_lib.pl \
---genome_fa ref_genome.fa \
---gtf ref_annot.gtf \
---fusion_annot_lib CTAT_HumanFusionLib.v0.1.0.dat.gz \
---annot_filter_rule AnnotFilterRule.pm \
---pfam_db current \
---dfam_db human \
---human_gencode_filter
+module load apptainer/1.1
+
+singularity exec -e /globalhome/hxo752/HPC/tools/star-fusion.v1.11.0.simg \
+                    /globalhome/hxo752/HPC/tools/STAR-Fusion-v1.11.0/ctat-genome-lib-builder/prep_genome_lib.pl \
+                 --genome_fa ref_genome.fa \
+                 --gtf ref_annot.gtf \
+                 
