03f26276ac0cb21f2b3d414f097b0a20cba947ca
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-21 08:42:19 -0600

    Update star_fusion_singularity.sh

--- 23-1ARMA/fusion_genes/star_fusion_singularity.sh
+++ 23-1ARMA/fusion_genes/star_fusion_singularity.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --account=hpc_p_anderson
 #SBATCH --constraint=skylake
-#SBATCH --job-name=genome_index
+#SBATCH --job-name=star_fusion
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=80G
