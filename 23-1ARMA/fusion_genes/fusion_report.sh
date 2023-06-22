2033df0b6887d93ce35bca3b617ef12bce178fe8
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-22 15:14:42 -0600

    Create fusion_report.sh

---
+++ 23-1ARMA/fusion_genes/fusion_report.sh
@@ -0,0 +1,7 @@
+
+fusion_report_tool=/globalhome/hxo752/HPC/tools/fusion-reports
+
+
+python ${fusion_report_tool}/create_fusion_report.py \
+--input_file_prefix /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/starFusion/D*/FusionInspector-validate/finspector.fusion_inspector_web.html \
+--html_output test.html
