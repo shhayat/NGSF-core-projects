bd468ae10fa736668ce1d7b58316dfed83e8de7b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-23 12:10:37 -0600

    Update sbatch_run.sh

--- 23-1ARMA/fusion_genes/sbatch_run.sh
+++ 23-1ARMA/fusion_genes/sbatch_run.sh
@@ -10,7 +10,6 @@ do
       sbatch ${SCRIPT_DIR}/01_FastQC.sh "${fq1}" "${fq2}"
  done
 
-#submit star alignment job
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/fastq
 
 for i in $DATA/*_1.fastq.gz
@@ -19,24 +18,16 @@ do
       sample_name=${path##*/};
       fq1=${DATA}/${sample_name}_1.fastq.gz;
       fq2=${DATA}/${sample_name}_2.fastq.gz;
-      sbatch ${SCRIPT_DIR}/02_star_mapping.sh "${sample_name}" "${fq1}" "${fq2}"
+      sbatch ${SCRIPT_DIR}/02_star_fusion_singularity.sh "${sample_name}" "${fq1}" "${fq2}"
 done
 
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/starFusion
 
-#star_dir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/star_alignment
-#for i in ${star_dir}/D*/Chimeric.out.junction
-#do
-#      sbatch ${SCRIPT_DIR}/04_StarFusion.sh "${i}" 
-#done
-
-DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/fastq
-
-for i in $DATA/*_1.fastq.gz
+for i in $DATA/*/finspector.FusionInspector.fusions.abridged.tsv
 do
-      path="${i%_1*}";
+      path="${i%/fin*}";
       sample_name=${path##*/};
-      fq1=${DATA}/${sample_name}_1.fastq.gz;
-      fq2=${DATA}/${sample_name}_2.fastq.gz;
-      sbatch ${SCRIPT_DIR}/star_fusion_singularity.sh "${sample_name}" "${fq1}" "${fq2}"
+
+      sbatch ${SCRIPT_DIR}/03_NTRK_fusion_genes.sh "${sample_name}"
 done
 
