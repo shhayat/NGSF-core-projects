2af51aeca9dd6b98635fd810a657f6e045f28404
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-26 16:32:41 -0600

    Update 00_download_data.sh

--- 23-1ARMA/fusion_genes/00_download_data.sh
+++ 23-1ARMA/fusion_genes/00_download_data.sh
@@ -3,11 +3,24 @@
 #for i in {6..9}
 #do
   ##NUM="${i:2}"
- # OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/fq_hystiocystic_sarcoma
+ # OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/fastq
  # mkdir -p $OUTDIR
  # echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/DRR345/DRR34598${i}/DRR34598${i}_1.fastq.gz" >> $OUTDIR/path_to_fastq_files_R1.txt
  # echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/DRR345/DRR34598${i}/DRR34598${i}_2.fastq.gz" >> $OUTDIR/path_to_fastq_files_R2.txt
 #done
+
+#fastq files for Canine Hemangiosarcoma
+#study: Genomically Complex Human Angiosarcoma and Canine Hemangiosarcoma Establish Convergent Angiogenic Transcriptional Programs Driven by Novel Gene Fusions
+for i in {10..60}
+do
+  #NUM="${i:2}"
+  OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hemangiosarcoma/fastq
+  mkdir -p $OUTDIR
+  echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR527/SRR52780${i}/SRR52780${i}_1.fastq.gz" >> $OUTDIR/path_to_fastq_files_R1.txt
+  echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR527/SRR52780${i}/DRR34598${i}_2.fastq.gz" >> $OUTDIR/path_to_fastq_files_R2.txt
+done
+
+
 
 
 #cortisol-secreting adrenocortical tumor
