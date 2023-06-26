3ef7c24c7dd2e0e9c7006f4f5f23764cc8825c15
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-26 15:21:56 -0600

    Update download_data.sh

--- 23-1ARMA/fusion_genes/download_data.sh
+++ 23-1ARMA/fusion_genes/download_data.sh
@@ -13,11 +13,11 @@
 #cortisol-secreting adrenocortical tumor
 #study: Transcriptome sequencing reveals two subtypes of cortisol-secreting adrenocortical tumors in dogs and identifies CYP26B1 as a potential new therapeutic target
 
-for i in {857..952}
-do
-  NUM="${i:2}"
-  OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/fq_adrenocorticalt_umor
-  mkdir -p $OUTDIR
-  echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR178/0${NUM}/SRR17882${i}/SRR17882${i}.fastq.gz" >> $OUTDIR/path_to_fastq_files_R1.txt
-done
-SRR17882857
+#for i in {857..952}
+#do
+#  NUM="${i:2}"
+#  OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/fq_adrenocorticalt_umor
+#  mkdir -p $OUTDIR
+#  echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR178/0${NUM}/SRR17882${i}/SRR17882${i}.fastq.gz" >> $OUTDIR/path_to_fastq_files_R1.txt
+#done
+
