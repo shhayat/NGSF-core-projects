a6f19d983b8e052f36e47e0134b103166005e8fc
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-27 12:20:04 -0600

    Update 03_NTRK_gene_fusions.sh

--- 23-1ARMA/fusion_genes/03_NTRK_gene_fusions.sh
+++ 23-1ARMA/fusion_genes/03_NTRK_gene_fusions.sh
@@ -10,11 +10,11 @@
 #SBATCH --output=%j.out
 
 
-dir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/
+dir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hemangiosarcoma/analysis/
 sample_name=$1;
 
 mkdir -p ${dir}/NTRK_fusions
 
 grep "NTRK" ${dir}/starFusion/${sample_name}/FusionInspector-validate/finspector.FusionInspector.fusions.abridged.tsv > \
-                                                                                                              ${dir}/NTRK_fusions/${sample_name}_fuisons.tsv
+                                                                                                              ${dir}/NTRK_fusions/${sample_name}_NTRKfuisons.tsv
 
