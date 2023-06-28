229873316935d070595399d4ebbf1760c6de3c51
Shahina <91986031+shhayat@users.noreply.github.com>
2023-06-28 14:09:55 -0600

    Update 00_download_data.sh

--- 23-1ARMA/fusion_genes/00_download_data.sh
+++ 23-1ARMA/fusion_genes/00_download_data.sh
@@ -12,7 +12,7 @@
 #fastq files for Canine Hemangiosarcoma
 #study: Genomically Complex Human Angiosarcoma and Canine Hemangiosarcoma Establish Convergent Angiogenic Transcriptional Programs Driven by Novel Gene Fusions
 #10..60
-#for i in {30..60}
+#for i in {10..29}
 #do
 #  NUM="${i:1}"
 #  OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hemangiosarcoma/fastq
@@ -23,17 +23,6 @@
 
 #31934
 #31948
-
-#fastq files for canine Urothelial Bladder Carcinoma
-#study: Comprehensive gene analysis of Canine Invasive Urothelial Bladder Carcinoma by RNA-Seq
-for i in {21..33}
-do
-  NUM="${i:2}"
- OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/urothelial_bladder/fastq
-  mkdir -p $OUTDIR
-  echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/DRR093/DRR0935${i}/DRR0935${i}_1.fastq.gz" >> $OUTDIR/path_to_fastq_files_R1.txt
-  echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/DRR093/DRR0935${i}/DRR0935${i}_2.fastq.gz" >> $OUTDIR/path_to_fastq_files_R2.txt
-done
 
 
 #cortisol-secreting adrenocortical tumor
