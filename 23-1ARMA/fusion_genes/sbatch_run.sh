06758c4de887d31ccb499a7884ba2165d4cc8e6d
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-04 10:21:21 -0600

    Update sbatch_run.sh

--- 23-1ARMA/fusion_genes/sbatch_run.sh
+++ 23-1ARMA/fusion_genes/sbatch_run.sh
@@ -22,7 +22,7 @@ do
 done
 
 #DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/starFusion
-DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/squamous_cell_carcinomas/analysis/
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/
 for i in $DATA/*/FusionInspector-validate/finspector.FusionInspector.fusions.abridged.tsv
 do
       path="${i%/FusionInspector*}";
