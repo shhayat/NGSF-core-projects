62e7cc1a15f30c3e3253575820fc052a0ef7034f
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-06 09:41:02 -0600

    Update sbatch_run.sh

--- 23-1ARMA/fusion_genes/sbatch_run.sh
+++ 23-1ARMA/fusion_genes/sbatch_run.sh
@@ -1,32 +1,37 @@
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes
 #submit fastqc job
-DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/fastq
-for i in $DATA/*_1.fastq.gz
-do
-      path="${i%_1*}";
-      sample_name=${path##*/};
-      fq1=${DATA}/${sample_name}_1.fastq.gz;
-      fq2=${DATA}/${sample_name}_2.fastq.gz;
-      sbatch ${SCRIPT_DIR}/01_FastQC.sh "${fq1}" "${fq2}"
- done
+#DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/fastq
+#for i in $DATA/*_1.fastq.gz
+#do
+#      path="${i%_1*}";
+#      sample_name=${path##*/};
+#      fq1=${DATA}/${sample_name}_1.fastq.gz;
+#      fq2=${DATA}/${sample_name}_2.fastq.gz;
+#      sbatch ${SCRIPT_DIR}/01_FastQC.sh "${fq1}" "${fq2}"
+ #done
 
 #DATA=/datastore/NGSF001/datasets/canine_datasets/hystiocystic_sarcoma
-DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/fastq
-for i in $DATA/*_1.fastq.gz
+#DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/fastq
+DATA=/datastore/NGSF001/datasets/canine_datasets/lymphoma
+#for i in $DATA/*_1.fastq.gz
+for i in $DATA/*.fastq.gz
 do
-      path="${i%_1*}";
+      path="${i%.fastq.gz*}";
       sample_name=${path##*/};
       fq1=${DATA}/${sample_name}_1.fastq.gz;
-      fq2=${DATA}/${sample_name}_2.fastq.gz;
-      sbatch ${SCRIPT_DIR}/02_star_fusion_singularity.sh "${sample_name}" "${fq1}" "${fq2}"
+      #fq2=${DATA}/${sample_name}_2.fastq.gz;
+     # sbatch ${SCRIPT_DIR}/02_star_fusion_singularity.sh "${sample_name}" "${fq1}" "${fq2}"
+     sbatch ${SCRIPT_DIR}/02_star_fusion_singularity.sh "${sample_name}" "${fq1}"
 done
 
 #DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/starFusion
-DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/
+#DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/lymphoma/analysis/
 for i in $DATA/*/FusionInspector-validate/finspector.FusionInspector.fusions.abridged.tsv
 do
       path="${i%/FusionInspector*}";
       sample_name=${path##*/};
       sbatch ${SCRIPT_DIR}/03_NTRK_gene_fusions.sh "${sample_name}"
 done
+
 
