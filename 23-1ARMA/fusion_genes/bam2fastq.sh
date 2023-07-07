2c6d8b7ec1c5aeb37bb46c796436f3d74f0f7530
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-07 15:22:47 -0600

    Update bam2fastq.sh

--- 23-1ARMA/fusion_genes/bam2fastq.sh
+++ 23-1ARMA/fusion_genes/bam2fastq.sh
@@ -4,25 +4,28 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=bam2fastq
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=10G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=20G
 #SBATCH --time=05:00:00
 #SBATCH --output=%j.out
 
-module load picard
+module load samtools
 
 DATA=/datastore/NGSF001/datasets/canine_datasets/icdc_data/bam
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/icdc_data/analysis/fastq
 mkdir -p ${OUTDIR}
 sample_name=$1;
-NCPU=2
 
-java -Xmx10G -XX:ParallelGCThreads=$NCPU -jar $EBROOTPICARD/picard.jar SamToFastq \
-                                    I=${DATA}/${sample_name}_sorted.bam \
-                                    F=${OUTDIR}/${sample_name}_1.fastq \
-                                    F2=${OUTDIR}${sample_name}_2.fastq
+#java -Xmx10G -XX:ParallelGCThreads=$NCPU -jar $EBROOTPICARD/picard.jar SamToFastq \
+#                                    I=${DATA}/${sample_name}_sorted.bam \
+#                                    F=${OUTDIR}/${sample_name}_1.fastq \
+#                                    F2=${OUTDIR}${sample_name}_2.fastq
                                     
-#samtools bam2fq ${DATA}/${sample_name}_sorted.bam  > ${OUTDIR}/${sample_name}.fastq
-#java -Xmx2g -jar Picard/SamToFastq.jar I=SAMPLE.bam F=SAMPLE_r1.fastq F2=SAMPLE_r2.fastq
+samtools bam2fq ${DATA}/${sample_name}_sorted.bam  > ${OUTDIR}/${sample_name}.fastq
+
+cat ${OUTDIR}/${sample_name}.fastq | grep '^@.*/1$' -A 3 --no-group-separator > ${OUTDIR}/${sample_name}_1.fastq
+cat ${OUTDIR}/${sample_name}.fastq | grep '^@.*/2$' -A 3 --no-group-separator > ${OUTDIR}/${sample_name}_2.fastq
+
+rm ${OUTDIR}/${sample_name}.fastq
 
 
