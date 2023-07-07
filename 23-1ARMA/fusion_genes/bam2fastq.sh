13794ecd744deb96731490591d6ee1c1ed857ed4
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-07 14:18:49 -0600

    Create bam2fastq.sh

---
+++ 23-1ARMA/fusion_genes/bam2fastq.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=star_fusion
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
+#SBATCH --time=05:00:00
+#SBATCH --output=%j.out
+
+module load samtools
+
+DATA=/datastore/NGSF001/datasets/canine_datasets/icdc_data/fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/lymphoma/analysis/fastq
+mkdir -p ${OUTDIR}
+sample_name=1;
+
+samtools bam2fq ${DATA}/${sample_name}_sorted.bam  > ${OUTDIR}/${sample_name}.fastq
+
