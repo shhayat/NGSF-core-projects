9fc2aeca0e39a82a7a8e39a159cf8d872e4d7479
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-10 13:47:08 -0600

    Update 00_download_data.sh

--- 23-1ARMA/fusion_genes/00_download_data.sh
+++ 23-1ARMA/fusion_genes/00_download_data.sh
@@ -54,8 +54,8 @@ do
   NUM="${i:1}"
   OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/urothelial_carcinoma/fastq
   mkdir -p $OUTDIR
-  echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR993/00${NUM}/SRR9936${i}/SRR9936${i}_1.fastq.gz" >> $OUTDIR/path_to_fastq_files.sh
-  echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR993/00${NUM}/SRR9936${i}/SRR9936${i}_2.fastq.gz" >> $OUTDIR/path_to_fastq_files.sh
+  echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR993/00${NUM}/SRR99367${i}/SRR99367${i}_1.fastq.gz" >> $OUTDIR/path_to_fastq_files.sh
+  echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR993/00${NUM}/SRR99367${i}/SRR99367${i}_2.fastq.gz" >> $OUTDIR/path_to_fastq_files.sh
 done
 
 
