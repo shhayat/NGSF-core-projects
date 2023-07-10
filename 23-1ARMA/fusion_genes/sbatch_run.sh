f873fd711e5d73fd95fda462870fac63f6e391dd
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-10 10:38:58 -0600

    Update sbatch_run.sh

--- 23-1ARMA/fusion_genes/sbatch_run.sh
+++ 23-1ARMA/fusion_genes/sbatch_run.sh
@@ -12,7 +12,7 @@ SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fus
 
 #DATA=/datastore/NGSF001/datasets/canine_datasets/hystiocystic_sarcoma
 #DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/fastq
-DATA=/datastore/NGSF001/datasets/canine_datasets/lymphoma
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/lymphoma/analysis/fastq
 #for i in $DATA/*_1.fastq.gz
 for i in $DATA/*.fastq.gz
 do
