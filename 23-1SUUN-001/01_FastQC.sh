d10bdde69eaa914eaafaf7066fd25a12a7ba130a
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-13 10:44:45 -0600

    Create 01_FastQC.sh

---
+++ 23-1SUUN-001/01_FastQC.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=fastqc
+#SBATCH --ntasks=6
+#BATCH --cpus-per-task=10
+#SBATCH --time=02:00:00
+#SBATCH --mem=20G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+module load fastqc
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SUUN-001/analysis/fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SUUN-001/analysis
+
+mkdir -p ${OUTDIR}/fastqc
+
+for fq in $DATA/R23*.fastq.gz
+do
+   fastqc -o ${OUTDIR}/fastqc --extract ${fq}
+   
+done 
+
+wait 
+
+cd /datastore/NGSF001/software/tools/
+./multiqc ${OUTDIR}/fastqc/*_fastqc.zip -o ${OUTDIR}/fastqc
