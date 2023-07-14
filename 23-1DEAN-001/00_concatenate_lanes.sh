95e7b586a56fc9bbd6dc958731f45f1419b47e8d
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-14 13:58:32 -0600

    Update 00_concatenate_lanes.sh

--- 23-1DEAN-001/00_concatenate_lanes.sh
+++ 23-1DEAN-001/00_concatenate_lanes.sh
@@ -13,11 +13,11 @@ set -eux
 mkdir -p /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/fastq 
 
 FASTQ_FOLDER=/datastore/NGSF001/NB551711/230713_NB551711_0077_AHGWY7BGXT/Alignment_1/20230713_234249/Fastq
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SUUN-001/analysis/fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/fastq
 	
 
-for i in $(seq -w 131 142)
+for i in $(seq -w 33 43)
 do
-	echo Combining library R2300${i}
-	cat ${FASTQ_FOLDER}/R2300${i}_*_R1_* > ${OUTDIR}/R2300${i}.fastq.gz
+	echo Combining library D23000${i}
+	cat ${FASTQ_FOLDER}/D23000${i}* > ${OUTDIR}/D23000${i}.fastq.gz
 done
