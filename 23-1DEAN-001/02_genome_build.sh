7ddc0f605dd282ed99120965e4a8b1b5a2c43f2e
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-14 14:17:59 -0600

    Update 02_genome_build.sh

--- 23-1DEAN-001/02_genome_build.sh
+++ 23-1DEAN-001/02_genome_build.sh
@@ -15,4 +15,4 @@ cd /globalhome/hxo752/HPC/tools/bowtie2-2.4.5-linux-x86_64
 GENOME=/datastore/NGSF001/analysis/references/iGenomes/Homo_sapiens/NCBI/GRCh38/Sequence/WholeGenomeFasta
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects//analysis/
 
-bowtie2-build ${GENOME}/genome.fa ${OUTDIR}/bowtie_index --threads ${NCPU}
+bowtie2-build ${GENOME}/genome.fa ${OUTDIR}/bowtie_index
