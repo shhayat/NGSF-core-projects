28dc31a5158ceb8b561c6cbcc454abe3ac151edf
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-17 09:38:31 -0600

    Update 03_bowtie_alignment.sh

--- 23-1DEAN-001/03_bowtie_alignment.sh
+++ 23-1DEAN-001/03_bowtie_alignment.sh
@@ -15,7 +15,7 @@ module load samtools
 cd /globalhome/hxo752/HPC/tools/bowtie2-2.4.5-linux-x86_64
 
 RAW_DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/fastq
-GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/indices
+GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/indices/bowtie_index
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis
 NCPU=4
 
