3a9763426eaf9867e1c625d593560dc8b40032fe
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-17 13:43:51 -0600

    Update 04_QC_phantompeakqualtools.sh

--- 23-1DEAN-001/04_QC_phantompeakqualtools.sh
+++ 23-1DEAN-001/04_QC_phantompeakqualtools.sh
@@ -23,5 +23,5 @@ cd /globalhome/hxo752/HPC/tools/phantompeakqualtools
 #cross correlation
 Rscript run_spp.R -c=${OUTDIR}/alignment/${sample_name}/${sample_name}.aligned_sort.bam -savp=${OUTDIR}/QC/phantompeakqualtools/xcor_${sample_name}.pdf -out=${OUTDIR}/QC/phantompeakqualtools/xcor_metrics_${sample_name}.txt
 
-module unload r/4.1.2
+module unload r/4.2.1
 module unload samtools
