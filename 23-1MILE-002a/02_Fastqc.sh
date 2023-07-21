7a484c2d792f7959d79444c6a1a1d5fa661cfc59
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-21 13:55:36 -0600

    Update 02_Fastqc.sh

--- 23-1MILE-002a/02_Fastqc.sh
+++ 23-1MILE-002a/02_Fastqc.sh
@@ -11,11 +11,16 @@
 set -eux
 
 module load fastqc
-DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002a/analysis/fastq
+
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MILE-002a/analysis
-mkdir -p ${OUTDIR}/fastqc
+mkdir -p ${OUTDIR}/fastqc_for_23-1MILE-002a_and_23-1MILE-002_with_umi_header
+mkdir -p ${OUTDIR}/fastqc_for_23-1MILE-002a
 
 fq1=$1; shift
 fq2=$1;
-fastqc -o ${OUTDIR}/fastqc --extract ${fq1}
-fastqc -o ${OUTDIR}/fastqc --extract ${fq2}
+fastqc -o ${OUTDIR}/fastqc_for_23-1MILE-002a_and_23-1MILE-002_with_umi_header --extract ${fq1}
+fastqc -o ${OUTDIR}/fastqc_for_23-1MILE-002a_and_23-1MILE-002_with_umi_header --extract ${fq2}
+
+fastqc -o ${OUTDIR}/fastqc_for_23-1MILE-002a --extract ${fq1}
+fastqc -o ${OUTDIR}/fastqc_for_23-1MILE-002a --extract ${fq2}
+
