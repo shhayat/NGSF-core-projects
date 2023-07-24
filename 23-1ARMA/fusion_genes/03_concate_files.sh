9fc2a82de00496de0d38d8240d1dbaa269c017dc
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-24 15:13:32 -0600

    Update 03_concate_files.sh

--- 23-1ARMA/fusion_genes/03_concate_files.sh
+++ 23-1ARMA/fusion_genes/03_concate_files.sh
@@ -5,7 +5,7 @@ for i in ${dir}/*/star-fusion.fusion_predictions.tsv
 do
   tail -n +2 ${i} > ${dir}/fusion_genes_tmp.txt
   cat ${dir}/fusion_genes_tmp.txt >> ${dir}/${tumor_type}_fusion_genes.txt
-  sed  -i '1i #FusionName	JunctionReadCount	SpanningFragCount	est_J	est_S	SpliceType	LeftGene	LeftBreakpoint	RightGene	RightBreakpointJunctionReads	SpanningFrags	LargeAnchorSupport	FFPM	LeftBreakDinuc	LeftBreakEntropy	RightBreakDinuc	RightBreakEntropy	annots' ${dir}/${tumor_type}_fusion_genes.txt
   rm ${dir}/fusion_genes_tmp.txt
 done
+sed -i '1i #FusionName  JunctionReadCount  SpanningFragCount  est_J  est_S  SpliceType  LeftGene  LeftBreakpoint  RightGene  RightBreakpointJunctionReads  SpanningFrags  LargeAnchorSupport  FFPM  LeftBreakDinuc  LeftBreakEntropy  RightBreakDinuc  RightBreakEntropy  annots' ${dir}/${tumor_type}_fusion_genes.txt
 
