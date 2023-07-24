b51db8da5a9b452489b1d8e809ed67ca47caf01b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-24 14:55:28 -0600

    Update 03_concate_files.sh

--- 23-1ARMA/fusion_genes/03_concate_files.sh
+++ 23-1ARMA/fusion_genes/03_concate_files.sh
@@ -1,13 +1,11 @@
 dir=/datastore/NGSF001/projects/ARMA_NTRK_Fusion/lymphoma
 tumor_type="lymphoma"
 
-mkdir -p ${dir}/NTRK_fusions
-
 for i in ${dir}/*/star-fusion.fusion_predictions.tsv
 do
   tail -n +2 ${i} > ${dir}/fusion_genes_tmp.txt
-  cat ${dir}/fusion_genes_mod.txt >> ${dir}/${tumor_type}_fusion_genes.txt
-  sed  -i '1i #FusionName	JunctionReadCount	SpanningFragCount	est_J	est_S	SpliceType	LeftGene	LeftBreakpoint	RightGene	RightBreakpointJunctionReads	SpanningFrags	LargeAnchorSupport	FFPM	LeftBreakDinuc	LeftBreakEntropy	RightBreakDinuc	RightBreakEntropy	annots' ${dir}_fusion_genes.txt
+  cat ${dir}/fusion_genes_tmp.txt >> ${dir}/${tumor_type}_fusion_genes.txt
+  sed  -i '1i #FusionName	JunctionReadCount	SpanningFragCount	est_J	est_S	SpliceType	LeftGene	LeftBreakpoint	RightGene	RightBreakpointJunctionReads	SpanningFrags	LargeAnchorSupport	FFPM	LeftBreakDinuc	LeftBreakEntropy	RightBreakDinuc	RightBreakEntropy	annots' ${dir}/${tumor_type}_fusion_genes.txtt
   rm ${dir}/fusion_genes_tmp.txt
 done
 
