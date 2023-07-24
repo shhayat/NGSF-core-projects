494540655b17f03bc02c89b7d9392c32b486390b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-24 12:37:47 -0600

    Update and rename concate_files to concate_files.sh

---
+++ 23-1ARMA/fusion_genes/concate_files.sh
@@ -0,0 +1,14 @@
+dir=/datastore/NGSF001/projects/ARMA_NTRK_Fusion/lymphoma
+
+sample_name=$1;
+
+mkdir -p ${dir}/NTRK_fusions
+
+for i in ${dir}/*/star-fusion.fusion_predictions.tsv
+do
+  tail -n +2 ${i} > ${dir}/fusion_genes_tmp.txt
+  cat ${dir}/fusion_genes_mod.txt >> ${dir}_fusion_genes.txt
+  sed  -i '1i #FusionName	JunctionReadCount	SpanningFragCount	est_J	est_S	SpliceType	LeftGene	LeftBreakpoint	RightGene	RightBreakpointJunctionReads	SpanningFrags	LargeAnchorSupport	FFPM	LeftBreakDinuc	LeftBreakEntropy	RightBreakDinuc	RightBreakEntropy	annots' ${dir}_fusion_genes.txt
+  rm ${dir}/fusion_genes_tmp.txt
+done
+
