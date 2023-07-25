9c9dfc3fe86194a3845e465c5f3579e3f07b81d9
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-25 10:27:12 -0600

    Update 03_concate_files.sh

--- 23-1ARMA/fusion_genes/03_concate_files.sh
+++ 23-1ARMA/fusion_genes/03_concate_files.sh
@@ -1,5 +1,5 @@
-dir=/datastore/NGSF001/projects/ARMA_NTRK_Fusion/urothelial_carcinoma
-tumor_type="urothelial_carcinoma"
+dir=/datastore/NGSF001/projects/ARMA_NTRK_Fusion/lymphoma
+tumor_type="lymphoma"
 
 for i in ${dir}/*/star-fusion.fusion_predictions.tsv
 do
@@ -7,4 +7,5 @@ do
   cat ${dir}/fusion_genes_tmp.txt >> ${dir}/${tumor_type}_fusion_genes.txt
   rm ${dir}/fusion_genes_tmp.txt
 done
-echo -e "FusionName\tJunctionReadCount\tSpanningFragCount\test_J\test_S\tSpliceType\tLeftGene\tLeftBreakpoint\tRightGene\tRightBreakpointJunctionReads\tSpanningFrags\tLargeAnchorSupport\tFFPM\tLeftBreakDinuc\tLeftBreakEntropy\tRightBreakDinuc\tRightBreakEntropy\tannots" > file2 && cat file2 ${dir}/${tumor_type}_fusion_genes.txt >> ${dir}/${tumor_type}_fusion_genes.txt
+echo -e "FusionName\tJunctionReadCount\tSpanningFragCount\test_J\test_S\tSpliceType\tLeftGene\tLeftBreakpoint\tRightGene\tRightBreakpointJunctionReads\tSpanningFrags\tLargeAnchorSupport\tFFPM\tLeftBreakDinuc\tLeftBreakEntropy\tRightBreakDinuc\tRightBreakEntropy\tannots" > file2 && cat file2 ${dir}/${tumor_type}_fusion_genes.txt >> ${dir}/${tumor_type}_fusion_genes_v1.txt
+mv ${dir}/${tumor_type}_fusion_genes_v1.txt ${dir}/${tumor_type}_fusion_genes.txt
