8b62e436bf990562834661a7ecb4d9abd26dc44e
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-28 10:53:32 -0600

    Create 08_QC_ChipQC_after_peak_calling.sh

---
+++ 23-1DEAN-001/08_QC_ChipQC_after_peak_calling.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=QC-chipqc
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=2:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+
+module load r/4.1.2
+
+Rscript QC_ChipQC_after_peak_calling.R
