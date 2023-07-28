b3e06c8ddf711b9e866f3f7e42c9b5a1c03909a4
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-28 14:11:20 -0600

    Update 07_MACS3_peakcalling.sh

--- 23-1DEAN-001/07_MACS3_peakcalling.sh
+++ 23-1DEAN-001/07_MACS3_peakcalling.sh
@@ -13,7 +13,7 @@
 #cd /globalhome/hxo752/HPC/anaconda3/bin
 cd /globalhome/hxo752/HPC/.local/lib/python3.7/site-packages
 
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/peakcall
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/peakcall_with_pval0.05
 controlBAM=$1; shift
 treatBAM=$1; shift
 sample_name=$1
@@ -23,15 +23,16 @@ mkdir -p ${OUTDIR}/${sample_name}
 macs3 callpeak \
 		--treatment ${treatBAM} \
                 --control ${controlBAM} \
+		--tempdir ${SLURM_TMPDIR} \
                 --format BAM \
 		--gsize hs \
 		--keep-dup all \
                 --name ${sample_name} -B \
-	        --qvalue 0.05 \
+	        --pvalue 5e-2 \
                 --outdir ${OUTDIR}/${sample_name}/ && \
                 sort -k8,8nr ${OUTDIR}/${sample_name}/${sample_name}_peaks.narrowPeak > ${OUTDIR}/${sample_name}/${sample_name}_peaks.sorted.narrowPeak 
 		 # --pvalue 5e-2 \
-
+#--qvalue 0.05 \
 #extracting most relevant columns
 cut -f 1-3 ${OUTDIR}/${sample_name}/${sample_name}_peaks.sorted.narrowPeak > ${OUTDIR}/${sample_name}/${sample_name}_peaks.sorted.narrowPeak_selected_columns.bed
 
