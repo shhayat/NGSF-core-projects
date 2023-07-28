221ab5f51046876ac430c9610a6348c9a8407ed9
Shahina <91986031+shhayat@users.noreply.github.com>
2023-07-28 10:55:29 -0600

    Create QC_after_peak_calling.R

---
+++ 23-1DEAN-001/QC_after_peak_calling.R
@@ -0,0 +1,16 @@
+#https://github.com/hbctraining/Intro-to-ChIPseq/blob/master/lessons/06_combine_chipQC_and_metrics.md
+
+library(ChIPQC)
+dir.create("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/QC/ChIPQC")
+setwd("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/analysis/QC")
+
+# reading in the sample information (metadata)
+sampl_info <- read.csv("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/chip-seq/sampleSheet_for_chiqQC.csv")
+
+res=dba(sampleSheet=sampl_info, config=data.frame(RunParallel=FALSE))
+
+#Create ChIPQC object
+resQC = ChIPQC(res,annotation="mm10", config=data.frame(RunParallel=FALSE))
+
+# Create ChIPQC report
+ChIPQCreport(resQC, reportName="ChIPQC Report", reportFolder="ChIPQC")
