648bd3f547610eabade166e7059ec5b5564091f7
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-03 08:51:16 -0600

    Create 10_ChIPseeker_annotatePeaks_and_visualize.sh

---
+++ 23-1DEAN-001/10_ChIPseeker_annotatePeaks_and_visualize.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=homer-annotate-peaks
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=2:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+
+
+module load r/4.1.2
+
+Rscript annotatePeaks_and_visualize.R
