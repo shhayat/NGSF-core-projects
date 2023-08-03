1b469ba1e4312b3b580dfd1ae41e9b9751c153ae
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-03 10:40:19 -0600

    Update 08_QC_ChipQC_after_peak_calling.sh

--- 23-1DEAN-001/08_QC_ChipQC_after_peak_calling.sh
+++ 23-1DEAN-001/08_QC_ChipQC_after_peak_calling.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=2:00:00
 #SBATCH --mem=40G
-#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+#SBATCH  --output=%j.out
 
 /globalhome/hxo752/HPC/anaconda3/envs/bioconductor-chipqc/bin/
 Rscript QC_after_peak_calling.R
