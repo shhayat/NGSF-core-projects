885f7137211d6d608b2a4e77766c97ac4d24455b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-09 17:59:57 -0600

    Update 09_IDR_handling_replicates.sh

--- 23-1DEAN-001/09_IDR_handling_replicates.sh
+++ 23-1DEAN-001/09_IDR_handling_replicates.sh
@@ -35,18 +35,10 @@ idr --samples ${files} \
 #Column 5 contains the scaled IDR value, min(int(log2(-125IDR), 1000) For example, peaks with an IDR of 0 have a score of 1000, 
 #peaks with an IDR of 0.05 have a score of int(-125log2(0.05)) = 540, and IDR of 1.0 has a score of 0.
 #select IDR of 0.05
-awk '{if($5 >= 540) print $0}' ${OUTDIR}/idr.bed > ${OUTDIR}/idr_filtered.bed
+awk '{if($5 >= 540) print $0}' ${OUTDIR}/${}_idr.bed > ${OUTDIR}/${}_idr_filtered.bed
 
 #prep file for motif discovery
 
-cut -f 1,2,3 ${OUTDIR}/idr_filtered.bed > ${OUTDIR}/idr_filtered_3_columns.bed 
-
-#for motif discovery step repeat-masked version of the genome is required where all repeat sequences have been replaced with Ns
-#we will generate masked genome based on peak intervals in idr_filtered.bed 
-cd /globalhome/hxo752/HPC/tools
-chmod a+x bedtools.static.binary
-./bedtools.static.binary getfasta -fi /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/genome.fa \
-                                  -bed ${OUTDIR}/idr_filtered_3_columns.bed \
-                                  -fo ${OUTDIR}/genome.masked.on.idr_intervals.fa
+cut -f 1,2,3 ${OUTDIR}/${}_idr_filtered.bed > ${OUTDIR}/${}_idr_filtered_3_columns.bed 
 
 deactivate
