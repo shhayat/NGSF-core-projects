cdb93cf6410733e61020454eab1906c100da13d6
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-10 15:20:10 -0600

    Update annotatePeaks_and_visualize.R

--- 23-1DEAN-001/annotatePeaks_and_visualize.R
+++ 23-1DEAN-001/annotatePeaks_and_visualize.R
@@ -1,7 +1,7 @@
 library(ChIPseeker)
 library(clusterProfiler)
 library(org.Hs.eg.db)
-library(diffloop)
+#library(diffloop)
 
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
@@ -9,9 +9,9 @@ txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
 dir.create("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/peak_annotation")
 setwd("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/peak_annotation")
 #list bed files
-samplefiles <- list.files("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/chipr/", pattern= "optimal.bed", full.names=T)
+samplefiles <- list.files("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/chipr", pattern= "optimal.bed", full.names=T)
 samplefiles <- as.list(samplefiles)
-names(samplefiles) <- c("GE1")
+names(samplefiles) <- c("BT549","HCC1806")
 
 #read peaks
 ReadPeakList <- lapply(samplefiles, readPeakFile)
@@ -25,37 +25,39 @@ GenomeInfoDb::seqlevels(txdb)
 
 
 #since seqlevels for txdb has chr string before chromosome number we have to add chr string to our peaks column 1
-PeakList_with_added_chr_str <- lapply(ReadPeakList, diffloop::addchr)
+#PeakList_with_added_chr_str <- lapply(ReadPeakList, diffloop::addchr)
 
 #annotate peaks
 #2000bp = 2Kbp
-peakAnnoList <- lapply(PeakList_with_added_chr_str, annotatePeak, TxDb=txdb,tssRegion=c(-2000, 2000), 
-                       verbose=TRUE, annoDb="org.Mm.eg.db")
+peakAnnoList <- lapply(ReadPeakList, annotatePeak, TxDb=txdb,tssRegion=c(-2000, 2000), 
+                       verbose=TRUE, annoDb="org.Hs.eg.db")
 
 
 #annotations for each peaks stored in dataframe
-annot_df <- data.frame(peakAnnoList[[1]]@anno)
+annot_df_BT549 <- data.frame(peakAnnoList[[1]]@anno)
+annot_df_HCC1806 <- data.frame(peakAnnoList[[2]]@anno)
 
 #selected columns
-annot_df=annot_df[c(1:5,23:33)]
+annot_df_BT549=annot_df_BT549[c(1:5,23:33)]
+annot_df_HCC1806=annot_df_HCC1806[c(1:5,23:33)]
 
 #add gene name to annot_df
-write.csv(annot_df, "peaks_with_annotations.csv")
+write.csv(annot_df_BT549, "BT549_peaks_with_annotations.csv")
+write.csv(annot_df_HCC1806, "HCC1806_peaks_with_annotations.csv")
 
 
-pdf("chip_profile.pdf")
+pdf("BT549_chip_profile.pdf")
  #coverage plot 
- covplot(PeakList_with_added_chr_str[[1]], weightCol="V5")
-
+ covplot(peakAnnoList[[1]], weightCol="V5")
  #from cov plot we saw most of the peaks are at chr 19. we check cov plot for chr 19
- covplot(PeakList_with_added_chr_str[[1]], weightCol="V5", chrs=c("chr19"))
+ #covplot(PeakList_with_added_chr_str[[1]], weightCol="V5", chrs=c("chr19"))
 
  #Profile of ChIP peaks binding to TSS regions
   promoter <- getPromoters(TxDb=txdb, upstream=2000, downstream=2000)
-  tagMatrix <- getTagMatrix(PeakList_with_added_chr_str[[1]], windows=promoter)
+  tagMatrix <- getTagMatrix(peakAnnoList[[1]], windows=promoter)
 
   # preparing tagMatrix list
-  tagMatrixList <- list(GE1=tagMatrix)
+  tagMatrixList <- list(BT549=tagMatrix)
   
   # plotting tagMatrix heatmap
   tagHeatmap(tagMatrixList, xlim=c(-2000, 2000), color=NULL)
@@ -67,15 +69,72 @@ dev.off()
 
 
 #annotation plot : there are more plot funtion available for multiple cell line comparisions
-pdf("annotation_plots.pdf")
+pdf("HCC1806_annotation_plots.pdf")
   #Barchart of genomic feature representation
   plotAnnoBar(peakAnnoList)
 
   #To view full annotation overlaps
   #two packages (ggupset,ggimage) were installed separatly for running upsetplot 
-  upsetplot(peakAnnoList[[1]] , vennpie=TRUE)
+  upsetplot(peakAnnoList[[2]] , vennpie=TRUE)
   
   #Distribution of TF-binding loci relative to TSS
   plotDistToTSS(peakAnnoList, title="Distribution of transcription factor-binding loci \n relative to TSS")
+
+dev.off()
+
+
+pdf("BT549_chip_profile.pdf")
+ #coverage plot 
+ covplot(peakAnnoList[[1]], weightCol="V5")
+ #from cov plot we saw most of the peaks are at chr 19. we check cov plot for chr 19
+ #covplot(PeakList_with_added_chr_str[[1]], weightCol="V5", chrs=c("chr19"))
+
+ #Profile of ChIP peaks binding to TSS regions
+  promoter <- getPromoters(TxDb=txdb, upstream=2000, downstream=2000)
+  tagMatrix <- getTagMatrix(peakAnnoList[[1]], windows=promoter)
+
+  # preparing tagMatrix list
+  tagMatrixList <- list(BT549=tagMatrix)
+  
+  # plotting tagMatrix heatmap
+  tagHeatmap(tagMatrixList, xlim=c(-2000, 2000), color=NULL)
+
+  # plotting average profile of ChIP peaks 
+  plotAvgProf(tagMatrixList, xlim=c(-2000, 2000), xlab="Genomic Region (5'->3')", ylab = "Read Count Frequency")
+
+dev.off()
+
+pdf("HCC1806_chip_profile.pdf")
+ #coverage plot 
+ covplot(peakAnnoList[[2]], weightCol="V5")
+ #from cov plot we saw most of the peaks are at chr 19. we check cov plot for chr 19
+ #covplot(PeakList_with_added_chr_str[[1]], weightCol="V5", chrs=c("chr19"))
+
+ #Profile of ChIP peaks binding to TSS regions
+  promoter <- getPromoters(TxDb=txdb, upstream=2000, downstream=2000)
+  tagMatrix <- getTagMatrix(peakAnnoList[[2]], windows=promoter)
+
+  # preparing tagMatrix list
+  tagMatrixList <- list(HCC1806=tagMatrix)
+  
+  # plotting tagMatrix heatmap
+  tagHeatmap(tagMatrixList, xlim=c(-2000, 2000), color=NULL)
+
+  # plotting average profile of ChIP peaks 
+  plotAvgProf(tagMatrixList, xlim=c(-2000, 2000), xlab="Genomic Region (5'->3')", ylab = "Read Count Frequency")
+
+dev.off()
+
+#annotation plot : there are more plot funtion available for multiple cell line comparisions
+pdf("BHCC1806_annotation_plots.pdf")
+  #Barchart of genomic feature representation
+  plotAnnoBar(peakAnnoList[[2]])
+
+  #To view full annotation overlaps
+  #two packages (ggupset,ggimage) were installed separatly for running upsetplot 
+  upsetplot(peakAnnoList[[2]] , vennpie=TRUE)
+  
+  #Distribution of TF-binding loci relative to TSS
+  plotDistToTSS(peakAnnoList[[2]], title="Distribution of transcription factor-binding loci \n relative to TSS")
 
 dev.off()
