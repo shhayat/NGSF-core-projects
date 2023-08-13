6c4edda6fc6ee2846b0c9309e97025f5e070feab
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-12 22:36:59 -0600

    Update 03_concate_files.sh

--- 23-1ARMA/fusion_genes/03_concate_files.sh
+++ 23-1ARMA/fusion_genes/03_concate_files.sh
@@ -5,7 +5,7 @@ tumor_type="squamous_cell_carcinomas"
 for i in ${dir}/*/star-fusion.fusion_predictions.tsv
 do
   tail -n +2 ${i} > ${dir}/fusion_genes_tmp.txt
-  cat ${dir}/fusion_genes_tmp.txt >> ${dir}/${tumor_type}_fusion_genes.txt
+  (cat ${dir}/fusion_genes_tmp.txt; echo '') >> ${dir}/${tumor_type}_fusion_genes.txt
   rm ${dir}/fusion_genes_tmp.txt
 done
 echo -e "FusionName\tJunctionReadCount\tSpanningFragCount\test_J\test_S\tSpliceType\tLeftGene\tLeftBreakpoint\tRightGene\tRightBreakpointJunctionReads\tSpanningFrags\tLargeAnchorSupport\tFFPM\tLeftBreakDinuc\tLeftBreakEntropy\tRightBreakDinuc\tRightBreakEntropy\tannots" > ${dir}/file2 && cat ${dir}/file2 ${dir}/${tumor_type}_fusion_genes.txt >> ${dir}/${tumor_type}_fusion_genes_v1.txt
