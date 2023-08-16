973969ec5a2fdd8b3231f80a77919c2ec58e17fc
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-16 10:01:39 -0600

    Update 03_concate_files.sh

--- 23-1ARMA/fusion_genes/03_concate_files.sh
+++ 23-1ARMA/fusion_genes/03_concate_files.sh
@@ -1,15 +1,15 @@
 #dir=/datastore/NGSF001/projects/ARMA_NTRK_Fusion/lymphoma
 dir=/datastore/NGSF001/projects/ARMA_NTRK_Fusion/Fusion_events/hystiocystic_sarcoma
-outdir=
+outdir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/analysis
 tumor_type="squamous_cell_carcinomas"
 
 for i in ${dir}/*/FusionInspector-validate/finspector.FusionInspector.fusions.tsv
 do
-  tail -n +2 ${i} > ${dir}/fusion_genes_tmp.txt
-  (cat ${dir}/fusion_genes_tmp.txt; echo '') >> ${dir}/${tumor_type}_fusion_genes.txt
-  rm ${dir}/fusion_genes_tmp.txt
+  tail -n +2 ${i} > ${outdir}/fusion_genes_tmp.txt
+  (cat ${outdir}/fusion_genes_tmp.txt; echo '') >> ${outdir}/${tumor_type}_fusion_genes.txt
+  rm ${outdir}/fusion_genes_tmp.txt
 done
-echo -e "FusionName\tJunctionReadCount\tSpanningFragCount\test_J\test_S\tLeftGene\tLeftLocalBreakpoint\tLeftBreakpoint\tRightGene\tRightLocalBreakpoint\tRightBreakpoint\tSpliceType\tLargeAnchorSupport\tJunctionReads\tSpanningFrags\tNumCounterFusionLeft\tCounterFusionLeftReads\tNumCounterFusionRight\tCounterFusionRightReads FAR_left\tFAR_right\tLeftBreakDinuc\tLeftBreakEntropy\tRightBreakDinuc RightBreakEntropy\tFFPM\tmicroh_brkpt_dist\tnum_microh_near_brkpt"  > ${dir}/file2 && cat ${dir}/file2 ${dir}/${tumor_type}_fusion_genes.txt >> ${dir}/${tumor_type}_fusion_genes_v2.txt
+echo -e "FusionName\tJunctionReadCount\tSpanningFragCount\test_J\test_S\tLeftGene\tLeftLocalBreakpoint\tLeftBreakpoint\tRightGene\tRightLocalBreakpoint\tRightBreakpoint\tSpliceType\tLargeAnchorSupport\tJunctionReads\tSpanningFrags\tNumCounterFusionLeft\tCounterFusionLeftReads\tNumCounterFusionRight\tCounterFusionRightReads FAR_left\tFAR_right\tLeftBreakDinuc\tLeftBreakEntropy\tRightBreakDinuc RightBreakEntropy\tFFPM\tmicroh_brkpt_dist\tnum_microh_near_brkpt"  > ${outdir}/file2 && cat ${outdir}/file2 ${outdir}/${tumor_type}_fusion_genes.txt >> ${outdir}/${tumor_type}_fusion_genes_v2.txt
 #echo -e "FusionName\tJunctionReadCount\tSpanningFragCount\test_J\test_S\tSpliceType\tLeftGene\tLeftBreakpoint\tRightGene\tRightBreakpointJunctionReads\tSpanningFrags\tLargeAnchorSupport\tFFPM\tLeftBreakDinuc\tLeftBreakEntropy\tRightBreakDinuc\tRightBreakEntropy\tannots" > ${dir}/file2 && cat ${dir}/file2 ${dir}/${tumor_type}_fusion_genes.txt >> ${dir}/${tumor_type}_fusion_genes_v1.txt
-mv ${dir}/${tumor_type}_fusion_genes_v2.txt ${dir}/${tumor_type}_fusion_genes_validated.txt
-rm ${dir}/file2
+mv ${outdir}/${tumor_type}_fusion_genes_v2.txt ${outdir}/${tumor_type}_fusion_genes_validated.txt
+rm ${outdir}/file2
