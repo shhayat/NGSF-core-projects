ee73f71a60dc75d5532fdc7799583de4fae1ac70
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-23 10:13:23 -0600

    Update srun_htseq.sh

--- 22-1MILE-002/pilot_study/srun_htseq.sh
+++ 22-1MILE-002/pilot_study/srun_htseq.sh
@@ -1,3 +1,4 @@
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002/pilot_study
 DATA=/datastore/NGSF001/projects/2022/22-1MILE-002/deduplication
 for i in ${DATA}/R2*/*.no-rRNA.primary-aln.dedup.bam
 do
