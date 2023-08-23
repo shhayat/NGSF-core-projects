1b08dc36cd4bcebb5e862aad573681953bd8654c
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-23 10:43:47 -0600

    Update srun_htseq.sh

--- 22-1MILE-002/pilot_study/srun_htseq.sh
+++ 22-1MILE-002/pilot_study/srun_htseq.sh
@@ -2,6 +2,8 @@ SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1MILE-002
 DATA=/datastore/NGSF001/projects/2022/22-1MILE-002/deduplication
 for i in ${DATA}/R2*.no-rRNA.primary-aln.dedup.bam
 do
-     sbatch ${SCRIPT_DIR}/htseq.sh "${DATA}/${sample_name}.no-rRNA.primary-aln.dedup.bam"
+     path="${i%/deduplication/}";
+     sample_name=$(echo "$path" | grep -o 'R[0-9]\+')
+     sbatch ${SCRIPT_DIR}/htseq.sh "${sample_name}", "${DATA}/${sample_name}.no-rRNA.primary-aln.dedup.bam"
      sleep 0.5
 done 
