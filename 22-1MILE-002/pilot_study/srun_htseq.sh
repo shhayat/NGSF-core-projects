6a389bccf16c8dc65f0a10a500b0aa78c7db0143
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-23 10:14:16 -0600

    Update srun_htseq.sh

--- 22-1MILE-002/pilot_study/srun_htseq.sh
+++ 22-1MILE-002/pilot_study/srun_htseq.sh
@@ -4,6 +4,6 @@ for i in ${DATA}/R2*/*.no-rRNA.primary-aln.dedup.bam
 do
      path="${i%/*.no-rRNA.primary-aln.dedup_sort.bam}";
      sample_name="${path##*/}"
-     sbatch ${SCRIPT_DIR}/htseq_count.sh "${sample_name}" "${DATA}/${sample_name}/${sample_name}.no-rRNA.primary-aln.dedup.bam"
+     sbatch ${SCRIPT_DIR}/htseq.sh "${sample_name}" "${DATA}/${sample_name}/${sample_name}.no-rRNA.primary-aln.dedup.bam"
      sleep 0.5
 done 
