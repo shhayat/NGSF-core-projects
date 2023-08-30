35dfcf5cc2329c4705c8dfe7fdc52aadbdf5193e
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-30 10:51:31 -0600

    Update 01_FastQC.sh

--- 23-1MICO-001/01_FastQC.sh
+++ 23-1MICO-001/01_FastQC.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=fastqc
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=02:00:00
+#SBATCH --mem=6G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+module load fastqc
+DATA=/datastore/NGSF001/projects/23-1MIC0-001/Fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MIC0-001/analysis/fastqc
+
+mkdir -p ${OUTDIR}
+
+for fq in $DATA/R23*.fastq.gz
+do
+   fastqc -o ${OUTDIR} --extract ${fq}
+   
+done 
+
+wait 
+
+cd /globalhome/hxo752/HPC/tools/
+multiqc ${OUTDIR}/*_fastqc.zip -o ${OUTDIR}
