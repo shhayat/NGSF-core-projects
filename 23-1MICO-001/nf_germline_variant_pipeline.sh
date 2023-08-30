54054fd08a9307ff41cd56eee5a854efcf423599
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-30 11:58:59 -0600

    Update nf_germline_variant_pipeline.sh

--- 23-1MICO-001/nf_germline_variant_pipeline.sh
+++ 23-1MICO-001/nf_germline_variant_pipeline.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=germline
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=40G
+#SBATCH --time=07:00:00
+#SBATCH --output=%j.out
+
+module load singularity/3.9.2
+
+sample_name=$1; shift
+fq1=$1; shift
+fq2=$1
