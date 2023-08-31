4bbf1890057cc627f4aed3628d52550d609ad8c6
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-31 13:04:23 -0600

    Update 03_filtering_variants.sh

--- 23-1MICO-001/03_filtering_variants.sh
+++ 23-1MICO-001/03_filtering_variants.sh
@@ -1,1 +1,13 @@
+#select snps
+gatk SelectVariants \
+    -R reference.fasta \
+    -V input.vcf \
+    --select-type-to-include SNP \
+    -O snps.vcf
 
+#select rare variants
+    gatk SelectVariants \
+    -R reference.fasta \
+    -V input.vcf \
+    --select "AF <= 0.01" \
+    -O rare_variants.vcf
