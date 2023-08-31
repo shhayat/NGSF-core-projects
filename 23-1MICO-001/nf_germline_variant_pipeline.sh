01ab68743fea71680d00ef6b120a0b1a31447562
Shahina <91986031+shhayat@users.noreply.github.com>
2023-08-31 10:19:36 -0600

    Update nf_germline_variant_pipeline.sh

--- 23-1MICO-001/nf_germline_variant_pipeline.sh
+++ 23-1MICO-001/nf_germline_variant_pipeline.sh
@@ -16,9 +16,6 @@ module load gentoo/2020
 module load singularity/3.9.2
                        
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/
-#REF=/datastore/NGSF001/analysis/references/cell_ranger_genome_builds/refdata-gex-mm10-2020-A/fasta/genome.fa
-#gtf=/datastore/NGSF001/analysis/references/cell_ranger_genome_builds/refdata-gex-mm10-2020-A/genes/genes.gtf
-
 mkdir -p ${DIR}/analysis
 
 nextflow run nf-core/sarek -profile singularity \
