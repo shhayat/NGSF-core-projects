6c677b015c14275921cff4418a5bc3a4ee6563ea
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-01 17:19:08 -0600

    Create run_sbatch.sh

---
+++ 23-1MICO-001/run_sbatch.sh
@@ -0,0 +1,12 @@
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/Fastq
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001
+
+for i in ${DATA}/D23*_R1.fastq.gz 
+do
+      path="${i%_R1*}";
+      sample_name=${path##*/};
+      fq1=${DATA}/${sample_name}_R1.fastq.gz;
+      fq2=${DATA}/${sample_name}_R2.fastq.gz;
+      
+      sbatch ${SCRIPT_DIR}/02_FastQC.sh "${fq1}" "${fq2}"
+ done
