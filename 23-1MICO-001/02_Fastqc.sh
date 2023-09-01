4b5bc10fae475cc5fc8b3601e0d5b1e4059afa1f
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-01 17:17:38 -0600

    Update and rename 01_Fastqc.sh to 02_Fastqc.sh

---
+++ 23-1MICO-001/02_Fastqc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=fastqc
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=5G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+module load fastqc
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/Fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis
+mkdir -p ${OUTDIR}/fastqc
+
+fq1=$1; shift
+fq2=$1;
+fastqc -o ${OUTDIR}/fastqc --extract ${fq1}
+fastqc -o ${OUTDIR}/fastqc --extract ${fq2}
