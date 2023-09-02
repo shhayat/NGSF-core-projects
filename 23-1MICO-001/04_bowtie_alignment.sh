b9628931bb62269fa503599ce12d6fab60e7257c
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-01 23:51:16 -0600

    Update 04_bowtie_alignment.sh

--- 23-1MICO-001/04_bowtie_alignment.sh
+++ 23-1MICO-001/04_bowtie_alignment.sh
@@ -14,7 +14,6 @@ set -eux
 module load samtools
 module load bowtie2/2.5.1
 
-RAW_DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/Fastq
 GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/indices/bowtie_index
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis
 NCPU=4
@@ -25,14 +24,14 @@ fq2=$1
 
 mkdir -p ${OUTDIR}/alignment/${sample_name}
 
-gunzip -c ${RAW_DATA}/${fq1} bowtie2 \
+gunzip -c ${fq1} bowtie2 \
 --phred33 \
 --mm \
 --very-sensitive \
 --threads ${NCPU} \
 -x ${GENOME} \
 -1 - \
--2 <(gunzip -c ${RAW_DATA}/${fq2}) \
+-2 <(gunzip -c ${fq2}) \
 -S ${OUTDIR}/alignment/${sample_name}/${sample_name}.sam 2> ${OUTDIR}/alignment/${sample_name}/${sample_name}_bowtie2.log \
 && samtools view -h -b ${OUTDIR}/alignment/${sample_name}/${sample_name}.sam > ${OUTDIR}/alignment/${sample_name}/${sample_name}.aligned.bam
 
