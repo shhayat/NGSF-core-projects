71639f494ad9514c068c00f2326f6ef5813e827f
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-01 18:21:45 -0600

    Update run_sbatch.sh

--- 23-1MICO-001/run_sbatch.sh
+++ 23-1MICO-001/run_sbatch.sh
@@ -17,3 +17,15 @@ do
 ########
 #bowtie
 ########
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/Fastq
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001
+
+for i in ${DATA}/D23*_R1.fastq.gz 
+do
+      path="${i%_R1*}";
+      sample_name=${path##*/};
+      fq1=${DATA}/${sample_name}_R1.fastq.gz;
+      fq2=${DATA}/${sample_name}_R2.fastq.gz;
+
+      sbatch ${SCRIPT_DIR}/03_bowtie_alignment.sh "${sample_name}" "${fq1}" "${fq2}"
+ done
