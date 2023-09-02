709874cf9e266bcf57f15d547b22b922342c759d
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-01 22:06:54 -0600

    Update 03_genome_build.sh

--- 23-1MICO-001/03_genome_build.sh
+++ 23-1MICO-001/03_genome_build.sh
@@ -14,4 +14,5 @@ module load bowtie2/2.5.1
 GENOME=/datastore/NGSF001/analysis/references/iGenomes/Homo_sapiens/NCBI/GRCh38/Sequence/WholeGenomeFasta
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/indices
 
+mkdir -p $OUTDIR
 bowtie2-build ${GENOME}/genome.fa ${OUTDIR}/bowtie_index
