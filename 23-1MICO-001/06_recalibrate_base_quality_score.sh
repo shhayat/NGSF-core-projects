a195057fac94e094dae382ded1ebb23457498c27
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-02 22:55:23 -0600

    Update 06_recalibrate_base_quality_score.sh

--- 23-1MICO-001/06_recalibrate_base_quality_score.sh
+++ 23-1MICO-001/06_recalibrate_base_quality_score.sh
@@ -1,1 +1,16 @@
 
+gatk BaseRecalibrator \
+   -I my_reads.bam \
+   -R reference.fasta \
+   --known-sites sites_of_variation.vcf \
+   --known-sites another/optional/setOfSitesToMask.vcf \
+   -O recal_data.table
+
+  gatk ApplyVQSR \
+   -R Homo_sapiens_assembly38.fasta \
+   -V input.vcf.gz \
+   -O output.vcf.gz \
+   --truth-sensitivity-filter-level 99.0 \
+   --tranches-file output.tranches \
+   --recal-file output.recal \
+   -mode SNP
