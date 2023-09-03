b1f00d2cc1d17b0572fdb33ccfa92b02fdc1313c
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-02 23:48:50 -0600

    Update 07_variant_calling.sh

--- 23-1MICO-001/07_variant_calling.sh
+++ 23-1MICO-001/07_variant_calling.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=markdup_add_RG
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=4:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=recalibrate.out
 
 gatk --java-options "-Djava.io.tmpdir=/lscratch/$SLURM_JOBID -Xms20G -Xmx20G -XX:ParallelGCThreads=2" HaplotypeCaller \
   -R /fdb/igenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa \
