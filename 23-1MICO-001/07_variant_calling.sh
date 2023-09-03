39618504e92d0d270c7f9b532e9987d4058f6bd0
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-02 23:48:30 -0600

    Update 07_variant_calling.sh

--- 23-1MICO-001/07_variant_calling.sh
+++ 23-1MICO-001/07_variant_calling.sh
@@ -1,1 +1,6 @@
 
+gatk --java-options "-Djava.io.tmpdir=/lscratch/$SLURM_JOBID -Xms20G -Xmx20G -XX:ParallelGCThreads=2" HaplotypeCaller \
+  -R /fdb/igenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa \
+  -I NA12891_markdup_bqsr.bam \
+  -O NA12891.g.vcf.gz \
+  -ERC GVCF
