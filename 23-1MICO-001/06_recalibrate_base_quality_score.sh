a489b29f9ad221cc0b2e4554214a84cb032f1261
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-02 23:03:52 -0600

    Update 06_recalibrate_base_quality_score.sh

--- 23-1MICO-001/06_recalibrate_base_quality_score.sh
+++ 23-1MICO-001/06_recalibrate_base_quality_score.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=markdup_add_RG
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=2:30:00
+#SBATCH --mem=64G
+#SBATCH  --output=markdup_add_RG.out
+
+module laod 
 gatk BaseRecalibrator \
    -I my_reads.bam \
    -R reference.fasta \
