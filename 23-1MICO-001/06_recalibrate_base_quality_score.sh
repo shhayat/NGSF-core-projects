a684476c8c67f5d5d05765134ab9f397f507ac82
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-02 23:20:14 -0600

    Update 06_recalibrate_base_quality_score.sh

--- 23-1MICO-001/06_recalibrate_base_quality_score.sh
+++ 23-1MICO-001/06_recalibrate_base_quality_score.sh
@@ -13,8 +13,17 @@ module laod
 
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/alignment
+REF=
 sample_name=1; shift
 BAM_FILE=1;
+
+gatk --java-options "-Djava.io.tmpdir=/lscratch/$SLURM_JOBID -Xms4G -Xmx4G -XX:ParallelGCThreads=2" BaseRecalibrator \
+  -I ${DIR}/${sample_name}/${BAM_FILE} \
+  -R /fdb/igenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa \
+  -O ${OUTDIR}/${sample_name}/${sample_name}_recal_data.table \
+  --known-sites /fdb/GATK_resource_bundle/hg38/dbsnp_146.hg38.vcf.gz \
+  --known-sites /fdb/GATK_resource_bundle/hg38/Homo_sapiens_assembly38.known_indels.vcf.gz \
+  --known-sites /fdb/GATK_resource_bundle/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
 
 
 gatk BaseRecalibrator \
@@ -24,11 +33,8 @@ gatk BaseRecalibrator \
    --known-sites another/optional/setOfSitesToMask.vcf \
    -O ${OUTDIR}/${sample_name}/${sample_name}_recal_data.table
 
-  gatk ApplyVQSR \
-   -R Homo_sapiens_assembly38.fasta \
-   -V input.vcf.gz \
-   -O output.vcf.gz \
-   --truth-sensitivity-filter-level 99.0 \
-   --tranches-file output.tranches \
-   --recal-file output.recal \
-   -mode SNP
+  gatk --java-options "-Djava.io.tmpdir=/lscratch/$SLURM_JOBID -Xms2G -Xmx2G -XX:ParallelGCThreads=2" ApplyBQSR \
+  -I ${DIR}/${sample_name}/${BAM_FILE} \
+  -R /fdb/igenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa \
+  --bqsr-recal-file ${OUTDIR}/${sample_name}/${sample_name}_recal_data.table \
+  -O NA12891_markdup_bqsr.bam
