4cc40931ca4c695bf71ad4ea0952993df840bf09
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-05 08:59:10 -0600

    Update 06_recalibrate_base_quality_score.sh

--- 23-1MICO-001/06_recalibrate_base_quality_score.sh
+++ 23-1MICO-001/06_recalibrate_base_quality_score.sh
@@ -11,7 +11,7 @@
 
 module load gatk/4.2.5.0 
 
-DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/
+#DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/alignment
 REF=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/genome.fa
 dbsnp=/datastore/NGSF001/analysis/dbsnp
@@ -19,14 +19,14 @@ sample_name=$1; shift
 BAM_FILE=$1;
 
 gatk BaseRecalibrator \
-  -I ${DIR}/${sample_name}/${BAM_FILE} \
+  -I ${BAM_FILE} \
   -R ${REF} \
   -O ${OUTDIR}/${sample_name}/${sample_name}_recal_data.table \
   --known-sites ${dbsnp}/Homo_sapiens_assembly38.dbsnp138.vcf  \
   --known-sites ${dbsnp}/1000G_phase1.snps.high_confidence.hg38.vcf.gz
 
 gatk ApplyBQSR \
-  -I ${DIR}/${sample_name}/${BAM_FILE} \
+  -I ${BAM_FILE} \
   -R ${REF} \
   --bqsr-recal-file ${OUTDIR}/${sample_name}/${sample_name}_recal_data.table \
   -O ${OUTDIR}/${sample_name}/${sample_name}_bqsr.bam
