519f70ab5b9a4398451c652aa50dd6c8f91db83e
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-04 21:58:08 -0600

    Update run_sbatch.sh

--- 23-1MICO-001/run_sbatch.sh
+++ 23-1MICO-001/run_sbatch.sh
@@ -52,6 +52,6 @@ DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analy
 
 for i in {43..44};
 do
-  sbatch ${SCRIPT_DIR}/06_recalibrate_base_quality_score.sh D230000${i} "${DATA}/D230000${i}/D230000${i}_mdup_rg_sort.bam
+  sbatch ${SCRIPT_DIR}/06_recalibrate_base_quality_score.sh D230000${i} "${DATA}/D230000${i}/D230000${i}_mdup_rg_sort.bam"
   sleep 0.2
 done
