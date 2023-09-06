9edf4c2faae41c0437fddc400515c21ce1a13797
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-06 11:42:54 -0600

    Update 09_CombineGVCFs_and_joint genotyping.sh

--- 23-1MICO-001/09_CombineGVCFs_and_joint genotyping.sh
+++ 23-1MICO-001/09_CombineGVCFs_and_joint genotyping.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=CombineGVCFs_jointGenotyping
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=4:00:00
+#SBATCH --mem=20G
+#SBATCH  --output=%j_variant_quality.out
 
  
  gatk CombineGVCFs \
