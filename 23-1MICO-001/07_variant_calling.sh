dd27e161456a9925c5b833c110ba9e02675af6f8
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-06 10:31:53 -0600

    Update 07_variant_calling.sh

--- 23-1MICO-001/07_variant_calling.sh
+++ 23-1MICO-001/07_variant_calling.sh
@@ -8,10 +8,15 @@
 #SBATCH --time=4:00:00
 #SBATCH --mem=20G
 #SBATCH  --output=%j_variantcalling.out
+
 #https://hpc.nih.gov/training/gatk_tutorial/haplotype-caller.html
-  
+
+DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/variants
+mkdir -p ${OUTDIR}
+
 gatk --java-options "-Xms20G -Xmx20G -XX:ParallelGCThreads=2" HaplotypeCaller \
   -R ${REF} \
-  -I ${OUTDIR}/${sample_name}/${sample_name}_bqsr.bam \
-  -O ${OUTDIR}/${sample_name}/${sample_name}.vcf.gz \
+  -I ${DIR}/${sample_name}/${sample_name}_bqsr.bam \
+  -O ${OUTDIR}/${sample_name}.vcf.gz \
   -ERC GVCF
