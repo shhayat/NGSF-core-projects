e0c3ab245536948d15a8fea79bd0159d0e6a7623
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-06 12:05:58 -0600

    Update run_sbatch.sh

--- 23-1MICO-001/run_sbatch.sh
+++ 23-1MICO-001/run_sbatch.sh
@@ -64,6 +64,6 @@ DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analy
 
 for i in {43..44};
 do
-  sbatch ${SCRIPT_DIR}/08_CombineGVCFs_and_joint genotyping.sh D230000${i};
+  sbatch ${SCRIPT_DIR}/08_CombineGVCFs_and_GenotypeGVCFs.sh D230000${i};
   sleep 0.2
 done
