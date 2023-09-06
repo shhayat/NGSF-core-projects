01a02ffc36d6feed4b8439c2150ae9e90a153be2
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-06 09:47:19 -0600

    Update 07_variant_calling.sh

--- 23-1MICO-001/07_variant_calling.sh
+++ 23-1MICO-001/07_variant_calling.sh
@@ -6,11 +6,11 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=4:00:00
-#SBATCH --mem=40G
+#SBATCH --mem=20G
 #SBATCH  --output=variantcalling.out
 #https://hpc.nih.gov/training/gatk_tutorial/haplotype-caller.html
   
-gatk --java-options "-Djava.io.tmpdir=/lscratch/$SLURM_JOBID -Xms20G -Xmx20G -XX:ParallelGCThreads=2" HaplotypeCaller \
+gatk --java-options "-Xms20G -Xmx20G -XX:ParallelGCThreads=2" HaplotypeCaller \
   -R ${REF} \
   -I ${OUTDIR}/${sample_name}/${sample_name}_bqsr.bam \
   -O ${OUTDIR}/${sample_name}/${sample_name}.vcf.gz \
