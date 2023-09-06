06040939cd8b478b0cb340c0d77a8b40c976d592
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-06 14:57:07 -0600

    Update 10_filtering_variants.sh

--- 23-1MICO-001/10_filtering_variants.sh
+++ 23-1MICO-001/10_filtering_variants.sh
@@ -1,4 +1,19 @@
-#select snps
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=filter_variants
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=4:00:00
+#SBATCH --mem=20G
+#SBATCH  --output=%j_variant_quality.out
+
+module load gatk/4.2.5.0
+
+REF=
+OUTDIR=
+#select only snps from vcf file
 gatk SelectVariants \
     -R reference.fasta \
     -V input.vcf \
