c33ed7961a044ad66e7bbed2d3c1d5dd5273d2bc
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-06 10:39:38 -0600

    Update 08_recalibrate_variant_quality_score.sh

--- 23-1MICO-001/08_recalibrate_variant_quality_score.sh
+++ 23-1MICO-001/08_recalibrate_variant_quality_score.sh
@@ -12,7 +12,7 @@
 #https://gatk.broadinstitute.org/hc/en-us/articles/360036510892-VariantRecalibrator
  gatk VariantRecalibrator \
    -R Homo_sapiens_assembly38.fasta \
-   -V input.vcf.gz \
+   -V ${sample_name}.vcf.gz \
    --resource hapmap,known=false,training=true,truth=true,prior=15.0:hapmap_3.3.hg38.sites.vcf.gz \
    --resource omni,known=false,training=true,truth=false,prior=12.0:1000G_omni2.5.hg38.sites.vcf.gz \
    --resource 1000G,known=false,training=true,truth=false,prior=10.0:1000G_phase1.snps.high_confidence.hg38.vcf.gz \
