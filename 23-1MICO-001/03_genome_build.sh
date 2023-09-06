a04ab163b162a81766490c1ed1ec864fc5bb5ee7
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-06 13:41:39 -0600

    Update 03_genome_build.sh

--- 23-1MICO-001/03_genome_build.sh
+++ 23-1MICO-001/03_genome_build.sh
@@ -16,5 +16,5 @@ GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/ana
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/indices
 
 mkdir -p $OUTDIR
-#bowtie2-build ${GENOME}/Homo_sapiens_assembly38.fasta ${OUTDIR}/bowtie_index
-bowtie2-build ${GENOME}/GCF_000001405.40_GRCh38.p14_genomic.fna ${OUTDIR}/bowtie_index
+bowtie2-build ${GENOME}/Homo_sapiens_assembly38.fasta ${OUTDIR}/bowtie_index
+#bowtie2-build ${GENOME}/GCF_000001405.40_GRCh38.p14_genomic.fna ${OUTDIR}/bowtie_index
