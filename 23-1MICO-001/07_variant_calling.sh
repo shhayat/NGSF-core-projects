4889156930aa8ff141353a90e435e6b4824ba1d2
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-06 09:45:54 -0600

    Update 07_variant_calling.sh

--- 23-1MICO-001/07_variant_calling.sh
+++ 23-1MICO-001/07_variant_calling.sh
@@ -2,18 +2,13 @@
 
 #SBATCH --account=hpc_p_anderson
 #SBATCH --constraint=skylake
-#SBATCH --job-name=markdup_add_RG
+#SBATCH --job-name=varaint_calling
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=4:00:00
 #SBATCH --mem=40G
 #SBATCH  --output=variantcalling.out
 #https://hpc.nih.gov/training/gatk_tutorial/haplotype-caller.html
-gatk --java-options "-Djava.io.tmpdir=/lscratch/$SLURM_JOBID -Xms2G -Xmx2G -XX:ParallelGCThreads=2" ApplyBQSR \
-  -I ${DIR}/${sample_name}/${BAM_FILE} \
-  -R ${REF} \
-  --bqsr-recal-file ${OUTDIR}/${sample_name}/${sample_name}_recal_data.table \
-  -O ${OUTDIR}/${sample_name}/${sample_name}_bqsr.bam
   
 gatk --java-options "-Djava.io.tmpdir=/lscratch/$SLURM_JOBID -Xms20G -Xmx20G -XX:ParallelGCThreads=2" HaplotypeCaller \
   -R ${REF} \
