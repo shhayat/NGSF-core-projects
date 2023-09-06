4be06e8001d26298e384a2f9bc128b4ff5bbd83a
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-06 11:49:36 -0600

    Update 08_CombineGVCFs_and_joint genotyping.sh

--- 23-1MICO-001/08_CombineGVCFs_and_joint genotyping.sh
+++ 23-1MICO-001/08_CombineGVCFs_and_joint genotyping.sh
@@ -5,8 +5,8 @@
 #SBATCH --job-name=CombineGVCFs_jointGenotyping
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --time=4:00:00
-#SBATCH --mem=20G
+#SBATCH --time=2:00:00
+#SBATCH --mem=10G
 #SBATCH  --output=%j_CombineGVCFs_jointGenotyping.out
 
  DIR=
@@ -14,10 +14,10 @@
  
  gatk --java-options "-Xms10G -Xmx10G -XX:ParallelGCThreads=2" CombineGVCFs \
    -R reference.fasta \
-   --variant sample1.g.vcf.gz \
-   --variant sample2.g.vcf.gz \
+   --variant /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/variants/D23000043.g.vcf.gz \
+   --variant /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/variants/D23000044.g.vcf.gz \
    -O combine.g.vcf.gz
 
 gatk --java-options "-Xms10G -Xmx10G -XX:ParallelGCThreads=2" GenotypeGVCFs \
   -R ${REF} \
-  -V  ${OUTDIR}/combine.g.vcf.gzz -O ${OUTDIR}/
+  -V  ${OUTDIR}/combine.g.vcf.gz -O ${OUTDIR}/genotyped.g.vcf.gz
