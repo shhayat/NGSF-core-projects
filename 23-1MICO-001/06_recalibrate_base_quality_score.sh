bfa509c1ecfd7e96dae3d95d2ba925d076bf21da
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-06 09:09:53 -0600

    Update 06_recalibrate_base_quality_score.sh

--- 23-1MICO-001/06_recalibrate_base_quality_score.sh
+++ 23-1MICO-001/06_recalibrate_base_quality_score.sh
@@ -22,7 +22,7 @@ gatk --java-options "-Xmx2G" BaseRecalibrator \
   -I ${BAM_FILE} \
   -R ${REF} \
   -O ${OUTDIR}/${sample_name}/${sample_name}_recal_data.table \
-  --known-sites ${dbsnp}/GCF_000001405.40.gz
+  --known-sites ${dbsnp}/Homo_sapiens_assembly38.dbsnp138.vcf
  # --known-sites ${dbsnp}/1000G_phase1.snps.high_confidence.hg38.vcf.gz
 
 gatk --java-options "-Xmx2G" ApplyBQSR \
