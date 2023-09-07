1efb3b04dffaaa04de4da4a03136a2d250626ef4
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-07 11:42:37 -0600

    Update 10_select_rare_variants.sh

--- 23-1MICO-001/10_select_rare_variants.sh
+++ 23-1MICO-001/10_select_rare_variants.sh
@@ -30,6 +30,6 @@ OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/ana
 
 gatk SelectVariants \
     -R ${REF} \
-    -V ${OUTDIR}/genotyped.g.vcf.gz \
+    -V ${OUTDIR}/D23000043.g.vcf.gz \
     --select "AF < 0.01" \
     -O ${OUTDIR}/rare_SNPs.vcf
