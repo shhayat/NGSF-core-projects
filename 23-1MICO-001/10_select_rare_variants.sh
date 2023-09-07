7a0fa6568e7b6129a97e68c657372d44376632bc
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-07 13:52:39 -0600

    Update 10_select_rare_variants.sh

--- 23-1MICO-001/10_select_rare_variants.sh
+++ 23-1MICO-001/10_select_rare_variants.sh
@@ -30,7 +30,7 @@ OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/ana
 
 gatk SelectVariants \
     -R ${REF} \
-    -V ${OUTDIR}/genotyped.g.vcf.gz \
+    -V ${OUTDIR}/SNP.recalibrated_99.9.vcf.gz \
     --select-type-to-include SNP \
-    -select "QUAL > 30.0 && DP == 10" \
+    -select "AF < 0.01" \
     -O ${OUTDIR}/rare_SNPs.vcf
