6e1c595b36dcef823228a34c0fd8b42361065799
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-06 23:08:11 -0600

    Update tranches.sh

--- 23-1MICO-001/tranches.sh
+++ 23-1MICO-001/tranches.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=feature-count
+#SBATCH --job-name=tranche_plot
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --time=3:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
 #SBATCH --mem=40G
 #SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 set -eux
 
-SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001B
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/variants
 module load r/4.1.2
 
-R CMD BATCH ${SCRIPT_DIR}/04_feature_count.R
+R CMD BATCH ${SCRIPT_DIR}/output.plots.R
