bbb41bc918cba9cc4dd7470d963f61cec45cba81
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-07 15:09:05 -0600

    Update variant_calling.sh

--- 23-1MICO-001/variant_calling.sh
+++ 23-1MICO-001/variant_calling.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=varaint_calling
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=08:00:00
+#SBATCH --mem=20G
+#SBATCH  --output=%j_variantcalling.out
+
+module module load varscan
+
+sample_name=$1;
+
+DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/alignment
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/variants
+REF=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/genome/genome.fa
+mkdir -p ${OUTDIR}
+
+java -jar $EBROOTVARSCAN/VarScan.v2.4.2.jar 
