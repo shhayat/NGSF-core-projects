0d2b8299dccfc729c92c025194d3e2c2fb4a2699
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-07 16:03:25 -0600

    Update variant_calling.sh

--- 23-1MICO-001/variant_calling.sh
+++ 23-1MICO-001/variant_calling.sh
@@ -12,16 +12,22 @@
 #https://genomemedicine.biomedcentral.com/articles/10.1186/s13073-020-00791-w
 module module load varscan
 
+sample_name=$1;shift
 BAM_FILE=$1;
-#sample_name=$1;
 
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/alignment
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/variants
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/SNPs_using_varscan2
 REF=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/genome/genome.fa
 mkdir -p ${OUTDIR}
 
+#base quality > 30 mapping quality > 20
 samtools mpileup -B -f ${REF} ${BAM_FILE} | \
             java -jar $EBROOTVARSCAN/VarScan.v2.4.2.jar mpileup2snp \
+            --min-avg-qual 30 \ #base quality
+            --min-var-freq 0.01 \
+            --variants SNP \
+            --output-vcf ${OUTDIR}/${sample_name}_snps.vcf
+            
             
 
 
