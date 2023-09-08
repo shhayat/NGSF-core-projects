8c83af16cd4a73e06d3c5c7eceaecb807a86ae96
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-08 16:11:41 -0600

    Update filter_varnats.sh

--- 23-1MICO-001/filter_varnats.sh
+++ 23-1MICO-001/filter_varnats.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=varaint_calling
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=08:00:00
+#SBATCH --mem=20G
+#SBATCH  --output=%j_filterSNPs.out
+
 module load bcftools/1.13
-
-
-bcftools view -i 'INFO/AF < 0.01' D23000043_snps_ReadDepth10_BaseQuality30.vcf > D23000043_snps_AF0.01.vcf
-bcftools view -i 'INFO/AF < 0.01' D23000044_snps_ReadDepth10_BaseQuality30.vcf > D23000044_snps_AF0.01.vcf
+bcftools view -i 'INFO/FREQ < 0.01' D23000043_snps_ReadDepth10_BaseQuality30.vcf > D23000043_snps_AF0.01.vcf
+bcftools view -i 'INFO/FREQ < 0.01' D23000044_snps_ReadDepth10_BaseQuality30.vcf > D23000044_snps_AF0.01.vcf
