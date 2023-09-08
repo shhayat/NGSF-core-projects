fe65078972b8523a549b95f8beb41ade1f192d82
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-08 16:00:30 -0600

    Update filter_varnats.sh

--- 23-1MICO-001/filter_varnats.sh
+++ 23-1MICO-001/filter_varnats.sh
@@ -1,3 +1,5 @@
 module load bcftools/1.13
 
-bcftools view -i 'INFO/AF < 0.01' input.vcf > output_filtered.vcf
+
+bcftools view -i 'INFO/AF < 0.01' D23000043_snps_ReadDepth10_BaseQuality30.vcf > D23000043_snps_AF0.01.vcf
+bcftools view -i 'INFO/AF < 0.01' D23000044_snps_ReadDepth10_BaseQuality30.vcf > D23000044_snps_AF0.01.vcf
