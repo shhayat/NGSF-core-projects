e7e82e06fa06bef4b7841f5aac5f9da420495693
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-10 12:22:26 -0600

    Update and rename filter_varnats.sh to filter_variants.sh

--- 23-1MICO-001/filter_variants.sh
+++ 23-1MICO-001/filter_variants.sh
@@ -9,6 +9,10 @@
 #SBATCH --mem=20G
 #SBATCH  --output=%j_filterSNPs.out
 
+
+module load StdEnv/2020
 module load bcftools/1.13
+module load gcc/9.3.0
+
 bcftools view -i 'INFO/FREQ < 0.01' D23000043_snps_ReadDepth10_BaseQuality30.vcf > D23000043_snps_AF0.01.vcf
 bcftools view -i 'INFO/FREQ < 0.01' D23000044_snps_ReadDepth10_BaseQuality30.vcf > D23000044_snps_AF0.01.vcf
