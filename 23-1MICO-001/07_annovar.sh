ba0b12f59e8197868d4d6e8e9ae1527708cc0d9c
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-11 13:18:41 -0600

    Update 07_annovar.sh

--- 23-1MICO-001/07_annovar.sh
+++ 23-1MICO-001/07_annovar.sh
@@ -1,5 +1,6 @@
 #detect low freq variants from general population and annotate variants
 #https://peerj.com/articles/600/
 
-
-convert2annovar.pl -format vcf4 variantfile > variant.avinput
+DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/SNPs_using_varscan2
+${DIR}/D23000043_snps_ReadDepth10_BaseQuality30.vcf 
+convert2annovar.pl -format vcf4 ${DIR}/D23000043_snps_ReadDepth10_BaseQuality30.vcf  > ${DIR}/D23000043_snps.avinput
