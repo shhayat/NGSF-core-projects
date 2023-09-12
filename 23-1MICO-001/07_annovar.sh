5ee3dccd58e22db9fc5cc5338315aadee06e9304
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-11 19:18:23 -0600

    Update 07_annovar.sh

--- 23-1MICO-001/07_annovar.sh
+++ 23-1MICO-001/07_annovar.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --account=hpc_p_anderson
 #SBATCH --constraint=skylake
-#SBATCH --job-name=varaint_calling
+#SBATCH --job-name=annovar
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=08:00:00
@@ -14,6 +14,10 @@
 
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/SNPs_using_varscan2
 annovar=/globalhome/hxo752/HPC/tools/annovar
+
 #First convert VCF file to the format accepted by annovar
 ${annovar}/convert2annovar.pl -format vcf4 ${DIR}/D23000043_snps_ReadDepth10_BaseQuality30.vcf  > ${DIR}/D23000043_snps.avinput
 ${annovar}/convert2annovar.pl -format vcf4 ${DIR}/D23000044_snps_ReadDepth10_BaseQuality30.vcf  > ${DIR}/D23000044_snps.avinput
+
+#Determining the population frequency for SNPs 
+annotate_variation.pl 
