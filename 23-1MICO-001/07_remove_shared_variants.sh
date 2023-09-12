a27d030b68584333faf233777c4bf42bf133a65b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-12 14:47:28 -0600

    Create 07_remove_shared_variants.sh

---
+++ 23-1MICO-001/07_remove_shared_variants.sh
@@ -0,0 +1,9 @@
+bcftools isec -C \
+              -c all \
+              -O z \
+              -w 1 \
+              -o ${OUTDIR}/${CLONE_ID}_${SAMPLE_ID}_I.vcf.gz \
+              ${INDUCED_SAMPLE_DIR}/${SAMPLE_ID}/${SAMPLE_ID}.vcf.gz \
+              ${UNINDUCED_SAMPLE_DIR}/${CLONE_ID}_U_concat.vcf.gz
+
+bcftools index -t ${OUTDIR}/${CLONE_ID}_${SAMPLE_ID}_I.vcf.gz
