c14ef9af5f572a7122680fa1f9bfc7fcfc90c8b2
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-11 19:10:26 -0600

    Update 07_annovar.sh

--- 23-1MICO-001/07_annovar.sh
+++ 23-1MICO-001/07_annovar.sh
@@ -13,7 +13,7 @@
 #https://peerj.com/articles/600/
 
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/SNPs_using_varscan2
-
+annovar=/globalhome/hxo752/HPC/tools/annovar
 #First convert VCF file to the format accepted by annovar
-convert2annovar.pl -format vcf4 ${DIR}/D23000043_snps_ReadDepth10_BaseQuality30.vcf  > ${DIR}/D23000043_snps.avinput
-convert2annovar.pl -format vcf4 ${DIR}/D23000044_snps_ReadDepth10_BaseQuality30.vcf  > ${DIR}/D23000044_snps.avinput
+${annovar}/convert2annovar.pl -format vcf4 ${DIR}/D23000043_snps_ReadDepth10_BaseQuality30.vcf  > ${DIR}/D23000043_snps.avinput
+${annovar}/convert2annovar.pl -format vcf4 ${DIR}/D23000044_snps_ReadDepth10_BaseQuality30.vcf  > ${DIR}/D23000044_snps.avinput
