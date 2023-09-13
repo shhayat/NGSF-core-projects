6351a72cddbaecc7f37b7d9073d05b0ca4b66343
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-12 20:46:20 -0600

    Update 08_annovar_determine_lowfreqVariants.sh

--- 23-1MICO-001/08_annovar_determine_lowfreqVariants.sh
+++ 23-1MICO-001/08_annovar_determine_lowfreqVariants.sh
@@ -14,9 +14,9 @@
 
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/SNPs_using_varscan2
 annovar=/globalhome/hxo752/HPC/tools/annovar
-
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/
 #First convert VCF file to the format accepted by annovar
-#${annovar}/convert2annovar.pl -format vcf4 ${DIR}/D23000043_snps_ReadDepth10_BaseQuality30.vcf  > ${DIR}/D23000043_snps.avinput
+${annovar}/convert2annovar.pl -format vcf4 ${DIR}/unique_to_D23000043.vcf.gz  > ${DIR}/D23000043_unique_snps.avinput
 #${annovar}/convert2annovar.pl -format vcf4 ${DIR}/D23000044_snps_ReadDepth10_BaseQuality30.vcf  > ${DIR}/D23000044_snps.avinput
 
 #Download databases for Determining the population frequency for SNPs
