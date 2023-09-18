5a8ea8019494e77989512a7617b03128ddb23553
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-18 16:15:52 -0600

    Create 04_RNASeQC.sh

---
+++ 23-1ANLE-004/04_RNASeQC.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=00:30:00
+#SBATCH --mem=5G
+
+set -eux
+
+# copy tool to tempdir and give execute permission
+cp /globalhome/hxo752/HPC/tools/src/rnaseqc.v2.4.2.linux ${SLURM_TMPDIR}
+chmod u+x ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux
+
+sample_name=$1; shift
+bam_file=$1
+#GTF file needs to be modified for running RNASeQC
+GTF=/datastore/NGSF001/analysis/references/human/gencode-40/gencode.v40.annotation_mod.gtf
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-004/analysis/rnaseqc
+
+mkdir -p ${OUTDIR}
+
+${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux ${GTF} \
+                        ${bam_file} \
+                        ${OUTDIR} \
+                        --sample=${sample_name}
