5b5710d9edef3b5534bc3fcd418c9a2fee8bb247
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-19 12:47:16 -0600

    Update 01_concatenate_fastq_and_FastQC.sh

--- 23-1ANLE-003/01_concatenate_fastq_and_FastQC.sh
+++ 23-1ANLE-003/01_concatenate_fastq_and_FastQC.sh
@@ -11,10 +11,18 @@
 set -eux
 
 module load fastqc
-DATA=/datastore/NGSF001/projects/23-1ANLE-001/Analysis/fastq
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/analysis
+
+FASTQ_FOLDER=/datastore/NGSF001/NB551711/230913_NB551711_0081_AH2FC2BGXV/Alignment_1/20230914_033142/Fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-003/analysis
 
 mkdir -p ${OUTDIR}/fastqc
+
+for i in $(seq -w 05 08)
+do
+	echo Combining library SC23000${i}
+	cat ${FASTQ_FOLDER}/SC23000${i}_*_R1_* > ${OUTDIR}/SC23000${i}_R1.fastq.gz
+	cat ${FASTQ_FOLDER}/SC23000${i}_*_R2_* > ${OUTDIR}/SC23000${i}_R2.fastq.gz
+done
 
 for fq in $DATA/*.fastq.gz
 do
@@ -23,5 +31,8 @@ do
 done
 wait 
 
-cd /datastore/NGSF001/software/tools/
-./multiqc ${OUTDIR}/fastqc/*_fastqc.zip -o ${OUTDIR}/fastqc
+rm ${OUTDIR}/SC23000${i}_R1.fastq.gz
+rm ${OUTDIR}/SC23000${i}_R2.fastq.gz
+
+cd /globalhome/hxo752/HPC/tools
+multiqc ${OUTDIR}/fastqc/*_fastqc.zip -o ${OUTDIR}/fastqc
