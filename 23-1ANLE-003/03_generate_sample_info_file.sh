6629f90883da4b886ebabc4cddfcc973cac8f120
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-19 17:55:14 -0600

    Update 03_generate_sample_info_file.sh

--- 23-1ANLE-003/03_generate_sample_info_file.sh
+++ 23-1ANLE-003/03_generate_sample_info_file.sh
@@ -3,25 +3,23 @@ DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-003/analys
 #generate sample info file per comparision
 for i in {05..06};
 do
-      echo "SC23000${i},${DIR}/SC23000${i}/outs/molecule_info.h5" >> ${DIR}/SC23000${i}/sample_info.csv
+      echo "SC23000${i},${DIR}/SC23000${i}/molecule_info.h5" >> ${DIR}/sample_info.csv
 done
 #add header to csv file
-sed -i 1i'sample_id,molecule_h5' ${DIR}/SC23000${i}/sample_info.csv
-
+sed -i 1i'sample_id,molecule_h5' ${DIR}/SC23000${i}/sample_info1.csv
 
 
 for i in {06..07};
 do
-      echo "SC23000${i},${DIR}/SC23000${i}/outs/molecule_info.h5" >> ${DIR}/SC23000${i}/sample_info.csv
+      echo "SC23000${i},${DIR}/SC23000${i}/molecule_info.h5" >> ${DIR}/sample_info_2.csv
 done
 #add header to csv file
-sed -i 1i'sample_id,molecule_h5' ${DIR}/SC23000${i}/sample_info.csv
-
+sed -i 1i'sample_id,molecule_h5' ${DIR}/sample_info.csv
 
 
 for i in {06..08};
 do
-      echo "SC23000${i},${DIR}/SC23000${i}/outs/molecule_info.h5" >> ${DIR}/SC23000${i}/sample_info.csv
+      echo "SC23000${i},${DIR}/SC23000${i}/molecule_info.h5" >> ${DIR}/sample_info_3.csv
 done
 #add header to csv file
 sed -i 1i'sample_id,molecule_h5' ${DIR}/SC23000${i}/sample_info.csv
