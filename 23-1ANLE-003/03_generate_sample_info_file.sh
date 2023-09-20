f72082860272b5d0126cbaa5771ea8c09a2e19ec
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-19 18:07:52 -0600

    Update 03_generate_sample_info_file.sh

--- 23-1ANLE-003/03_generate_sample_info_file.sh
+++ 23-1ANLE-003/03_generate_sample_info_file.sh
@@ -1,7 +1,7 @@
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-003/analysis/count_files
 
 #generate sample info file per comparision
-for i in {05..06};
+for i in 05 06
 do
       echo "SC23000${i},${DIR}/SC23000${i}/molecule_info.h5" >> ${DIR}/sample_info_1.csv
 done
@@ -9,7 +9,7 @@ done
 sed -i 1i'sample_id,molecule_h5' ${DIR}/sample_info_1.csv
 
 
-for i in {06..07};
+for i in 06 07
 do
       echo "SC23000${i},${DIR}/SC23000${i}/molecule_info.h5" >> ${DIR}/sample_info_2.csv
 done
@@ -17,7 +17,7 @@ done
 sed -i 1i'sample_id,molecule_h5' ${DIR}/sample_info_2.csv
 
 
-for i in {06..08};
+for i in 06 08
 do
       echo "SC23000${i},${DIR}/SC23000${i}/molecule_info.h5" >> ${DIR}/sample_info_3.csv
 done
