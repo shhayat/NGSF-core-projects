f3f94a42567c391e6272a1292a5f92f3f37b4177
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-20 13:34:04 -0600

    Update 05_differential_expression_analysis.R

--- 23-1ANLE-003/05_differential_expression_analysis.R
+++ 23-1ANLE-003/05_differential_expression_analysis.R
@@ -1,14 +1,44 @@
+#Finding markers differentailly expressed between conditions
 library(Seurat)
 
 # Load the feature barcode matrix (.mtx)
 setwd("/Users/shahina/Desktop/23-1ANLE-003")
 #counts_matrix <- Read10X(data.dir = "/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-003/analysis/agreggate/Scrambled_PBS/agreggate/outs/count/filtered_feature_bc_matrix/")
-counts_matrix <- Read10X(data.dir = "filtered_feature_bc_matrix")
-levels(counts_matrix)
+
+DEG <- function(condition1, condition2, comparison_name){
+data_dir <- paste("/Users/shahina/Desktop/23-1ANLE-003/",comparison_name,"/filtered_feature_bc_matrix", sep="")
+expression_matrix <- Read10X(data.dir = data_dir)
 # Create a Seurat object
-seurat_obj <- CreateSeuratObject(counts = counts_matrix)
+SeuratObject <- CreateSeuratObject(counts = expression_matrix,min.cells = 3,names.delim = "-", names.field = 2)
 
-monocyte.de.markers <- FindMarkers(seurat_obj, ident.1 = "CD14+ Mono", ident.2 = "FCGR3A+ Mono")
+#For creating metadata manually use following steps and then add metadata to seurat object
+# Create dataframe by sample that contains matching orig.ident code
+metadata_by_sample <- tibble::tribble(~orig.ident,  ~sample_name, 
+                                  1, condition1,
+                                  2, condition2)
 
+# Change orig.ident column to factor so that it can be joined later
+metadata_by_sample$orig.ident <- as.factor(metadata_by_sample$orig.ident)
 
+# Pull existing meta data where samples are specified by orig.ident and remove everything but orig.ident
+OBJ_metadata <- SeuratObject@meta.data %>% 
+  select(orig.ident) %>% 
+  rownames_to_column("barcodes")
 
+# Use full join with object meta data in x position so that by sample meta dataframe is propagated across the by cell meta dataframe from the object.  And then remove orig.ident because it's already present in object meta data.
+full_new_meta <- full_join(x = OBJ_metadata, y = metadata_by_sample) %>% 
+  column_to_rownames("barcodes") %>% 
+  select(-orig.ident)
+
+#Add Metadata
+SeuratObject<- AddMetaData(SeuratObject, full_new_meta)
+#head(SeuratObject@meta.data)
+de.markers <- FindMarkers(SeuratObject, ident.1 = "2", ident.2 = "1")
+de <- de.markers[de.markers$p_val <= 0.05,] 
+
+write.csv(de,file=sprintf("%s/DEG_%s_vs_%s_filter_on_pval0.05.csv",comparison_name,condition2,condition1),quote=FALSE, row.names = TRUE)
+}
+
+DEG("PBS","Scrambled","Scrambled_PBS")
+DEG("Scrambled","BLR200","BLR200_Scrambled")
+DEG("Scrambled","CCN2","CCN2_Scrambled")
