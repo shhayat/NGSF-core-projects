7d8a1e06d2de483521967f2649dac9b9ef42e43d
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-19 19:56:18 -0600

    Update 05_differential_expression_analysis.R

--- 23-1ANLE-003/05_differential_expression_analysis.R
+++ 23-1ANLE-003/05_differential_expression_analysis.R
@@ -1,7 +1,7 @@
 library(Seurat)
 
 # Load the feature barcode matrix (.mtx)
-setwd(/Users/shahina/Desktop/23-1ANLE-003)
+setwd("/Users/shahina/Desktop/23-1ANLE-003")
 #counts_matrix <- Read10X(data.dir = "/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-003/analysis/agreggate/Scrambled_PBS/agreggate/outs/count/filtered_feature_bc_matrix/")
 counts_matrix <- Read10X(data.dir = "filtered_feature_bc_matrix")
 levels(counts_matrix)
