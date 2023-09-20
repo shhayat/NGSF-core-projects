2dcee978c6d54ceb9f24346eca4317df1941aa1b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-19 19:45:06 -0600

    Update 05_differential_expression_analysis.R

--- 23-1ANLE-003/05_differential_expression_analysis.R
+++ 23-1ANLE-003/05_differential_expression_analysis.R
@@ -2,22 +2,12 @@ library(Seurat)
 
 # Load the feature barcode matrix (.mtx)
 
-counts_matrix <- Read10X(data.dir = "/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-003/analysis/agreggate/Scrambled_PBS/agreggate/outs/count/filtered_feature_bc_matrix/matrix.mtx")
+counts_matrix <- Read10X(data.dir = "/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-003/analysis/agreggate/Scrambled_PBS/agreggate/outs/count/filtered_feature_bc_matrix/")
 
 # Create a Seurat object
 seurat_obj <- CreateSeuratObject(counts = counts_matrix)
 
-# Optionally, load cell and gene annotations
-# Replace 'path/to/cell_barcodes.tsv' and 'path/to/gene_list.tsv' with the actual paths to your cell barcode and gene list files
-cell_barcodes <- read.table("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-003/analysis/agreggate/Scrambled_PBS/agreggate/outs/count/barcodes.tsv", header = TRUE)
-gene_list <- read.table("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-003/analysis/agreggate/Scrambled_PBS/agreggate/outs/count/features.tsv", header = TRUE)
-
-# Add cell and gene annotations to the Seurat object
-seurat_obj$meta.data <- cell_barcodes
-rownames(seurat_obj) <- gene_list$gene_id
-
-
-
+monocyte.de.markers <- FindMarkers(seurat_obj, ident.1 = "CD14+ Mono", ident.2 = "FCGR3A+ Mono")
 
 
 
