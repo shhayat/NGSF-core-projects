6512302b2f31c9cab0b2f6a87f3282987f7fc311
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-19 19:54:24 -0600

    Update 05_differential_expression_analysis.R

--- 23-1ANLE-003/05_differential_expression_analysis.R
+++ 23-1ANLE-003/05_differential_expression_analysis.R
@@ -1,8 +1,9 @@
 library(Seurat)
 
 # Load the feature barcode matrix (.mtx)
-
-counts_matrix <- Read10X(data.dir = "/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-003/analysis/agreggate/Scrambled_PBS/agreggate/outs/count/filtered_feature_bc_matrix/")
+setwd(/Users/shahina/Desktop/23-1ANLE-003)
+#counts_matrix <- Read10X(data.dir = "/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-003/analysis/agreggate/Scrambled_PBS/agreggate/outs/count/filtered_feature_bc_matrix/")
+counts_matrix <- Read10X(data.dir = "filtered_feature_bc_matrix")
 levels(counts_matrix)
 # Create a Seurat object
 seurat_obj <- CreateSeuratObject(counts = counts_matrix)
