136a59e1f5096cc6f45dbdba4ac2843aba849c84
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-21 12:00:43 -0600

    Update 08_annovar_determine_lowfreqVariants.sh

--- 23-1MICO-001/08_annovar_determine_lowfreqVariants.sh
+++ 23-1MICO-001/08_annovar_determine_lowfreqVariants.sh
@@ -30,19 +30,19 @@ ${annovar}/convert2annovar.pl -format vcf4 ${DIR}/shared_snps.vcf > ${OUTDIR}/sh
 #${annovar}/annotate_variation.pl -buildver hg38 -downdb -webfrom annovar 1000g2015aug ${annovar}/humandb/
 #{annovar}/annotate_variation.pl -buildver hg38 -downdb -webfrom annovar ensGene41 ${annovar}/humandb/
 #{annovar}/annotate_variation.pl -buildver hg38 -downdb -webfrom annovar gnomad312_genome ${annovar}/humandb/
+{annovar}/annotate_variation.pl -buildver hg38 -downdb -webfrom annovar clinvar_20221231 ${annovar}/humandb/
 
-#latest clinvar file was downloaded from https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar_20230917.vcf.gz and converted it to format required by annovar using https://github.com/mobidic/update_annovar_db
+#latest clinvar file was downloaded from https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar_20230917.vcf.gz and converted it to format required by annovar 
 #python /globalhome/hxo752/HPC/tools/update_annovar_db/update_resources.py -d clinvar -hp /globalhome/hxo752/HPC/tools/annovar/humandb/ -a /globalhome/hxo752/HPC/tools/annovar -g GRCh38
 #${vt}/vt decompose /globalhome/hxo752/HPC/tools/annovar/humandb/clinvar_20230917.vcf.gz -o ${annovar}/humandb/temp.split.vcf
 #${annovar}/prepare_annovar_user.pl -dbtype clinvar_preprocess2 ${annovar}/humandb/temp.split.vcf -out ${annovar}/humandb/temp.split2.vcf
-${annovar}/convert2annovar.pl -dbtype clinvar_preprocess2 ${annovar}/humandb/temp.split.vcf ${annovar}/humandb/
 #${vt}/vt normalize ${annovar}/humandb/temp.split2.vcf -r ${GENOME} -o ${annovar}/humandb/temp.norm.vcf -w 2000000
 #${annovar}/prepare_annovar_user.pl -dbtype clinvar2 ${annovar}/humandb/temp.norm.vcf -out ${annovar}/humandb/hg38_clinvar_20230917_raw.txt
 #${annovar}/index_annovar.pl ${annovar}/humandb/hg38_clinvar_20230917_raw.txt -out ${annovar}/humandb/hg38_clinvar_20230917.txt -comment ${annovar}/humandb/comment_20230917.txt
           
 #Determining the population frequency
-${annovar}/table_annovar.pl ${OUTDIR}/D23000043_unique_snps.avinput /globalhome/hxo752/HPC/tools/annovar/humandb/ -buildver hg38 -out ${OUTDIR}/D23000043_snp_annotation -remove -protocol ensGene41,gnomad312_genome,gnomad_exome,exac03,avsnp150,1000g2015aug_all -operation g,f,f,f,f,f -nastring . -csvout -polish
-${annovar}/table_annovar.pl ${OUTDIR}/shared_snps.avinput /globalhome/hxo752/HPC/tools/annovar/humandb/ -buildver hg38 -out ${OUTDIR}/shared_snp_annotation -remove -protocol ensGene41,gnomad312_genome,gnomad_exome,exac03,avsnp150,1000g2015aug_all -operation g,f,f,f,f,f -nastring . -csvout -polish
+${annovar}/table_annovar.pl ${OUTDIR}/D23000043_unique_snps.avinput /globalhome/hxo752/HPC/tools/annovar/humandb/ -buildver hg38 -out ${OUTDIR}/D23000043_snp_annotation -remove -protocol ensGene41,clinvar_20221231,gnomad312_genome,gnomad_exome,exac03,avsnp150,1000g2015aug_all -operation g,f,f,f,f,f -nastring . -csvout -polish
+${annovar}/table_annovar.pl ${OUTDIR}/shared_snps.avinput /globalhome/hxo752/HPC/tools/annovar/humandb/ -buildver hg38 -out ${OUTDIR}/shared_snp_annotation -remove -protocol ensGene41,clinvar_20221231,gnomad312_genome,gnomad_exome,exac03,avsnp150,1000g2015aug_all -operation g,f,f,f,f,f -nastring . -csvout -polish
 
 #${annovar}/annotate_variation.pl -filter -dbtype 1000g2015aug -buildver hg38 -out ${DIR}/D23000043_annotation_1000g ${DIR}/D23000043_snps.avinput /globalhome/hxo752/HPC/tools/annovar/humandb/ -maf 0.05 -reverse
 #${annovar}/annotate_variation.pl -filter -dbtype 1000g2015aug -buildver hg38 -out ${DIR}/D23000044_annotation_1000g ${DIR}/D23000044_snps.avinput /globalhome/hxo752/HPC/tools/annovar/humandb/ -maf 0.05 -reverse
