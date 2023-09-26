27ba3d833288fd39ba210b7fbba69bdd343cc68e
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-26 09:03:00 -0600

    Update 01_concatenate_lane.sh

--- 23-1ANLE-004/01_concatenate_lane.sh
+++ 23-1ANLE-004/01_concatenate_lane.sh
@@ -4,11 +4,11 @@
 #SBATCH --job-name=combine_fastq
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=2:00:00
+#SBATCH --time=1:00:00
 #SBATCH --mem=8G
 
 set -eux
-	
+
 mkdir -p /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-004/analysis/fastq 
 FASTQ_FOLDER=/datastore/NGSF001/NB551711/230915_NB551711_0082_AHNHKFAFX5/Alignment_1/20230916_014510/Fastq
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-004/analysis/fastq
