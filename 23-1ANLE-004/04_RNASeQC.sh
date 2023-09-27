defbf81464efe8351fb5cea6af4538193a8e868d
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-27 11:05:48 -0600

    Update 04_RNASeQC.sh

--- 23-1ANLE-004/04_RNASeQC.sh
+++ 23-1ANLE-004/04_RNASeQC.sh
@@ -6,10 +6,7 @@
 #SBATCH --mem=5G
 
 set -eux
-
-# copy tool to tempdir and give execute permission
-cp /globalhome/hxo752/HPC/tools/src/rnaseqc.v2.4.2.linux ${SLURM_TMPDIR}
-chmod u+x ${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux
+module load python/3.9
 
 sample_name=$1; shift
 bam_file=$1
@@ -19,7 +16,9 @@ OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-004/ana
 
 mkdir -p ${OUTDIR}
 
-${SLURM_TMPDIR}/rnaseqc.v2.4.2.linux ${GTF} \
+cd /globalhome/hxo752/HPC/tools/rnaseqc/python
+
+python3.9 -m rnaseqc run ${GTF} \
                         ${bam_file} \
                         ${OUTDIR} \
                         --sample=${sample_name}
