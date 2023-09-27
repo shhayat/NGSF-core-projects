a2be75ffbe45309e4f8469aa7a02b20c74600a29
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-27 13:13:58 -0600

    Update 06_DESEQ2.R

--- 23-1ANLE-004/06_DESEQ2.R
+++ 23-1ANLE-004/06_DESEQ2.R
@@ -1,21 +1,27 @@
+setwd("/Users/shahina/Projects/23-1ANLE-004/")
+
 library("DESeq2")
 library("ggplot2")
 library("xlsx")
 
-setwd("/Users/shahina/Projects/23-1ANLE-004")
 dir.create("DESEQ2", recursive=TRUE, showWarnings = FALSE) 
 
 load("feature_count.RData")
-feature_count1 <- as.data.frame(feature_count)
+feature_count <- as.data.frame(feature_count)
+
+#remove number after decimal point from ensembl ID
+geneID <- gsub(".[0-9]*$", "", rownames(feature_count))
+rownames(feature_count) <- geneID
 
 #your first columns which are gene id and gene name
-feature_annotation <- cbind(GeneID=rownames(feature_count1),gene_name=feature_count1$gene_name)
-DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2, str)
+feature_annotation <- data.frame(GeneID=geneID,gene_name=feature_count[2])
+
+
+DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2)
 {
-  #feature_count <- feature_count[colnum]
-  feature_count <- feature_count1[colnum]
-  #at least one column has number
-  #feature_count <- feature_count[apply(feature_count,1,function(z) any(z!=0)),]
+  feature_count <- feature_count[colnum]
+  #remove row with sum zero
+  feature_count <- feature_count[rowSums(feature_count[,c(1:ncol(feature_count))])>0, ]
   
   sampleInfo=data.frame(sample_name=dput(as.character(names(feature_count))),
                         sample_type=dput(as.character(names(feature_count))),
@@ -36,29 +42,42 @@ DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2, str)
   #PCA PLOT
   ##########
   #gernate rlog for PCA
-  rld <-rlog(dds,blind=FALSE)
-  pdf(sprintf("DESEQ2/PCA_%s_%s_%s.pdf",cond2,cond1,str), width=8,height=8)
-  nudge <- position_nudge(y = 0.5)
-  p <- plotPCA(rld,intgroup=c("sample_group"))  
-  p <- p + geom_text(aes_string(label = "name"), color="black", position = nudge, size=2.8)
-  print(p)
-  dev.off()
+  #rld <-rlog(dds,blind=FALSE)
+  #pdf(sprintf("PCA_%s_%s.pdf",cond2,cond1), width=8,height=8)
+  #nudge <- position_nudge(y = 0.5)
+  #p <- plotPCA(rld,intgroup=c("sample_group"))  
+  #p <- p + geom_text(aes_string(label = "name"), color="black", position = nudge, size=2.8)
+  #print(p)
+  #dev.off()
   
   dds_wald <- DESeq(dds, betaPrior=FALSE, minReplicatesForReplace=Inf)
   res <- results(dds_wald, contrast=c("sample_group",cond2,cond1))
   
   resDF <- data.frame(GeneID=rownames(res),res)
   resDF <- merge(feature_annotation,resDF, by="GeneID")
+  resDF <- resDF[order(resDF$pvalue),]
+  log2FC <- resDF$log2FoldChange
+  resDF$Fold_Change = ifelse(log2FC > 0, 2 ^ log2FC, -1 / (2 ^ log2FC))
   
-  resDF1 <- subset(resDF, pvalue <= 0.05)
-  #order on FDR
-  resDF1 <- resDF1[order(resDF1$pvalue),]
-  log2FC1 <- resDF1$log2FoldChange
-  resDF1$Fold_Change = ifelse(log2FC1 > 0, 2 ^ log2FC1, -1 / (2 ^ log2FC1))
-
+  #filter on FC -1.5/+1.5 (used for significant genes only)
+  #down=resDF[resDF$Fold_Change <= -1.5,] 
+  #up=resDF[resDF$Fold_Change >= 1.5,]
+  #resDF=rbind(down,up)
+  
+  #remove rows with all NAs
+  #resDF1 <- subset(resDF, pvalue <= 0.05)
+  
   #return(dim(resDF1))
   #All significant
-  write.xlsx(resDF1,file=sprintf("DESEQ2/DEG_%s_vs_%s_%s_filter_on_pval.xlsx",cond2,cond1,str), row.names = FALSE)
+  #write.xlsx(resDF1,file=sprintf("DESEQ2/DEG_%s_vs_%s_filter_on_pval_and_Foldchange.xlsx",cond2,cond1), row.names = FALSE)
+  #All Genes
+  write.xlsx(resDF,file=sprintf("DESEQ2/DEG_%s_vs_%s_all_genes.xlsx",cond2,cond1), row.names = FALSE)
   
 }
-DEG_analysis(c(c(3,6,7,9:11,13)),"T0","T100","T0",3,4, "selected_samples")
+#analysis for all genes
+#DEG_analysis(c(3,5,7,4,6,8),"CONTROL","DMOG","CONTROL",3,3)
+DEG_analysis(c(3,7,4,8),"CONTROL2","DMOG2","CONTROL2",2,2)
+
+#analysis for significant genes
+DEG_analysis(c(3,5,7,4,6,8),"CONTROL3","DMOG3","CONTROL3",3,3)
+DEG_analysis(c(3,7,4,8),"CONTROL2","DMOG2","CONTROL2",2,2)
