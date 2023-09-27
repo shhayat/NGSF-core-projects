54763cee56d80ac68f10fe5fbbaf1bddfc57d4c3
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-27 08:49:58 -0600

    Create run_feature_count.sh

---
+++ 23-1ANLE-004/run_feature_count.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=feature-count
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=3:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/22-1BETO-001B
+module load r/4.1.2
+
+R CMD BATCH ${SCRIPT_DIR}/06_feature_count.R
