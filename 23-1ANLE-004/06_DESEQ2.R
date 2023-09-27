751365600d4bd124a393f18568651393cc82ec1a
Shahina <91986031+shhayat@users.noreply.github.com>
2023-09-27 15:00:41 -0600

    Update 06_DESEQ2.R

--- 23-1ANLE-004/06_DESEQ2.R
+++ 23-1ANLE-004/06_DESEQ2.R
@@ -42,13 +42,13 @@ DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2)
   #PCA PLOT
   ##########
   #gernate rlog for PCA
-  #rld <-rlog(dds,blind=FALSE)
-  #pdf(sprintf("PCA_%s_%s.pdf",cond2,cond1), width=8,height=8)
-  #nudge <- position_nudge(y = 0.5)
-  #p <- plotPCA(rld,intgroup=c("sample_group"))  
-  #p <- p + geom_text(aes_string(label = "name"), color="black", position = nudge, size=2.8)
-  #print(p)
-  #dev.off()
+  rld <-rlog(dds,blind=FALSE)
+  pdf(sprintf("PCA_%s_%s.pdf",cond2,cond1), width=8,height=8)
+  nudge <- position_nudge(y = 0.5)
+  p <- plotPCA(rld,intgroup=c("sample_group"))  
+  p <- p + geom_text(aes_string(label = "name"), color="black", position = nudge, size=2.8)
+  print(p)
+  dev.off()
   
   dds_wald <- DESeq(dds, betaPrior=FALSE, minReplicatesForReplace=Inf)
   res <- results(dds_wald, contrast=c("sample_group",cond2,cond1))
@@ -58,26 +58,11 @@ DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2)
   resDF <- resDF[order(resDF$pvalue),]
   log2FC <- resDF$log2FoldChange
   resDF$Fold_Change = ifelse(log2FC > 0, 2 ^ log2FC, -1 / (2 ^ log2FC))
-  
-  #filter on FC -1.5/+1.5 (used for significant genes only)
-  #down=resDF[resDF$Fold_Change <= -1.5,] 
-  #up=resDF[resDF$Fold_Change >= 1.5,]
-  #resDF=rbind(down,up)
-  
-  #remove rows with all NAs
-  #resDF1 <- subset(resDF, pvalue <= 0.05)
-  
-  #return(dim(resDF1))
-  #All significant
-  #write.xlsx(resDF1,file=sprintf("DESEQ2/DEG_%s_vs_%s_filter_on_pval_and_Foldchange.xlsx",cond2,cond1), row.names = FALSE)
+
+  resDF1 <- resDF[resDF$pvalue <= 0.05,]
   #All Genes
-  write.xlsx(resDF,file=sprintf("DESEQ2/DEG_%s_vs_%s_all_genes.xlsx",cond2,cond1), row.names = FALSE)
+  write.xlsx(resDF1,file=sprintf("DESEQ2/DEG_%s_vs_%s.xlsx",cond2,cond1), row.names = FALSE)
   
 }
-#analysis for all genes
-#DEG_analysis(c(3,5,7,4,6,8),"CONTROL","DMOG","CONTROL",3,3)
-DEG_analysis(c(3,7,4,8),"CONTROL2","DMOG2","CONTROL2",2,2)
+DEG_analysis(c(3,5,7,4,6,8),"CONTROL","DMOG","CONTROL",3,3)
 
-#analysis for significant genes
-DEG_analysis(c(3,5,7,4,6,8),"CONTROL3","DMOG3","CONTROL3",3,3)
-DEG_analysis(c(3,7,4,8),"CONTROL2","DMOG2","CONTROL2",2,2)
