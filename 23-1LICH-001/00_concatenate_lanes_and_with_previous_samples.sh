06e86172d78f912856b3e46893ce6cfab8529294
Shahina <91986031+shhayat@users.noreply.github.com>
2023-10-02 10:58:21 -0600

    Update 00_concatenate_lanes_and_with_previous_samples.sh

--- 23-1LICH-001/00_concatenate_lanes_and_with_previous_samples.sh
+++ 23-1LICH-001/00_concatenate_lanes_and_with_previous_samples.sh
@@ -10,7 +10,7 @@
 set -eux
 mkdir -p /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1LICH-001/analysis/fastq 
 FASTQ_FOLDER=/datastore/NGSF001/NB551711/230919_NB551711_0083_AH2FG5BGXV/Alignment_1/20230920_064527/Fastq
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANLE-004/analysis/fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1LICH-001/analysis/fastq
 
 #/datastore/NGSF001/projects/22-1LICH-001/fastq
 
