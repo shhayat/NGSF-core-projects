a21439753dbc1d8de48ca387bb1c0567d4f1959e
Shahina <91986031+shhayat@users.noreply.github.com>
2023-10-02 12:39:21 -0600

    Update 00_concatenate_lanes_and_with_previous_samples.sh

--- 23-1LICH-001/00_concatenate_lanes_and_with_previous_samples.sh
+++ 23-1LICH-001/00_concatenate_lanes_and_with_previous_samples.sh
@@ -34,10 +34,11 @@ cat ${FASTQ_FOLDER2}/R2300133_R2.fastq.gz ${OUTDIR1}/R2300144_R2.fastq.gz > ${OU
 #A3A I4 (1)(2)
 cat ${OUTDIR1}/R2300150_R1.fastq.gz ${OUTDIR1}/R2300152_R1.fastq.gz > ${OUTDIR2}/A3A_I4_R1.fastq.gz
 cat ${OUTDIR1}/R2300150_R2.fastq.gz ${OUTDIR1}/R2300152_R2.fastq.gz > ${OUTDIR2}/A3A_I4_R2.fastq.gz
-
 #A3A U1 (1)(2)
 cat ${OUTDIR1}/R2300149_R1.fastq.gz ${OUTDIR1}/R2300151_R1.fastq.gz > ${OUTDIR2}/A3A_U1_R1.fastq.gz
 cat ${OUTDIR1}/R2300149_R2.fastq.gz ${OUTDIR1}/R2300151_R2.fastq.gz > ${OUTDIR2}/A3A_U1_R2.fastq.gz
+
+
 #A3B U2
 cat ${FASTQ_FOLDER2}/R2300134_R1.fastq.gz ${OUTDIR1}/R2300145_R1.fastq.gz > ${OUTDIR2}/A3B_U2_R1.fastq.gz
 cat ${FASTQ_FOLDER2}/R2300134_R2.fastq.gz ${OUTDIR1}/R2300145_R2.fastq.gz > ${OUTDIR2}/A3B_U2_R2.fastq.gz
@@ -47,17 +48,20 @@ cat ${FASTQ_FOLDER2}/R2300135_R1.fastq.gz ${OUTDIR1}/R2300146_R2.fastq.gz > ${OU
 #A3B U1 (1)(2)
 cat ${OUTDIR1}/R2300153_R1.fastq.gz ${OUTDIR1}/R2300168_R1.fastq.gz > ${OUTDIR2}/A3B_U1_R1.fastq.gz
 cat ${OUTDIR1}/R2300153_R2.fastq.gz ${OUTDIR1}/R2300168_R2.fastq.gz > ${OUTDIR2}/A3B_U1_R2.fastq.gz
-#A3B U2 (1)(2)
-cat ${OUTDIR1}/R2300147_R1.fastq.gz ${OUTDIR}/R2300147_R1.fastq.gz > ${OUTDIR2}/A3B_U2_R1.fastq.gz
-cat ${OUTDIR1}/R2300147_R2.fastq.gz ${OUTDIR}/R2300147_R2.fastq.gz > ${OUTDIR2}/A3B_U2_R2.fastq.gz
+#A3B I2 (1)(2)
+cat ${OUTDIR1}/R2300154_R1.fastq.gz ${OUTDIR1}/R2300156_R1.fastq.gz > ${OUTDIR2}/A3B_U2_R1.fastq.gz
+cat ${OUTDIR1}/R2300154_R2.fastq.gz ${OUTDIR1}/R2300156_R2.fastq.gz > ${OUTDIR2}/A3B_U2_R2.fastq.gz
+
 
 #A3H U1
 cat ${FASTQ_FOLDER2}/R2300136_R1.fastq.gz ${OUTDIR1}/R2300147_R1.fastq.gz > ${OUTDIR2}/A3H_U1_R1.fastq.gz
 cat ${FASTQ_FOLDER2}/R2300136_R2.fastq.gz ${OUTDIR1}/R2300147_R2.fastq.gz > ${OUTDIR2}/A3H_U1_R2.fastq.gz
-#A3H I4
-cat ${FASTQ_FOLDER2}/R2300137_R1.fastq.gz ${OUTDIR1}/R2300157_R1.fastq.gz > ${OUTDIR2}/A3H_I4_R1.fastq.gz
-cat ${FASTQ_FOLDER2}/R2300137_R1.fastq.gz ${OUTDIR1}/R2300157_R2.fastq.gz > ${OUTDIR2}/A3H_I4_R2.fastq.gz
 #A3H U2 (1)(2)
 cat ${OUTDIR1}/R2300157_R1.fastq.gz ${OUTDIR1}/R2300159_R1.fastq.gz > ${OUTDIR2}/A3H_U2_R1.fastq.gz
 cat ${OUTDIR}/R2300157_R2.fastq.gz ${OUTDIR1}/R2300159_R2.fastq.gz > ${OUTDIR2}/A3H_U2_R2.fastq.gz
-
+#A3H I4
+cat ${FASTQ_FOLDER2}/R2300137_R1.fastq.gz ${OUTDIR1}/R2300167_R1.fastq.gz > ${OUTDIR2}/A3H_I4_R1.fastq.gz
+cat ${FASTQ_FOLDER2}/R2300137_R1.fastq.gz ${OUTDIR1}/R2300167_R2.fastq.gz > ${OUTDIR2}/A3H_I4_R2.fastq.gz
+#A3H I1
+cat ${OUTDIR1}/R2300158_R1.fastq.gz ${OUTDIR1}/R2300160_R1.fastq.gz > ${OUTDIR2}/A3H_I1_R1.fastq.gz
+cat ${OUTDIR1}/R2300158_R1.fastq.gz ${OUTDIR1}/R2300160_R2.fastq.gz > ${OUTDIR2}/A3H_I1_R2.fastq.gz
