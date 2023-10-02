eb08f8b0c1903f43050e074bd17cb513f9abf392
Shahina <91986031+shhayat@users.noreply.github.com>
2023-10-02 16:20:06 -0600

    Update 06_DESEQ2.R

--- 23-1ANLE-004/06_DESEQ2.R
+++ 23-1ANLE-004/06_DESEQ2.R
@@ -43,13 +43,13 @@ DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2)
   #PCA PLOT
   ##########
   #gernate rlog for PCA
-  rld <-rlog(dds,blind=FALSE)
-  pdf(sprintf("PCA_%s_%s.pdf",cond2,cond1), width=8,height=8)
-  nudge <- position_nudge(y = 0.5)
-  p <- plotPCA(rld,intgroup=c("sample_group"))  
-  p <- p + geom_text(aes_string(label = "name"), color="black", position = nudge, size=2.8)
-  print(p)
-  dev.off()
+ # rld <-rlog(dds,blind=FALSE)
+ # pdf(sprintf("PCA_%s_%s.pdf",cond2,cond1), width=8,height=8)
+ # nudge <- position_nudge(y = 0.5)
+ # p <- plotPCA(rld,intgroup=c("sample_group"))  
+ # p <- p + geom_text(aes_string(label = "name"), color="black", position = nudge, size=2.8)
+  #print(p)
+  #dev.off()
   
   dds_wald <- DESeq(dds, betaPrior=FALSE, minReplicatesForReplace=Inf)
   res <- results(dds_wald, contrast=c("sample_group",cond2,cond1))
@@ -60,9 +60,10 @@ DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2)
   log2FC <- resDF$log2FoldChange
   resDF$Fold_Change = ifelse(log2FC > 0, 2 ^ log2FC, -1 / (2 ^ log2FC))
 
-  resDF1 <- resDF[resDF$pvalue <= 0.05,]
+  #resDF1 <- resDF[resDF$pvalue <= 0.05,]
   #All Genes
-  write.xlsx(resDF1,file=sprintf("DESEQ2/DEG_%s_vs_%s.xlsx",cond2,cond1), row.names = FALSE)
+  write.xlsx(resDF,file=sprintf("DESEQ2/DEG_%s_vs_%s_all_genes.xlsx",cond2,cond1), row.names = FALSE)
+  #write.xlsx(resDF1,file=sprintf("DESEQ2/DEG_%s_vs_%s.xlsx",cond2,cond1), row.names = FALSE)
   
 }
 DEG_analysis(c(3,5,7,4,6,8),"CONTROL","DMOG","CONTROL",3,3)
