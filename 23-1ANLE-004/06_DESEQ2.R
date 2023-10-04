f00864cd3a8ba5031b713c0ff9f71cf6ed89c54e
Shahina <91986031+shhayat@users.noreply.github.com>
2023-10-04 11:04:17 -0600

    Update 06_DESEQ2.R

--- 23-1ANLE-004/06_DESEQ2.R
+++ 23-1ANLE-004/06_DESEQ2.R
@@ -59,12 +59,15 @@ DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2)
   resDF <- resDF[order(resDF$pvalue),]
   log2FC <- resDF$log2FoldChange
   resDF$Fold_Change = ifelse(log2FC > 0, 2 ^ log2FC, -1 / (2 ^ log2FC))
+  increased_DF=resDF[resDF$Fold_Change >= 1.5,]
+  decreased_DF=resDF[resDF$Fold_Change <= -1.5,]
 
   #resDF1 <- resDF[resDF$pvalue <= 0.05,]
   #All Genes
-  write.xlsx(resDF,file=sprintf("DESEQ2/DEG_%s_vs_%s_all_genes.xlsx",cond2,cond1), row.names = FALSE)
+  #write.xlsx(resDF,file=sprintf("DESEQ2/DEG_%s_vs_%s_all_genes.xlsx",cond2,cond1), row.names = FALSE)
   #write.xlsx(resDF1,file=sprintf("DESEQ2/DEG_%s_vs_%s.xlsx",cond2,cond1), row.names = FALSE)
-  
+  write.xlsx(increased_DF,file=sprintf("DESEQ2/DEG_%s_vs_%s_increased.xlsx",cond2,cond1), row.names = FALSE)
+  write.xlsx(decreased_DF,file=sprintf("DESEQ2/DEG_%s_vs_%s_decreased.xlsx",cond2,cond1), row.names = FALSE)
 }
 DEG_analysis(c(3,5,7,4,6,8),"CONTROL","DMOG","CONTROL",3,3)
 
