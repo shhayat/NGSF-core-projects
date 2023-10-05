0b5c930773dd44b913c6fef753a892279137d644
Shahina <91986031+shhayat@users.noreply.github.com>
2023-10-05 09:16:44 -0600

    Create run_featureCount.sh

---
+++ 23-1LICH-001/run_featureCount.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=feature-count
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=3:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1LICH-001
+module load r/4.1.2
+
+R CMD BATCH ${SCRIPT_DIR}/05_feature_count.R
