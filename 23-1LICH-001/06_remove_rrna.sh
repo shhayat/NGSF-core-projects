fea33d7c51ebd5cbd4676a10c672abd68d32599f
Shahina <91986031+shhayat@users.noreply.github.com>
2023-10-05 12:31:32 -0600

    Create 06_remove_rrna.sh

---
+++ 23-1LICH-001/06_remove_rrna.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=dedup
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=2
+#SBATCH --time=03:00:00
+#SBATCH --mem=20G
+#SBATCH --output=%j.out
+
+set -eux
+
+#work adapted from https://github.com/ngsf-usask/scripts/tree/main/RNAseq/22-1MILE-002
+module load samtools
+DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1LICH-001/analysis/star_alignment
+RRNA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1LICH-001/analysis/rrna_intervals/rRNA_intervals_merged.bed
+NCPU=2
+sample_name=$1; shift
+BAM=$1;
+
+
+echo "Dropping ribosomal RNA reads"
+samtools view -@ ${NCPU} \
+              -U ${DIR}/${sample_name}/${BAM} \
+              -O BAM \
+              -L ${RRNA} \
+              ${DIR}/${sample_name}/${BAM}
