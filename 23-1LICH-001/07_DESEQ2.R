3053a5f7a3394fc65c75d55a04b7231a6269a525
Shahina <91986031+shhayat@users.noreply.github.com>
2023-10-10 17:32:39 -0600

    Update and rename DESEQ2.R to 07_DESEQ2.R

--- 23-1LICH-001/07_DESEQ2.R
+++ 23-1LICH-001/07_DESEQ2.R
@@ -53,14 +53,21 @@ DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2)
   
   resDF <- data.frame(GeneID=rownames(res),res)
   resDF <- merge(feature_annotation,resDF, by="GeneID")
+  resDF <- resDF[resDF$pvalue <= 0.05,]
   resDF <- resDF[order(resDF$pvalue),]
-  log2FC <- resDF$log2FoldChange
   resDF$Fold_Change = ifelse(log2FC > 0, 2 ^ log2FC, -1 / (2 ^ log2FC))
-  increased_DF=resDF[resDF$Fold_Change >= 1.5,]
-  decreased_DF=resDF[resDF$Fold_Change <= -1.5,]
+  write.xlsx(resDF,file=sprintf("DESEQ2/DEG_%s_vs_%s_increased.xlsx",cond2,cond1), row.names = FALSE)
+}
 
-  #resDF1 <- resDF[resDF$pvalue <= 0.05,]
-  #All Genes
-  #write.xlsx(resDF,file=sprintf("DESEQ2/DEG_%s_vs_%s_all_genes.xlsx",cond2,cond1), row.names = FALSE)
-  #write.xlsx(resDF1,file=sprintf("DESEQ2/DEG_%s_vs_%s.xlsx",cond2,cond1), row.names = FALSE)
-  write.xlsx(increased_DF,file=sprintf("DESEQ2/DEG_%s_vs_%s_increased.xlsx",cond2,cond1), row.names = FALSE)
+#A3A_I5 vs A3A_U6 (n=2)
+DEG_analysis(c(6,4),"A3A_U6","A3A_I5","A3A_U6",2,2)
+#A3B_I5 vs A3B_U5 (n=2)
+DEG_analysis(c(8,10),"A3B_U5","A3B_I5","A3B_U5",2,2)
+#A3H_I4 vs A3H_U1 (n=2)
+DEG_analysis(c(13,12),"A3H_U1","A3H_I4","A3H_U1",2,2)
+#A3A_I4 vs A3A_U1 (n=2)
+DEG_analysis(c(5,3),"A3A_U1","A3A_I4","A3A_U1",2,2)
+#A3B_I2 vs A3B_U1 (n=2)
+DEG_analysis(c(9,7),"A3B_U1","A3B_I2","A3B_U1",2,2)
+#A3H_I1 vs A3H_U2 (n=2)
+DEG_analysis(c(14,11),"A3H_U2","A3H_I1","A3H_U2",2,2)
