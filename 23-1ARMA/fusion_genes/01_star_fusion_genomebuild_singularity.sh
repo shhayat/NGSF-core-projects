6029a60970cc067be0051b0c17b817007e7f7346
Shahina <91986031+shhayat@users.noreply.github.com>
2023-10-25 10:45:56 -0600

    Update 01_star_fusion_genomebuild_singularity.sh

--- 23-1ARMA/fusion_genes/01_star_fusion_genomebuild_singularity.sh
+++ 23-1ARMA/fusion_genes/01_star_fusion_genomebuild_singularity.sh
@@ -11,8 +11,8 @@
 
 module load singularity/3.9.2
 
-GTF=/globalhome/hxo752/HPC/ngsf_git_repos/CanFam3.1/Canis_lupus_familiaris.ROS_Cfam_1.0.109.gtf
-GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/CanFam3.1/Canis_lupus_familiaris.ROS_Cfam_1.0.dna.toplevel.fa
+GTF=/datastore/NGSF001/analysis/references/ROS_Cfam_1.0/Canis_lupus_familiaris.ROS_Cfam_1.0.109.gtf
+GENOME=/datastore/NGSF001/analysis/references/ROS_Cfam_1.0/Canis_lupus_familiaris.ROS_Cfam_1.0.dna.toplevel.fa
 DFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/Dfam/canis_lupus_familiaris_dfam.hmm
 PFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/Pfam/Pfam-A.hmm
 
