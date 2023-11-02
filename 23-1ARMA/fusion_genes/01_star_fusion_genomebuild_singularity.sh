ce49b34f6c8541bcb1ca5bb72b92b5ae323b2d2a
Shahina <91986031+shhayat@users.noreply.github.com>
2023-11-02 11:19:41 -0600

    Update 01_star_fusion_genomebuild_singularity.sh

--- 23-1ARMA/fusion_genes/01_star_fusion_genomebuild_singularity.sh
+++ 23-1ARMA/fusion_genes/01_star_fusion_genomebuild_singularity.sh
@@ -13,8 +13,8 @@ module load singularity/3.9.2
 
 #GTF=/datastore/NGSF001/analysis/references/ROS_Cfam_1.0/Canis_lupus_familiaris.ROS_Cfam_1.0.109.gtf
 #GENOME=/datastore/NGSF001/analysis/references/ROS_Cfam_1.0/Canis_lupus_familiaris.ROS_Cfam_1.0.dna.toplevel.fa
-GTF=/datastore/NGSF001/analysis/references/Cfam3.1_NCBI/genomic.gtf
-GENOME=/datastore/NGSF001/analysis/references/Cfam3.1_NCBI/GCF_000002285.3_CanFam3.1_genomic.fna
+GTF=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/Cfam3.1_NCBI/genomic.gtf
+GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/Cfam3.1_NCBI/GCF_000002285.3_CanFam3.1_genomic.fna
 #DFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/Dfam/canis_lupus_familiaris_dfam.hmm
 #PFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/Pfam/Pfam-A.hmm
 DFAM_DATABASE=/datastore/NGSF001/projects/ARMA_NTRK_Fusion/Fusion_events/genome_build_and_required_databases/Dfam/canis_lupus_familiaris_dfam.hmm
