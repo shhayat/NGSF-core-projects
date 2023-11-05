fc083f11a9ebe627c590fb72988d24a2d8d6b157
Shahina <91986031+shhayat@users.noreply.github.com>
2023-11-04 19:36:08 -0600

    Update autonomics_latest_run.R

--- 20-1JOHO-001/autonomics_latest_run.R
+++ 20-1JOHO-001/autonomics_latest_run.R
@@ -1,9 +1,6 @@
 library("autonomics")
 library("magrittr")
-library("biomaRt")
-library("org.Rn.eg.db")
-
-
+library("ggplot2")
 setwd("/Users/shahina/Projects/20-1JOHO-001")
 res <- read.table("20-1JOHO-001_htseq_counts.txt", head=TRUE)
 #Contrasts
@@ -19,36 +16,41 @@ names(res) <-c("gene_name","treated_female_R25","treated_female_R26","control_ma
                "control_male_R61","treated_female_R62","treated_male_R63","treated_male_R64")
 
 
-write.table(res, "/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_htseq_counts_treated_control_40_male_female.txt", sep="\t", quote=FALSE,  row.names=FALSE)
-
+write.table(res,"/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_htseq_counts_treated_control_40_male_female.txt", sep="\t", quote=FALSE,  row.names=FALSE)
 
 res_treated_and_control_male <- data.frame(res[1], res[grep("treated_male",names(res))], res[grep("control_male",names(res))])
 write.table(res_treated_and_control_male, "/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_htseq_counts_treated_control_20_male.txt", sep="\t", quote=FALSE,  row.names=FALSE)
+
 
 res_treated_and_control_female <- data.frame(res[1], res[grep("treated_female",names(res))], res[grep("control_female",names(res))])
 write.table(res_treated_and_control_female, "/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_htseq_counts_treated_control_20_female.txt", sep="\t", quote=FALSE,  row.names=FALSE)
 
 
-#####################
-#ANALYSIS FOR MALES
-#####################
 #run autonomics on 20 male samples
 object <-  read_rnaseq_counts(file ="/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_htseq_counts_treated_control_20_male.txt",pca=TRUE, plot = FALSE)
 object$subgroup <- as.factor(c("treated","treated","treated","treated","treated","treated","treated","treated","treated","treated","treated",
                                "control","control","control","control","control","control","control","control","control"))
 
-pdf("/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_20male_samples.pdf", width=10)
-  biplot(object, pca1, pca2,color=subgroup)
-dev.off()
+#tiff("/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_20male_samples.tif")
+#biplot(object, pca1, pca2, label=sample_id,color=subgroup) + ggtitle("All 20 males")
+biplot(object, pca1, pca2,color=subgroup) + ggtitle("All 20 males")
 
-object1 <- fit_limma(object, formula = ~ 0 + subgroup, contrastdefs = c('treated - control'), plot = FALSE)
+#dev.off()
 
-fdata(object1) %<>% cbind(limma(object1))
-fdata1_select <- fdata(object1)[c("feature_name","treated - control.effect","treated - control.p","treated - control.fdr")]
+object1 <- fit_limma(object, formula = ~ 0 + subgroup, contrastdefs = c('treated - control'), plot = TRUE)
+
+fdata1 <- data.frame(fdt(object1))
+fdata1_select <- fdata1[c(2,5,6,7)]
 fdata1_pval=fdata1_select[fdata1_select[3] <=0.05,]
+#NROW(fdata1_pval)
+#[1] 2101
 names(fdata1_pval) <- c("gene_name","effects","pvalue", "fdr")
 
+#NROW(fdata1_select[fdata1_select[4] <=0.05,])
+#[1] 0
 write.csv(fdata1_pval, "/Users/shahina/Projects/20-1JOHO-001/latest_analysis/DEG_20male_samples_treated_vs_control.csv")
+
+
 
 
 #After excluding 5 samples from 20 samples
@@ -66,93 +68,60 @@ object$subgroup <- as.factor(c("treated","treated","treated","treated","treated"
 object <- object %>% filter_samples(!sample_id %in% c("control_male_R56","control_male_R59","treated_male_R28","treated_male_R40","treated_male_R63"))
 
 pdf("/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_15male_samples.pdf", width=10)
- biplot(object, pca1, pca2,label=sample_id,color=subgroup)
+biplot(object, pca1, pca2,color=subgroup)
 dev.off()
 
-object1 <- fit_limma(object, formula = ~ 0 + subgroup, contrastdefs = c('treated - control'), plot = FALSE)
+object2 <- fit_limma(object, formula = ~ 0 + subgroup, contrastdefs = c('subgrouptreated - subgroupcontrol'), plot = TRUE)
 
-fdata(object1) %<>% cbind(limma(object1))
-fdata1_select <- fdata(object1)[c("feature_name","treated - control.effect","treated - control.p","treated - control.fdr")]
+
+fdata1 <- data.frame(fdt(object2))
+fdata1_select <- fdata1[c(2,5,6,7)]
 fdata1_pval=fdata1_select[fdata1_select[3] <=0.05,]
+#NROW(fdata1_pval)
 names(fdata1_pval) <- c("gene_name","effects","pvalue", "fdr")
-write.csv(fdata1_pval, "/Users/shahina/Projects/20-1JOHO-001/latest_analysis/DEG_15male_samples_treated_vs_control.csv")
+#[1] 7966
+fdata1_fdr=fdata1_select[fdata1_select[4] <=0.05,]
+names(fdata1_fdr) <- c("gene_name","effects","pvalue", "fdr")
+fdata1_fdr_order <- fdata1_fdr[order(fdata1_fdr$fdr),]
+#NROW(fdata1_fdr)
+#[1] 6660
+write.csv(fdata1_fdr_order, "/Users/shahina/Projects/20-1JOHO-001/latest_analysis/DEG_15male_samples_treated_vs_control_at_fdr0.05.csv")
+
 
 #####################
 #ANALYSIS FOR FEMALES
 #####################
-#run autonomics on 20 male samples
-object <-  read_rnaseq_counts(file ="/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_htseq_counts_treated_control_20_female.txt",pca=TRUE, plot = TRUE)
+#run autonomics on 20 female samples
+object <-  read_rnaseq_counts(file ="/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_htseq_counts_treated_control_20_female.txt",pca=TRUE, plot = FALSE)
 object$subgroup <- as.factor(c("treated","treated","treated","treated","treated","treated","treated","treated","treated","treated","treated",
                                "control","control","control","control","control","control","control","control","control"))
 
-pdf("/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_20female_samples.pdf", width=10)
-  biplot(object, pca1, pca2,color=subgroup)
-dev.off()
+#pdf("/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_20female_samples.pdf", width=10)
+biplot(object, pca1, pca2,color=subgroup) + ggtitle("All 20 Females")
+#dev.off()
 
 object1 <- fit_limma(object, formula = ~ 0 + subgroup, contrastdefs = c('treated - control'), plot = FALSE)
-
 fdata(object1) %<>% cbind(limma(object1))
 fdata1_select <- fdata(object1)[c("feature_name","treated - control.effect","treated - control.p","treated - control.fdr")]
 fdata1_pval=fdata1_select[fdata1_select[3] <=0.05,]
 names(fdata1_pval) <- c("gene_name","effects","pvalue", "fdr")
-
 write.csv(fdata1_pval, "/Users/shahina/Projects/20-1JOHO-001/latest_analysis/DEG_20female_samples_treated_vs_control.csv")
 
 
-################################
-#ANALYSIS FOR FEMALES AND MALES
-################################
-
-object <-  read_rnaseq_counts(file ="/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_htseq_counts_treated_control_40_male_female.txt",pca=TRUE, plot = TRUE)
-#object$subgroup <- as.factor(c("treated","treated","control","control","control","treated","control","treated","control","treated","control",
-#                               "control","control","control","treated","control","control","treated","control","treated","control","treated","treated",
-#                              "treated","treated","treated","control", "treated","treated","treated","control","treated","control","control","treated","control","treated","treated","treated"))
 
 
-pdf("/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_40male_female_samples.pdf", width=10)
-  biplot(object, pca1, pca2,color=subgroup)
-dev.off()
+#################################
+#ANALYSIS FOR FEMALES AND MALE
+#################################
 
-object$subgroup <- as.factor(c("treated","treated","control","treated_male","control","control","treated","control","treated","control","treated","control",
-                               "control","control","control","treated","control","control","treated","control","treated","control","treated","treated",
-                              "treated","treated","treated","control", "treated","treated","treated","control","treated","control","control","treated","control","treated","treated","treated"))
+#run autonomics on male and female samples
+object <-  read_rnaseq_counts(file ="/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_htseq_counts_treated_control_40_male_female.txt",pca=TRUE, plot = FALSE)
+
+biplot(object, pca1, pca2,color=subgroup) + ggtitle("All 40 Samples (20 Males and 20 Females)")
 
 object1 <- fit_limma(object, formula = ~ 0 + subgroup, contrastdefs = c('treated - control'), plot = FALSE)
-
 fdata(object1) %<>% cbind(limma(object1))
 fdata1_select <- fdata(object1)[c("feature_name","treated - control.effect","treated - control.p","treated - control.fdr")]
 fdata1_pval=fdata1_select[fdata1_select[3] <=0.05,]
 names(fdata1_pval) <- c("gene_name","effects","pvalue", "fdr")
-
 write.csv(fdata1_pval, "/Users/shahina/Projects/20-1JOHO-001/latest_analysis/DEG_40male_female_samples_treated_vs_control.csv")
-
-
-##########
-#JUNK CODE
-##########
-#CALCULATE cpm for 15 males
-object <-  read_rnaseq_counts(file ="/Users/shahina/Projects/20-1JOHO-001/latest_analysis/20-1JOHO-001_htseq_counts_treated_control_20_male.txt",pca=FALSE, plot = FALSE, cpm=FALSE)
-object$subgroup <- as.factor(c("treated","treated","treated","treated","treated","treated","treated","treated","treated","treated","treated",
-                               "control","control","control","control","control","control","control","control","control"))
-#drop samples
-object <- object %>% filter_samples(!sample_id %in% c("control_male_R56","control_male_R59","treated_male_R28","treated_male_R40","treated_male_R63"))
-
-library(edgeR)
-## as DGEList
-dge <- DGEList(counts=counts(object))
-## calculate norm. factors
-dge <- calcNormFactors(dge, method="TMM")
-## get normalized counts
-normalized.counts <- cpm(dge)
-normalized.counts1 <- normalized.counts[rownames(normalized.counts) == "Penk",]
-
-write.csv(normalized.counts1, "/Users/shahina/Projects/20-1JOHO-001/penk_cpm.csv")
-
-
-#libsize <- scaledlibsizes(values(object))
-
-#cpm <- counts2cpm(counts(object))
-
-#cpm[rownames(cpm) == "Penk",]
-
-
