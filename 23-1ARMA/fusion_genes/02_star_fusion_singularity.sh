9181e3517ce1e0c7e7882cef83d325b707f803e3
Shahina <91986031+shhayat@users.noreply.github.com>
2023-11-09 12:13:47 -0600

    Update 02_star_fusion_singularity.sh

--- 23-1ARMA/fusion_genes/02_star_fusion_singularity.sh
+++ 23-1ARMA/fusion_genes/02_star_fusion_singularity.sh
@@ -15,7 +15,7 @@ module load singularity/3.9.2
 #output_dir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/
 output_dir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/analysis/hemangiosarcoma
 #output_dir=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/squamous_cell_carcinomas/analysis/
-#CanineStarFusionBuild=/datastore/NGSF001/projects/ARMA_NTRK_Fusion/ctat_genome_lib_build_dir
+#CanineStarFusionBuild=/datastore/NGSF001/projects/ARMA_NTRK_Fusion/ctat_genome_lib_build_dir_ROS_Cfam_1.0
 CanineStarFusionBuild=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/ctat_genome_lib_build_dir_Cfam3.1_NCBI
 
 sample_name=$1; shift
