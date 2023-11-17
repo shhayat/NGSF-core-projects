b34dbe87bf2e31f28bd7c0dad55ef697a92a9bfd
Shahina <91986031+shhayat@users.noreply.github.com>
2023-11-17 15:14:00 -0600

    Update 06_DESEQ2.R

--- 23-1TOSH-001/06_DESEQ2.R
+++ 23-1TOSH-001/06_DESEQ2.R
@@ -42,7 +42,7 @@ DEG_analysis <- function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2, group_na
   pdf(sprintf("DESEQ2/PCA_%s_%s_%s.pdf",cond2,cond1,group_name), width=8,height=8)
   nudge <- position_nudge(y = 0.5)
   p <- plotPCA(rld,intgroup=c("sample_group"))  
-  p + geom_text(aes_string(label = sample_names), color="black", position = nudge, size=2.8)
+  p + geom_text(aes_string(label = "names"), color="black", position = nudge, size=2.8)
   print(p)
   dev.off()
   
