f353de92b5d3cd53c1215618d8210c4235989522
Shahina <91986031+shhayat@users.noreply.github.com>
2023-11-30 17:30:38 -0600

    Update 01_concatenate_lanes.sh

--- 23-1ANFA-001/01_concatenate_lanes.sh
+++ 23-1ANFA-001/01_concatenate_lanes.sh
@@ -1,8 +1,8 @@
 ngsf1=/datastore/NGSF001/NB551711/230927_NB551711_0084_AH2FGFBGXV/Alignment_1/20230928_022021/Fastq
 ngsf2=/datastore/NGSF001/NB551711/230928_NB551711_0085_AH2KFHBGXV/Alignment_1/20230929_024416/Fastq
 
-opal1=/datastore/NGSF001/projects/23-1ANFA-001/data/Sequencing results downloaded from OPAL/23-1ANFA-001-OPAL/samples lane 1
-opal2=/datastore/NGSF001/projects/23-1ANFA-001/data/Sequencing results downloaded from OPAL/23-1ANFA-001-OPAL/samples lane 2
+opal1=/datastore/NGSF001/projects/23-1ANFA-001/data/Sequencing_results_downloaded_from_OPAL/23-1ANFA-001-OPAL/samples_lane_1
+opal2=/datastore/NGSF001/projects/23-1ANFA-001/data/Sequencing_results_downloaded_from_OPAL/23-1ANFA-001-OPAL/samples_lane_2
 
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANFA-001/analysis/fastq
 
