fe13f4bbf6e571546376fa83b1062381acbcce53
Shahina <91986031+shhayat@users.noreply.github.com>
2023-12-01 11:52:22 -0600

    Create 03_cellranger_count.sh

---
+++ 23-1ANFA-001/03_cellranger_count.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=cellranger-count
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=24:00:00
+#SBATCH --mem=128G
+#SBATCH  --output=%j.out
+
+export PATH=/globalhome/hxo752/HPC/tools/cellranger-7.1.0/bin:$PATH
+
+NCPUS=16
+RAM_MEMORY=128
+
+FASTQS=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANFA-001/analysis/fastq
+REF=/datastore/NGSF001/analysis/references/cell_ranger_genome_builds/refdata-gex-mm10-2020-A/
+OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/SingleCellSequencing/analysis/count_files
+mkdir -p ${OUTPUT}
+cd ${OUTPUT}
+SAMPLE_NAME=$1;
+
+/globalhome/hxo752/HPC/tools/cellranger-7.1.0/bin/cellranger count --id=${SAMPLE_NAME} \
+                 --transcriptome=${REF} \
+                 --fastqs=${FASTQS} \
+                 --sample=${SAMPLE_NAME} \
+                 --localcores=${NCPUS} \
+                 --localmem=${RAM_MEMORY}
+#done
