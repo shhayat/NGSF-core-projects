cc2c8dbe8baaf24db6b057e7973915cff88cfcac
Shahina <91986031+shhayat@users.noreply.github.com>
2023-12-01 14:51:14 -0600

    Update 01_concatenate_lanes.sh

--- 23-1ANFA-001/01_concatenate_lanes.sh
+++ 23-1ANFA-001/01_concatenate_lanes.sh
@@ -20,23 +20,23 @@ mkdir -p $OUTDIR
 for i in 15 16 17 18
 do
 
-	cat ${ngsf1}/SC23000${i}_*_R1_001.fastq.gz ${ngsf2}/SC23000${i}_*_R1_001.fastq.gz > ${OUTDIR}/SC23000${i}_R1.fastq.gz
-	cat ${ngsf1}/SC23000${i}_*_R2_001.fastq.gz ${ngsf2}/SC23000${i}_*_R2_001.fastq.gz > ${OUTDIR}/SC23000${i}_R2.fastq.gz
+	cat ${ngsf1}/SC23000${i}_*_R1_001.fastq.gz ${ngsf2}/SC23000${i}_*_R1_001.fastq.gz > ${OUTDIR}/SC23000${i}_S1_L001_R1_001.fastq.gz
+	cat ${ngsf1}/SC23000${i}_*_R2_001.fastq.gz ${ngsf2}/SC23000${i}_*_R2_001.fastq.gz > ${OUTDIR}/SC23000${i}_S1_L001_R2_001.fastq.gz
 done
 
-#cat ${opal1}/OS010364_r1.fastq.gz ${opal2}/OS01036A_r1.fastq.gz > ${OUTDIR}/SC2300009_R1.fastq.gz
-#cat ${opal1}/OS010365_r1.fastq.gz ${opal2}/OS01036B_r1.fastq.gz > ${OUTDIR}/SC2300010_R1.fastq.gz
-#cat ${opal1}/OS010366_r1.fastq.gz ${opal2}/OS01036C_r1.fastq.gz > ${OUTDIR}/SC2300011_R1.fastq.gz
-#cat ${opal1}/OS010367_r1.fastq.gz ${opal2}/OS01036D_r1.fastq.gz > ${OUTDIR}/SC2300012_R1.fastq.gz
-#cat ${opal1}/OS010368_r1.fastq.gz ${opal2}/OS01036E_r1.fastq.gz > ${OUTDIR}/SC2300013_R1.fastq.gz
-#cat ${opal1}/OS010369_r1.fastq.gz ${opal2}/OS01036F_r1.fastq.gz > ${OUTDIR}/SC2300014_R1.fastq.gz
+#cat ${opal1}/OS010364_r1.fastq.gz ${opal2}/OS01036A_r1.fastq.gz > ${OUTDIR}/SC2300009_S1_L001_R1_001.fastq.gz
+#cat ${opal1}/OS010365_r1.fastq.gz ${opal2}/OS01036B_r1.fastq.gz > ${OUTDIR}/SC2300010_S1_L001_R1_001.fastq.gz
+#cat ${opal1}/OS010366_r1.fastq.gz ${opal2}/OS01036C_r1.fastq.gz > ${OUTDIR}/SC2300011_S1_L001_R1_001.fastq.gz
+#cat ${opal1}/OS010367_r1.fastq.gz ${opal2}/OS01036D_r1.fastq.gz > ${OUTDIR}/SC2300012_S1_L001_R1_001.fastq.gz
+#cat ${opal1}/OS010368_r1.fastq.gz ${opal2}/OS01036E_r1.fastq.gz > ${OUTDIR}/SC2300013_S1_L001_R1_001.fastq.gz
+#cat ${opal1}/OS010369_r1.fastq.gz ${opal2}/OS01036F_r1.fastq.gz > ${OUTDIR}/SC2300014_S1_L001_R1_001.fastq.gz
 
 
-#cat ${opal1}/OS010364_r2.fastq.gz ${opal2}/OS01036A_r2.fastq.gz > ${OUTDIR}/SC2300009_R2.fastq.gz
-#cat ${opal1}/OS010365_r2.fastq.gz ${opal2}/OS01036B_r2.fastq.gz > ${OUTDIR}/SC2300010_R2.fastq.gz
-#cat ${opal1}/OS010366_r2.fastq.gz ${opal2}/OS01036C_r2.fastq.gz > ${OUTDIR}/SC2300011_R2.fastq.gz
-#cat ${opal1}/OS010367_r2.fastq.gz ${opal2}/OS01036D_r2.fastq.gz > ${OUTDIR}/SC2300012_R2.fastq.gz
-#cat ${opal1}/OS010368_r2.fastq.gz ${opal2}/OS01036E_r2.fastq.gz > ${OUTDIR}/SC2300013_R2.fastq.gz
-#cat ${opal1}/OS010369_r2.fastq.gz ${opal2}/OS01036F_r2.fastq.gz > ${OUTDIR}/SC2300014_R2.fastq.gz
+#cat ${opal1}/OS010364_r2.fastq.gz ${opal2}/OS01036A_r2.fastq.gz > ${OUTDIR}/SC2300009_S1_L001_R2_001.fastq.gz
+#cat ${opal1}/OS010365_r2.fastq.gz ${opal2}/OS01036B_r2.fastq.gz > ${OUTDIR}/SC2300010_S1_L001_R2_001.fastq.gz
+#cat ${opal1}/OS010366_r2.fastq.gz ${opal2}/OS01036C_r2.fastq.gz > ${OUTDIR}/SC2300011_S1_L001_R2_001.fastq.gz
+#cat ${opal1}/OS010367_r2.fastq.gz ${opal2}/OS01036D_r2.fastq.gz > ${OUTDIR}/SC2300012_S1_L001_R2_001.fastq.gz
+#cat ${opal1}/OS010368_r2.fastq.gz ${opal2}/OS01036E_r2.fastq.gz > ${OUTDIR}/SC2300013_S1_L001_R2_001.fastq.gz
+#cat ${opal1}/OS010369_r2.fastq.gz ${opal2}/OS01036F_r2.fastq.gz > ${OUTDIR}/SC2300014_S1_L001_R2_001.fastq.gz
 
 
