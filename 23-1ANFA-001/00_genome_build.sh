86300a960ce8d94ffd14768ac1fcceb4afe9478b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-12-01 12:16:22 -0600

    Update 00_genome_build.sh

--- 23-1ANFA-001/00_genome_build.sh
+++ 23-1ANFA-001/00_genome_build.sh
@@ -1,1 +1,26 @@
+#!/bin/sh
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --job-name=genome_index
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=10
+#SBATCH --time=03:00:00
+#SBATCH --mem=80G
+#SBATCH --output=%j.out
+
+export PATH=/globalhome/hxo752/HPC/tools/cellranger-7.1.0/bin:$PATH
+
+GENOME=/datastore/NGSF001/analysis/references/iGenomes/Bos_taurus/Ensembl/UMD3.1/Sequence/WholeGenomeFasta/genome.fa
+GTF=/datastore/NGSF001/analysis/references/iGenomes/Bos_taurus/Ensembl/UMD3.1/Annotation/Genes/genes.gtf
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANFA-001/analysis/indices
+
+NCPU=10
+
+mkdir -p $OUTDIR
+cd ${OUTDIR}
+
+cellranger mkref \
+         --nthreads=${NCPUS} \
+         --genome=${OUTDIR} \
+         --fasta=${GENOME} \
+         --genes=${GTF}
