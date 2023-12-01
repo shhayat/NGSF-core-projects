6d0d430b90ebfde5e17217aedbdbee4bcb2a1c7b
Shahina <91986031+shhayat@users.noreply.github.com>
2023-12-01 13:23:06 -0600

    Update 00_genome_build.sh

--- 23-1ANFA-001/00_genome_build.sh
+++ 23-1ANFA-001/00_genome_build.sh
@@ -12,14 +12,13 @@ export PATH=/globalhome/hxo752/HPC/tools/cellranger-7.1.0/bin:$PATH
 
 GENOME=/datastore/NGSF001/analysis/references/iGenomes/Bos_taurus/Ensembl/UMD3.1/Sequence/WholeGenomeFasta/genome.fa
 GTF=/datastore/NGSF001/analysis/references/iGenomes/Bos_taurus/Ensembl/UMD3.1/Annotation/Genes/genes.gtf
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANFA-001/analysis/Bos_taurus_genome_index
-GENOME_PREFIX="Bos_taurus_"
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ANFA-001/analysis/
+GENOME_FOLDER_NAME="Bos_taurus_genome_index"
 NCPUS=10
-mkdir -p ${OUTDIR}
 cd ${OUTDIR}
 
 cellranger mkref \
          --nthreads=${NCPUS} \
-         --genome=${GENOME_PREFIX} \
+         --genome=${GENOME_FOLDER_NAME} \
          --fasta=${GENOME} \
          --genes=${GTF}
