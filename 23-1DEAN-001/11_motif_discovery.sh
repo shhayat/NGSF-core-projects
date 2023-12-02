1262325aed4c79a14eed018534a3abbb0abd00a9
Shahina <91986031+shhayat@users.noreply.github.com>
2023-12-01 20:20:48 -0600

    Update 11_motif_discovery.sh

--- 23-1DEAN-001/11_motif_discovery.sh
+++ 23-1DEAN-001/11_motif_discovery.sh
@@ -24,16 +24,16 @@ cellLine=$1;
 
 #for motif discovery step repeat-masked version of the genome is required where all repeat sequences have been replaced with Ns
 #we will generate masked genome based on peak intervals in idr_filtered.bed 
-#cd /globalhome/hxo752/HPC/tools
-#chmod a+x bedtools.static.binary
-#./bedtools.static.binary getfasta -fi /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/genome.fa \
- #                                 -bed ${DIR}/${cellLine}_optimal_filtered_3_columns.bed \
- #                                 -fo ${OUTDIR}/${cellLine}_genome.masked.on.idr_intervals.fa
+cd /globalhome/hxo752/HPC/tools
+chmod a+x bedtools.static.binary
+./bedtools.static.binary getfasta -fi /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/genome.fa \
+                                 -bed ${DIR}/${cellLine}_optimal_filtered_3_columns.bed \
+                                  -fo ${OUTDIR}/${cellLine}_genome.masked.on.idr_intervals.fa
 
 
 #select peaks with the strongest signal for motif finding
 #sort -k 7,7nr  ${DIR}/${bed_peak} | head -n 200 > ${DIR}/motif_discovery/${sample_name}_top.bed
 
-cd ${OUTDIR}
+#cd ${OUTDIR}
 #since we have limited number of peaks we will not select top peaks and proceed with meme-chip
-/globalhome/hxo752/HPC/anaconda3/envs/meme/bin/meme-chip -oc ${cellLine}_motif_discovery ${cellLine}_genome.masked.on.idr_intervals.fa
+#/globalhome/hxo752/HPC/anaconda3/envs/meme/bin/meme-chip -oc ${cellLine}_motif_discovery ${cellLine}_genome.masked.on.idr_intervals.fa
