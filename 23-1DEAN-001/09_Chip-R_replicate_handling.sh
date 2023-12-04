26e660760f91b012720293a7820978a874c89ccd
Shahina <91986031+shhayat@users.noreply.github.com>
2023-12-03 21:12:53 -0600

    Update 09_Chip-R_replicate_handling.sh

--- 23-1DEAN-001/09_Chip-R_replicate_handling.sh
+++ 23-1DEAN-001/09_Chip-R_replicate_handling.sh
@@ -26,6 +26,7 @@ chipr -i ${files} \
       -m 1 \
       -o ${OUTDIR}/${cellLine}
 
+
 awk '{if($5 >= 540) print $0}' ${OUTDIR}/${cellLine}_optimal.bed > ${OUTDIR}/${cellLine}_optimal_filtered.bed
 
 #sort on -log10(pvalue)
