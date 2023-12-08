12489d88426a9f8b373f86ad13db214d48020f34
Shahina <91986031+shhayat@users.noreply.github.com>
2023-12-08 15:33:22 -0600

    Update annotatePeaks_and_visualize.R

--- 23-1DEAN-001/annotatePeaks_and_visualize.R
+++ 23-1DEAN-001/annotatePeaks_and_visualize.R
@@ -51,8 +51,7 @@ pdf("BT549_chip_profile.pdf")
  #coverage plot 
  covplot(ReadPeakList$BT549, weightCol="V5",chrs=c("chr1", "chr2","chr3", "chr4","chr5", "chr6","chr7", "chr8","chr9", 
                                                    "chr10","chr11", "chr12","chr13", "chr14","chr15","chr16", "chr17",
-                                                   "chr18","chr19", "chr20"))
- #from cov plot we saw most of the peaks are at chr 19. we check cov plot for chr 19
+                                                   "chr18","chr19", "chr20","chr21","chr22","chrX","chrY","chrM"))
  #covplot(PeakList_with_added_chr_str[[1]], weightCol="V5", chrs=c("chr19"))
 
  #Profile of ChIP peaks binding to TSS regions
@@ -89,8 +88,9 @@ dev.off()
 
 pdf("HCC1806_chip_profile.pdf")
  #coverage plot 
- covplot(ReadPeakList$HCC1806, weightCol="V5")
- #from cov plot we saw most of the peaks are at chr 19. we check cov plot for chr 19
+ covplot(ReadPeakList$HCC1806, weightCol="V5",chrs=c("chr1", "chr2","chr3", "chr4","chr5", "chr6","chr7", "chr8","chr9", 
+                                                   "chr10","chr11", "chr12","chr13", "chr14","chr15","chr16", "chr17",
+                                                   "chr18","chr19", "chr20","chr21","chr22","chrX","chrY","chrM")) 
  #covplot(PeakList_with_added_chr_str[[1]], weightCol="V5", chrs=c("chr19"))
 
  #Profile of ChIP peaks binding to TSS regions
