599c3f0d6ad50937c2cc10afe1f6b7d712518635
Shahina <91986031+shhayat@users.noreply.github.com>
2023-12-16 21:10:55 -0600

    Update 04_picard_dedup_and_rm_blacklist_reads.sh

--- 23-1DEAN-001/04_picard_dedup_and_rm_blacklist_reads.sh
+++ 23-1DEAN-001/04_picard_dedup_and_rm_blacklist_reads.sh
@@ -13,10 +13,11 @@ set -eux
 
 #module load picard/2.23.3 
 #module load samtools
-module load nixpkgs/16.09
-module load picard
-module load gcc/5.4.0
-module load intel/2016.4
+#module load nixpkgs/16.09
+#module load picard
+#module load gcc/5.4.0
+#module load intel/2016.4
+module load sambamba
 module load bedtools/2.26.0
 
 BAMDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/alignment
@@ -24,25 +25,28 @@ NCPU=4
 
 sample_name=$1
 
-java -Xmx80G -XX:ParallelGCThreads=$NCPU -Djava.io.tmpdir=$SLURM_TMPDIR -jar $EBROOTPICARD/picard.jar AddOrReplaceReadGroups \
-	I=${BAMDIR}/${sample_name}/${sample_name}.aligned.bam \
-	O=${BAMDIR}/${sample_name}/${sample_name}.aligned_sort.bam \
-	SO=coordinate \
-	RGID=4 \
-	RGLB=lib1 \
-	RGPL=ILLUMINA \
-	RGPU=unit1 \
-	RGSM=20
+#java -Xmx80G -XX:ParallelGCThreads=$NCPU -Djava.io.tmpdir=$SLURM_TMPDIR -jar $EBROOTPICARD/picard.jar AddOrReplaceReadGroups \
+#	I=${BAMDIR}/${sample_name}/${sample_name}.aligned.bam \
+#	O=${BAMDIR}/${sample_name}/${sample_name}.aligned_sort.bam \
+#	SO=coordinate \
+#	RGID=4 \
+#	RGLB=lib1 \
+#	RGPL=ILLUMINA \
+#	RGPU=unit1 \
+#	RGSM=20
 
-java -Xmx80G -XX:ParallelGCThreads=$NCPU -Djava.io.tmpdir=$SLURM_TMPDIR -jar $EBROOTPICARD/picard.jar MarkDuplicates \
-            I=${BAMDIR}/${sample_name}/${sample_name}.aligned_sort.bam \
-            O=${BAMDIR}/${sample_name}/${sample_name}.aligned_dedup.bam \
-            M=${BAMDIR}/${sample_name}/dedup_metrics.txt \
-    	    VALIDATION_STRINGENCY=LENIENT \
-     	    REMOVE_DUPLICATES=true \
-      	    ASSUME_SORTED=true 2> ${BAMDIR}/${sample_name}/${sample_name}_picard.log && \
-	    samtools index ${BAMDIR}/${sample_name}/${sample_name}.aligned_dedup.bam
+#java -Xmx80G -XX:ParallelGCThreads=$NCPU -Djava.io.tmpdir=$SLURM_TMPDIR -jar $EBROOTPICARD/picard.jar MarkDuplicates \
+ #           I=${BAMDIR}/${sample_name}/${sample_name}.aligned_sort.bam \
+  #          O=${BAMDIR}/${sample_name}/${sample_name}.aligned_dedup.bam \
+   #         M=${BAMDIR}/${sample_name}/dedup_metrics.txt \
+    #	    VALIDATION_STRINGENCY=LENIENT \
+     #	    REMOVE_DUPLICATES=true \
+      #	    ASSUME_SORTED=true 2> ${BAMDIR}/${sample_name}/${sample_name}_picard.log && \
+	#    samtools index ${BAMDIR}/${sample_name}/${sample_name}.aligned_dedup.bam
 
+sambamba view -h -t 2 -f bam -F "[XS] == null and not unmapped and not duplicate" \
+           ${BAMDIR}/${sample_name}/${sample_name}.aligned.bam > ${BAMDIR}/${sample_name}/${sample_name}.aligned_dedup.bam && \
+	   samtools index ${BAMDIR}/${sample_name}/${sample_name}.aligned_dedup.bam
 
 #Remove reads from *.aligned_dedup.bam which are present in blacklist
 bedtools intersect -v -a ${BAMDIR}/${sample_name}/${sample_name}.aligned_dedup.bam \
