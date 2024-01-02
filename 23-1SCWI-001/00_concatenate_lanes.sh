3861ef77cb3aef0fb1b02e0a28523ca136151142
Shahina <91986031+shhayat@users.noreply.github.com>
2024-01-02 12:08:41 -0600

    Update 00_concatenate_lanes.sh

--- 23-1SCWI-001/00_concatenate_lanes.sh
+++ 23-1SCWI-001/00_concatenate_lanes.sh
@@ -10,15 +10,16 @@
 	
 set -eux
 	
-mkdir -p /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1CHXI-001/analysis/Fastq 
+mkdir -p /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SCWI-001/analysis/Fastq 
 
-FASTQ_FOLDER=/datastore/NGSF001/NB551711/230208_NB551711_0063_AHT2WGBGXN/Alignment_1/20230208_231511/Fastq
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1CHXI-001/analysis/Fastq
+FASTQ_FOLDER1=/datastore/NGSF001/NB551711/231122_NB551711_0087_AHK2NHBGXV/Alignment_1/20231123_013827/Fastq
+FASTQ_FOLDER2=/datastore/NGSF001/NB551711/231123_NB551711_0088_AHK2TKBGXV/Alignment_1/20231124_020057/Fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SCWI-001/analysis/Fastq
 	
 
-for i in $(seq -w 03 18)
+for i in $(seq -w 181 204)
 do
-	echo Combining library R22000${i}
-	cat ${FASTQ_FOLDER}/R23000${i}_*_R1_* > ${OUTDIR}/R23000${i}_R1.fastq.gz
-	cat ${FASTQ_FOLDER}/R23000${i}_*_R2_* > ${OUTDIR}/R23000${i}_R2.fastq.gz
+	echo Combining library R300${i}
+	cat ${FASTQ_FOLDER1}/R2300${i}_*_R1_* > ${OUTDIR}/R2300${i}_R1.fastq.gz
+	cat ${FASTQ_FOLDER1}/R23000${i}_*_R2_* > ${OUTDIR}/R2300${i}_R2.fastq.gz
 done
