502cbbf232dc8d704a4060e00e5dbb9bc3568f1d
Shahina <91986031+shhayat@users.noreply.github.com>
2024-01-04 14:00:31 -0600

    Update 05_DESEQ2.R

--- 23-1SCWI-001/05_DESEQ2.R
+++ 23-1SCWI-001/05_DESEQ2.R
@@ -9,7 +9,8 @@ dir.create("DESEQ2", recursive=TRUE, showWarnings = FALSE)
 
 load("feature_count.RData")
 feature_count <- as.data.frame(feature_count)
-
+colnames(feature_count) <- c("geneID","gene_name","1M","2M","3M","4M","5M","6M","7M","8M","9M","10M","11M","12M",
+                            "1F","2F","3F","4F","5F","6F","7F","8F","9F","10F","11F","12F")
 #remove number after decimal point from ensembl ID
 geneID <- gsub(".[0-9]*$", "", rownames(feature_count))
 rownames(feature_count) <- geneID
@@ -59,14 +60,10 @@ DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2)
   resDF <- resDF[order(resDF$pvalue),]
   log2FC <- resDF$log2FoldChange
   resDF$Fold_Change = ifelse(log2FC > 0, 2 ^ log2FC, -1 / (2 ^ log2FC))
-  increased_DF=resDF[resDF$Fold_Change >= 1.5,]
-  decreased_DF=resDF[resDF$Fold_Change <= -1.5,]
 
   #resDF1 <- resDF[resDF$pvalue <= 0.05,]
   #All Genes
   #write.xlsx(resDF,file=sprintf("DESEQ2/DEG_%s_vs_%s_all_genes.xlsx",cond2,cond1), row.names = FALSE)
-  #write.xlsx(resDF1,file=sprintf("DESEQ2/DEG_%s_vs_%s.xlsx",cond2,cond1), row.names = FALSE)
-  write.xlsx(increased_DF,file=sprintf("DESEQ2/DEG_%s_vs_%s_increased.xlsx",cond2,cond1), row.names = FALSE)
-  write.xlsx(decreased_DF,file=sprintf("DESEQ2/DEG_%s_vs_%s_decreased.xlsx",cond2,cond1), row.names = FALSE)
+
 }
-DEG_analysis(c(3,5,7,4,6,8),"CONTROL","DMOG","CONTROL",3,3)
+DEG_analysis(c(3,),"CONTROL","DMOG","CONTROL",3,3)
