3d774b019c0b399af00b3cbd5aa629607cc1fe46
Shahina <91986031+shhayat@users.noreply.github.com>
2024-01-05 14:41:37 -0600

    Update multiqc.sh

--- 23-1CHXI-001/multiqc.sh
+++ 23-1CHXI-001/multiqc.sh
@@ -3,10 +3,15 @@ OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1CHXI-001/ana
 cd /globalhome/hxo752/HPC/tools
 multiqc ${DIR}/*_fastqc.zip -o ${OUTDIR} -n fastqc
 
+DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1CHXI-001/analysis/fastqc_trimmed
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1CHXI-001/analysis/multiqc
+cd /globalhome/hxo752/HPC/tools
+multiqc ${DIR}/*_fastqc.zip -o ${OUTDIR} -n fastqc_trimmed
+
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1CHXI-001/analysis/star_alignment
 cd /globalhome/hxo752/HPC/tools
 multiqc ${DIR}/*/Log.final.out -o ${OUTDIR} -n alignment
 
 DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1CHXI-001/analysis/rnaseqc
 cd /globalhome/hxo752/HPC/tools
-./multiqc ${DIR}/*.metrics.tsv -o ${OUTDIR} -n rnaseqc
+multiqc ${DIR}/*.metrics.tsv -o ${OUTDIR} -n rnaseqc
