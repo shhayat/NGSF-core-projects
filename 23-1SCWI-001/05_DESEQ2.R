5e2da3422ad199c3cb90f7cc8c9bbb50fa0241e8
Shahina <91986031+shhayat@users.noreply.github.com>
2024-01-25 16:17:56 -0600

    Update 05_DESEQ2.R

--- 23-1SCWI-001/05_DESEQ2.R
+++ 23-1SCWI-001/05_DESEQ2.R
@@ -77,13 +77,11 @@ DEG_analysis(c(7,10,13,16,19,22,25,3,6,12,15,18,21,24),"GFP","CRE","GFP",7,7,"Ma
 load("feature_count.RData")
 feature_count <- as.data.frame(feature_count)
 feature_count <- feature_count[c(1,2,3,6,9,12,15,18,21,24,4,7,10,13,16,19,22,25,5,8,11,14,17,20,23,26)]
-feature_count1 <- feature_count[rowSums(feature_count[,c(3:ncol(feature_count))])>1, ]
+feature_count <- feature_count[rowSums(feature_count[,c(3:ncol(feature_count))])>1, ]
 
-colnames(feature_count1) <- c("geneID","gene_name","1M_CRE","2M_CRE","3M_CRE","4M_CRE","5M_CRE","6M_CRE","7M_CRE","8M_CRE",
+colnames(feature_count) <- c("geneID","gene_name","1M_CRE","2M_CRE","3M_CRE","4M_CRE","5M_CRE","6M_CRE","7M_CRE","8M_CRE",
                              "9M_GFP","10M_GFP","11M_GFP","12M_GFP","1F_GFP","2F_GFP","3F_GFP","4F_GFP",
                              "5F_HnRF1","6F_HnRF1","7F_HnRF1","8F_HnRF1","9F_HnRF1","10F_HnRF1","11F_HnRF1","12F_HnRF1")
-#remove number after decimal point from ensembl ID
-feature_annotation <- data.frame(GeneID=geneID,gene_name=feature_count[2])
 
 #remove number after decimal point from ensembl ID
 geneID <- gsub(".[0-9]*$", "", rownames(feature_count))
