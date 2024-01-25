e07786bf66b9beda1a2f8e37b7ece3d4d1ad4c39
Shahina <91986031+shhayat@users.noreply.github.com>
2024-01-25 15:56:01 -0600

    Update 05_DESEQ2.R

--- 23-1SCWI-001/05_DESEQ2.R
+++ 23-1SCWI-001/05_DESEQ2.R
@@ -20,7 +20,7 @@ feature_annotation <- data.frame(GeneID=geneID,gene_name=feature_count[2])
 DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2, str,str1)
 {
   feature_count <- feature_count[colnum]
-  #remove row with sum zero
+  #keep row with sum greater than 1
   feature_count <- feature_count[rowSums(feature_count[,c(1:ncol(feature_count))])>1, ]
   
   sampleInfo=data.frame(sample_name=dput(as.character(names(feature_count))),
@@ -73,7 +73,36 @@ DEG_analysis(c(7,10,13,16,19,22,25,3,6,12,15,18,21,24),"GFP","CRE","GFP",7,7,"Ma
 #DEG_analysis(c(16,19,22,25,17,20,23,26),"GFP","HnRF1","GFP",4,4,"Females","all_samples")
 
 
+#GET RAW COUNTS
+load("feature_count.RData")
+feature_count <- as.data.frame(feature_count)
+colnames(feature_count) <- c("geneID","gene_name","1M","2M","3M","4M","5M","6M","7M","8M","9M","10M","11M","12M",
+                            "1F","2F","3F","4F","5F","6F","7F","8F","9F","10F","11F","12F")
 
+feature_annotation <- data.frame(GeneID=geneID,gene_name=feature_count[2])
+
+#remove number after decimal point from ensembl ID
+geneID <- gsub(".[0-9]*$", "", rownames(feature_count))
+rownames(feature_count) <- geneID
+
+#keep row with sum greater than 1
+feature_count <- feature_count[rowSums(feature_count[,c(3:ncol(feature_count))])>1, ]
+
+#your first columns which are gene id and gene name
+resDF <- merge(feature_annotation,resDF, by="GeneID")
+
+write.xlsx(resDF,file="DESEQ2/raw_counts.xlsx", row.names = FALSE)
+
+
+#GET NORMALIZED COUNTS
+
+dds <- estimateSizeFactors(dds)
+norm.expr <- counts(dds, normalized=TRUE)
+norm.expr <- as.data.frame(norm.expr)
+norm.expr <- data.frame(rownames(norm.expr),norm.expr)
+
+norm_counts <- merge(feature_annotation,norm.expr, by="GeneID")
+write.csv(norm_counts,file="All_normalized_genes.csv", quote=FALSE, row.names = FALSE)
 
 
 
