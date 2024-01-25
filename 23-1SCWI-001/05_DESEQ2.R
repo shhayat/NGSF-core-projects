a9aed8189e83a2e4a62ea592f93891c797e068cc
Shahina <91986031+shhayat@users.noreply.github.com>
2024-01-25 12:10:55 -0600

    Update 05_DESEQ2.R

--- 23-1SCWI-001/05_DESEQ2.R
+++ 23-1SCWI-001/05_DESEQ2.R
@@ -63,8 +63,8 @@ DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2, str,str
 
 }
 #DEG_analysis(c(4,7,10,13,16,19,22,25,3,6,9,12,15,18,21,24),"GFP","CRE","GFP",8,8,"Males_Females","all_samples")
-DEG_analysis(c(4,7,10,13,16,19,22,25,11,14,17,20,23,26),"GFP","HnRF1","GFP",8,6,"Males_Females","excluded_2outliers")
 #DEG_analysis(c(4,7,10,13,16,19,22,25,5,8,11,14,17,20,23,26),"GFP","HnRF1","GFP",8,8,"Males_Females","all_samples")
+DEG_analysis(c(4,7,10,13,16,19,22,25,5,14,17,20,23,26),"GFP","HnRF1","GFP",8,6,"Males_Females","excluded_2samples")
 #DEG_analysis(c(7,10,13,5,14),"GFP","HnRF1","GFP",3,2,"Males","filtered")
 
 #DEG_analysis(c(4,7,10,13,3,6,9,12),"GFP","CRE","GFP",4,4,"Males","all_samples")
