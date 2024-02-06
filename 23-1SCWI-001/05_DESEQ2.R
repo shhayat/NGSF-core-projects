6a1bbf955f48a26187de38cfee0396d2bd0dbec7
Shahina <91986031+shhayat@users.noreply.github.com>
2024-02-05 19:24:28 -0600

    Update 05_DESEQ2.R

--- 23-1SCWI-001/05_DESEQ2.R
+++ 23-1SCWI-001/05_DESEQ2.R
@@ -144,12 +144,12 @@ normalized_count <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2, str
   write.table(norm_counts,file=sprintf("DESEQ2/normalized_count_%s_vs_%s_%s.txt",cond2,cond1,str), row.names = FALSE, sep="\t")
 
 }
-normalized_count(c(4,7,10,13,16,19,22,25,3,6,9,12,15,18,21,24),"GFP","CRE","GFP",8,8,"Males_Females",c("9M_GFP","10M_GFP","11M_GFP","12M_GFP","1F_GFP","2F_GFP","3F_GFP","4F_GFP","1M_CRE","2M_CRE","3M_CRE","4M_CRE","5M_CRE","6M_CRE","7M_CRE","8M_CRE"))
+normalized_count(c(4,7,10,13,16,19,22,25,3,6,9,12,15,18,21,24),"GFP","CRE","GFP",8,8,"Males_Females",c("2M_GFP","5M_GFP","8M_GFP","11M_GFP","2F_GFP","8F_GFP","11F_GFP","11F_GFP","1M_CRE","4M_CRE","7M_CRE","10M_CRE","1F_CRE","4F_CRE","7F_CRE","10F_CRE"))
 normalized_count(c(4,7,10,13,16,19,22,25,5,8,11,14,17,20,23,26),"GFP","HnRF1","GFP",8,8,"Males_Females",c("9M_GFP","10M_GFP","11M_GFP","12M_GFP","1F_GFP","2F_GFP","3F_GFP","4F_GFP","5F_HnRF1","6F_HnRF1","7F_HnRF1","8F_HnRF1","9F_HnRF1","10F_HnRF1","11F_HnRF1","12F_HnRF1"))
 
 
-
-
+colnames(feature_count) <- c("geneID","gene_name","1M","2M","3M","4M","5M","6M","7M","8M","9M","10M","11M","12M",
+                            "1F","2F","3F","4F","5F","6F","7F","8F","9F","10F","11F","12F")
 
 
 
