270145f2f6d3e578797159a47d5680863d28d6c0
Shahina <91986031+shhayat@users.noreply.github.com>
2024-02-06 16:40:31 -0600

    Update 02_cellranger_count.sh

--- 23-1ARMA-002/02_cellranger_count.sh
+++ 23-1ARMA-002/02_cellranger_count.sh
@@ -15,7 +15,7 @@ NCPUS=16
 RAM_MEMORY=128
 
 FASTQS=/datastore/NGSF001/projects/23-1ARMA-002/Fastq
-REF=/datastore/NGSF001/analysis/references/cell_ranger_genome_builds/Bos_taurus_genome_index/
+REF=
 OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA-002/analysis/count_files
 mkdir -p ${OUTPUT}
 cd ${OUTPUT}
