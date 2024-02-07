8c7ef599ec018f7b15c9adbb70db7b9de17460ae
Shahina <91986031+shhayat@users.noreply.github.com>
2024-02-07 11:58:23 -0600

    Update 02_genome_build.sh

--- 23-1ARMA-002/02_genome_build.sh
+++ 23-1ARMA-002/02_genome_build.sh
@@ -9,7 +9,6 @@
 #SBATCH --output=%j.out
 
 export PATH=/globalhome/hxo752/HPC/tools/cellranger-7.1.0/bin:$PATH
-
 GENOME=/datastore/NGSF001/analysis/references/ROS_Cfam_1.0/Canis_lupus_familiaris.ROS_Cfam_1.0.dna.toplevel.fa
 GTF=/datastore/NGSF001/analysis/references/ROS_Cfam_1.0/Canis_lupus_familiaris.ROS_Cfam_1.0.109.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA-002/analysis/
