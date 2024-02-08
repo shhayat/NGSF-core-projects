1cbbf74d1bae46d5097cb4a29ef0d143a4ea5f99
Shahina <91986031+shhayat@users.noreply.github.com>
2024-02-08 10:08:17 -0600

    Update 01_demultiplex.sh

--- 23-1ARMA-002/01_demultiplex.sh
+++ 23-1ARMA-002/01_demultiplex.sh
@@ -1,3 +1,16 @@
-FASTQ_FOLDER=/datastore/NGSF001/NB551711/231219_NB551711_0089_AHKH3NBGXV/Alignment_1/20231220_044335/Fastq
+#!/bin/bash
 
-cellranger demux --run=/sequencing/140101_D00123_0111_AHAWT7ADXX
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=cellranger-count
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=24:00:00
+#SBATCH --mem=128G
+#SBATCH  --output=%j.out
+
+export PATH=/globalhome/hxo752/HPC/tools/cellranger-7.1.0/bin:$PATH
+
+BCL_FOLDER=/datastore/NGSF001/NB551711/231219_NB551711_0089_AHKH3NBGXV/Data/Intensities/BaseCalls
+
+cellranger demux --run=${BCL_FOLDER}
