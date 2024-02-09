ea9048cd381288a70f8f7e2f7db97474a0af776f
Shahina <91986031+shhayat@users.noreply.github.com>
2024-02-09 12:48:40 -0600

    Update 03_cellranger_count.sh

--- 23-1ARMA-002/03_cellranger_count.sh
+++ 23-1ARMA-002/03_cellranger_count.sh
@@ -14,7 +14,7 @@ export PATH=/globalhome/hxo752/HPC/tools/cellranger-7.1.0/bin:$PATH
 NCPUS=16
 RAM_MEMORY=128
 
-FASTQS=/datastore/NGSF001/projects/2023/23-1ARMA-002/Fastq
+FASTQS=/datastore/NGSF001/projects/2023/23-1ARMA-002/analysis/Fastq
 REF=/datastore/NGSF001/analysis/references/cell_ranger_genome_builds/Canine_index
 OUTPUT=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA-002/analysis/count_files
 mkdir -p ${OUTPUT}
