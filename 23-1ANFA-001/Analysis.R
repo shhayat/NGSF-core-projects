d445c2d72229748a6f7f4154bc5e313cf1bd1742
Shahina <91986031+shhayat@users.noreply.github.com>
2024-02-12 12:49:11 -0600

    Update Analysis.R

--- 23-1ANFA-001/Analysis.R
+++ 23-1ANFA-001/Analysis.R
@@ -1,5 +1,9 @@
 #Batch Correction
 https://www.10xgenomics.com/analysis-guides/batch-effect-correction-in-chromium-single-cell-atac-data
 
+expression_matrix <- Read10X(data.dir = data_dir)
+# Create a Seurat object
+SeuratObject <- CreateSeuratObject(counts = expression_matrix,min.cells = 3,names.delim = "-", names.field = 2)
+
 
 SC2300015 vs SC2300013
