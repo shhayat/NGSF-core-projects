0bda1e26d367830974a0408e4436634fbb29f0c9
Shahina <91986031+shhayat@users.noreply.github.com>
2024-02-12 14:09:00 -0600

    Update Analysis.R

--- 23-1ANFA-001/Analysis.R
+++ 23-1ANFA-001/Analysis.R
@@ -1,6 +1,20 @@
 #Batch Correction
 https://www.10xgenomics.com/analysis-guides/batch-effect-correction-in-chromium-single-cell-atac-data
 
+library(Seurat)
+library(magrittr)
+library(dplyr)
+library(tibble)
+
+#https://hbctraining.github.io/scRNA-seq_online/lessons/pseudobulk_DESeq2_scrnaseq.html
+# Load the feature barcode matrix (.mtx)
+setwd("/Users/shahina/Projects/23-1ANFA-001/count_files/")
+
+data_dir <- paste(comparison_name,"/outs/count/filtered_feature_bc_matrix", sep="")
+expression_matrix <- Read10X(data.dir = data_dir)
+# Create a Seurat object
+SeuratObject <- CreateSeuratObject(counts = expression_matrix,min.cells = 3,names.delim = "-", names.field = 2)
+
 expression_matrix <- Read10X(data.dir = data_dir)
 # Create a Seurat object
 SeuratObject <- CreateSeuratObject(counts = expression_matrix,min.cells = 3,names.delim = "-", names.field = 2)
