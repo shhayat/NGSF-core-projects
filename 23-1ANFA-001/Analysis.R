1ad519fc1c246047bd09ac5ae3cb71c2cadd5277
Shahina <91986031+shhayat@users.noreply.github.com>
2024-02-12 16:32:14 -0600

    Update Analysis.R

--- 23-1ANFA-001/Analysis.R
+++ 23-1ANFA-001/Analysis.R
@@ -1,23 +1,26 @@
-#Batch Correction
-https://www.10xgenomics.com/analysis-guides/batch-effect-correction-in-chromium-single-cell-atac-data
-
+#https://satijalab.org/seurat/archive/v3.0/pbmc3k_tutorial.html
+#https://github.com/hbctraining/scRNA-seq_online/tree/master/lessons
 library(Seurat)
 library(magrittr)
 library(dplyr)
 library(tibble)
 
-#https://hbctraining.github.io/scRNA-seq_online/lessons/pseudobulk_DESeq2_scrnaseq.html
 # Load the feature barcode matrix (.mtx)
-setwd("/Users/shahina/Projects/23-1ANFA-001/count_files/")
+setwd("Desktop/core-projects/23-1ANFA-001/count_files/")
 
-data_dir <- paste(comparison_name,"/outs/count/filtered_feature_bc_matrix", sep="")
-expression_matrix <- Read10X(data.dir = data_dir)
+data_dir1 <- paste("SC2300017","/raw_feature_bc_matrix", sep="")
+data_dir2 <- paste("SC2300013","/raw_feature_bc_matrix", sep="")
+
+expression_49loopC <- Read10X(data.dir = data_dir1)
+expression_49DPP1 <- Read10X(data.dir = data_dir2)
+
 # Create a Seurat object
-SeuratObject <- CreateSeuratObject(counts = expression_matrix,min.cells = 3,names.delim = "-", names.field = 2)
-
-expression_matrix <- Read10X(data.dir = data_dir)
-# Create a Seurat object
-SeuratObject <- CreateSeuratObject(counts = expression_matrix,min.cells = 3,names.delim = "-", names.field = 2)
+obj_49loopC <- CreateSeuratObject(counts = expression_49loopC,min.features=100,names.delim = "-", names.field = 2)
+obj_49DPP1 <- CreateSeuratObject(counts = expression_49DPP1,min.features=100,names.delim = "-", names.field = 2)
 
 
-SC2300015 vs SC2300013
+#Perform Batch Correction
+
+
+
+#
