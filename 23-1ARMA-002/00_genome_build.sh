26a88a060ed0be299611854524335022c04969f9
Shahina <91986031+shhayat@users.noreply.github.com>
2024-02-14 21:46:05 -0600

    Update 00_genome_build.sh

--- 23-1ARMA-002/00_genome_build.sh
+++ 23-1ARMA-002/00_genome_build.sh
@@ -16,7 +16,7 @@ export PATH=/globalhome/hxo752/HPC/tools/cellranger-7.1.0/bin:$PATH
 GENOME=/datastore/NGSF001/analysis/references/CanFam6/GCF_000002285.5_Dog10K_Boxer_Tasha_rna.fna
 GTF=/datastore/NGSF001/analysis/references/CanFam6/GCF_000002285.5_Dog10K_Boxer_Tasha_genomic.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA-002/analysis/
-GENOME_FOLDER_NAME="Canine_index"
+GENOME_FOLDER_NAME="CanFam6_index"
 NCPUS=10
 cd ${OUTDIR}
 
