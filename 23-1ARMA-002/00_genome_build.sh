1af26ec19a1cdbd41a82640efcb62190f30e1004
Shahina <91986031+shhayat@users.noreply.github.com>
2024-02-15 13:42:34 -0600

    Update 00_genome_build.sh

--- 23-1ARMA-002/00_genome_build.sh
+++ 23-1ARMA-002/00_genome_build.sh
@@ -19,7 +19,7 @@ export PATH=/globalhome/hxo752/HPC/tools/cellranger-7.1.0/bin:$PATH
 #also added to ROS_Cfam_1.0 fasta and combined.fasta was generated
 
 GENOME=/datastore/NGSF001/analysis/references/dog/ROS_Cfam_1.0_Ensembl/combined.fa
-GTF=/datastore/NGSF001/analysis/references/ROS_Cfam_1.0_Ensembl/combined.gtf
+GTF=/datastore/NGSF001/analysis/references/dog/ROS_Cfam_1.0_Ensembl/combined.gtf
 #GENOME=/datastore/NGSF001/analysis/references/CanFam6/GCF_000002285.5_Dog10K_Boxer_Tasha_rna.fna
 #GTF=/datastore/NGSF001/analysis/references/CanFam6/GCF_000002285.5_Dog10K_Boxer_Tasha_genomic.gtf
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA-002/analysis/
