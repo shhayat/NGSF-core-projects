7e70513a2d0ff4db2225bc199c0a7f29087d13ee
Shahina <91986031+shhayat@users.noreply.github.com>
2024-02-15 20:22:32 -0600

    Update 05_DESEQ2.R

--- 23-1CHXI-001/05_DESEQ2.R
+++ 23-1CHXI-001/05_DESEQ2.R
@@ -67,18 +67,20 @@ DEG_analysis(c(10,11,13,14,4,5,6,7,8),"CONTROL","HFD","CONTROL",4,5,"filtered_sa
 #################
 #volcano plot
 #################
+setwd("/Users/shahina/Projects/2023_projects/23-1CHX1-001/")
+
 library(ggplot2)
 res <- read.csv("DESEQ2/DEG_HFD_vs_CONTROL_all_sample.csv")
 res1 <- as.data.frame(res)
 
 #assign up and down regulation and non signif based on log2fc
-res1$direction <- ifelse(res1$effects < -1, "down_regulated", 
-                         ifelse(res1$effects > 1, "up_regulated", "signif" ))
+res1$direction <- ifelse(res1$log2FoldChange < -1, "down_regulated", 
+                         ifelse(res1$log2FoldChange > 1, "up_regulated", "signif" ))
 
 tiff("Volcano_plot.tiff",res=600, width = 7, height = 7, units = 'in')
 
 #png("/Users/shahina/Projects/20-1JOHO-001/plots/Volcano_plot.png", width=1300, height=500, res=120)
-ggplot(res1, aes(effects, -log10(fdr))) +
+ggplot(res1, aes(effects, -log10(padj))) +
   geom_point(aes(col=direction),
              size=0.5,
              show.legend = FALSE) +
@@ -86,8 +88,7 @@ ggplot(res1, aes(effects, -log10(fdr))) +
   theme(axis.text.x = element_text(size=11),
         axis.text.y = element_text(size=11),
         text = element_text(size=11)) +
-  annotate("text",x=2.2,y=1.7,hjust = 0,label=" Total (FDR <=0.05) = 6678 \n Total Upregulated = 3138 \n Total Down Regulated = 3540 \n Log2FC > 1 = 120 \n Log2FC < -1 = 90", size = 3)  +
-  xlab("log2(FC)") +
-  ylab("-log10(FDR)") 
-  
+        xlab("log2(FC)") +
+        ylab("-log10(FDR)") 
+  #annotate("text",x=2.2,y=1.7,hjust = 0,label=" Total (FDR <=0.05) = 6678 \n Total Upregulated = 3138 \n Total Down Regulated = 3540 \n Log2FC > 1 = 120 \n Log2FC < -1 = 90", size = 3)  +
 dev.off()
