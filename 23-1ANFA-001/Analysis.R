55448b49f0414680470395551ac071446d12f2ed
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-01 15:33:29 -0600

    Update Analysis.R

--- 23-1ANFA-001/Analysis.R
+++ 23-1ANFA-001/Analysis.R
@@ -2,5 +2,5 @@
 #https://github.com/hbctraining/scRNA-seq_online/tree/master/lessons
 #https://www.sciencedirect.com/science/article/pii/S2090123221002289#s0010
 
-conda activate /globalhome/hxo752/HPC/anaconda3/envs/igraph
+conda activate /globalhome/hxo752/HPC/anaconda3/envs/r-igraph
 
