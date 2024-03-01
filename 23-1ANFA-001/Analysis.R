1fbbf86700c39d16e9801acd585a3c1bc77b8554
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-01 15:18:34 -0600

    Update Analysis.R

--- 23-1ANFA-001/Analysis.R
+++ 23-1ANFA-001/Analysis.R
@@ -1,27 +1,6 @@
 #https://satijalab.org/seurat/archive/v3.0/pbmc3k_tutorial.html
 #https://github.com/hbctraining/scRNA-seq_online/tree/master/lessons
 #https://www.sciencedirect.com/science/article/pii/S2090123221002289#s0010
-library(Seurat)
-library(magrittr)
-library(dplyr)
-library(tibble)
 
-# Load the feature barcode matrix (.mtx)
-setwd("Desktop/core-projects/23-1ANFA-001/count_files/")
+conda activate /globalhome/hxo752/HPC/anaconda3/envs/igraph
 
-data_dir1 <- paste("SC2300017","/raw_feature_bc_matrix", sep="")
-data_dir2 <- paste("SC2300013","/raw_feature_bc_matrix", sep="")
-
-expression_49loopC <- Read10X(data.dir = data_dir1)
-expression_49DPP1 <- Read10X(data.dir = data_dir2)
-
-# Create a Seurat object
-obj_49loopC <- CreateSeuratObject(counts = expression_49loopC,min.features=100,names.delim = "-", names.field = 2)
-obj_49DPP1 <- CreateSeuratObject(counts = expression_49DPP1,min.features=100,names.delim = "-", names.field = 2)
-
-
-#Perform Batch Correction
-
-
-
-#
