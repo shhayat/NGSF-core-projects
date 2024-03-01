939c67ff101a43ff552cd54a9360a183b60ae253
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-01 16:10:23 -0600

    Update Analysis.R

--- 23-1ANFA-001/Analysis.R
+++ 23-1ANFA-001/Analysis.R
@@ -1,6 +1,9 @@
 #https://satijalab.org/seurat/archive/v3.0/pbmc3k_tutorial.html
 #https://github.com/hbctraining/scRNA-seq_online/tree/master/lessons
 #https://www.sciencedirect.com/science/article/pii/S2090123221002289#s0010
-
-conda activate /globalhome/hxo752/HPC/anaconda3/envs/r-igraph
-
+#under r-seurat seurat and harmony was installed to load packages together in R
+conda activate /globalhome/hxo752/HPC/anaconda3/envs/r-seurat
+#Start R in terminal
+R
+library("Seurat")
+library("harmony")
