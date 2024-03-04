4568bd19139d143caeb85ddc5e085f094f63b1c1
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-04 16:12:24 -0600

    Update 00_data_download.sh

--- 23-1SCWI-001/Additional_Study/00_data_download.sh
+++ 23-1SCWI-001/Additional_Study/00_data_download.sh
@@ -1,4 +1,4 @@
-for i in {63..91}
+for i in 63 64 65 66 72 73 74 75 80 81 82 83 84 89 90 91 92 93
 do
   NUM="${i:1}"
   OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SCWI-001/analysis/Additional_Study/fastq
