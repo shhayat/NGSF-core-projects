2888aa33ee8d9c8a46004f10332322b3e40ca019
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-04 17:00:32 -0600

    Create 01_Fastqc.sh

---
+++ 23-1SCWI-001/Additional_Study/01_Fastqc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=fastqc
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=5G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+
+module load fastqc
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SCWI-001/Additional_Study/fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SCWI-001/Additional_Study/analysis
+mkdir -p ${OUTDIR}/fastqc
+
+fq1=$1; shift
+fq2=$1;
+fastqc -o ${OUTDIR}/fastqc --extract ${fq1}
+fastqc -o ${OUTDIR}/fastqc --extract ${fq2}
