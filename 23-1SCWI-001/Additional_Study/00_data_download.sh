ba0141de2e7756a36e5b35756d827656aecfcbd0
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-04 16:24:08 -0600

    Update 00_data_download.sh

--- 23-1SCWI-001/Additional_Study/00_data_download.sh
+++ 23-1SCWI-001/Additional_Study/00_data_download.sh
@@ -1,7 +1,7 @@
 for i in 63 64 65 66 72 73 74 75 80 81 82 83 84 89 90 91 92 93
 do
   NUM="${i:1}"
-  OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SCWI-001/Additional_Study/fastq
-  mkdir -p $OUTDIR
+  OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SCWI-001/Additional_Study/
+  mkdir -p $OUTDIR/fastq
   echo "wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR716/00${NUM}/SRR71663${i}/SRR71663${i}.fastq.gz" >> $OUTDIR/path_to_fastq_files.sh
 done
