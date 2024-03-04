32af83fc0b0bea4f28121c5a30462051d702be4d
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-04 17:09:31 -0600

    Create feature_count.R

---
+++ 23-1SCWI-001/Additional_Study/feature_count.R
@@ -0,0 +1,36 @@
+library(Rsubread)
+library(magrittr)
+setwd("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SCWI-001/Additional_Study/analysis/")
+result_dir="star_alignment"
+sample_names <- list.files(result_dir, "SRR")
+
+
+#count features using featureCounts function
+#count features using featureCounts function
+feature_count <- sapply(sample_names, function(x)
+			   featureCounts(files = sprintf('%s/%s/Aligned.sortedByCoord.out.bam',result_dir, x),
+			   annot.ext="/datastore/NGSF001/analysis/references/mouse/gencode-m32/gencode.vM32.primary_assembly.annotation.gtf",
+			   isGTFAnnotationFile = TRUE,
+			   strandSpecific = 1,
+			   GTF.featureType = 'exon',
+			   GTF.attrType.extra  = c('gene_name'),
+			   nthreads = 8, 
+			   isPairedEnd = FALSE), 
+			   simplify = FALSE, 
+			   USE.NAMES = TRUE)
+
+#convet list to a dataframe
+#COUNTS
+feature_count1 <- feature_count %>% lapply(function(x) x$counts) %>% 
+	do.call(cbind, .) %>% 
+	magrittr::set_colnames(names(feature_count))
+
+#ANNOTATIONS					  
+feature_annotation <- feature_count %>% lapply(function(x) x$annotation) %>% 
+	do.call(cbind, .) %>% 
+	magrittr::extract(,c(1,7)) %>%
+	magrittr::set_colnames(c('GeneID', 'gene_name'))
+
+feature_count <- cbind(feature_annotation,feature_count1)
+
+save(feature_count, file = 'feature_count.RData', compress = 'xz')
