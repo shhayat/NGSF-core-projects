30337fa82584e62ec8b61354a23fc281e4531d24
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-05 20:17:54 -0600

    Update 01_trim_reads_and_redo_fastq.sh

--- 23-1SCWI-001/Additional_Study/01_trim_reads_and_redo_fastq.sh
+++ 23-1SCWI-001/Additional_Study/01_trim_reads_and_redo_fastq.sh
@@ -10,14 +10,27 @@
 #SBATCH --output=%j.out
 set -eux
 
-NCPU=2
 source $HOME/.bashrc
-conda activate trimmomatic
 
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SCWI-001/Additional_Study/fastq
 OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1SCWI-001/Additional_Study/fastq_trimmed
 #used parameter setting from paper sent by sccot https://www-sciencedirect-com.cyber.usask.ca/science/article/pii/S0092867419301138?via%3Dihub
 mkdir -p ${OUTDIR}
+NCPU=2
+
+conda activate cutadapt
+for i in $DATA/SRR*.fastq.gz
+do
+         path="${i%.fastq*}";
+         sample_name=${path##*/};
+         cutadapt -a A{7,} \
+                  -o ${OUTDIR}/${sample_name}_clipped.fastq.gz \
+                   ${DATA}/${sample_name}.fastq.gz
+ done                  
+conda deactivate
+
+conda activate trimmomatic
+
 for i in $DATA/SRR*.fastq.gz
 do
          path="${i%.fastq*}";
@@ -25,14 +38,14 @@ do
           trimmomatic SE \
                      -threads $NCPU \
                      -phred33 \
-                     ${DATA}/${sample_name}.fastq.gz \
-                     ${OUTDIR}/${sample_name}.fastq.gz \
-                     ILLUMINACLIP:/globalhome/hxo752/HPC/anaconda3/envs/trimmomatic/share/trimmomatic/adapters/TruSeq3-SE.fa:2:30:10 \
+                     ${OUTDIR}/${sample_name}_clipped.fastq.gz \
+                     ${OUTDIR}/${sample_name}_clipped_trimmed.fastq.gz \
                      LEADING:15 \
                      TRAILING:15 \
                      MINLEN:35
 done
 conda deactivate
+
 wait
 
 module load fastqc
