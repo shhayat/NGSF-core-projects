35c526805c8e4d4ae23a01b6928c3e78b121b31f
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-15 14:40:01 -0600

    Update 05_DESEQ2.R

--- 23-1CHXI-001/05_DESEQ2.R
+++ 23-1CHXI-001/05_DESEQ2.R
@@ -78,15 +78,6 @@ DEG_analysis(c(9,10,11,12,13,14,3,4,5,6,7,8),"CONTROL","HFD","CONTROL",6,6, "all
 
 
 
-
-
-
-
-
-
-
-
-
 #################
 #volcano plot
 #################
@@ -141,4 +132,124 @@ dev.off()
 
 
 
+
+library("DESeq2")
+library("ggplot2")
+library("xlsx")
+
+dir.create("DESEQ2", recursive=TRUE, showWarnings = FALSE) 
+
+load("feature_count.RData")
+feature_count <- as.data.frame(feature_count)
+
+#your first columns which are gene id and gene name
+feature_annotation <- data.frame(GeneID=feature_count$GeneID,gene_name=feature_count[2])
+colnames(feature_count) <- c("GeneID","gene_name","H17","H18","H19","H20","H21","H22","C18","C19","C20","C21","C22","C23")
+feature_count <- feature_count[rowSums(feature_count[,c(3:ncol(feature_count))])>0, ]
+
+  write.xlsx(feature_count,file="DESEQ2/raw_counts.xlsx", row.names = FALSE)
+
+DEG_analysis <-  function(colnum,cond1, cond2, ref, rep_cond1,rep_cond2,str)
+{
+  feature_count <- feature_count[colnum]
+  #remove row with sum zero
+  feature_count <- feature_count[rowSums(feature_count[,c(1:ncol(feature_count))])>0, ]
+  
+  sampleInfo=data.frame(sample_name=dput(as.character(names(feature_count))),
+                        sample_type=dput(as.character(names(feature_count))),
+                        sample_group=dput(as.character(c(rep(cond1,rep_cond1),rep(cond2,rep_cond2)))))
+  
+  
+  group <- data.frame(sample_group=sampleInfo$sample_group)
+  
+  dds <- DESeqDataSetFromMatrix(countData=feature_count,
+                                colData=group,
+                                design=~sample_group)
+  
+  
+  dds$sample_group <-relevel(dds$sample_group,ref=ref)
+  
+  dds_wald <- DESeq(dds, betaPrior=FALSE, minReplicatesForReplace=Inf)
+  res <- results(dds_wald, contrast=c("sample_group",cond2,cond1))
+  
+  resDF <- data.frame(GeneID=rownames(res),res)
+  resDF <- merge(feature_annotation,resDF, by="GeneID")
+  resDF <- resDF[order(resDF$pvalue),]
+  log2FC <- resDF$log2FoldChange
+  resDF$Fold_Change = ifelse(log2FC > 0, 2 ^ log2FC, -1 / (2 ^ log2FC))
+
+  resDF1 <- resDF[resDF$pvalue <= 0.05,]
+
+
+nt <- normTransform(dds_wald)
+log2.norm.counts <- assay(nt)[select,]
+log2.norm.counts<- as.data.frame(log2.norm.counts)
+                     
+log2.norm.counts1 <- data.frame(Gene=feature_count1$gene_name, log2.norm.counts)
+colnames(log2.norm.counts1) <- c("Gene",names(feature_count1[,3:26]))
+#DF <- rbind(select_up_cols,select_down_cols)
+#DF <- DF[complete.cases(DF), ]
+#log2.norm.counts1 <- merge(DF,log2.norm.counts1, by=c("GeneID"))
+log2.norm.counts2 <- log2.norm.counts1[,-1]
+rownames(log2.norm.counts2) <-  make.names(log2.norm.counts1[,1],TRUE)
+bwcolor = grDevices::colorRampPalette(c("yellow","grey", "blue"))
+
+##########
+#HEATMAP
+##########
+setwd("/Users/shahina/Projects/23-1CHXI-001")
+
+library("DESeq2")
+library("ggplot2")
+library("xlsx")
+
+
+load("feature_count.RData")
+feature_count <- as.data.frame(feature_count)
+
+#your first columns which are gene id and gene name
+feature_annotation <- data.frame(GeneID=feature_count$GeneID,gene_name=feature_count[2])
+feature_count1 <- feature_count[c(9,10,11,12,13,14,3,4,5,6,7,8)]
+  
+  sampleInfo=data.frame(sample_name=dput(as.character(names(feature_count1))),
+                        sample_type=dput(as.character(names(feature_count1))),
+                        sample_group=dput(as.character(c(rep("CONTROL",6),rep("HFD",6)))))
+  
+  
+  group <- data.frame(sample_group=sampleInfo$sample_group)
+  
+  dds <- DESeqDataSetFromMatrix(countData=feature_count1,colData=group,design=~sample_group)
+  
+  dds$sample_group <-relevel(dds$sample_group,ref="CONTROL")
+
+  dds <- estimateSizeFactors(dds)
+  norm.expr <- counts(dds, normalized=TRUE)
+  norm.expr <- as.data.frame(norm.expr)
+     
+norm.expr <- data.frame(Gene=feature_count$gene_name, norm.expr)
+colnames(norm.expr) <- c("Gene",names(feature_count1[,1:12]))
+
+norm.expr <- norm.expr[grep("^(acox2|ddlr)$", norm.expr$Gene,ignore.case = TRUE), ]
+  
+norm.expr1 <- norm.expr[,-1]
+rownames(norm.expr1) <-  make.names(norm.expr1[,1],TRUE)
+
+  
+bwcolor = grDevices::colorRampPalette(c("yellow","grey", "blue"))
+
+  
+pdf("Heatmap.pdf")
+pheatmap(
+      norm.expr1,
+      clustering_dist_rows = "correlation",
+      scale      = 'row',
+      cellheight = 8,
+      cellwidth =  8,
+      fontsize   = 6,
+      col = bwcolor(50),
+      treeheight_row = 0,
+      treeheight_col = 0,
+      cluster_cols = FALSE,
+      border_color = NA)
+dev.off()
 
