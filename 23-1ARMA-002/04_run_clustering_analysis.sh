c8eae14969691797aafbd0716e96d248fb545af5
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-19 15:11:32 -0600

    Create 04_run_clustering_analysis.sh

---
+++ 23-1ARMA-002/04_run_clustering_analysis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=seurat_run
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=15:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+module load r/4.3.1
+SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA-002
+R CMD BATCH ${SCRIPT_DIR}/Clustering_Analysis.R
+conda deactivate
