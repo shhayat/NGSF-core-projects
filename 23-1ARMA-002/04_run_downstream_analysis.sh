78e359f96a8425389e5ad313742a58c5d549750c
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-19 15:39:55 -0600

    Update and rename 04_run_clustering_analysis.sh to 04_run_downstream_analysis.sh

--- 23-1ARMA-002/04_run_downstream_analysis.sh
+++ 23-1ARMA-002/04_run_downstream_analysis.sh
@@ -10,4 +10,5 @@ set -eux
 
 module load r/4.3.1
 SCRIPT_DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA-002
-R CMD BATCH ${SCRIPT_DIR}/Cluster_Analysis.R
+#R CMD BATCH ${SCRIPT_DIR}/1_filter_low_quality_reads.R
+R CMD BATCH ${SCRIPT_DIR}/2_cluster_analysis.R
