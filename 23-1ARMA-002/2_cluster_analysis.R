e416e5d598b11f271c94a87b2b82a5e7ae365013
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-19 15:20:25 -0600

    Update 2_cluster_analysis.R

--- 23-1ARMA-002/2_cluster_analysis.R
+++ 23-1ARMA-002/2_cluster_analysis.R
@@ -1,1 +1,6 @@
+setwd("/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA-002/analysis")
 
+load("seurat_object_SC2300019.RData")
+load("seurat_object_SC2300020.RData")
+load("seurat_object_SC2300021.RData")
+load("seurat_object_SC2300022.RData")
