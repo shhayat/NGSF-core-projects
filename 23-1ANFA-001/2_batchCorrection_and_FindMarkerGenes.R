b81196efb10968e7ed769453500fc242d5847aec
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-20 12:54:22 -0600

    Update 2_batchCorrection_and_FindMarkerGenes.R

--- 23-1ANFA-001/2_batchCorrection_and_FindMarkerGenes.R
+++ 23-1ANFA-001/2_batchCorrection_and_FindMarkerGenes.R
@@ -81,8 +81,8 @@ markers <- FindAllMarkers(object = merged_seurat,return.thresh=0.05)
 write.table(markers,file=sprintf("%s_marker_genes.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
 
 #calculate AUC
-markers <- FindAllMarkers(object = merged_seurat, test.use="roc")  
-write.table(markers,file=sprintf("%s_marker_genes_auc_calculated.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
+#markers <- FindAllMarkers(object = merged_seurat, test.use="roc")  
+#write.table(markers,file=sprintf("%s_marker_genes_auc_calculated.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
 
 
   
@@ -152,8 +152,8 @@ batch_correction_and_find_markers_per_cluster <- function(seuratList,condition_n
   write.table(markers,file=sprintf("%s_marker_genes.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
 
   #calculate AUC
-  markers <- FindAllMarkers(object = harmonized_seurat, test.use="roc")  
-  write.table(markers,file=sprintf("%s_marker_genes_auc_calculated.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
+  #markers <- FindAllMarkers(object = harmonized_seurat, test.use="roc")  
+  #write.table(markers,file=sprintf("%s_marker_genes_auc_calculated.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
 
 
 }
