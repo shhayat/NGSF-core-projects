ed6e051778ab28b40e37eab1ba4df10b631cd7fb
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-20 15:18:55 -0600

    Update 2_batchCorrection_and_FindMarkerGenes.R

--- 23-1ANFA-001/2_batchCorrection_and_FindMarkerGenes.R
+++ 23-1ANFA-001/2_batchCorrection_and_FindMarkerGenes.R
@@ -77,12 +77,12 @@ merged_seurat <- PrepSCTFindMarkers(object = merged_seurat)
 
 #Find differentially Expressed genes per cluster p val <=0.05
 #markers <- FindAllMarkers(object = merged_seurat,return.thresh=0.05)  
-markers <- FindAllMarkers(object = merged_seurat,return.thresh=0.05)  
-write.table(markers,file=sprintf("%s_marker_genes.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
+#markers <- FindAllMarkers(object = merged_seurat,return.thresh=0.05)  
+#write.table(markers,file=sprintf("%s_marker_genes.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
 
 #calculate AUC
-#markers <- FindAllMarkers(object = merged_seurat, test.use="roc")  
-#write.table(markers,file=sprintf("%s_marker_genes_auc_calculated.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
+markers <- FindAllMarkers(object = merged_seurat, test.use="roc")  
+write.table(markers,file=sprintf("%s_marker_genes_auc_calculated.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
 
 
   
@@ -148,12 +148,12 @@ batch_correction_and_find_markers_per_cluster <- function(seuratList,condition_n
   harmonized_seurat <- PrepSCTFindMarkers(object = harmonized_seurat)
 
   #Find differentially Expressed genes per cluster p val <=0.05
-  markers <- FindAllMarkers(object = harmonized_seurat,return.thresh=0.05)  
-  write.table(markers,file=sprintf("%s_marker_genes.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
+#  markers <- FindAllMarkers(object = harmonized_seurat,return.thresh=0.05)  
+#  write.table(markers,file=sprintf("%s_marker_genes.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
 
   #calculate AUC
-  #markers <- FindAllMarkers(object = harmonized_seurat, test.use="roc")  
-  #write.table(markers,file=sprintf("%s_marker_genes_auc_calculated.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
+  markers <- FindAllMarkers(object = harmonized_seurat, test.use="roc")  
+  write.table(markers,file=sprintf("%s_marker_genes_auc_calculated.txt",conds), row.names = FALSE, quote=FALSE, sep="\t")
 
 
 }
