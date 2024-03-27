c076d6abd13d3d6bf93233c9c76c76e58020b5aa
Shahina <91986031+shhayat@users.noreply.github.com>
2024-03-27 13:08:04 -0600

    Update get_peak_sequences.sh

--- 23-1DEAN-001/get_peak_sequences.sh
+++ 23-1DEAN-001/get_peak_sequences.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=get_peak_sequences
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=6:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=%j.out
+
+set -eux
+module load bedtools
+
 
