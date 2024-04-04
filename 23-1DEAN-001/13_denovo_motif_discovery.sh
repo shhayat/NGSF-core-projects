9aec2b62c056a8fe7389341caea730d97de1ce13
Shahina <91986031+shhayat@users.noreply.github.com>
2024-04-04 11:27:58 -0600

    Update 13_denovo_motif_discovery.sh

--- 23-1DEAN-001/13_denovo_motif_discovery.sh
+++ 23-1DEAN-001/13_denovo_motif_discovery.sh
@@ -1,2 +1,6 @@
 
-/globalhome/hxo752/HPC/tools/chipmunk_v8_src.jar
+java -Xms512M -Xmx1G /globalhome/hxo752/HPC/tools/chipmunk_v8_src.jar -cp chipmunk.jar \
+                                                                      ru.autosome.ChIPMunk \
+                                                                      s:common_peaks_sequences_with_2000bp_upstream_and_downstream.fa \
+                                                                      1>results.txt \
+                                                                      2>log.txt
