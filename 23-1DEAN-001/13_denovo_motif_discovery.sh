51abe0bfce71141104bb3c3d1ad5a2c78e90b1af
Shahina <91986031+shhayat@users.noreply.github.com>
2024-04-04 11:44:08 -0600

    Update 13_denovo_motif_discovery.sh

--- 23-1DEAN-001/13_denovo_motif_discovery.sh
+++ 23-1DEAN-001/13_denovo_motif_discovery.sh
@@ -4,14 +4,17 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=2:00:00
-#SBATCH --mem=40G
+#SBATCH --mem=80G
 #SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 
 set -eux
+$NCPU=2
+
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis
 
 java -Xms512M -XX:ParallelGCThreads=$NCPU /globalhome/hxo752/HPC/tools/chipmunk_v8_src.jar \
                                                                       -cp chipmunk.jar \
                                                                       ru.autosome.ChIPMunk \
-                                                                      s:common_peaks_sequences_with_2000bp_upstream_and_downstream.fa \
-                                                                      1> /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/results.txt \
-                                                                      2> /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/log.txt
+                                                                      s:$DATA/common_peaks_sequences_with_2000bp_upstream_and_downstream.fa \
+                                                                      1> $DATA/results.txt \
+                                                                      2> $DATA/log.txt
