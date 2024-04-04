ae341cbac9040caabc652e15b46664c20be606e9
Shahina <91986031+shhayat@users.noreply.github.com>
2024-04-04 12:53:13 -0600

    Update 12_denovo_motif_discovery.sh

--- 23-1DEAN-001/12_denovo_motif_discovery.sh
+++ 23-1DEAN-001/12_denovo_motif_discovery.sh
@@ -14,11 +14,15 @@ NCPU=2
 
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis
 
-java -Xms512M -XX:ParallelGCThreads=$NCPU -cp /globalhome/hxo752/HPC/tools/chipmunk.jar \
-                                          ru.autosome.ChIPMunk \
-                                          s:$DATA/common_peaks_sequences_with_2000bp_upstream_and_downstream.fa > $DATA/results_common_peaks_sequences_with_2000bp_upstream_and_downstream.txt
+#ChIPMunk
+#java -Xms512M -XX:ParallelGCThreads=$NCPU -cp /globalhome/hxo752/HPC/tools/chipmunk.jar \
+#                                          ru.autosome.ChIPMunk \
+#                                          s:$DATA/common_peaks_sequences_with_2000bp_upstream_and_downstream.fa > $DATA/results_common_peaks_sequences_with_2000bp_upstream_and_downstream.txt
 
 
-java -Xms512M -XX:ParallelGCThreads=$NCPU -cp /globalhome/hxo752/HPC/tools/chipmunk.jar \
-                                          ru.autosome.ChIPMunk \
-                                          s:$DATA/common_peak_sequences.fa > $DATA/results_common_peak_sequences.txt
+#java -Xms512M -XX:ParallelGCThreads=$NCPU -cp /globalhome/hxo752/HPC/tools/chipmunk.jar \
+#                                          ru.autosome.ChIPMunk \
+#                                          s:$DATA/common_peak_sequences.fa > $DATA/results_common_peak_sequences.txt
+
+
+#MEME
