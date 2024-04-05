788afd6c0e8e675a882b89ca6c2006ae26a98f56
Shahina <91986031+shhayat@users.noreply.github.com>
2024-04-05 15:14:39 -0600

    Update 12_denovo_motif_discovery.sh

--- 23-1DEAN-001/12_denovo_motif_discovery.sh
+++ 23-1DEAN-001/12_denovo_motif_discovery.sh
@@ -13,8 +13,8 @@ set -eux
 NCPU=2
 
 DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/motif_discovery/ChIPMunk
-mkdir -p $OUTDIR
+#OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/motif_discovery/ChIPMunk
+#mkdir -p $OUTDIR
 
 #peaks located in promoter region of the gene were extracted from the peak annotation files and sequences were retrived 
 #In this step the sequneces will be passed through different denovo motif discovery tools to find the motif that is conseverd across sequences
@@ -29,8 +29,8 @@ mkdir -p $OUTDIR
 #                                          s:$DATA/common_peak_sequences.fa > $OUTDIR/results_common_peak_sequences.txt
                                   
 #MEME
-OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/motif_discovery/MEME
-mkdir -p $OUTDIR
+#OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/motif_discovery/MEME
+#mkdir -p $OUTDIR
 #/globalhome/hxo752/HPC/anaconda3/envs/meme/bin/meme -oc $OUTDIR/common_peaks_sequences_with_2000bp_upstream_and_downstream \
 #                                                    -nmotifs 10 \
 #                                                    -maxsize 800000 \
@@ -42,7 +42,7 @@ mkdir -p $OUTDIR
 #                                                    $DATA/common_peak_sequences.fa
 
 #GLAM2
-#OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/motif_discovery/GLAM2
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/motif_discovery/GLAM2
 #/globalhome/hxo752/HPC/anaconda3/envs/meme/bin/glam2 n $DATA/common_peaks_sequences_with_2000bp_upstream_and_downstream.fa \
                    #                                 -r 10 \
                     #                                -o $OUTDIR/common_peaks_sequences_with_2000bp_upstream_and_downstream
