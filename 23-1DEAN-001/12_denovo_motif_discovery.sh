6228037ec12be517ee1dee782ff61a11fb977e82
Shahina <91986031+shhayat@users.noreply.github.com>
2024-04-05 17:32:45 -0600

    Update 12_denovo_motif_discovery.sh

--- 23-1DEAN-001/12_denovo_motif_discovery.sh
+++ 23-1DEAN-001/12_denovo_motif_discovery.sh
@@ -20,10 +20,10 @@ OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/ana
 #In this step the sequneces will be passed through different denovo motif discovery tools to find the motif that is conseverd across sequences
 
 #ChIPMunk (get the primary major motif from the 7 to 22 nucleotides motif lengths range)
-java -Xms512M -XX:ParallelGCThreads=$NCPU -cp /globalhome/hxo752/HPC/tools/chipmunk.jar \
+java -Xms512M -XX:ParallelGCThreads=$NCPU -jar /globalhome/hxo752/HPC/tools/chipmunk.jar \
                                           s:$DATA/common_peaks_sequences_with_2000bp_upstream_and_downstream.fa > $OUTDIR/results_common_peaks_sequences_with_2000bp_upstream_and_downstream_short_motifs.txt
 
-java -Xms512M -XX:ParallelGCThreads=$NCPU -cp /globalhome/hxo752/HPC/tools/chipmunk.jar \
+java -Xms512M -XX:ParallelGCThreads=$NCPU -jar /globalhome/hxo752/HPC/tools/chipmunk.jar \
                                           s:$DATA/common_peak_sequences.fa > $OUTDIR/results_common_peak_sequences_short_motifs.txt
 
 #ChIPMunk ru.autosome.ChIPMunk
