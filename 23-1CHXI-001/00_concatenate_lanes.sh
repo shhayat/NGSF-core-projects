cc5c68facb44d3addfee9ceed63ada77dd279b9a
Shahina <91986031+shhayat@users.noreply.github.com>
2024-04-08 10:16:25 -0600

    Update 00_concatenate_lanes.sh

--- 23-1CHXI-001/00_concatenate_lanes.sh
+++ 23-1CHXI-001/00_concatenate_lanes.sh
@@ -1,22 +1,25 @@
+
 #!/bin/bash
 	
 #SBATCH --account=hpc_p_anderson
 #SBATCH --job-name=combine_fastq
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=4:00:00
+#SBATCH --time=2:00:00
 #SBATCH --mem=8G
 
 	
 set -eux
 	
-FASTQ_FOLDER=/datastore/TumorBiobank/4.\ Projects/Project\ 3\ -\ Dr\ Hopkins\ -\ TFRI\ -\ PR2C\ Cohortes/Terry\ Fox\ Research\ Institute/RNA
-OUTDIR=/datastore/TumorBiobank/4.\ Projects/Project\ 3\ -\ Dr\ Hopkins\ -\ TFRI\ -\ PR2C\ Cohortes/Terry\ Fox\ Research\ Institute/RNA/Fastq
-mkdir -p $OUTDIR	
+mkdir -p /globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1CHXI-001/analysis/Fastq 
 
-for i in $(seq -w 688 706)
+FASTQ_FOLDER=/datastore/NGSF001/NB551711/231025_NB551711_0086_AHGC7TBGXV/Alignment_1/20231026_002823/Fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1CHXI-001/analysis/Fastq
+	
+
+for i in $(seq -w 169 180)
 do
-	echo Combining library Li39${i}
-	cat ${FASTQ_FOLDER}/Li39${i}_*_R1_* > ${OUTDIR}/Li39${i}_R1.fastq.gz
-	cat ${FASTQ_FOLDER}/Li39${i}_*_R2_* > ${OUTDIR}/Li39${i}_R2.fastq.gz
+	echo Combining library R2300${i}
+	cat ${FASTQ_FOLDER}/R2300${i}_*_R1_* > ${OUTDIR}/R2300${i}_R1.fastq.gz
+	cat ${FASTQ_FOLDER}/R2300${i}_*_R2_* > ${OUTDIR}/R2300${i}_R2.fastq.gz
 done
