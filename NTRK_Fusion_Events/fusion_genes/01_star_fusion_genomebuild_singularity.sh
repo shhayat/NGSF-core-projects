9c88efd2e7dd43f15124619f828d987cfed0a6b4
Shahina <91986031+shhayat@users.noreply.github.com>
2024-05-17 12:48:02 -0600

    Update 01_star_fusion_genomebuild_singularity.sh

--- NTRK_Fusion_Events/fusion_genes/01_star_fusion_genomebuild_singularity.sh
+++ NTRK_Fusion_Events/fusion_genes/01_star_fusion_genomebuild_singularity.sh
@@ -11,17 +11,20 @@
 
 module load singularity/3.9.2
 
-#GTF=/datastore/NGSF001/analysis/references/ROS_Cfam_1.0/Canis_lupus_familiaris.ROS_Cfam_1.0.109.gtf
-#GENOME=/datastore/NGSF001/analysis/references/ROS_Cfam_1.0/Canis_lupus_familiaris.ROS_Cfam_1.0.dna.toplevel.fa
+#latest annotation file
+GTF=/datastore/NGSF001/analysis/references/ROS_Cfam_1.0/Canis_lupus_familiaris.ROS_Cfam_1.0.109.gtf
+#latest genome
+GENOME=/datastore/NGSF001/analysis/references/ROS_Cfam_1.0/Canis_lupus_familiaris.ROS_Cfam_1.0.dna.toplevel.fa
+#old annotations which was modified to make it compatible with prep_genome_lib.pl. The modified file is called genomic_1.gtf which was passed to prep_genome_lib.pl script
 #GTF=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/Cfam3.1_NCBI/genomic.gtf
-GTF=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/Cfam3.1_NCBI/genomic_1.gtf
-GENOME=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/Cfam3.1_NCBI/GCF_000002285.3_CanFam3.1_genomic.fna
-#DFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/Dfam/canis_lupus_familiaris_dfam.hmm
-#PFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/hystiocystic_sarcoma/analysis/Pfam/Pfam-A.hmm
-#DFAM_DATABASE=/datastore/NGSF001/projects/ARMA_NTRK_Fusion/Fusion_events/genome_build_and_required_databases/Dfam/canis_lupus_familiaris_dfam.hmm
-#PFAM_DATABASE=/datastore/NGSF001/projects/ARMA_NTRK_Fusion/Fusion_events/genome_build_and_required_databases/Pfam/Pfam-A.hmm
+#GTF_old=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/Cfam3.1_NCBI/genomic_1.gtf
+#old reference genome
+#GENOME_oldref=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/Cfam3.1_NCBI/GCF_000002285.3_CanFam3.1_genomic.fna
+
 DFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/Dfam/canis_lupus_familiaris_dfam.hmm
 PFAM_DATABASE=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1ARMA/fusion_genes/Pfam/Pfam-A.hmm
+
+#nextflow pipline for building reference genome
 singularity exec -e /globalhome/hxo752/HPC/tools/star-fusion.v1.11.0.simg \
                     /globalhome/hxo752/HPC/tools/STAR-Fusion-v1.11.0/ctat-genome-lib-builder/prep_genome_lib.pl \
                 --genome_fa ${GENOME} \
