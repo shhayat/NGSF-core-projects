6040c43833c86aa29216736cae0948670a3feb98
Shahina <91986031+shhayat@users.noreply.github.com>
2024-06-07 14:32:16 -0600

    Update 04_select_variants.sh

--- 20-1LICH-001/04_select_variants.sh
+++ 20-1LICH-001/04_select_variants.sh
@@ -28,19 +28,19 @@ mkdir -p ${OUTPUT_DIR2}
 #SAMPLE_INFO=$1;
 sample_name=$1
 
-gunzip -k ${INPUT_DIR}/${sample_name}.vcf.gz
+gunzip -k ${INPUT_DIR}/${sample_name}/${sample_name}.vcf.gz
 #gunzip -k ${INPUT_DIR}/${CLONE_ID}_${SAMPLE_INFO}_concat.vcf.gz
 
 echo "convert vcf to bed file"
 #step1: Convert vcf to bed file
 #/globalhome/hxo752/HPC/tools/bedops/convert2bed -i vcf < ${INPUT_DIR}/${CLONE_ID}_${SAMPLE_INFO}_concat.vcf -d >  ${INPUT_DIR}/${CLONE_ID}_${SAMPLE_INFO}_concat.bed
-/globalhome/hxo752/HPC/tools/bedops/convert2bed -i vcf < ${INPUT_DIR}/${sample_name}.vcf -d >  ${INPUT_DIR}/${sample_name}.bed
+/globalhome/hxo752/HPC/tools/bedops/convert2bed -i vcf < ${INPUT_DIR}/${sample_name}/${sample_name}.vcf -d >  ${INPUT_DIR}/${sample_name}/${sample_name}.bed
 
 echo "Base Conversions"
 
 #step2: Extract C to T or G conversions and select first 3 columns (chrom, start, end position, ref allele and alternate allele)
-awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${sample_name}.bed | grep -P '\tC\tG$' >> ${OUTPUT_DIR1}/${sample_name}_base_conversion.bed
-awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${sample_name}.bed | grep -P '\tC\tT$' >> ${OUTPUT_DIR1}/${sample_name}_base_conversion.bed
+awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${sample_name}/${sample_name}.bed | grep -P '\tC\tG$' >> ${OUTPUT_DIR1}/${sample_name}_base_conversion.bed
+awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${sample_name}/${sample_name}.bed | grep -P '\tC\tT$' >> ${OUTPUT_DIR1}/${sample_name}_base_conversion.bed
 #only select CC as REF
 #awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}.bed | grep -P '\tCC\tTG$' >> ${OUTPUT_DIR1}/${CLONE_ID}_${COND}_base_conversion.bed
 #awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}.bed | grep -P '\tCC\tGT$' >> ${OUTPUT_DIR1}/${CLONE_ID}_${COND}_base_conversion.bed
@@ -49,8 +49,8 @@ awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${sample_name}.bed | grep 
 #awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${CLONE_ID}_${COND}.bed | grep -P '\tCCC\tGGG$' >> ${OUTPUT_DIR}/${CLONE_ID}_${COND}_base_conversion.bed
 
 ##step2: Extract G to A or C conversions and select first 3 columns (chrom, start, end position, ref allele and alternate allele)
-awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${sample_name}.bed | grep -P '\tG\tA$' >> ${OUTPUT_DIR2}/${sample_name}_base_conversion.bed
-awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${sample_name}.bed | grep -P '\tG\tC$' >> ${OUTPUT_DIR2}/${sample_name}_base_conversion.bed
+awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${sample_name}/${sample_name}.bed | grep -P '\tG\tA$' >> ${OUTPUT_DIR2}/${sample_name}_base_conversion.bed
+awk -v OFS='\t' '{print $1,$2,$3,$6,$7}' ${INPUT_DIR}/${sample_name}/${sample_name}.bed | grep -P '\tG\tC$' >> ${OUTPUT_DIR2}/${sample_name}_base_conversion.bed
 
 echo "select 2 bases upstream and downstream of bases"
 #step3: select 2 bases upstream and downstream of bases in step2
