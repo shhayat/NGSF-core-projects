e7d9a51c8ded0200db2856073fe43ed1a06245a0
Shahina <91986031+shhayat@users.noreply.github.com>
2024-06-13 11:56:30 -0600

    Update 01_FastQC.sh

--- 20-1LICH-001/01_FastQC.sh
+++ 20-1LICH-001/01_FastQC.sh
@@ -1,1 +1,31 @@
-/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/concatenated_fastq
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=fastqc
+#SBATCH --ntasks=6
+#BATCH --cpus-per-task=4
+#SBATCH --time=02:00:00
+#SBATCH --mem=6G
+#SBATCH --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
+set -eux
+
+module load fastqc
+DATA=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/concatenated_fastq
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis
+
+mkdir -p ${OUTDIR}/fastqc
+
+for fq in $DATA/*.fastq.gz
+do
+   fastqc -o ${OUTDIR}/fastqc --extract ${fq}
+   
+done 
+
+wait 
+
+cd /datastore/NGSF001/software/tools/
+./multiqc ${OUTDIR}/fastqc/*_fastqc.zip -o ${OUTDIR}/fastqc
+
+
+
