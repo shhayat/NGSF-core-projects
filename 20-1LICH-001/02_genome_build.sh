55354b270fd053af3ce13c767ced5d3ba6684c2e
Shahina <91986031+shhayat@users.noreply.github.com>
2024-06-13 12:14:49 -0600

    Update and rename 02_bwa_alignment.sh to 02_genome_build.sh

---
+++ 20-1LICH-001/02_genome_build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=genome_index
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=8
+#SBATCH --time=72:00:00
+#SBATCH --mem=120G
+#SBATCH --output=%j.out
+
+module load bowtie2/2.5.1
+
+GENOME=/datastore/NGSF001/analysis/references/iGenomes/Homo_sapiens/NCBI/GRCh38/Sequence/WholeGenomeFasta
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1MICO-001/analysis/indices
+
+mkdir -p $OUTDIR
+bowtie2-build ${GENOME}/genome.fa ${OUTDIR}/bowtie_index
