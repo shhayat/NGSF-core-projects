c8e2f1fdd1715ee3f3df91005bd70091ad4d5fcf
Shahina <91986031+shhayat@users.noreply.github.com>
2024-06-18 11:44:16 -0600

    Update 05_mutect2_calling.sh

--- 20-1LICH-001/05_mutect2_calling.sh
+++ 20-1LICH-001/05_mutect2_calling.sh
@@ -5,11 +5,10 @@
 #SBATCH --job-name=gatk-mutect2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=2:00:00
+#SBATCH --time=5:00:00
 #SBATCH --mem=40G
 #SBATCH  --output=/globalhome/hxo752/HPC/slurm_logs/%j.out
 set -eux
-
 
 #loading required modules
 module load StdEnv/2020
@@ -17,13 +16,12 @@ module load gatk/4.2.2.0
 
 #reference file
 REF='/datastore/NGSF001/analysis/references/human/gencode-30/GRCh38.primary_assembly.genome.fa'
-INPUT_DIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/alignment"
+INPUT_DIR="/project/anderson/alignment/"
 INTERVALS='/datastore/NGSF001/analysis/references/human/hg38/agilent_sureselect_human_all_exon_v8_hg38/S33266340_Covered.noheader.noAlt.bed'
-OUTDIR='/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/'
+OUTDIR='/project/anderson/'
 mkdir -p ${OUTDIR}
 
 sample_name=$1;
-
 
 #Run Mutect2 in tumor only mode (https://gatk.broadinstitute.org/hc/en-us/articles/360035531132--How-to-Call-somatic-mutations-using-GATK4-Mutect2
 #https://gatk.broadinstitute.org/hc/en-us/articles/360037593851-Mutect2)
