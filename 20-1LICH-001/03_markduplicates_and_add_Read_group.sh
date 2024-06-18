d6b78a69f31d3ae39e34b64b165e77bbaf940189
Shahina <91986031+shhayat@users.noreply.github.com>
2024-06-17 18:02:11 -0600

    Update 03_markduplicates_and_add_Read_group.sh

--- 20-1LICH-001/03_markduplicates_and_add_Read_group.sh
+++ 20-1LICH-001/03_markduplicates_and_add_Read_group.sh
@@ -15,7 +15,7 @@ module load picard
 module load samtools
 
 #reference file
-REF='/datastore/NGSF001/analysis/references/human/gencode-30/GRCh38.primary_assembly.genome.fa'
+REF='/datastore/NGSF001/analysis/references/iGenomes/Homo_sapiens/NCBI/GRCh38/Sequence/WholeGenomeFasta/genome.fa'
 OUTDIR='/project/anderson/alignment'
 SAMPLE_NAME=$1
 BAM_FILE=$2
