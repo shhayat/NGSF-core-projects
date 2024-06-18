d4b936e7a349481ada29ad7bdf46f46ffbf661a7
Shahina <91986031+shhayat@users.noreply.github.com>
2024-06-18 15:11:39 -0600

    Update multiqc.sh

--- 20-1LICH-001/multiqc.sh
+++ 20-1LICH-001/multiqc.sh
@@ -1,21 +1,12 @@
 OUTDIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis/multiqc"
-#DIR="/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/20-1LICH-001/analysis"
-DIR="/project/anderson"
 mkdir -p ${OUTDIR}
 
 cd /globalhome/hxo752/HPC/tools/
 
-#FASTQC
-DIR="/datastore/NGSF001/projects/2020/20-1LICH-001/fastqc"
-multiqc -d ${DIR}/fastqc/*_fastqc -o ${OUTDIR}/fastqc -n fastqc
+DIR1="/datastore/NGSF001/projects/2020/20-1LICH-001/fastqc"
+DIR2="/project/anderson"
 
-#BOWTIE ALIGNMENT
-
-#PICARD
-#multiqc -d ${DIR}/alignment/*/*_dup_metrics.txt  -o ${OUTDIR}/picard -n picard
-
-#RECALIBRATION
-multiqc -d ${DIR}/alignment/*/*bowtie2.log  -o ${OUTDIR}/align_dedup_and_recalibrate -n align_dedup_and_recalibrate
-multiqc -d ${DIR}/alignment/*/*_dup_metrics.txt -o ${OUTDIR}/align_dedup_and_recalibrate -n align_dedup_and_recalibrate
-multiqc -d ${DIR}/alignment/*/*.table  -o ${OUTDIR}/align_dedup_and_recalibrate -n align_dedup_and_recalibrate
-
+multiqc -o ${OUTDIR} -n multiqc_report ${DIR1}/fastqc/*_fastqc \
+                                       ${DIR2}/alignment/*/*bowtie2.log \
+                                       ${DIR2}/alignment/*/*_dup_metrics.txt \
+                                       ${DIR}/alignment/*/*.table
