0bc75a0cab30a24cc0e87e289bbd6668474f59b0
Shahina <91986031+shhayat@users.noreply.github.com>
2024-06-25 14:49:44 -0600

    Update 01_star_mapping.sh

--- 24-1LICH-001/01_star_mapping.sh
+++ 24-1LICH-001/01_star_mapping.sh
@@ -25,28 +25,28 @@ fq2=$1
 cd ${OUTDIR}
 
 STAR --genomeDir $GENOME \
-	--readFilesCommand zcat \
-	--readFilesIn ${fq1} ${fq2} \
-	--outSAMtype BAM SortedByCoordinate \
-	--runThreadN ${NCPU} \
-  --alignIntronMax 1000000 \
-  --alignIntronMin 20 \
-  --alignMatesGapMax 1000000 \
-  --alignSJDBoverhangMin 1 \
-  --alignSJoverhangMin 8 \
-  --alignSoftClipAtReferenceEnds Yes \
-  --chimJunctionOverhangMin 15 \
-  --chimMainSegmentMultNmax 1 \
-  --chimOutJunctionFormat 1 \
-  --chimSegmentMin 15 \
-  --limitSjdbInsertNsj 1200000 \
-  --outFilterIntronMotifs None \
-  --outFilterMatchNminOverLread 0.33 \
-  --outFilterMismatchNmax 999 \
-  --outFilterMismatchNoverLmax 0.1 \
-  --outFilterMultimapNmax 20 \
-  --outFilterScoreMinOverLread 0.33 \
-  --twopassMode Basic \
-  --outSAMmapqUnique 60 \
-  --outFileNamePrefix ${sample_name}_
-  && samtools index ${sample_name}_Aligned.sortedByCoord.out.bam 
+     --readFilesCommand zcat \
+     --readFilesIn ${fq1} ${fq2} \
+     --outSAMtype BAM SortedByCoordinate \
+     --runThreadN ${NCPU} \
+     --alignIntronMax 1000000 \
+     --alignIntronMin 20 \
+     --alignMatesGapMax 1000000 \
+     --alignSJDBoverhangMin 1 \
+     --alignSJoverhangMin 8 \
+     --alignSoftClipAtReferenceEnds Yes \
+     --chimJunctionOverhangMin 15 \
+     --chimMainSegmentMultNmax 1 \
+     --chimOutJunctionFormat 1 \
+     --chimSegmentMin 15 \
+     --limitSjdbInsertNsj 1200000 \
+     --outFilterIntronMotifs None \
+     --outFilterMatchNminOverLread 0.33 \
+     --outFilterMismatchNmax 999 \
+     --outFilterMismatchNoverLmax 0.1 \
+     --outFilterMultimapNmax 20 \
+     --outFilterScoreMinOverLread 0.33 \
+     --twopassMode Basic \
+     --outSAMmapqUnique 60 \
+     --outFileNamePrefix ${sample_name}_
+     && samtools index ${sample_name}_Aligned.sortedByCoord.out.bam 
