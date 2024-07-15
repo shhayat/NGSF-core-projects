7720c25ea630b9dc5c267e8d98e0f559788596e2
Shahina <91986031+shhayat@users.noreply.github.com>
2024-07-15 14:50:27 -0600

    Update Gen2Epi.sh

--- 24-1VEHO-001/Gen2Epi.sh
+++ 24-1VEHO-001/Gen2Epi.sh
@@ -1,1 +1,10 @@
 
+source /globalhome/hxo752/HPC/.bashrc
+
+module load python/3.10.13
+module load fastqc/0.12.1
+module load trimmomatic/0.39
+module load bowtie2/2.5.2
+module load blast/2.2.26  
+module load prodigal/2.6.3
+module load bbmap/39.06  
