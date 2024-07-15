a3cb7a146773a642702013b2519e6cf256878aa5
Shahina <91986031+shhayat@users.noreply.github.com>
2024-07-15 15:08:28 -0600

    Update Gen2Epi.sh

--- 24-1VEHO-001/Gen2Epi.sh
+++ 24-1VEHO-001/Gen2Epi.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name=star-align
+#SBATCH --job-name=Gen2Epi
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=1:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mem=40G
 #SBATCH  --output=%j.out
 
 source /globalhome/hxo752/HPC/.bashrc
 
+module load perl/5.36.1
 module load python/3.10.13
 module load fastqc/0.12.1
 module load trimmomatic/0.39
@@ -19,4 +20,9 @@ module load bbmap/39.06
 
 NCPU=4
 Gen2Epi_Scripts=/globalhome/hxo752/HPC/tools/Gen2Epi/Gen2Epi_Scripts
+
+#create sample sheet for fastq files
+perl Prepare_Input.pl <path-to-fastq-files> <number e.g 12>
+
+
 
