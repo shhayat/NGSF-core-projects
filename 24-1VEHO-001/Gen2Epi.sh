fd4f5fe7a7dc08b04ff683515327da365e24650b
Shahina <91986031+shhayat@users.noreply.github.com>
2024-07-15 14:59:46 -0600

    Update Gen2Epi.sh

--- 24-1VEHO-001/Gen2Epi.sh
+++ 24-1VEHO-001/Gen2Epi.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=star-align
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=1:00:00
+#SBATCH --mem=40G
+#SBATCH  --output=%j.out
 
 source /globalhome/hxo752/HPC/.bashrc
 
@@ -8,3 +16,7 @@ module load bowtie2/2.5.2
 module load blast/2.2.26  
 module load prodigal/2.6.3
 module load bbmap/39.06  
+
+NCPU=4
+Gen2Epi_Scripts=/globalhome/hxo752/HPC/tools/Gen2Epi/Gen2Epi_Scripts
+
