28375fc292a71801e19ce77aac6449b71c918205
Shahina <91986031+shhayat@users.noreply.github.com>
2024-07-23 18:40:33 -0600

    Update 02_add_sample_id.sh

--- 24-1LICH-001/02_add_sample_id.sh
+++ 24-1LICH-001/02_add_sample_id.sh
@@ -14,13 +14,14 @@ DIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/24-1LICH-001/analys
 cd ${DIR}
 
 sample_name=$1;
-
-/globalhome/hxo752/HPC/tools/IMAPR/tools/gatk-4.1.8.1/gatk AddOrReplaceReadGroups \
-                                                            I=$DIR/${sample_name}_Aligned.sortedByCoord.out.bam \
-                                                            O=$DIR/${sample_name}_Aligned.sortedByCoord.RG.bam \
-                                                            RGID=1 \
-                                                            RGLB=lib1 \
-                                                            RGPL=illumina \
-                                                            RGPU=unit1 \
-                                                            RGSM=${sample_name} \
-                                                            && samtools index ${OUTDIR}/${sample_name}_Aligned.sortedByCoord.RG.bam
+java -Xmx64G -XX:ParallelGCThreads=$NCPU -jar $EBROOTPICARD/picard.jar AddOrReplaceReadGroups \
+                                    I=$DIR/${sample_name}_Aligned.sortedByCoord.out.bam \
+                                    O=$DIR/${sample_name}_Aligned.sortedByCoord.RG.bam \
+                                    SO=coordinate \
+                                    RGID=1 \
+                                    RGLB=lib1 \
+                                    RGPL=ILLUMINA \
+                                    RGPU=unit1 \
+                                    RGSM=${sample_name} \
+                                    TMP_DIR=$OUTDIR/${SAMPLE_NAME}/tmdir
+                                    && samtools index ${OUTDIR}/${sample_name}_Aligned.sortedByCoord.RG.bam
