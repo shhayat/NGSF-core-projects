9ac3188dc7ff0a753587ae94514675eb3d83cd98
Shahina <91986031+shhayat@users.noreply.github.com>
2024-07-31 14:55:36 -0600

    Update adaptor_trimming.sh

--- 24-1VEHO-001/adaptor_trimming.sh
+++ 24-1VEHO-001/adaptor_trimming.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=trim
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=5G
+#SBATCH --output=/project/anderson/%j.out
 
 sample_name=$1;
 
