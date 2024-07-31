9c04d7ff67ec6974e85136534a63510f1766e19b
Shahina <91986031+shhayat@users.noreply.github.com>
2024-07-31 15:06:49 -0600

    Update alignment.sh

--- 24-1VEHO-001/alignment.sh
+++ 24-1VEHO-001/alignment.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=alignment
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=5G
+#SBATCH --output=/project/anderson/%j.out
+
+
 
