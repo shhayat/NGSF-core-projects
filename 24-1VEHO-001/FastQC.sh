b9bbf7bdb31099630fe5a4789b64a2625b9edb41
Shahina <91986031+shhayat@users.noreply.github.com>
2024-07-31 14:30:38 -0600

    Update FastQC.sh

--- 24-1VEHO-001/FastQC.sh
+++ 24-1VEHO-001/FastQC.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=fastqc
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=5G
+#SBATCH --output=/project/anderson/%j.out
+
+module load fastqc
+#DATA=/project/anderson/concatenated_fastq
+#OUTDIR=/project/anderson/FastQC
+DATA=/datastore/ChamberlainLab/trimmed_fastqs
+OUTDIR=/project/anderson/FastQC_after_trimming
+mkdir -p ${OUTDIR}
+
+fq1=$1; shift
+fq2=$1;
+fastqc -o ${OUTDIR} --extract ${fq1}
+fastqc -o ${OUTDIR} --extract ${fq2}
