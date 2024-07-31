315b9552b48708b5c850ff53399e9c86ff2f839c
Shahina <91986031+shhayat@users.noreply.github.com>
2024-07-31 14:55:47 -0600

    Update adaptor_trimming.sh

--- 24-1VEHO-001/adaptor_trimming.sh
+++ 24-1VEHO-001/adaptor_trimming.sh
@@ -15,4 +15,4 @@ trim_galore=/$HOME/venvs/trim-glore/TrimGalore-0.6.10
 ${trim_galore}/trim_galore \
                            --paired ${sample_name}_R1_read_trimmed.fq.gz ${sample_name}_R2_read_trimmed.fq.gz \
                            --cores ${NCPU} \
-                           --nextera
+                           --illumina
