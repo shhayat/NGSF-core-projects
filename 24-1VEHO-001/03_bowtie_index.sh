d6b4b2a1fa5b500c6e3e9b74251c2bdf20c7553d
Shahina <91986031+shhayat@users.noreply.github.com>
2024-08-17 12:28:46 -0600

    Update 03_bowtie_index.sh

--- 24-1VEHO-001/03_bowtie_index.sh
+++ 24-1VEHO-001/03_bowtie_index.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=genome_index
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=4
+#SBATCH --time=10:00:00
+#SBATCH --mem=60G
+#SBATCH --output=%j.out
+set -eux
+
+cd /globalhome/hxo752/HPC/tools/bowtie2-2.4.5-linux-x86_64
+
+GENOME=/datastore/NGSF001/analysis/references/iGenomes/Homo_sapiens/NCBI/GRCh38/Sequence/WholeGenomeFasta
+OUTDIR=/globalhome/hxo752/HPC/ngsf_git_repos/NGSF-core-projects/23-1DEAN-001/analysis/indices
+
+bowtie2-build ${GENOME}/genome.fa ${OUTDIR}/bowtie_index
