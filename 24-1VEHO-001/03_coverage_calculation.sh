58752e5babbdc621c5ad77294843ca56a040eb6f
Shahina <91986031+shhayat@users.noreply.github.com>
2024-08-19 11:57:06 -0600

    Update 03_coverage_calculation.sh

--- 24-1VEHO-001/03_coverage_calculation.sh
+++ 24-1VEHO-001/03_coverage_calculation.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=coverage
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=5G
+#SBATCH --output=/project/anderson/%j.out
