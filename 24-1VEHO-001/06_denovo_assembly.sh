397df2b7ced05324a0f01ae0b5a2ca327e6107f0
Shahina <91986031+shhayat@users.noreply.github.com>
2024-08-21 12:52:33 -0600

    Update 06_denovo_assembly.sh

--- 24-1VEHO-001/06_denovo_assembly.sh
+++ 24-1VEHO-001/06_denovo_assembly.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --job-name=Gen2Epi
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=
+#SBATCH --cpus-per-task=20
+#SBATCH --time=240
 #SBATCH --mem=40G
-#SBATCH  --output=%j.out
+#SBATCH --output=/project/anderson/%j.out
 
 source /globalhome/hxo752/HPC/.bashrc
 
@@ -18,15 +18,13 @@ module load blast/2.2.26
 module load prodigal/2.6.3
 module load bbmap/39.06  
 
-NCPU=
+NCPU=20
 Gen2Epi_Scripts=/globalhome/hxo752/HPC/tools/Gen2Epi/Gen2Epi_Scripts
 FASTQ_DIR=/project/anderson/trimmed_fastq
 #create sample sheet for fastq files
 #perl Prepare_Input.pl ${FASTQ_DIR} 179
 
 #denovo assembly
-perl WGS_SIBP_P2.pl /project/anderson/Prepare_Input.txt ${FASTQ_DIR} trimmed
-
-Trimming trimmed 2 --cov-cutoff auto --careful --kmer 21335571 ${NCPU}
+perl WGS_SIBP_P2.pl /project/anderson/Prepare_Input.txt ${FASTQ_DIR} trimmed ${NCPU}
 
 
