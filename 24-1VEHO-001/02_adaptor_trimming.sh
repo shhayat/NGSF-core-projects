907fb5f6b6552b86ef16eb76bcaaef6428c69d8a
Shahina <91986031+shhayat@users.noreply.github.com>
2024-08-26 12:58:41 -0600

    Update 02_adaptor_trimming.sh

--- 24-1VEHO-001/02_adaptor_trimming.sh
+++ 24-1VEHO-001/02_adaptor_trimming.sh
@@ -9,54 +9,33 @@
 #SBATCH --mem=5G
 #SBATCH --output=/project/anderson/%j.out
 
-#module purge
-#module load python/3.10
-#module load StdEnv/2020
-#module load scipy-stack/2023a
-#module load fastp/0.23.4
+module purge
+module load python/3.10
+module load StdEnv/2020
+module load scipy-stack/2023a
+module load fastp/0.23.4
 
-#OUTDIR=/project/anderson/trimmed_fastq
+OUTDIR=/project/anderson/trimmed_fastq
 
-#NCPU=1
+NCPU=1
 
-#sample_name=$1; shift
-#fq1=$1; shift
-#fq2=$1;
+sample_name=$1; shift
+fq1=$1; shift
+fq2=$1;
 
-#mkdir -p ${OUTDIR}
+mkdir -p ${OUTDIR}
 
- #fastp -i ${fq1} \
- #      -I ${fq2} \
- ##      -o ${OUTDIR}/${sample_name}_R1.fastq.gz \
- #      -O ${OUTDIR}/${sample_name}_R2.fastq.gz \
- #      -h ${OUTDIR}/${sample_name}.fastp.html \
- #      --thread $NCPU \
- #      --length_required 100 \
- #      --average_qual 20 \
- #      --trim_poly_x \
- #      --trim_poly_g \
- #      --detect_adapter_for_pe
+fastp -i ${fq1} \
+       -I ${fq2} \
+       -o ${OUTDIR}/${sample_name}_R1.fastq.gz \
+       -O ${OUTDIR}/${sample_name}_R2.fastq.gz \
+       -h ${OUTDIR}/${sample_name}.fastp.html \
+       --thread $NCPU \
+       --length_required 100 \
+       --average_qual 20 \
+       --trim_poly_x \
+       --trim_poly_g \
+       --detect_adapter_for_pe \
+       --unpaired1 ${OUTDIR}/${sample_name}_R1_unpaired.fastq.gz \
+       --unpaired2 ${OUTDIR}/${sample_name}_R2_unpaired.fastq.gz
  
-source /globalhome/hxo752/HPC/.bashrc
-
-module load perl/5.36.1
-module load python/3.10.13
-module load fastqc/0.12.1
-module load trimmomatic/0.39
-module load bowtie2/2.5.2
-module load blast/2.2.26  
-module load prodigal/2.6.3
-module load bbmap/39.06  
-
-NCPU=30
-Gen2Epi_Scripts=/globalhome/hxo752/HPC/tools/Gen2Epi/Gen2Epi_Scripts
-FASTQ_DIR=/project/anderson/TB_DATA/PHO
-OUTDIR=/project/anderson/trimmed
-mkdir -p $OUTDIR
-cd $OUTDIR
-#create sample sheet for fastq files
-#perl ${Gen2Epi_Scripts}/Prepare_Input.pl ${FASTQ_DIR} 109
-
-#quality check and trimming
-perl ${Gen2Epi_Scripts}/WGS_SIBP_P1.pl Prepare_Input.txt /project/anderson/TB_DATA/PHO both 3 3 4:15 30
-
