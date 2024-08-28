6a90e70872156e8789a749aecc62ad451e455fdb
Shahina <91986031+shhayat@users.noreply.github.com>
2024-08-28 11:36:58 -0600

    Update 07_denovo_assembly_annotation.sh

--- 24-1VEHO-001/07_denovo_assembly_annotation.sh
+++ 24-1VEHO-001/07_denovo_assembly_annotation.sh
@@ -1,2 +1,13 @@
+ #!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=denovo
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=30
+#SBATCH --time=240:00:00
+#SBATCH --mem=200G
+#SBATCH --output=/project/anderson/%j.out
+
 prokka=/globalhome/hxo752/HPC/tools/prokka/bin
 
