86fbd26c94f80b206ede1655991ae2d9b8889c79
Shahina <91986031+shhayat@users.noreply.github.com>
2024-08-29 15:35:18 -0600

    Update 06_denovo_assembly_p1.sh

--- 24-1VEHO-001/06_denovo_assembly_p1.sh
+++ 24-1VEHO-001/06_denovo_assembly_p1.sh
@@ -5,19 +5,11 @@
 #SBATCH --job-name=denovo
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=240:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=100G
 #SBATCH --output=/project/anderson/%j.out
 
-#source /globalhome/hxo752/HPC/.bashrc
 
-#module load perl/5.36.1
-#module load python/3.10.13
-#module load fastqc/0.12.1
-#module load trimmomatic/0.39
-#module load bowtie2/2.5.2
-#module load blast/2.2.26  
-#module load prodigal/2.6.3
 module load bbmap/39.06  
 
 sample_name=$1; shift
