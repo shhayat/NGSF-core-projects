a1e1c22ffac985c1b0bb0164eb85fa27568c4f1f
Shahina <91986031+shhayat@users.noreply.github.com>
2024-08-29 16:14:48 -0600

    Update 06_denovo_assembly_p2.sh

--- 24-1VEHO-001/06_denovo_assembly_p2.sh
+++ 24-1VEHO-001/06_denovo_assembly_p2.sh
@@ -4,20 +4,12 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=denovo
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=240:00:00
-#SBATCH --mem=100G
+#SBATCH --cpus-per-task=10
+#SBATCH --time=24:00:00
+#SBATCH --mem=200G
 #SBATCH --output=/project/anderson/%j.out
 
-#source /globalhome/hxo752/HPC/.bashrc
 
-#module load perl/5.36.1
-#module load python/3.10.13
-#module load fastqc/0.12.1
-#module load trimmomatic/0.39
-#module load bowtie2/2.5.2
-#module load blast/2.2.26  
-#module load prodigal/2.6.3
 module load bbmap/39.06  
 
 sample_name=$1; shift
@@ -32,7 +24,6 @@ OUTDIR=/project/anderson/denovo_assembly
 
 mkdir -p ${OUTDIR}/Plasmid_AssemblyTrimmedReads
 mkdir -p ${OUTDIR}/PlasmidContigAssemblytrimmedStat
-
 mkdir -p ${OUTDIR}/Plasmid_AssemblyTrimmedReads/${sample_name}
 
 
