9e91bb5caa1e5908352c7deefe21d44eb0b21adb
Shahina <91986031+shhayat@users.noreply.github.com>
2024-08-29 22:24:53 -0600

    Update 06_denovo_assembly_p1.sh

--- 24-1VEHO-001/06_denovo_assembly_p1.sh
+++ 24-1VEHO-001/06_denovo_assembly_p1.sh
@@ -4,7 +4,7 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=denovo
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=10
 #SBATCH --time=24:00:00
 #SBATCH --mem=40G
 #SBATCH --output=/project/anderson/%j.out
