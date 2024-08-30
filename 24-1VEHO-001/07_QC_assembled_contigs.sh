9bf79d5384e73f5c591be0b78285bd51a5b5e9fa
Shahina <91986031+shhayat@users.noreply.github.com>
2024-08-30 13:02:01 -0600

    Update 07_QC_assembled_contigs.sh

--- 24-1VEHO-001/07_QC_assembled_contigs.sh
+++ 24-1VEHO-001/07_QC_assembled_contigs.sh
@@ -1,2 +1,32 @@
+#!/bin/bash
 
-/globalhome/hxo752/HPC/tools/quast-quast_5.2.0
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=QC
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=48:00:00
+#SBATCH --mem=40G
+#SBATCH --output=/project/anderson/%j.out
+
+
+
+quast_tool=/globalhome/hxo752/HPC/tools/quast-quast_5.2.0
+OUTDIR=/project/anderson/QC_for_assembled_contigs
+REF=/project/anderson/genome/sequence.fasta
+GFF=/project/anderson/genome/sequence.gff3
+NCPU=10
+
+fq1=$1; shift
+fq2=$1; shift
+sample_name=$1; shift
+
+
+${quast_tool}/quast.py 
+                       -o ${OUTDIR} \
+                       -r ${REF} \
+                       -g ${GFF} \
+                       -1 ${fq1} \
+                       -2 ${fq2} \
+                       --threads ${NCPU}
+                       
