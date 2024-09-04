92924463d1924df79987ff007e616f120359f917
Shahina <91986031+shhayat@users.noreply.github.com>
2024-09-04 12:20:53 -0600

    Update 05_run_repair_and_align.sh

--- 24-1VEHO-001/05_run_repair_and_align.sh
+++ 24-1VEHO-001/05_run_repair_and_align.sh
@@ -11,16 +11,42 @@
 
 module load bbmap//39.06
 
+sample_name=$1; shift
+fq1=$1;shift
+fq2=$1;
+
+
 DATA=/project/anderson/trimmed_fastq
 OUTDIR=/project/anderson/trimmed_fastq
 
 mkdir -p $OUTDIR
 
-repair.sh in= \
-          in2= \
-          out= \
-          out2= \
-          outs= \
+repair.sh in=${fq1} \
+          in2=${fq2} \
+          out=${OUTDIR}/{sample_name}_R1_repair.fastq.gz \
+          out2=${OUTDIR}/{sample_name}_R2_repair.fastq.gz \
+          outs=${OUTDIR}/{sample_name}_singletons.fastq.gz \
           repair=t
 
 
+module load bowtie2/2.5.2
+module load samtools
+
+NCPU=10
+index=/project/anderson/index/bowtie_index
+OUTDIR=/project/anderson/mapping
+
+mkdir -p ${OUTDIR}
+
+gunzip -c ${OUTDIR}/{sample_name}_R1_repair.fastq.gz | bowtie2 \
+--threads ${NCPU} \
+-x ${index} \
+-1 - \
+-2 <(gunzip -c ${OUTDIR}/{sample_name}_R2_repair.fastq.gz) \
+-S ${OUTDIR}/${sample_name}.sam 2> ${OUTDIR}/${sample_name}_bowtie2.log \
+&& samtools view -h -b ${OUTDIR}/${sample_name}.sam > ${OUTDIR}/${sample_name}.aligned.bam
+
+rm ${OUTDIR}/${sample_name}.sam
+
+module unload samtools
+module unload bowtie2/2.5.2
