46b14018e055b5b909f6f88a8b49347fdae8ecfb
Shahina <91986031+shhayat@users.noreply.github.com>
2024-09-04 12:11:14 -0600

    Update and rename 05_run_repair.sh to 05_run_repair_and_align.sh

---
+++ 24-1VEHO-001/05_run_repair_and_align.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=run_repair
+#SBATCH --ntasks=1
+#BATCH --cpus-per-task=10
+#SBATCH --time=20:00:00
+#SBATCH --mem=5G
+#SBATCH --output=/project/anderson/%j.out
+
+module load bbmap
+
+
