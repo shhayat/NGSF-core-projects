34f94728bf8e49faabdc88937976065ef8af04ff
Shahina <91986031+shhayat@users.noreply.github.com>
2024-09-06 15:53:45 -0600

    Update 09_lineage_classification_and_Drug_resistance_prediction.sh

--- 24-1VEHO-001/09_lineage_classification_and_Drug_resistance_prediction.sh
+++ 24-1VEHO-001/09_lineage_classification_and_Drug_resistance_prediction.sh
@@ -1,8 +1,23 @@
+#!/bin/bash
 
-
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=mykrobe
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=20:00:00
+#SBATCH --mem=10G
+#SBATCH --output=/project/anderson/%j.out
 
 
 source /globalhome/hxo752/HPC/.bashrc
 
 mykrobe=/globalhome/hxo752/HPC/tools/mykrobe-0.13.0
 OUTDIR=/project/anderson/assembly_annotation
+
+
+mykrobe predict --sample my_sample_name \
+                --species tb \
+                --output ${OUTDIR}//out.json \
+                --format json \
+                --seq reads.fq.gz
