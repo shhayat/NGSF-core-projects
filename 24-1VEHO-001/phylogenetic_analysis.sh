a5ef7015764f4cc4f4fa46012123bbfb685f40ed
Shahina <91986031+shhayat@users.noreply.github.com>
2024-09-09 15:54:02 -0600

    Update phylogenetic_analysis.sh

--- 24-1VEHO-001/phylogenetic_analysis.sh
+++ 24-1VEHO-001/phylogenetic_analysis.sh
@@ -1,3 +1,25 @@
-/globalhome/hxo752/HPC/tools/raxml-ng/bin/
+#!/bin/bash
 
-raxml-ng
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=pylogentic_analysis
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=480:00:00
+#SBATCH --mem=200G
+#SBATCH --output=/project/anderson/%j.out
+
+
+sample_name=$1;
+NCPU=32
+OUTDIR=/project/anderson/pylogenetic_analysis
+module load raxml-ng/1.2.0 
+
+mkdir -p ${OUTDIR}
+
+raxml-ng --msa \
+        --model GTR+G \
+         --bs-trees 200 \
+         --threads ${NCPU}
+         
+         
