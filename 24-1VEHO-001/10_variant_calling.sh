897dd26cce87fa98aed7ad44798716ae2695bd61
Shahina <91986031+shhayat@users.noreply.github.com>
2024-09-10 15:33:11 -0600

    Update 10_variant_calling.sh

--- 24-1VEHO-001/10_variant_calling.sh
+++ 24-1VEHO-001/10_variant_calling.sh
@@ -4,9 +4,9 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=variant_call
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=30
+#SBATCH --cpus-per-task=20
 #SBATCH --time=480:00:00
-#SBATCH --mem=300G
+#SBATCH --mem=200G
 #SBATCH --output=/project/anderson/%j.out
 
 
