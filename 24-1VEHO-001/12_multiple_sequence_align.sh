fb35c6080843095fe1232e6367b451884f823552
Shahina <91986031+shhayat@users.noreply.github.com>
2024-09-11 13:08:19 -0600

    Update 12_multiple_sequence_align.sh

--- 24-1VEHO-001/12_multiple_sequence_align.sh
+++ 24-1VEHO-001/12_multiple_sequence_align.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+#SBATCH --account=hpc_p_anderson
+#SBATCH --constraint=skylake
+#SBATCH --job-name=msa
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=48:00:00
+#SBATCH --mem=20G
+#SBATCH --output=/project/anderson/%j.out
