3932d12ec977f2101d099549cf305aaf95c6fd99
Shahina <91986031+shhayat@users.noreply.github.com>
2024-09-11 11:09:01 -0600

    Update 10_variant_calling.sh

--- 24-1VEHO-001/10_variant_calling.sh
+++ 24-1VEHO-001/10_variant_calling.sh
@@ -4,7 +4,7 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=variant_call
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=10
 #SBATCH --time=12:00:00
 #SBATCH --mem=200G
 #SBATCH --output=/project/anderson/%j.out
@@ -12,7 +12,7 @@
 
 source /globalhome/hxo752/HPC/.bashrc
 sample_name=$1;
-NCPU=20
+NCPU=10
 
 mtbseq=/globalhome/hxo752/HPC/miniconda/bin
 #OUTDIR=/project/anderson/variant_calling
