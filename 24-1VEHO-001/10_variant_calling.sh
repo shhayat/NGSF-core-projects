a4650c56c22a307eb2b6acd105dfb97ac38a6854
Shahina <91986031+shhayat@users.noreply.github.com>
2024-10-21 13:08:58 -0600

    Update 10_variant_calling.sh

--- 24-1VEHO-001/10_variant_calling.sh
+++ 24-1VEHO-001/10_variant_calling.sh
@@ -4,7 +4,7 @@
 #SBATCH --constraint=skylake
 #SBATCH --job-name=variant_call
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=20
 #SBATCH --time=240:00:00
 #SBATCH --mem=100G
 #SBATCH --output=/project/anderson/%j.out
@@ -18,9 +18,20 @@ DATA=/project/anderson/trimmed_fastq
 cd ${DATA}
 
 module load apptainer
+#apptainer exec -B $TMPDIR:/tmp /opt/software/singularity-images/MTBseq.sif \
+#            ${mtbseq}/MTBseq --step TBfull \
+#                 --threads ${NCPU}
 apptainer exec -B $TMPDIR:/tmp /opt/software/singularity-images/MTBseq.sif \
-            ${mtbseq}/MTBseq --step TBfull \
-                 --threads ${NCPU}
+           ${mtbseq}/MTBseq --step TBpile \
+                            --step TBpile \
+                            --step TBlist \
+                            --step TBvariants \
+                            --step TBstats \
+                            --step TBstrains \
+                            --step TBjoin \
+                            --step TBamend \
+                            --step TBgroups \
+                            --threads ${NCPU}
                   
 #mv GATK_Bam/ ${OUTDIR}
 #mv Mpileup/ ${OUTDIR}
